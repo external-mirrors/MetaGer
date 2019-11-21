7003d90bfb956dfd194f976b10222966dcafb2f7
Dominik Hebeler <dominik@suma-ev.de>
2019-11-21 13:29:41 +0100

    Added a commandline tool to spawn the right amount of workers

--- app/Jobs/Searcher.php
+++ app/Jobs/Searcher.php
@@ -12,6 +12,8 @@ class Searcher implements ShouldQueue
 {
     use InteractsWithQueue, Queueable, SerializesModels;
 
+    public $tries = 1;
+
     protected $name, $ch, $pid, $counter, $lastTime, $connectionInfo, $user, $password, $headers;
     protected $proxyhost, $proxyuser, $proxypassword;
     # Each Searcher will shutdown after a specified time(s) or number of requests

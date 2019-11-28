3602a44fd567d8cd3cf9b9ae1da350443ff46f43
Dominik Hebeler <dominik@suma-ev.de>
2019-11-28 15:20:21 +0100

    Timeout for job is now higher than maximum runtime of job

--- app/Jobs/Searcher.php
+++ app/Jobs/Searcher.php
@@ -13,6 +13,12 @@ class Searcher implements ShouldQueue
     use InteractsWithQueue, Queueable, SerializesModels;
 
     public $tries = 1;
+    /**
+     * The number of seconds the job can run before timing out.
+     *
+     * @var int
+     */
+    public $timeout = 300;
 
     protected $name, $ch, $pid, $counter, $lastTime, $connectionInfo, $user, $password, $headers;
     protected $proxyhost, $proxyuser, $proxypassword;

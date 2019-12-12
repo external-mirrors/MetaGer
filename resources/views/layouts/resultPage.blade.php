3ffb2c3d0dc858bcf96a279ae7b25e427fc81001
Dominik Hebeler <dominik@suma-ev.de>
2019-12-12 10:14:37 +0100

    added squared apple touch icons

--- resources/views/layouts/resultPage.blade.php
+++ resources/views/layouts/resultPage.blade.php
@@ -5,6 +5,15 @@
 		<title>{{ $eingabe }} - MetaGer</title>
 		<link href="/favicon.ico" rel="icon" type="image/x-icon" />
 		<link href="/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+		<link rel="apple-touch-icon" href="/img/apple/touch-icon.png">
+		<link rel="apple-touch-icon" sizes="57x57" href="/img/apple/touch-icon-57.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="/img/apple/touch-icon-72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="/img/apple/touch-icon-76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/img/apple/touch-icon-114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="/img/apple/touch-icon-120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="/img/apple/touch-icon-144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="/img/apple/touch-icon-152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="/img/apple/touch-icon-180.png">
 		<meta content="width=device-width, initial-scale=1.0, user-scalable=no" name="viewport" />
 		<meta name="p" content="{{ getmypid() }}" />
 		<meta name="q" content="{{ $eingabe }}" />

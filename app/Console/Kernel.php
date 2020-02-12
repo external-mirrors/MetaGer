b02d474a974764c20d4d4b8eb319f7e9188a1034
Dominik Hebeler <dominik@suma-ev.de>
2020-02-12 12:14:10 +0100

    removed cache gc since redis cleans up itself

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,6 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('requests:gather')->everyFifteenMinutes();
         $schedule->command('requests:useragents')->everyFiveMinutes();
-        $schedule->command('cache:gc')->hourly();
 
         $schedule->call(function () {
             DB::table('monthlyrequests')->truncate();

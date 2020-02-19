f80e27c2cb4e0215df94301cae283c72c3fe3fa1
Dominik Hebeler <dominik@suma-ev.de>
2020-02-19 19:52:58 +0100

    not using sentinel anymore

--- config/cache.php
+++ config/cache.php
@@ -69,7 +69,7 @@ return [
         ],
 
         'redis' => [
-            'driver' => env('REDIS_CACHE_DRIVER', 'redis'),
+            'driver' => 'redis',
             'connection' => 'cache',
         ],
 

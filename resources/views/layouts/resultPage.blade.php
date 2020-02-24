1a7e2f120bb2a6eae790d6e5db00f2be20f5947c
Aria Givi <aria@pc-karl.fritz.box>
2020-02-24 13:07:21 +0100

    changed color variables for result-page related stylesheets

--- resources/views/layouts/resultPage.blade.php
+++ resources/views/layouts/resultPage.blade.php
@@ -25,7 +25,8 @@
 		<link href="/fonts/liberationsans/stylesheet.css" rel="stylesheet">
 		<link type="text/css" rel="stylesheet" href="{{ mix('css/fontawesome.css') }}" />
 		<link type="text/css" rel="stylesheet" href="{{ mix('css/fontawesome-solid.css') }}" />
-		<link type="text/css" rel="stylesheet" href="{{ mix('css/themes/metager.css') }}" />
+		<link type="text/css" rel="stylesheet alternate" href="{{ mix('css/themes/metager-dark.css') }}" title="MetaGer Dark"/>
+		<link type="text/css" rel="stylesheet" href="{{ mix('css/themes/metager.css') }}" title="MetaGer"/>
 		<meta name="referrer" content="origin">
 		<meta name="age-meta-label" content="age=18"/>
 		@include('parts.utility')

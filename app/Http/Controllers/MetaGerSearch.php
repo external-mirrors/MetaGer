c8141ce23fc4ccb714d04c84696637fa2545773c
Dominik Hebeler <dominik@suma-ev.de>
2020-05-25 11:33:54 +0200

    cancelling search if query is empty after parsing form data

--- app/Http/Controllers/MetaGerSearch.php
+++ app/Http/Controllers/MetaGerSearch.php
@@ -61,6 +61,10 @@ class MetaGerSearch extends Controller
             $timings["checkSpecialSearches"] = microtime(true) - $time;
         }
 
+        if(empty($metager->getQ())){
+            return $metager->createView();
+        }
+        
         if ($spamEntry !== null && Cache::has('spam.' . $metager->getFokus() . "." . md5($spamEntry))) {
             $responseContent = Cache::get('spam.' . $metager->getFokus() . "." . md5($spamEntry));
             $responseContent = preg_replace('/(name="eingabe"\s+value=")[^"]+/', "$1$eingabe", $responseContent);

0f01c4088a1f0af79c0897550aa33e9ea26ab384
Dominik Hebeler <dominik@suma-ev.de>
2020-09-15 13:57:25 +0200

    fixed html head of result page

--- resources/views/layouts/resultpage/verificationHeader.blade.php
+++ resources/views/layouts/resultpage/verificationHeader.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{!! trans('staticPages.meta.language') !!}">
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="/index.css?id={{ $key }}">

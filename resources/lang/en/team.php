726483e7239cc523bd9ef5b65dd961d57986744a
Davide <davide@suma-ev.de>
2020-10-05 16:23:03 +0200

    Adding trainee to team, adding trainee role

--- resources/lang/en/team.php
+++ resources/lang/en/team.php
@@ -10,6 +10,8 @@ return [
     "role.6" => "programmer",
     "role.7" => "secretary",
     "role.8" => "founder",
+    "role.9" => "trainee",
+    
     "contact.1" => "Please mail your questions/problems about MetaGer etc. to <a href=\"mailto:office@suma-ev.de\">office@suma-ev.de</a> or use the <a href=\"/en/kontakt/\">contact form with encryption</a>.",
     "contact.2" => "If you get any emails with strange contents from us please read more about this: <a href=\"https://metager.de/wsb/fakemail/\">https://metager.de/wsb/fakemail/</a>",
     "contact.3" => "Only in reasoned exceptions, if you want to reach someone directly, you should mail them. Because team members might be on vacation, sick etc.",

4a4849b83c0d38f05ec93e67ff187ef62191246c
Phil Höfer <phil@suma-ev.de>
2020-10-15 10:00:52 +0000

    Localize hidden service URL

--- resources/views/tor.blade.php
+++ resources/views/tor.blade.php
@@ -5,5 +5,5 @@
 @section('content')
 	<h1>MetaGer hidden service</h1>
 	<p>@lang('tor.description')</p>
-	<a class="btn btn-primary" href="http://metagerv65pwclop2rsfzg4jwowpavpwd6grhhlvdgsswvo6ii4akgyd.onion/" role="button">{{trans('tor.torbutton')}}</a>
+	<a class="btn btn-primary" href="{{trans('tor.torurl')}}" role="button">{{trans('tor.torbutton')}}</a>
 @endsection

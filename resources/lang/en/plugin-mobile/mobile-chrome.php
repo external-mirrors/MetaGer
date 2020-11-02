256f1721e679bcd46dafa89b1d084f807be6b641
Carsten Riel <carsten@suma-ev.de>
2020-11-02 11:57:14 +0000

    Update resources/lang/de/plugin-desktop/desktop-chrome.php, resources/lang/de/plugin-desktop/desktop-edge.php, resources/lang/de/plugin-desktop/desktop-firefox.php, resources/lang/de/plugin-desktop/desktop-opera.php, resources/lang/de/plugin-mobile/mobile-chrome.php, resources/lang/de/plugin-mobile/mobile-edge.php, resources/lang/de/plugin-mobile/mobile-firefox-klar.php, resources/lang/de/plugin-mobile/mobile-firefox.php, resources/lang/de/plugin-desktop/desktop-ie.php, resources/lang/en/plugin-desktop/desktop-edge.php, resources/lang/en/plugin-desktop/desktop-firefox.php, resources/lang/en/plugin-desktop/desktop-ie.php, resources/lang/en/plugin-desktop/desktop-vivaldi.php, resources/lang/en/plugin-mobile/mobile-chrome.php, resources/lang/en/plugin-mobile/mobile-edge.php, resources/lang/en/plugin-mobile/mobile-firefox.php, resources/lang/en/plugin-mobile/mobile-opera.php files

--- resources/lang/en/plugin-mobile/mobile-chrome.php
+++ resources/lang/en/plugin-mobile/mobile-chrome.php
@@ -4,12 +4,12 @@
         'default-search-v83.1' => 'Tap "<i class="fas fa-ellipsis-v"></i>" in the top right corner.',
         'default-search-v83.2' => 'Choose "Settings".',
         'default-search-v83.3' => 'Under "Basics" pick "Search engine".',
-        'default-search-v83.4' => 'Under "Recently Visited" choose MetaGer.',
+        'default-search-v83.4' => 'Under "Recently Visited" choose "MetaGer".',
         'default-search-v83.5' => 'If MetaGer does not appear in the list use the search bar at the end of this instructions and try again.',
 
         'default-page-v83.1' => 'Tap "<i class="fas fa-ellipsis-v"></i>" in the top right corner.',
         'default-page-v83.2' => 'Choose "Settings".',
-        'default-page-v83.3' => 'Under "Advanced pick "Homepage".',
+        'default-page-v83.3' => 'Under "Advanced" pick "Homepage".',
         'default-page-v83.4' => 'Choose "Enter custom web address" and enter ":link" .',
 
     ];

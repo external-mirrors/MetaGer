496e13c972f7b823df0a8d7ed317e5b8dc16ca7f
Dominik Hebeler <dominik@suma-ev.de>
2020-11-03 10:56:20 +0100

    readded science focus as we added it now to en version

--- resources/views/parts/sidebar.blade.php
+++ resources/views/parts/sidebar.blade.php
@@ -131,10 +131,10 @@
           <li>
             <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/meta/settings?fokus=produkte&url=" . urlencode(url()->full())) }}" >{{ trans('index.foki.produkte') }}</a>
           </li>
-        @if(LaravelLocalization::getCurrentLocale() !== "en")
           <li>
             <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/meta/settings?fokus=nachrichten&url=" . urlencode(url()->full())) }}" >{{ trans('index.foki.nachrichten') }}</a>
           </li>
+        @if(LaravelLocalization::getCurrentLocale() !== "en")
           <li>
             <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/meta/settings?fokus=science&url=" . urlencode(url()->full())) }}" >{{ trans('index.foki.science') }}</a>
           </li>

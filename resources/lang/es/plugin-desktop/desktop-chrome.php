1a4cd365e4cff742c32f8e93e319dfca749c80bb
Carsten Riel <carsten@suma-ev.de>
2020-11-05 10:43:40 +0000

    Update resources/lang/es/plugin-desktop/desktop-chrome.php, resources/lang/es/index.php, resources/lang/es/passwords.php, resources/lang/es/spendenaufruf.php, resources/lang/es/zitatsuche.php files

--- resources/lang/es/plugin-desktop/desktop-chrome.php
+++ resources/lang/es/plugin-desktop/desktop-chrome.php
@@ -1,14 +1,17 @@
 <?php
 
 return [
-    "default-search-v49.1"	=>	"En la parte superior derecha de su aplicación Chrome  haga clic en <i class = \"fas fa-bars\"></i> y en el menú desplegable siguiente haga clic en \"Configuración\" para abrir las configuraciones de su Chrome.",
+    "default-search-v49.1"	=>	"En la parte superior derecha de la aplicación Chrome  haga clic en <i class = \"fas fa-bars\"></i> y en el menú siguiente de opciones haga clic en \"Configuración\" para abrir las configuraciones de Chrome.",
     "default-search-v49.2"	=>	"En \"Buscar\", haga clic en \"Administrar motores de búsqueda ...\".",
-    "default-search-v53.1"	=>	"En la parte superior derecha de su aplicación Chrome haga clic en <i class=\"fa fa-ellipsis-v\"></i> y en el siguiente menú de opciones haga clic en \"Configuración\" para abrir las configuraciónes de su Chrome.",
-    "default-search-v53.2"	=>	"En el área \"Buscar\", haga clic en \"Administrar buscadores...\".",
-    "default-search-v59.1"	=>	"En la parte superior derecha de su aplicación Chrome haga clic en <i class=\"fa fa-ellipsis-v\"></i> y en el menú despegable siguiente haga clic en \"Configuración\" para abrir las configuraciónes de Chrome.\r\n",
-    "default-search-v59.2"	=>	"En \"Buscador\", haga clic en \"Administrar buscadores...\"",
+    "default-search-v49.3"	=>	"En la mitad inferior del menú que aparece, mueva el mouse sobre la página web de MetaGer y  haga clic en el botón azul \"Establecer como predeterminado\" que aparece en el lado derecho",
+    "default-search-v53.1"	=>	"En la parte superior derecha de la aplicación Chrome haga clic en <i class=\"fa fa-ellipsis-v\"></i> y en el siguiente menú de opciones haga clic en \"Configuración\" para abrir las configuraciónes de Chrome.",
+    "default-search-v53.2"	=>	"En la sección \"Buscar\", haga clic en \"Administrar buscadores...\".",
+    "default-search-v53.3"	=>	"En la mitad inferior del menú que aparece, mueva el mouse sobre la página web de MetaGer y  haga clic en el botón azul \"Establecer como predeterminado\" que aparece en el lado derecho",
+    "default-search-v59.1"	=>	"En la parte superior derecha de la aplicación Chrome haga clic en <i class=\"fa fa-ellipsis-v\"></i> y en el siguiente menú de opciones haga clic en \"Configuración\" para abrir las configuraciónes de Chrome.",
+    "default-search-v59.2"	=>	"En la sección \"Motor de búsqueda\", haga clic en \"Administrar buscadores...\"",
     "default-search-v59.3"	=>	"En la lista mostrada aparece \"MetaGer\". Mueva el ratón sobre \"MetaGer\" y haga clic a la derecha en <i class=\"fa fa-ellipsis-v\"></i> y después en \"Establecer como predeterminado\"",
     "default-page-v49.1"	=>	"En la esquina superior derecha del navegador haga clic en <i class=\"fa fa-ellipsis-v\"></i> y abra la \"Configuración\".",
-    "default-page-v49.2"	=>	"\"En inicio\", seleccione la opción \"Abrir una página específica o conjunto de páginas\" y luego haga clic en el enlace \"Establecer páginas\".",
-    "default-page-v49.3"	=>	"Ingrese \":link\" como URL en \"Agregar nueva página\"."
+    "default-page-v49.2"	=>	"En la sección \"En inicio\", seleccione la opción \"Abrir una página específica o conjunto de páginas\" y luego haga clic en el enlace \"Agregar nueva página\".",
+    "default-page-v49.3"	=>	"Escriba \":link\" como URL de la página web en el cuadro \"Agregar nueva página\".",
+    "default-page-v49.4"	=>	"NOTA:\r\nTodas las páginas web que aparecen en la lista actual se abriràn cuando se inicie el navegador.\r\nPara eliminar una página web de la lista, mueva el mouse sobre la página web y haga clic en el botón \"X\" a la derecha."
 ];

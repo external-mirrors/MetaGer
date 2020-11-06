b9514f242976184acba930f71b092657052722ea
Carsten Riel <carsten@suma-ev.de>
2020-11-06 13:39:18 +0000

    Update resources/lang/en/team.php, resources/lang/es/team.php files

--- resources/lang/en/team.php
+++ resources/lang/en/team.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-    "role.0" => "CEO",
-    "role.1" => "executive",
-    "role.1.1" => "executive",
-    "role.2" => "youth protection commissioner",
-    "role.3" => "press secretary",
-    "role.5" => "programmer",
-    "role.6" => "programmer",
-    "role.7" => "secretary",
-    "role.8" => "founder",
-    "role.9" => "trainee",
+    "role.0"    => "CEO",
+    "role.1"    => "executive",
+    "role.1.1"  => "executive",
+    "role.2"    => "Youth protection commissioner",
+    "role.3"    => "press secretary",
+    "role.5"    => "programmer",
+    "role.6"    => "programmer",
+    "role.7"    => "secretary",
+    "role.8"    => "founder",
+    "role.9"    => "trainee",
     
     "contact.1" => "Please mail your questions/problems about MetaGer etc. to <a href=\"mailto:office@suma-ev.de\">office@suma-ev.de</a> or use the <a href=\"/en/kontakt/\">contact form with encryption</a>.",
     "contact.2" => "If you get any emails with strange contents from us please read more about this: <a href=\"https://metager.de/wsb/fakemail/\">https://metager.de/wsb/fakemail/</a>",

a975b6c55801ab7e29ea32925f89b9999d50c7c3
Davide Aprea <davide@suma-ev.de>
2020-11-06 11:11:30 +0100

    removed key validation in atom10/api view

--- resources/views/resultpages/metager3resultsatom10.blade.php
+++ resources/views/resultpages/metager3resultsatom10.blade.php
@@ -11,7 +11,6 @@
   <link rel="next" href="{{ htmlspecialchars($metager->nextSearchLink() ,ENT_QUOTES) }}" type="application/atom+xml"/>
   <id>urn:uuid:1d634a8c-2764-424f-b082-6c96494b7240</id>
   @include('layouts.atom10ad', ['ad' => $metager->popAd()])
-  @if(app('App\Models\Key')->getStatus())
   @foreach($metager->getResults() as $index => $result)
     @if(($index+1) % 5 === 0)
       @include('layouts.atom10ad', ['ad' => $metager->popAd()])
@@ -25,13 +24,4 @@
       </content>
     </entry>
   @endforeach
-  @else
-  <ad:advertisement>
-   <ad:callOut type="TEXT">Fehler</ad:callOut>
-   <ad:title type="TEXT">Falscher Schlüssel angegeben</ad:title>
-   <ad:displayUrl type="TEXT">https://metager.de/meta/key</ad:displayUrl>
-   <ad:subTitle type="TEXT">Sie haben einen ungültigen Schlüssel angegeben</ad:subTitle>
-   <link href="https://metager.de/meta/key" />
- </ad:advertisement> 
-  @endif
 </feed>

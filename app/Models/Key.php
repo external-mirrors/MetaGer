655fe0a0fa702f072c2ae939c34c015eec8a843b
Dominik Hebeler <dominik@suma-ev.de>
2020-11-11 11:27:56 +0100

    Using cluster keyserver

--- app/Models/Key.php
+++ app/Models/Key.php
@@ -6,14 +6,14 @@ class Key
 {
     public $key;
     public $status; # valid key = true, invalid key = false, unidentified key = null
-    private $keyserver = "https://key.metager3.de/";
+    private $keyserver = "https://key.metager.de/";
 
     public function __construct($key, $status = null)
     {
         $this->key = $key;
         $this->status = $status;
         if (getenv("APP_ENV") !== "production") {
-            $this->keyserver = "https://key.metager3.de/";
+            $this->keyserver = "https://dev.key.metager.de/";
         }
     }
 

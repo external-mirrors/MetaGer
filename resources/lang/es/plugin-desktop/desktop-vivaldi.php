2f438bf75a2f23deeecb6d0a61f20c248697cc93
Carsten Riel <carsten@suma-ev.de>
2020-11-12 07:55:36 +0000

    Update resources/lang/es/widget.php, resources/lang/es/plugin-desktop/desktop-ie.php, resources/lang/es/plugin-desktop/desktop-opera.php, resources/lang/es/plugin-desktop/desktop-vivaldi.php files

---
+++ resources/lang/es/plugin-desktop/desktop-vivaldi.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "default-search-v3-3.1"	=>	"Haga un clic derecho del ratón en el campo de entrada de búsqueda que tiene debajo de la instrucción.",
+    "default-search-v3-3.2"	=>	"En el menú contextual seleccione la opción \"Crear búsqueda...\"",
+    "default-search-v3-3.3"	=>	"En la ventana emergente, haga clic en \"Establecer como búsqueda predeterminada\" y a continuación en \"Agregar\".",
+    "default-page-v3-3.1"	=>	"En la esquina superior izquierda haga clic en el símbolo de Vivaldi y en \"Extras\" seleccione el elemento del menú \"<i class=\" fa fa-cog\" aria-hidden=\" true \"></i> Configuración\"",
+    "default-page-v3-3.2"	=>	"En la ventana que aparece debajo de \"Página de inicio\", seleccione \"Personalizado:\" e ingrese :link."
+];

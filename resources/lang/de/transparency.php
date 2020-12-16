1aa14a1d002d765432f05afab777ec53c0cdc44b
Kim Höfer <kim@suma-ev.de>
2020-12-16 12:16:04 +0100

    add Text to transparency page

--- resources/lang/de/transparency.php
+++ resources/lang/de/transparency.php
@@ -2,34 +2,20 @@
 
 return [
     'head.1' => 'Transparenzerklärung',
-    'head.3' => 'Was MetaGer auszeichnet',
+    'head.2' => 'MetaGer ist transparent',
+    'head.3' => 'Was ist eine Meta-Suchmaschine überhaupt?',
+    'head.4' => 'Was ist der Vorteil einer Meta-Suchmaschine?',
+    'head.5' => 'Wie setzt sich unser Ranking zusammen?',
 
-    '3.0' => 'Verwirklichung von freiem Wissenszugang und digitaler Demokratie, durch:',
-    '3.1' => 'Wissenszugang ohne Überwachung und ohne Bevormundung durch Staaten oder Konzerne',
-    '3.2' => 'verzicht auf Speicherung von Nutzerdaten und höchste Anforderungen an Datenschutz und Privatsphäre',
-    '3.3' => 'anonymen Zugang, AUCH zu den SuchERGEBNISSEN',
-    '3.4' => 'Realisierung auf mehreren Wegen: durch anonymen Proxy und TOR-hidden-Zugang',
-    '3.5' => 'Open-Source-Software und damit nachprüfbar (nur Open-Source-Software ist wirklich vertrauenswürdig)',
-    '3.6' => 'Integration in die Informationssuche und für die Routenplanung und geographische Suche (<a href="https://maps.metager.de" target="_blank">maps.metager.de</a>), OHNE Speicherung des Standortverlaufes der Nutzer',
-    '3.7' => 'eine transparente Organisationsstrukur (gemeinnütziger Träger-Verein <a href="https://suma-ev.de" target="_blank">SUMA-EV</a>), in der JEDE/R Mitglied werden kann',
-    'head.2' => 'Welche Vorteile bietet die Nutzung von MetaGer?',
 
-    'list.1' => 'Datenschutz und Privatsphäre sind bei uns einfach und selbstverständlich: Fertig eingebaut und automatisch bei jeder Suche angewandt. <a href="/datenschutz/">Mehr dazu…</a>',
-    'list.2' => 'Wir arbeiten nicht gewinnorientiert, wir sind ein <a href="/spende/">gemeinnütziger Verein</a>: Wir haben nicht das Ziel, uns durch Ihre Klicks und schon gar nicht durch Ihre Daten zu bereichern.',
-    'list.3' => '<a href="https://de.wikipedia.org/wiki/MetaGer" target="_blank" rel="noopener">MetaGer</a> ist primär eine <a href="https://de.wikipedia.org/wiki/Metasuchmaschine" target="_blank" rel="noopener"><em>Meta</em>-Suchmaschine:</a> Wir fragen bis zu 50 Suchmaschinen ab. Damit können wir echte Vielfalt in den Ergebnissen liefern.',
-    'list.4' => 'Wir bevorzugen in unseren Suchergebnissen nicht das, <a href="https://de.wikipedia.org/wiki/Filterblase" target="_blank" rel="noopener">was viel angeklickt wird</a>: Auch dadurch erhalten Sie nicht nur den Mainstream, sondern Vielfältigkeit.',
-    'list.5' => 'MetaGer ist seit mehr als 20 Jahren am Netz, entstanden in der Uni-Hannover am <a href="http://noack-grasdorf.de" target="_blank" rel="noopener">RRZN</a>: Unsere Erfahrung ist Ihr Vorteil - wir wissen was wir tun.',
-    'list.6' => 'Unsere Server werden ausschließlich mit nachhaltig produzierter Energie (Wasserkraft) betrieben.',
-    'list.7' => 'Aber auch wir sind nicht fehlerfrei: Wenn Ihnen bei uns Merkwürdiges begegnet: Bitte <a href="/kontakt/" target="_blank" rel="noopener">kontaktieren Sie uns</a>! Wir nehmen Ihre Hinweise ernst: <em>Sie</em> sind uns das Wichtigste.',
-    'head.4' => 'Wie kann ich MetaGer bzw. den SUMA-EV unterstützen?',
+    'text.1' => 'MetaGer ist Transparent. Unser Quellcode ist Open-Source und für alle öffentlich einsehbar. Wir verzichten auf die Speicherung von Nutzerdaten und schätzen Datenschutz sowie Privatsphäre. Deshalb gewähren wir anonymen Zugang zu den Suchergebnissen. Dies ist durch einen anonymen Proxy und TOR-hidden-Zugang möglich. Hinzu kommt eine transparente Organisationsstruktur, da MetaGer von dem gemeinnützigern Verein SUMA-EV getragen wird, in dem jeder Mitglied werden kann.',
+    'text.2' => 'Eine Meta-Suchmaschine ist eine Suchmaschine die mehrere andere Suchmaschinen gleichzeitig abfragt. Sie sammelt alle Ergebnisse der verschiedenen Suchmaschinen und wertet diese erneut nach eigenem Schema aus. Das heißt, dass die Metasuchmaschine keinen eigenen Index (Datenbestand) hat.',
+    'text.3' => 'Ein klarer Vorteil von Meta-Suchmaschinen ist es, dass der Nutzer nur eine einzige Suchanfrage braucht um an die Ergebnisse mehrerer Suchmaschinen zu kommen. Die Meta-Suchmaschine gibt die relevanten Ergebnisse in einer nochmals sortierten Ergebnisliste aus. MetaGer ist keine reine Metasuchmaschine, da wir auch kleine eigene Indexe verwenden.',
+    'text.4' => 'Wir übernehmen das Ranking unserer Quell-Suchmaschinen und gewichten diese. Diese Bewertungen werden dann in Punktzahlen umgewandelt. Außerdem wird das Vorkommen der Suchbegriffe in der URL und im Snippet, sowie das übermäßige Vorkommen von besonderen Zeichen (andere Schriftzeichen wie kyrillisch). Wir verwenden zudem noch eine Sperrliste, um einzelne Seiten von der Ergebnisliste rauszunehmen. Hier werden Anzeigen  gesperrt, die wir aus juristischer Sicht nicht anzeigen dürfen, sowie Ergebnisse, dessen Qualität so schlecht ist, dass diese keinen Nutzen für die Allgemeinheit haben oder dieser sogar schaden könnte.',
+    'text.5' => 'Sollte es weitere Fragen oder Unklarheiten geben, können Sie gerne unser Kontaktformular nutzen und uns Ihre Fragen stellen!',
+    'text.' => '',
+    'text.' => '',
+    'text.' => '',
 
-    'about.1' => 'Über uns',
-    'about.2' => 'Die deutsche Suchmaschine MetaGer wird betrieben und stetig weiterentwickelt vom SUMA-EV - Verein für freien Wissenszugang. Wir sind ein gemeinnützig anerkannter Verein, eines unserer Ziele ist die Entwicklung und der Betrieb von Suchmaschinen. Wir erhalten keine öffentlichen Fördergelder und sind daher auf Ihre Spenden angewiesen. Wir haben unsere Werbung stark reduziert, denn wir vertrauen auf IHRE Unterstützung.
-Helfen Sie mit, dass freie Suchmaschinen im Internet frei bleiben und stetig weiterentwickelt werden!
-Das können Sie mit einer Spende auf dieser Seite tun. Oder, wenn Sie freie Suchmaschinen auch langfristig sichern wollen:
-            <a href="https://metager.de/beitritt" target="_blank" rel="noopener">Werden Sie Mitglied im Trägerverein von MetaGer, dem SUMA-EV.</a>',
-    'about.3' => 'Für Spenden können Sie unser <a href="/spende" rel="noopener" target="_blank">Spendenformular</a> nutzen. Oder werden Sie Mitglied (<a href="/beitritt" target="_blank" rel="noopener">zum Aufnahmeantrag</a>); Sie haben dann die Möglichkeit, auf den Seiten des SUMA-EV gegebenfalls Ihr Logo mit einem kurzen Text unterzubringen. Wie das aussieht, sehen Sie hier: <a href="https://suma-ev.de/suma-links/index.html#sponsors" target="_blank" rel="noopener">Sponsorenseite</a> und auch auf dieser Seite: <a href="https://suma-ev.de/mitglieder/liste-unserer-mitglieder/" target="_blank" rel="noopener">SUMA-EV Mitglieder</a>. Oder <a href="/spende" target="_blank" rel="noopener">werden Sie SUMA-EV Förderer!</a>',
-    'about.4' => 'JEDE Form Ihrer Unterstützung hilft mit, dass freie Suchmaschinen und freier Wissenszugang im Internet eine Chance haben. Zum freien Wissenszugang gehört es auch, dass Ihre Daten weder überwacht, noch Ihre Internet-Adressen und Verbindungsdaten gespeichert werden. Bei uns wird Ihre Internet-Adresse bereits während der Suche anonymisiert, sie wird nicht gespeichert und nicht an Dritte weitergegeben. Freie Internet-Suche ohne Überwachung: <a href="/" target="_blank" rel="noopener">MetaGer.de!</a>',
-    'about.5' => 'Eine weitere Möglichkeit, MetaGer zu fördern, besteht darin, dass Sie Ihren nächsten Online-Einkauf bei MetaGer-Fördershops machen. Damit wir auf diesem Weg unterstützt werden können, haben wir uns in das Netzwerk zur Förderung gemeinnützig anerkannter Organisationen eingebracht, das Projekt <a href="https://www.wecanhelp.de/430159004" target="_blank" rel="noopener">www.wecanhelp.de</a> Unter dem Dach dieses Projektes sind ca. 400 Online-Shops (von 1und1 bis Zooplus) vereint, die sich bereit erklärt haben, von allen Verkäufen etwa 6% an das Projekt zu spenden. Statt wie bisher direkt zum Online-Shop zu surfen, gehen Sie zunächst auf <a href="/" target="_blank" rel="noopener">MetaGer.de!</a> und klicken dort oben in der Navigationsleiste auf "Fördern", dann auf <a href="https://www.wecanhelp.de/430159004" target="_blank" rel="noopener">"Einkaufen bei MetaGer Fördershops"</a>. Dieser Klick führt Sie in die Shop-Auswahl des Boost-Projektes. Dort suchen Sie sich Ihren Shop aus und machen wie gewohnt Ihren Einkauf. Oder klicken Sie für Einkäufe aus dem Bereich Schreibwaren, Büro- und Geschenkartikel beispielsweise direkt auf <a href="https://www.gladizon.com/" target="_blank" rel="noopener">www.gladizon.com</a>. Das ist alles. Wenn genug Menschen dies tun, dann brauchen wir keine Werbung mehr. Nur zwei Mausklicks für Sie - für alle eine Chance für den freien Wissenszugang in der digitalen Welt.',
 
 ];
--- resources/views/transparency.blade.php
+++ resources/views/transparency.blade.php
@@ -15,26 +15,23 @@
 	<div id="about">
 		<h1 class="page-title">{{ trans('transparency.head.1') }}</h1>
 		<div class="card-heavy">
-			<h2>MetaGer ist transparent</h2>
-			<p> MetaGer ist transparent ... Quellcode frei einlesbar ... </p>
+			<h2>{{ trans('transparency.head.2') }}</h2>
+			<p>{{ trans('transparency.text.1') }}</p>
 		</div>
 		<div class="card-heavy">
-			<h2>Was ist eine Meta-Suchmaschine überhaupt?</h2>
-			<p> Eine Meta-Suchmaschine ist eine Suchmaschine die mehrere andere Suchmaschinen gleichzeitig nutzt. Sie sammelt alle Ergebnisse der verschiedenen Suchmaschinen und wertet diese erneut nach eigenem Schema. Das heißt, dass die Metasuchmaschine keinen eigenen Index hat, also keinen eigenen Datenbestand.</p>
+			<h2>{{ trans('transparency.head.3') }}</h2>
+			<p>{{ trans('transparency.text.2') }}</p>
 		</div>
 		<div class="card-heavy">
-			<h2>Was ist der Vorteil einer Meta-Suchmaschine?</h2>
-			<p>Ein klarer Vorteil von Meta-Suchmaschinen ist es, dass der Nutzer nur eine einzige Suchanfrage braucht um an die Ergebnisse mehrerer Suchmaschinen zu kommen. Die Meta-Suchmaschine gibt die relevanten Ergebnisse in einer nochmals sortierten Ergebnisliste aus.</p>
+			<h2>{{ trans('transparency.head.4') }}</h2>
+			<p>{{ trans('transparency.text.3') }}</p>
 		</div>
 		<div class="card-heavy">
-		<h2>Wie setzt sich unser Ranking zusammen? </h2>
-			<p>Wir übernehmen das Ranking unserer Quell-Suchmaschinen und gewichten diese. Diese Bewertungen werden dann in Punktzahlen umgewandelt.
-Außerdem wird das Vorkommen der Suchbegriffe in der URL und im Snippet, sowie das übermäßige Vorkommen von Besonderer Zeichen (andere Schriftzeichen wie kyrillisch) …
-
-Wir verwenden zudem noch eine Sperrliste, um einzelne Seiten von der Ergebnisliste rauszunehmen. Hier werden Anzeigen  gesperrt, die wir aus juristischer Sicht nicht anzeigen dürfen, sowie Ergebnisse, dessen Qualität so schlecht ist, dass diese keinen Nutzen für die Allgemeinheit haben oder dieser sogar schaden könnte.</p>
+		<h2>{{ trans('transparency.head.5') }}</h2>
+			<p>{{ trans('transparency.text.4') }}</p>
 		</div>
 		<div class="card-heavy">
-		<p>Sollte es weitere Fragen oder Unklarheiten geben, können Sie gerne unser Kontaktformular nutzen und uns Ihre Fragen stellen!</p>
+		<p>{{ trans('transparency.text.5') }}</p>
 		</div>
 	</div>
 @endsection

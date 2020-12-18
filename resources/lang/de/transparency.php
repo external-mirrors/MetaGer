5bbd4051d63f245c9b6bd28f3ebe67d802e9ec38
Kim Höfer <kim@suma-ev.de>
2020-12-18 11:07:38 +0100

    fix typos and change text

--- resources/lang/de/transparency.php
+++ resources/lang/de/transparency.php
@@ -3,15 +3,15 @@
 return [
     'head.1' => 'Transparenzerklärung',
     'head.2' => 'MetaGer ist transparent',
-    'head.3' => 'Was ist eine Meta-Suchmaschine überhaupt?',
-    'head.4' => 'Was ist der Vorteil einer Meta-Suchmaschine?',
+    'head.3' => 'Was ist eine Metasuchmaschine überhaupt?',
+    'head.4' => 'Was ist der Vorteil einer Metasuchmaschine?',
     'head.5' => 'Wie setzt sich unser Ranking zusammen?',
 
 
-    'text.1' => 'MetaGer ist Transparent. Unser Quellcode ist Open-Source und für alle öffentlich einsehbar. Wir verzichten auf die Speicherung von Nutzerdaten und schätzen Datenschutz sowie Privatsphäre. Deshalb gewähren wir anonymen Zugang zu den Suchergebnissen. Dies ist durch einen anonymen Proxy und TOR-hidden-Zugang möglich. Hinzu kommt eine transparente Organisationsstruktur, da MetaGer von dem gemeinnützigern Verein SUMA-EV getragen wird, in dem jeder Mitglied werden kann.',
-    'text.2' => 'Eine Meta-Suchmaschine ist eine Suchmaschine die mehrere andere Suchmaschinen gleichzeitig abfragt. Sie sammelt alle Ergebnisse der verschiedenen Suchmaschinen und wertet diese erneut nach eigenem Schema aus. Das heißt, dass die Metasuchmaschine keinen eigenen Index (Datenbestand) hat.',
-    'text.3' => 'Ein klarer Vorteil von Meta-Suchmaschinen ist es, dass der Nutzer nur eine einzige Suchanfrage braucht um an die Ergebnisse mehrerer Suchmaschinen zu kommen. Die Meta-Suchmaschine gibt die relevanten Ergebnisse in einer nochmals sortierten Ergebnisliste aus. MetaGer ist keine reine Metasuchmaschine, da wir auch kleine eigene Indexe verwenden.',
-    'text.4' => 'Wir übernehmen das Ranking unserer Quell-Suchmaschinen und gewichten diese. Diese Bewertungen werden dann in Punktzahlen umgewandelt. Außerdem wird das Vorkommen der Suchbegriffe in der URL und im Snippet, sowie das übermäßige Vorkommen von besonderen Zeichen (andere Schriftzeichen wie kyrillisch). Wir verwenden zudem noch eine Sperrliste, um einzelne Seiten von der Ergebnisliste rauszunehmen. Hier werden Anzeigen  gesperrt, die wir aus juristischer Sicht nicht anzeigen dürfen, sowie Ergebnisse, dessen Qualität so schlecht ist, dass diese keinen Nutzen für die Allgemeinheit haben oder dieser sogar schaden könnte.',
+    'text.1' => 'MetaGer ist transparent. Unser Quellcode ist frei lizensieret und für alle öffentlich einsehbar. Wir verzichten auf die Speicherung von Nutzerdaten und schätzen Datenschutz sowie Privatsphäre. Deshalb gewähren wir anonymen Zugang zu den Suchergebnissen. Dies ist durch einen anonymen Proxy und TOR-hidden-Zugang möglich. Hinzu kommt eine transparente Organisationsstruktur, da MetaGer von dem gemeinnütziger Verein SUMA-EV getragen wird, in dem jeder Mitglied werden kann.',
+    'text.2' => 'Eine Metasuchmaschine ist eine Suchmaschine, die mehrere andere Suchmaschinen gleichzeitig abfragt. Sie sammelt alle Ergebnisse der verschiedenen Suchmaschinen und wertet diese erneut nach eigenem Schema aus. Das heißt, dass die Metasuchmaschine keinen eigenen Index (Datenbestand) hat.',
+    'text.3' => 'Ein klarer Vorteil von Metasuchmaschinen ist es, dass der Nutzer nur eine einzige Suchanfrage braucht, um an die Ergebnisse mehrerer Suchmaschinen zu kommen. Die Metasuchmaschine gibt die relevanten Ergebnisse in einer nochmals sortierten Ergebnisliste aus. MetaGer ist keine reine Metasuchmaschine, da wir auch kleine eigene Indexe verwenden.',
+    'text.4' => 'Wir übernehmen das Ranking unserer Quell-Suchmaschinen und gewichten diese. Diese Bewertungen werden dann in Punktzahlen umgewandelt. Außerdem wird das Vorkommen der Suchbegriffe in der URL und im Snippet, sowie das übermäßige Vorkommen von besonderen Zeichen (andere Schriftzeichen wie Kyrillisch) berücksichtigt. Wir verwenden zudem noch eine Sperrliste, um einzelne Seiten aus der Ergebnisliste zu entfernen. Wir sperren Webseiten in der Anzeige, wenn wir rechtlich dazu verpflichtet sind. Außerdem behalten wir uns vor, Webseiten mit nachweislich fehlerhaften Informationen, Webseiten von extrem schlechter Qualität und andere besonders zweifelhafte Webseiten zu sperren.',
     'text.5' => 'Sollte es weitere Fragen oder Unklarheiten geben, können Sie gerne unser Kontaktformular nutzen und uns Ihre Fragen stellen!',
     'text.' => '',
     'text.' => '',

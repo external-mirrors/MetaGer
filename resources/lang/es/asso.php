2db11e22b378dd7253dff688323a2b3427700adb
Carsten Riel <carsten@suma-ev.de>
2021-01-19 07:18:52 +0000

    Update resources/lang/es/asso.php, resources/lang/es/sidebar.php, resources/lang/es/spende.php files

--- resources/lang/es/asso.php
+++ resources/lang/es/asso.php
@@ -1,8 +1,10 @@
-<?php return [
+<?php
+
+return [
     "head.1"	=>	"Asociador - MetaGer",
-    "1.1"	=>	"No sabe otras palabras para buscar? Pregunta nuestro asociador.",
+    "1.1"	=>	"Si no obtiene el resultado deseado con las palabras clave utilizadas, quizás es porque le faltan las palabras  correctas o mas adecuadas. Consulte nuestro asociador: Ingrese una o más palabras para las que está buscando términos nuevos o mejores. Ahora intente una nueva búsqueda haciendo clic en \r\nuno de los términos nuevos que aparecen en la página de resultados. Se aplica nuestra política de protección de datos\r\n",
     "1.2"	=>	"Protección de Datos",
-    "reasso.title"	=>	"Asociar esto término",
-    "searchasso.title"	=>	"Buscar (MetaGer) con esto término",
-    "search.placeholder"	=>	"Término para asociar"
+    "reasso.title"	=>	"Asociar este término",
+    "searchasso.title"	=>	"Realizar una búsqueda en MetaGer de este término",
+    "search.placeholder"	=>	"El término a asociar"
 ];

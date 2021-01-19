10db64b1e048300bb6334cd2461acd594d39ae32
Phil Höfer <phil@suma-ev.de>
2021-01-19 12:02:28 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- resources/lang/es/asso.php
+++ resources/lang/es/asso.php
@@ -2,7 +2,7 @@
 
 return [
     "head.1"	=>	"Asociador - MetaGer",
-    "1.1"	=>	"Si no obtiene el resultado deseado con las palabras clave utilizadas, quizás es porque le faltan las palabras  correctas o mas adecuadas. Consulte nuestro asociador: Ingrese una o más palabras para las que está buscando términos nuevos o mejores. Ahora intente una nueva búsqueda haciendo clic en \r\nuno de los términos nuevos que aparecen en la página de resultados. Se aplica nuestra política de protección de datos\r\n",
+    "1.1"	=>	"Si no obtiene el resultado deseado con las palabras clave utilizadas, quizás es porque le faltan las palabras  correctas o mas adecuadas. Consulte nuestro asociador: Ingrese una o más palabras para las que está buscando términos nuevos o mejores. Ahora intente una nueva búsqueda haciendo clic en uno de los términos nuevos que aparecen en la página de resultados. Se aplica nuestra política de protección de datos",
     "1.2"	=>	"Protección de Datos",
     "reasso.title"	=>	"Asociar este término",
     "searchasso.title"	=>	"Realizar una búsqueda en MetaGer de este término",

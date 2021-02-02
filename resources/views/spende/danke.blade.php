7f8c29ba71e173b936a4a9a1d0cedcf65da08474
Davide Aprea <davide@suma-ev.de>
2021-02-02 14:00:57 +0100

    avoid showing invalid key

--- resources/views/spende/danke.blade.php
+++ resources/views/spende/danke.blade.php
@@ -43,9 +43,11 @@
 				</table>
 			</div>
 			<button type="button" class="btn btn-primary noprint" onclick="window.print();">{{ trans('spende.drucken') }}</button>
+			@if($data['key'] !== false)
 			<div class="card-light">
 				<p style="text-align: center; width:100%;">{!! trans('spende.danke.schluessel') . ' ' . $data['key'] !!}</p>
 			</div>
+			@endif
 		</div>
 		<div class="col-lg-6 col-md-12 col-sm-12 others noprint" id="right">
 			<h2>{{ trans('about.about.1') }}</h2>

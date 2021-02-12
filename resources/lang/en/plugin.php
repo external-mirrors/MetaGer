5796acf08bfdb46b09e938836f0e4825b6275c8e
Davide Aprea <davide@suma-ev.de>
2021-02-12 13:02:56 +0100

    add seperate shortnames for locales

--- resources/lang/en/plugin.php
+++ resources/lang/en/plugin.php
@@ -1,5 +1,8 @@
 <?php
 
 return [
-    "description"	=>	"MetaGer: secure search & find, protecting privacy"
+    "description"	=>	"MetaGer: secure search & find, protecting privacy",
+    "production" => "MetaGer (en)",
+    "development" => "MetaGer Development (en)"
+
 ];

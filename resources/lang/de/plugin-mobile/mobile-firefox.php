62ddeac2daa74f9fdf73f24879b92b5f21135ea9
Kim Höfer <kim@suma-ev.de>
2021-03-26 10:48:21 +0100

    add icon and exchange old icon with new icon

--- resources/lang/de/plugin-mobile/mobile-firefox.php
+++ resources/lang/de/plugin-mobile/mobile-firefox.php
@@ -9,11 +9,11 @@
         'default-search-vlt80.3' => 'In den Browsereinstellungen haben Sie nun die Möglichkeit, im Bereich "Suchen" MetaGer als Standardsuchmaschine zu wählen.',
 
         //Instructions for Firefox Mobile version >= 80
-        'default-search-v80.1' => 'Tippen Sie in Ihrem Browser unten rechts auf "<i class="fas fa-ellipsis-v"></i>", um das Browsermenü zu öffnen.',
+        'default-search-v80.1' => 'Tippen Sie in Ihrem Browser unten rechts auf "<img class= "mg-icon" src="/img/ellipsis.svg">", um das Browsermenü zu öffnen.',
         'default-search-v80.2' => 'Wählen Sie den Menüpunkt "Einstellungen".',
         'default-search-v80.3' => 'In dem dann erscheinenden Menü wählen Sie in der Kategorie "Allgemein" den Menüpunkt "Suchen".',
         'default-search-v80.4' => 'Tippen Sie auf "+ Suchmaschine hinzufügen".',
         'default-search-v80.5' => 'Wählen Sie "Andere" aus und tragen darunter den Namen (MetaGer) und den Such-String ein.',
-        'default-search-v80.6' => 'Bestätigen Sie mit "<i class="fas fa-check"></i>" in der rechten oberen Ecke.',
+        'default-search-v80.6' => 'Bestätigen Sie mit "<img class= "mg-icon" src="/img/icon-check.svg">" in der rechten oberen Ecke.',
         'default-search-v80.7' => 'Tippen Sie in der dann erscheinenden Liste auf MetaGer.',
     ];

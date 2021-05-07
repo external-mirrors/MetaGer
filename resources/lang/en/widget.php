9744ad6da89b120abadce2984b5e25c1173b8b02
Carsten Riel <carsten@suma-ev.de>
2021-05-07 06:30:14 +0000

    Update resources/lang/en/widget.php

--- resources/lang/en/widget.php
+++ resources/lang/en/widget.php
@@ -7,5 +7,5 @@ return [
 Please choose the scope of your widget:',
     'body.2' => 'Search the Web',
     'body.3' => 'Search one Domain',
-    'body.4' => 'Please note: You must not use this widget, if your implementation suggests that MetaGer is your service or that your site is the real MetaGer homepage (all this has alreday happend). Therefore it is especially forbidden to remove our logo.',
+    'body.4' => 'Please note: You must not use this widget, if your implementation suggests that MetaGer is your service or that your site is the real MetaGer homepage (all this has already happend). Therefore it is especially forbidden to remove our logo.',
 ];

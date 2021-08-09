be1677881c0b35ac35577442a5465c5bce12b8c0
Kim <kim@suma-ev.de>
2021-08-09 09:50:08 +0200

    minor text changes

--- resources/lang/en/about.php
+++ resources/lang/en/about.php
@@ -9,16 +9,16 @@ return [
    
 
     'timeline.1' => 'March 1996: Cebit Hanover',
-    'timeline.1.1' => 'The idea of MetaGer arose during the CeBIT computer fair over lunch on a paper napkin. Work on the first prototype begins that same evening. ',
+    'timeline.1.1' => 'The idea of MetaGer arises during the CeBIT computer fair over lunch on a paper napkin. Work on the first prototype begins that same evening. ',
    
     'timeline.2' => 'End of 1996: Launch of MetaGer',
-    'timeline.2.1' => 'MetaGer is being developed as a research project by the University of Hanover and the regional data center in Lower Saxony. ',
+    'timeline.2.1' => 'MetaGer is developed as a research project by the University of Hanover and the regional data center in Lower Saxony. ',
    
     'timeline.3' => '1997-2006: MetaGer Startpage',
-    'timeline.3.1' => 'This is what MetaGer looks like when it first goes online. MetaGer provides an innovative new service at that time.',
+    'timeline.3.1' => 'This is what MetaGer looks like as it first goes online. MetaGer provides an innovative new service at that time.',
    
     'timeline.4' => '01.10.2012: Change of Sponsorship to SUMA-EV ',
-    'timeline.4.1' => 'MetaGer is transferred to the non-profit association SUMA-EV - Association for Free Access to Knowledge. The cooperation with the University of Hannover will be continued.',
+    'timeline.4.1' => 'MetaGer is transferred to the non-profit association SUMA-EV - Association for Free Access to Knowledge. The cooperation with the University of Hannover is continued.',
    
     'timeline.5' => '2006-2015: Introduction of New Designs',
     'timeline.5.1' => '
@@ -28,16 +28,16 @@ return [
     'timeline.6.1' => 'For the first time, the interface of MetaGer is also offered in English.',
 
     'timeline.7' => 'December  2013: Tor-Service',
-    'timeline.7.1' => 'MetaGer offers access within the Tor anonymization network as a security-related feature.',
+    'timeline.7.1' => 'MetaGer offers access within the Tor anonymization network as a security feature.',
 
     'timeline.8' => 'March 2014: Implementation of the "Open Anonymously" Function',
     'timeline.8.1' => 'The new "Open anonymously" function provides a proxy service. Through this MetaGer users remain protected even after the search.',
 
     'timeline.9' => '2015-2016: MetaGer Startpage',
-    'timeline.9.1' => 'As part of more extensive modernizations, a new simplified home page is offered.',
+    'timeline.9.1' => 'As part of more extensive modernizations, a new simplified home page is introduced.',
 
     'timeline.10' => 'March 2016: Search Results under 1.3 Seconds',
-    'timeline.10.1' =>'Extensive modernization at the core of MetaGer reduces the average response time of the search engine to less than 1.3 seconds. Over the next few years, this drops further to almost half.',
+    'timeline.10.1' =>'Optimizations at MetaGer\'s Core reduce reduces the average response time of the search engine to less than 1.3 seconds. Over the next few years, this drops further to almost half.',
 
     'timeline.11' => '2016-2019: MetaGer Startpage',
     'timeline.11.1' => 'Other small adjustments gradually solidify orange as the MetaGer color.  ',
@@ -46,19 +46,19 @@ return [
     'timeline.12.1' => 'MetaGer is released as open source under the free GNU AGPL license.',
 
     'timeline.13' => 'December 2016: Launch of MetaGer Maps',
-    'timeline.13.1' => 'Maps.MetaGer.de is launched as a privacy-friendly map service. The maps are based on data from the OpenStreetMap project, but have their own display and route planners.',
+    'timeline.13.1' => 'Maps.MetaGer.de is launched as a privacy-friendly map service. The maps are based on data from the OpenStreetMap project, but have their own display and route planner.',
 
     'timeline.14' => '2017: Relaunch of English MetaGer Version',
     'timeline.14.1' => 'The english MetaGer version is greatly improved by extensive fine-tuning and own result sources.',
 
     'timeline.15' => 'September 2017: Revision of the Quicktips',
-    'timeline.15.1' => 'The Quicktips, which offer hints beyond normal search results, have been expanded in functionality and spun off into a separate project. Instead of being located above the results, they are now in a sidebar.',
+    'timeline.15.1' => 'The Quicktips, which offer hints beyond normal search results, are expanded in functionality and spun off into a separate project. Instead of being located above the results, they are now in a sidebar.',
 
     'timeline.16' => '2019-2020: MetaGer Startpage',
     'timeline.16.1' => 'On the new home page, four characteristic features of MetaGer are highlighted and promoted for the first time.',
 
     'timeline.17' => '26.10.2020: Current Startpage and Dark Mode',
-    'timeline.17.1' => 'As part of an extensive redesign of the start page, MetaGer\'s four unique selling points have been revisited and presented in an expanded form. In order not to overload the central function of the web search, the descriptions can be reached by scrolling. <br> Another feature of the new revision is the page-wide dark mode. This allows MetaGer to be used on all pages in a dark design.',
+    'timeline.17.1' => 'As part of an extensive redesign of the start page, MetaGer\'s four unique selling points have been revisited and presented in an expanded form. In order to avoid overloading the central function of the web search, the descriptions can be reached by scrolling. <br> Another feature of the new revision is the page-wide dark mode. This allows MetaGer to be in a dark design on all pages.',
 
     'timeline.18' => 'Today',
     'timeline.18.1' => 'Even today, MetaGer is constantly evolving.'

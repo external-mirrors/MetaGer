64088a011e0b1769c1f85fe056ec58dfe34c6036
Kim <kim@suma-ev.de>
2021-08-12 14:21:13 +0200

    add text to help privacy protection subpage

---
+++ resources/lang/de/help/help-privacy-protection.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    "datenschutz.title" => "Anonymität und Datensicherheit",
+    "datenschutz.faktencheck.heading" => "Fakten-Prüfung contra Fake-News:",
+    "datenschutz.faktencheck.body.1" => '<a href="http://www.password-online.de/?wysija-page=1&controller=email&action=view&email_id=280" target="_blanK" rel="noopener">Ausführliche Anleitung und Beschreibung</a> von <a href="http://www.ude.de/" target="_blanK" rel="noopener">Albrecht Ude</a>',
+    "datenschutz.faktencheck.body.2" => '<a href="/hilfe/faktencheck">Fakten-Checkliste</a>',
+
+    "datenschutz.1" => 'Tracking-Cookies, Session-IDs und IP-Adressen',
+    "datenschutz.2" => 'Nichts von alldem wird hier bei MetaGer verwendet, gespeichert, aufgehoben oder sonst irgendwie verarbeitet (Ausnahme: Kurzfristige Speicherung gegen Hacking- und Bot-Attacken). Weil wir diese Thematik für extrem wichtig halten, haben wir auch Möglichkeiten geschaffen, die Ihnen helfen können, hier ein Höchstmaß an Sicherheit zu erreichen: den MetaGer-TOR-Hidden-Service und unseren anonymisierenden Proxyserver.',
+    "datenschutz.3" => "Mehr Informationen finden Sie weiter unten. Die Funktionen sind unter \"Dienste\" in der Navigationsleiste erreichbar.",
+
+    "tor.title" => "Tor-Hidden-Service",
+    "tor.1" => "Bei MetaGer werden schon seit vielen Jahren die IP-Adressen ausgeblendet und nicht gespeichert. Nichtsdestotrotz sind diese Adressen auf dem MetaGer-Server zeitweise, während eine Suche läuft, sichtbar: wenn MetaGer also einmal kompromittiert sein sollte, dann könnte dieser Angreifer Ihre Adressen mitlesen und speichern. Um dem höchsten Sicherheitsbedürfnis entgegenzukommen, unterhalten wir eine MetaGer-Instanz im Tor-Netzwerk: den MetaGer-TOR-hidden-Service - erreichbar über: <a href=\"/tor/\" target=\"_blank\" rel=\"noopener\">https://metager.de/tor/</a>. Für die Benutzung benötigen Sie einen speziellen Browser, den Sie auf <a href=\"https://www.torproject.org/\" target=\"_blank\" rel=\"noopener\">https://www.torproject.org/</a> herunter laden können.",
+    "tor.2" => "MetaGer erreichen Sie im Tor-Browser dann unter: http://metagerv65pwclop2rsfzg4jwowpavpwd6grhhlvdgsswvo6ii4akgyd.onion .",
+
+    "proxy.title" => "Anonymisierender MetaGer-Proxyserver",
+    "proxy.1" => "Um ihn zu verwenden, müssen Sie auf der MetaGer-Ergebnisseite nur auf \"ANONYM ÖFFNEN\" am unteren Rand des Ergebnisses klicken. Dann wird Ihre Anfrage an die Zielwebseite über unseren anonymisierenden Proxy-Server geleitet und Ihre persönlichen Daten bleiben weiterhin völlig geschützt. Wichtig: wenn Sie ab dieser Stelle den Links auf den Seiten folgen, bleiben Sie durch den Proxy geschützt. Sie können aber oben im Adressfeld keine neue Adresse ansteuern. In diesem Fall verlieren Sie den Schutz. Ob Sie noch geschützt sind, sehen Sie ebenfalls im Adressfeld. Es zeigt: https://proxy.suma-ev.de/?url=hier steht die eigentlich Adresse.",
+
+    "maps.title" => "MetaGer Maps",
+    "maps.1" => 'Die Sicherung der Privatsphäre im Zeitalter der globalen Datenkraken hat uns auch bewogen, <a href="https://maps.metager.de" target="_blank">https://maps.metager.de</a> zu entwickeln: Der (unseres Wissens) einzige Routenplaner, der die vollen Funktionalitäten via Browser und App bietet - ohne dass Nutzer-Standorte gespeichert werden.  All dies ist nachprüfbar, denn unsere Softwaren sind Open-Source. Für die Nutzung von maps.metager.de empfehlen wir unsere schnelle App-Version. Unsere Apps können Sie unter <a href="/app" target="_blank">App beziehen</a> downloaden (oder natürlich auch über den Play-Store).',
+    "maps.2" => "Diese Kartenfunktion kann auch von der MetaGer-Suche aufgerufen werden (und umgekehrt). Sobald Sie bei MetaGer nach einem Begriff gesucht haben, sehen Sie oben rechts einen neuen Suchfokus \"Maps\".  Beim Klick darauf gelangen Sie zu einer dazugehörigen Karte. .",
+    "maps.3" => "Die Karte zeigt nach dem Aufrufen die auch in der Spalte rechts wiedergegebenen von MetaGer gefundenen Punkte (POIs = Points of Interest). Beim Zoomen passt sich diese Liste an den Kartenausschnitt an.  Wenn Sie die Maus über eine Markierung in der Karte oder in der Liste halten, wird das jeweilige Gegenstück hervorgehoben.  Klicken Sie auf \"Details\", um genauere Informationen zu diesem Punkt aus der darunter liegenden Datenbank zu erhalten.",
+];
---
+++ resources/views/help/help-privacy-protection.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.subPages', ['page' => 'hilfe'])
+
+@section('title', $title )
+
+@section('content')
+<h2>{!! trans('help/help-privacy-protection.datenschutz.title') !!}</h2>
+	<section id="factcheck">
+		<h3>{!! trans('help/help-privacy-protection.datenschutz.faktencheck.heading') !!}</h3>
+		<div>
+			<p>@lang('help/help-privacy-protection.datenschutz.faktencheck.body.1')</p>
+			<p>@lang('help/help-privacy-protection.datenschutz.faktencheck.body.2')</p>
+		</div>
+	</section>
+	<section id="tracking">
+		<h3>{!! trans('help/help-privacy-protection.datenschutz.1') !!}</h3>
+		<div>
+			<p>{!! trans('help/help-privacy-protection.datenschutz.2') !!}</p>
+			<p>{!! trans('help/help-privacy-protection.datenschutz.3') !!}</p>
+		</div>
+	</section>
+	<section id="torhidden">
+		<h3>{!! trans('help/help-privacy-protection.tor.title') !!}</h3>
+		<div>
+			<p>{!! trans('help/help-privacy-protection.tor.1') !!}</p>
+			<p>{!! trans('help/help-privacy-protection.tor.2') !!}</p>
+		</div>
+	</section>
+	<section id="proxy">
+		<h3>{!! trans('help/help-privacy-protection.proxy.title') !!}</h3>
+		<div>
+			<p>{!! trans('help/help-privacy-protection.proxy.1') !!}</p>
+		</div>
+	</section>
+
+	<section id="maps">
+		<h3>{!! trans('help/help-privacy-protection.maps.title') !!}</h3>
+		<div>
+			<p>{!! trans('help/help-privacy-protection.maps.1') !!}</p>
+			<p>{!! trans('help/help-privacy-protection.maps.2') !!}</p>
+			<p>{!! trans('help/help-privacy-protection.maps.3') !!}</p>
+		</div>
+	</section>
+    @endsection

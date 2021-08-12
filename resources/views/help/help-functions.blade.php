cef1a273a6175e41486a42f75660bf45ea1f39b0
Kim <kim@suma-ev.de>
2021-08-12 14:18:53 +0200

    add text to help function subpage

---
+++ resources/views/help/help-functions.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.subPages', ['page' => 'hilfe'])
+
+@section('title', $title )
+
+@section('content')
+<h1 class="page-title">{!! trans('help/help-functions.title') !!}</h1>
+<section>
+		<h3>{!! trans('help/help-functions.stopworte.title') !!}</h3>
+		<div>
+			<p>{!! trans('help/help-functions.stopworte.1') !!}</p>
+			<ul class="dotlist">
+				<li>{!! trans('help/help-functions.stopworte.2') !!}</li>
+				<li class="nodot"><div class="search-example">{!! trans('help/help-functions.stopworte.3') !!}</div></li>
+			</ul>
+		</div>
+	</section>
+	<section id="severalwords">
+		<h3>{!! trans('help/help-functions.mehrwortsuche.title') !!}</h3>
+		<div>
+			<p>{!! trans('help/help-functions.mehrwortsuche.1') !!}</p>
+			<p>{!! trans('help/help-functions.mehrwortsuche.2') !!}</p>
+			<ul class="dotlist">
+				<li>{!! trans('help/help-functions.mehrwortsuche.3') !!}</li>
+				<li class="nodot"><div class = "search-example">{!! trans('help/help-functions.mehrwortsuche.3.example') !!}</div></li>
+				<li>{!! trans('help/help-functions.mehrwortsuche.4') !!}</li>
+				<li class="nodot"><div class = "search-example">{!! trans('help/help-functions.mehrwortsuche.4.example') !!}</div></li>
+			</ul>
+		</div>
+	</section>
+
+	<section id="capitalizationrules">
+		<h3>{!! trans('help/help-functions.grossklein.title') !!}</h3>
+		<div>
+			<p>{!! trans('help/help-functions.grossklein.1') !!}</p>
+			<ul class="dotlist">
+				<li>{!! trans('help/help-functions.grossklein.2') !!}</li>
+				<li class="nodot"><div class="search-example">{!! trans('help/help-functions.grossklein.2.example') !!}</div></li>
+				<li class="nodot">{!! trans('help/help-functions.grossklein.3') !!}</li>
+				<li class="nodot"><div class="search-example">{!! trans('help/help-functions.grossklein.3.example') !!}</div></li>
+			</ul>
+		</div>
+	</section>
+	<section id="urls">
+		<h3>{!! trans('help/help-functions.urls.title') !!}</h3>
+		<div>
+			<p>{!! trans('help/help-functions.urls.explanation') !!}</p>
+			<ul class="dotlist">
+				<li>{!! trans('help/help-functions.urls.example.1') !!}</li>
+				<li class="nodot"><div class = "search-example">{!! trans('help/help-functions.urls.example.2') !!}</div></li>
+			</ul>
+		</div>
+	</section>
+	<section id="bangs">
+		<h3>{!! trans('help/help-functions.bang.title') !!}</h3>
+		<div>
+			<p>{!! trans('help/help-functions.bang.1') !!}</p>
+		</div>
+	</section>
+	<section id="searchinsearch">
+		<h3>{!! trans('help/help-functions.searchinsearch.title') !!}</h3>
+		<div>
+			<p>{!! trans('help/help-functions.searchinsearch.1') !!}</p>
+		</div>
+	</section>
+
+@endsection

0af79056336e04c394807134d3a245d20a2b8c64
Kim <kim@suma-ev.de>
2021-08-26 11:33:01 +0200

    add cookie link to help page

--- resources/views/help/help-mainpages.blade.php
+++ resources/views/help/help-mainpages.blade.php
@@ -45,7 +45,7 @@
 			</div>
         <h3 id="einstellungen">{!! trans('help/help-mainpages.settings.title') !!}</h3>
             <ul>
-                <li>{!! trans('help/help-mainpages.settings.1') !!}</li>
+                <li>@lang('help/help-mainpages.settings.1', ["link" => LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), route('showAllSettings', ['url' => url()->full()])) ])</li>
                 <li>{!! trans('help/help-mainpages.settings.2') !!}</li>
                 <li>{!! trans('help/help-mainpages.settings.3') !!}</li>
                 <li>{!! trans('help/help-mainpages.settings.4') !!}</li>

42ae0f10e4934fa5be46c342abec261f9f64b285
Kim <kim@suma-ev.de>
2021-09-16 15:47:43 +0200

    add translations

--- resources/lang/en/help/help.php
+++ resources/lang/en/help/help.php
@@ -2,7 +2,6 @@
 return [
     "achtung"               => "Warning! Structure and functionality of our website are subject to constant development and changes. We try to update our help pages as quickly as possible, but can not prevent temporary mistakes.",
     "title"                 => "MetaGer - Help",
-
    
     'tableofcontents.title' => '',
     'tableofcontents.1' => 'Use of the search engine',
@@ -10,7 +9,6 @@ return [
     'tableofcontents.1.2' => 'The search field',
     'tableofcontents.1.3' => 'The result page',
     'tableofcontents.1.4' => 'Settings',
-
 
     'tableofcontents.2' => 'Useful functions and hints',
     'tableofcontents.2.1' => 'Exclude single words',
@@ -21,15 +19,12 @@ return [
     'tableofcontents.2.6' => 'Search in search',
     'tableofcontents.2.7' => 'Add MetaGer',
 
-
-
     'tableofcontents.4' => 'Services',
     'tableofcontents.4.1' => 'Android-App',
     'tableofcontents.4.2' => 'Browser-Plugin',
     'tableofcontents.4.3' => 'MetaGer-Web-Associator',
     'tableofcontents.4.4' => 'MetaGer widget',
     'tableofcontents.4.5' => 'MetaGer maps',
-
 
     'tableofcontents.3' => 'Privacy',
     'tableofcontents.3.1' => 'Fact-Check versus fake news',
@@ -38,12 +33,6 @@ return [
     'tableofcontents.3.4' => 'MetaGer proxy server',
     'tableofcontents.3.5' => 'inappropriate Content/ youth protection',
 
-
-
-
-
-  
-   
     'faq.title' => 'FAQ',
 
     'searchengine.title' => 'How does MetaGer query other search engines while preserving user anonymity?',

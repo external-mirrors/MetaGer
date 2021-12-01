2107ddce75743370a0e12707a1483fe0857521eb
Carsten Riel <carsten@suma-ev.de>
2021-12-01 11:33:26 +0000

    Update resources/lang/de/footer.php

--- resources/lang/de/footer.php
+++ resources/lang/de/footer.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'sumaev.1' => 'Ein Projekt des',
+    'sumaev.1' => 'MetaGer - Ein Projekt des SUMA-EV',
     'sumaev.2' => 'SUMA-EV',
     'sumaev.link' => 'https://suma-ev.de',
 ];

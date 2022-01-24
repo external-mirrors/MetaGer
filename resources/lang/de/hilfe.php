c80cf86ea917137cbe59e4743d975b7f12f49484
Carsten Riel <carsten@suma-ev.de>
2022-01-24 09:08:46 +0000

    Update resources/lang/de/hilfe.php

--- resources/lang/de/hilfe.php
+++ resources/lang/de/hilfe.php
@@ -6,7 +6,7 @@ return [
 
     "title.2"               => 'Benutzung der Suchmaschine',
     "startpage.title"       => 'Die Startseite',
-    "startpage.info"        => 'Die Sartseite beinhaltet das Suchfeld, oben rechts einen Button zum Aufrufen des Menus und unter dem Suchfeld einen Link, mit dem Sie MetaGer zu Ihrem Browser hinzufügen können. Im unteren Bereich finden Sie Informationen zu MetaGer und dem Trägerverein SUMA-EV.',
+    "startpage.info"        => 'Die Startseite beinhaltet das Suchfeld, oben rechts einen Button zum Aufrufen des Menus und unter dem Suchfeld einen Link, mit dem Sie MetaGer zu Ihrem Browser hinzufügen können. Im unteren Bereich finden Sie Informationen zu MetaGer und dem Trägerverein SUMA-EV.',
     "searchfield.title"     => 'Das Suchfeld',
     "searchfield.info"      => 'Das Suchfeld besteht aus mehreren Teilen:',
     "searchfield.memberkey" => 'dem Schlüsselsymbol (nur für Vereinsmitglieder): Hier können die Mitglieder des SUMA-EV Ihren Schlüssel eingeben, um die werbefreie Suche nutzen zu können. Sie wollen das auch? Werden Sie Mitglied: <a href = "/beitritt/">Beitrittsantrag</a>',

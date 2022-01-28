caaf94ef99293d2204ffba6695ffe09e0d55351c
Kim <kim@suma-ev.de>
2022-01-28 13:28:19 +0100

    fix translation links

--- resources/views/help/easy-language/help-mainpages.blade.php
+++ resources/views/help/easy-language/help-mainpages.blade.php
@@ -3,58 +3,58 @@
 @section('title', $title )
 
 @section('content')
-	<h1 class="page-title">{!! trans('help/help-mainpages.title') !!}</h1>
+	<h1 class="page-title">{!! trans('help/easy-language/help-mainpages.title') !!}</h1>
 
 	<section id="startpage">
-		<a  class=help-back-button href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/hilfe") }}"><img class="back-arrow" src=/img/back-arrow.svg>{!! trans('help/help-mainpages.backarrow') !!}</a>
-		<h2>{!! trans('help/help-mainpages.title.2') !!}</h2>
+		<a  class=help-back-button href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/hilfe") }}"><img class="back-arrow" src=/img/back-arrow.svg>{!! trans('help/easy-language/help-mainpages.backarrow') !!}</a>
+		<h2>{!! trans('help/easy-language/help-mainpages.title.2') !!}</h2>
 
-		<h3 id="startseite">{!! trans('help/help-mainpages.startpage.title') !!}</h3>
-		<p>{!! trans('help/help-mainpages.startpage.info') !!}</p>
-		<h3 id="suchfeld">{!! trans('help/help-mainpages.searchfield.title') !!}</h3>
+		<h3 id="startseite">{!! trans('help/easy-language/help-mainpages.startpage.title') !!}</h3>
+		<p>{!! trans('help/easy-language/help-mainpages.startpage.info') !!}</p>
+		<h3 id="suchfeld">{!! trans('help/easy-language/help-mainpages.searchfield.title') !!}</h3>
 		<div>
-			<p>{!! trans('help/help-mainpages.searchfield.info') !!}</p>
+			<p>{!! trans('help/easy-language/help-mainpages.searchfield.info') !!}</p>
 			<ul class="dotlist">
-				<li>{!! trans('help/help-mainpages.searchfield.memberkey') !!}</li>
-				<li>{!! trans('help/help-mainpages.searchfield.slot') !!}</li>
-				<li>{!! trans('help/help-mainpages.searchfield.search') !!}</li>
-				<li>{!! trans('help/help-mainpages.searchfield.morefunctions') !!}</li>
+				<li>{!! trans('help/easy-language/help-mainpages.searchfield.memberkey') !!}</li>
+				<li>{!! trans('help/easy-language/help-mainpages.searchfield.slot') !!}</li>
+				<li>{!! trans('help/easy-language/help-mainpages.searchfield.search') !!}</li>
+				<li>{!! trans('help/easy-language/help-mainpages.searchfield.morefunctions') !!}</li>
 			</ul>
 		</div>
-        <h3 id="ergebnis">{!! trans('help/help-mainpages.resultpage.title') !!}</h3>
+        <h3 id="ergebnis">{!! trans('help/easy-language/help-mainpages.resultpage.title') !!}</h3>
 		    <div>
 			    <ul class="dotlist">
-				    <li>{!! trans('help/help-mainpages.resultpage.foci') !!}</li>
-				    <li>{!! trans('help/help-mainpages.resultpage.choice') !!}</li>
+				    <li>{!! trans('help/easy-language/help-mainpages.resultpage.foci') !!}</li>
+				    <li>{!! trans('help/easy-language/help-mainpages.resultpage.choice') !!}</li>
 				        <ul class="dotlist">
-				        	<li>{!! trans('help/help-mainpages.resultpage.filter') !!}</li>
-				        	<li id="difset">{!! trans('help/help-mainpages.resultpage.settings') !!}</li>
+				        	<li>{!! trans('help/easy-language/help-mainpages.resultpage.filter') !!}</li>
+				        	<li id="difset">{!! trans('help/easy-language/help-mainpages.resultpage.settings') !!}</li>
             </div>
-		<h3>{!! trans('help/help-mainpages.result.title') !!}</h3>
+		<h3>{!! trans('help/easy-language/help-mainpages.result.title') !!}</h3>
 			<div>
-				<p>{!! trans('help/help-mainpages.result.info.1') !!}</p>
+				<p>{!! trans('help/easy-language/help-mainpages.result.info.1') !!}</p>
 				<ul class = "dotlist">
-					<li>{!! trans('help/help-mainpages.result.info.open') !!}</li>
-					<li>{!! trans('help/help-mainpages.result.info.newtab') !!}</li>
-					<li>{!! trans('help/help-mainpages.result.info.anonym') !!}</li>
-					<li>{!! trans('help/help-mainpages.result.info.more') !!}</li>
+					<li>{!! trans('help/easy-language/help-mainpages.result.info.open') !!}</li>
+					<li>{!! trans('help/easy-language/help-mainpages.result.info.newtab') !!}</li>
+					<li>{!! trans('help/easy-language/help-mainpages.result.info.anonym') !!}</li>
+					<li>{!! trans('help/easy-language/help-mainpages.result.info.more') !!}</li>
 				</ul>
-				<p>{!! trans('help/help-mainpages.result.info.2') !!}</p>
+				<p>{!! trans('help/easy-language/help-mainpages.result.info.2') !!}</p>
 				<ul class = "dotlist">
-					<li>{!! trans('help/help-mainpages.result.info.saveresult') !!}</li>
-					<li>{!! trans('help/help-mainpages.result.info.domainnewsearch') !!}</li>
-					<li>{!! trans('help/help-mainpages.result.info.hideresult') !!}</li>
+					<li>{!! trans('help/easy-language/help-mainpages.result.info.saveresult') !!}</li>
+					<li>{!! trans('help/easy-language/help-mainpages.result.info.domainnewsearch') !!}</li>
+					<li>{!! trans('help/easy-language/help-mainpages.result.info.hideresult') !!}</li>
 				</ul>
 			</div>
-        <h3 id="einstellungen">{!! trans('help/help-mainpages.settings.title') !!}</h3>
+        <h3 id="einstellungen">{!! trans('help/easy-language/help-mainpages.settings.title') !!}</h3>
             <ul>
-                <li>@lang('help/help-mainpages.settings.1', ["link" => LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), route('showAllSettings', ['url' => url()->full()])) ])</li>
-                <li>{!! trans('help/help-mainpages.settings.2') !!}</li>
-                <li>{!! trans('help/help-mainpages.settings.3') !!}</li>
-                <li>{!! trans('help/help-mainpages.settings.4') !!}</li>
-                <li>{!! trans('help/help-mainpages.settings.5') !!}</li>
-                <li>{!! trans('help/help-mainpages.settings.6') !!}</li>
-                <li>{!! trans('help/help-mainpages.settings.7') !!}</li>
+                <li>@lang('help/easy-language/help-mainpages.settings.1', ["link" => LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), route('showAllSettings', ['url' => url()->full()])) ])</li>
+                <li>{!! trans('help/easy-language/help-mainpages.settings.2') !!}</li>
+                <li>{!! trans('help/easy-language/help-mainpages.settings.3') !!}</li>
+                <li>{!! trans('help/easy-language/help-mainpages.settings.4') !!}</li>
+                <li>{!! trans('help/easy-language/help-mainpages.settings.5') !!}</li>
+                <li>{!! trans('help/easy-language/help-mainpages.settings.6') !!}</li>
+                <li>{!! trans('help/easy-language/help-mainpages.settings.7') !!}</li>
     </section>
 
 
--- resources/views/help/easy-language/help-services.blade.php
+++ resources/views/help/easy-language/help-services.blade.php
@@ -3,38 +3,38 @@
 @section('title', $title )
 
 @section('content')
-<h1 class="page-title">{!! trans('help/help-services.title') !!}</h1>
-<a  class=help-back-button href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/hilfe") }}"><img class="back-arrow" src=/img/back-arrow.svg>{!! trans('help/help-services.backarrow') !!}</a>
-<h2 id="dienste">{!! trans('help/help-services.dienste') !!}</h2>
-	<h3><img class= "mg-icon" src="/img/angle-double-right.svg" alt="{{ trans('angle-double-right.alt') }}" aria-hidden= "true"> {!! trans('help/help-services.dienste.kostenlos') !!}</h3>
+<h1 class="page-title">{!! trans('help/easy-language/help-services.title') !!}</h1>
+<a  class=help-back-button href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/hilfe") }}"><img class="back-arrow" src=/img/back-arrow.svg>{!! trans('help/easy-language/help-services.backarrow') !!}</a>
+<h2 id="dienste">{!! trans('help/easy-language/help-services.dienste') !!}</h2>
+	<h3><img class= "mg-icon" src="/img/angle-double-right.svg" alt="{{ trans('angle-double-right.alt') }}" aria-hidden= "true"> {!! trans('help/easy-language/help-services.dienste.kostenlos') !!}</h3>
 	<section id="app">
 		<div id="mg-app" style="margin-top: -100px"></div>
 		<div style="margin-top: 100px"></div>
-		<h3>{!! trans('help/help-services.app.title') !!}</h3>
+		<h3>{!! trans('help/easy-language/help-services.app.title') !!}</h3>
 		<div>
-			<p>{!! trans('help/help-services.app.1') !!}</p>
+			<p>{!! trans('help/easy-language/help-services.app.1') !!}</p>
 		</div>
 	</section>
 
 	<section id="asso">
-		<h3>{!! trans('help/help-services.suchwortassoziator.title') !!}</h3>
+		<h3>{!! trans('help/easy-language/help-services.suchwortassoziator.title') !!}</h3>
 		<div>
-			<p>{!! trans('help/help-services.suchwortassoziator.1') !!}</p>
-			<p>{!! trans('help/help-services.suchwortassoziator.2') !!}</p>
-			<p>{!! trans('help/help-services.suchwortassoziator.3') !!}</p>
+			<p>{!! trans('help/easy-language/help-services.suchwortassoziator.1') !!}</p>
+			<p>{!! trans('help/easy-language/help-services.suchwortassoziator.2') !!}</p>
+			<p>{!! trans('help/easy-language/help-services.suchwortassoziator.3') !!}</p>
 		</div>
 	</section>
 	<section id="widget">
-		<h3>{!! trans('help/help-services.widget.title') !!}</h3>
+		<h3>{!! trans('help/easy-language/help-services.widget.title') !!}</h3>
 		<div>
-			<p>{!! trans('help/help-services.widget.1') !!}</p>
+			<p>{!! trans('help/easy-language/help-services.widget.1') !!}</p>
 		</div>
 	</section>
 	<section id="maps">
-		<h3>{!! trans('help/help-services.maps.title') !!}</h3>
+		<h3>{!! trans('help/easy-language/help-services.maps.title') !!}</h3>
 		<div>
-			<p>{!! trans('help/help-services.maps.1') !!}</p>
-			<p>{!! trans('help/help-services.maps.2') !!}</p>
-			<p>{!! trans('help/help-services.maps.3') !!}</p>
+			<p>{!! trans('help/easy-language/help-services.maps.1') !!}</p>
+			<p>{!! trans('help/easy-language/help-services.maps.2') !!}</p>
+			<p>{!! trans('help/easy-language/help-services.maps.3') !!}</p>
 		</div>
 @endsection

528507b893aa9562ca8c4962962556c2322ffdff
Dominik Hebeler <dominik@suma-ev.de>
2022-03-01 15:40:03 +0100

    translated texts

--- resources/lang/de/content-warning.php
+++ resources/lang/de/content-warning.php
@@ -1,9 +1,8 @@
 <?php
 
 return [
-    'title' => "Achtung: Bitte Informationen überprüfen",
-    'header1' => "Achtung: Diese Webseite könnte Fehlinformationen enthalten",
-    'p1' => "In der aktuellen Situation ist es wichtiger denn je geworden, Informationen auf Webseiten nicht blind zu vertrauen. Wir möchten diese Gelegenheit nutzen, um darauf hinzuweisen, dass es ratsam ist \"Fakten\" auf Webseiten stets auf Ihre Glaubwürdigkeit und Richtigkeit hin zu <a href=\":url\" target=\"_blank\">überprüfen</a>.",
+    'title' => "Achtung: Diese Webseite könnte Fehlinformationen enthalten",
+    'p1' => "In der heutigen Zeit ist es wichtiger denn je geworden, Informationen auf Webseiten nicht blind zu vertrauen. Durch die Menge der im Zusammenhang mit dem <a href=\":urlwiki\" target=\"_blank\" rel=\"noopener\">russischen Überfall auf die Ukraine</a> gestreuten Falschinformationen sahen wir uns gezwungen, diese Vorschaltseite bei einigen Ergebnissen zu zeigen. Diese betrifft viele Webseiten, die wahrscheinlich keine Falschinformationen verbreiten. Wir möchten diese Gelegenheit trotzdem nutzen, um darauf hinzuweisen, dass es ratsam ist \"Fakten\" auf Webseiten stets auf Ihre Glaubwürdigkeit und Richtigkeit hin zu <a href=\":url\" target=\"_blank\">überprüfen</a>.",
     'to_website' => 'Weiter zur Webseite',
     'back_to_search' => 'Zurück zur Ergebnisliste',
 ];
--- resources/views/content-warning.blade.php
+++ resources/views/content-warning.blade.php
@@ -4,8 +4,8 @@
 
 @section('content')
 <div class="card">
-    <h1>@lang('content-warning.header1')</h1>
-    <p>@lang('content-warning.p1', ["url" => route('faktencheck')])</p>
+    <h1>@lang('content-warning.title')</h1>
+    <p>@lang('content-warning.p1', ["urlwiki" => "https://de.wikipedia.org/wiki/Russischer_%C3%9Cberfall_auf_die_Ukraine_2022", "url" => route('faktencheck')])</p>
     <div class="actions">
         <a class="btn btn-default" href="{{ Request::input('url') }}">@lang('content-warning.to_website')</a>
         <a href="{{ Request::input('result-page') }}" onclick="history.back(); return history.length == 1 ? true : false;" rel="prev" class="btn btn-inverted">@lang('content-warning.back_to_search')</a>

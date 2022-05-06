108b386c8b1f3c24d0986a7964e04142ecd83b90
Dominik Hebeler <dominik@suma-ev.de>
2022-05-06 18:30:08 +0200

    escaping

--- .gitlab/deployment_scripts/update_secret.sh
+++ .gitlab/deployment_scripts/update_secret.sh
@@ -7,10 +7,11 @@ COMMAND_ARGS=""
 # Loop through all variables
 while IFS='=' read -r name value ; do
   if [[ $name == 'K8S_SECRET'* ]]; then
+    $value = escape($value);
     if [ -f $name ]; then
-        COMMAND_ARGS="${COMMAND_ARGS} --from-file=$value"
+        COMMAND_ARGS="${COMMAND_ARGS} --from-file='$value'"
     else
-        COMMAND_ARGS="${COMMAND_ARGS} --from-literal=$value"
+        COMMAND_ARGS="${COMMAND_ARGS} --from-literal='$value'"
     fi
   fi
 done < <(env)
@@ -19,3 +20,8 @@ done < <(env)
 echo "kubectl -n $KUBE_NAMESPACE create secret generic $CI_COMMIT_REF_SLUG $COMMAND_ARGS"
 
 echo "test";
+
+sub escape {
+    $_[0] =~ s/([^a-zA-Z0-9_])/\\$1/g;
+    return $_[0];
+}

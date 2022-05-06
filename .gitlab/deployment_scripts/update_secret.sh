335257e71de84c8a0695761a4aa9291e892a1ac0
Dominik Hebeler <dominik@suma-ev.de>
2022-05-06 19:25:31 +0200

    creating secret

--- .gitlab/deployment_scripts/update_secret.sh
+++ .gitlab/deployment_scripts/update_secret.sh
@@ -6,12 +6,13 @@ COMMAND_ARGS=""
 
 # Loop through all variables
 while read key; do
-  if [ -f $value ]; then
-    COMMAND_ARGS="${COMMAND_ARGS} --from-file=${!key@Q}"
+  if [ -f ${!key} ]; then
+    COMMAND_ARGS="${COMMAND_ARGS} --from-file=$key=${!key@Q}"
   else
-    COMMAND_ARGS="${COMMAND_ARGS} --from-literal=${!key@Q}"
+    COMMAND_ARGS="${COMMAND_ARGS} --from-literal=$key=${!key@Q}"
   fi
 done < <(compgen -v | grep -i '^K8S_SECRET')
 
 # Create/Update the secret
 echo "kubectl -n $KUBE_NAMESPACE create secret generic $CI_COMMIT_REF_SLUG $COMMAND_ARGS"
+kubectl -n $KUBE_NAMESPACE create secret generic $CI_COMMIT_REF_SLUG $COMMAND_ARGS

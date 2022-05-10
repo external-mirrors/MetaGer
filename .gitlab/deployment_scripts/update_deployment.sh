12b55f597640b67958d7934dd3e9b54ecf7fe0e7
Dominik Hebeler <dominik@suma-ev.de>
2022-05-10 14:18:15 +0200

    setting correct image tag and waiting for deployment

--- .gitlab/deployment_scripts/update_deployment.sh
+++ .gitlab/deployment_scripts/update_deployment.sh
@@ -4,4 +4,6 @@ helm -n $KUBE_NAMESPACE upgrade --install \
     review-${CI_COMMIT_REF_SLUG} \
     chart/ \
     -f $DEPLOYMENT_HELM_VALUES \
-    --set ingress.hosts[0].host=$DEPLOYMENT_URL
+    --set ingress.hosts[0].host=$DEPLOYMENT_URL \
+    --set image.fpm.tag=$DOCKER_FPM_IMAGE_TAG \
+    --set image.nginx.tag=$DOCKER_NGINX_IMAGE_TAG

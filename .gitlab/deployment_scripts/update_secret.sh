8655b847efc0764bc8112bcae41f68f2947ab843
Dominik Hebeler <dominik@suma-ev.de>
2022-05-11 13:55:48 +0200

    added remaining secrets

--- .gitlab/deployment_scripts/update_secret.sh
+++ .gitlab/deployment_scripts/update_secret.sh
@@ -7,6 +7,12 @@ kubectl -n $KUBE_NAMESPACE create secret generic $CI_COMMIT_REF_SLUG \
   --from-file=${ENV_PRODUCTION} \
   --from-file=${SUMAS} \
   --from-file=${SUMASEN} \
+  --from-file=${ADBLACKLIST_DOMAINS} \
+  --from-file=${ADBLACKLIST_URL} \
+  --from-file=${BLACKLIST_DESCRIPTION_URL} \
+  --from-file=${BLACKLIST_DOMAINS} \
+  --from-file=${BLACKLIST_URL} \
+  --from-file=${USERSEEDER} \
   --dry-run \
   --save-config \
   -o yaml | \

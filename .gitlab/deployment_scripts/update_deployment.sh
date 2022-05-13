457daf73bbad1ce72b6bea121721aa97d0f38e8f
Dominik Hebeler <dominik@suma-ev.de>
2022-05-13 14:51:23 +0200

    wrapping host in strings

--- .gitlab/deployment_scripts/update_deployment.sh
+++ .gitlab/deployment_scripts/update_deployment.sh
@@ -5,7 +5,7 @@ helm -n $KUBE_NAMESPACE upgrade --install \
     chart/ \
     -f $DEPLOYMENT_HELM_VALUES \
     --set environment=$APP_ENV \
-    --set ingress.hosts[0].host=$DEPLOYMENT_URL \
+    --set ingress.hosts[0].host="$DEPLOYMENT_URL" \
     --set image.fpm.tag=$DOCKER_FPM_IMAGE_TAG \
     --set image.nginx.tag=$DOCKER_NGINX_IMAGE_TAG \
     --set app_url=$APP_URL \

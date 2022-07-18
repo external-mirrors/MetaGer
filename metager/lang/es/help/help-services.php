f39075b9303a6e922e5a3fb9f6fad8b66d0094b6
Kim <kim@suma-ev.de>
2022-07-18 09:50:54 +0200

    add translation to help services page

--- metager/lang/es/help/help-services.php
+++ metager/lang/es/help/help-services.php
@@ -3,5 +3,25 @@
 return [
 
     "title"	=>	"Ayuda de MetaGer",
+
+    "backarrow"=> 'Volver',
+    "dienste" => "Más servicios en torno a la búsqueda",
+    "dienste.kostenlos"=> "Por supuesto, todos nuestros servicios son gratuitos",
+
+    "app.title" => "Aplicación Android",
+    "app.1" => "También puedes usar MetaGer como una app. Sólo tienes que descargar <a href=\"https://metager.de/app\" target=\"_blank\" rel=\"noopener\">la aplicación MetaGer</a> en tu smartphone Android",
+   
+    "suchwortassoziator.title" => "Buscar asociador de palabras",
+    "suchwortassoziator.1" => "Como ayuda a la indexación de un entorno de términos, hemos desarrollado el <a href=\"/asso\" target=\"_blank\" rel=\"noopener\">Asociador Web MetaGer</a>. También puede encontrar la herramienta directamente en la pestaña \"Servicios\". Si introduces en él una palabra de búsqueda que se acerque de algún modo al área temática que quieres investigar, intentará extraer de la WWW los términos técnicos típicos de esta área.",
+    "suchwortassoziator.2" => "Ejemplo: Le gustaría saber más sobre las picaduras de garrapatas y sus peligros, pero ya no puede pensar en los términos médicos de las enfermedades de este ámbito. Si se introduce la palabra \"picaduras de garrapata\" en el asociador web, aparecerán, entre otros, los términos \"enfermedad de Lyme\" y \"fsme\".",
+    "suchwortassoziator.3" => "Dado que este análisis de asociación se obtiene, entre otras cosas, de los propios documentos web, es independiente del idioma; es decir, puede obtener términos técnicos de cualquier idioma al introducir palabras en alemán (y viceversa). Si, por el contrario, observa análisis de asociaciones que podrían ser mejores con la ayuda de sus conocimientos especializados, no dude en hacérnoslo saber, junto con su sugerencia de mejora, a través de nuestro <a href=\"/kontakt/\" target=\"_blank\" rel=\"noopener\">formulario de contacto</a>.",
+  
+    "widget.title" => "MetaGer Widget",
+    "widget.1" => "Este es un generador de código que le permite incrustar MetaGer en su sitio web. A continuación, puede utilizarlo para buscar a voluntad en su propio sitio o en Internet. Para todas las preguntas: <a href=\"/kontakt/\" target=\"_blank\" rel=\"noopener\">nuestro formulario de contacto</a>",
+   
+    "maps.title" => "MetaGer Maps",
+    "maps.1" => "Asegurar la privacidad en la era de los pulpos de datos globales también nos ha conmovido, <a href=\"https://maps.metager.de\" target=\"_blank\">https://maps.metager.de</a> para desarrollar. El único (hasta donde sabemos) planificador de rutas que ofrece todas las funcionalidades a través del navegador y la aplicación, sin almacenar las ubicaciones del usuario. Todo esto es verificable, porque nuestro software es de código abierto. Para el uso de maps.metager.de recomendamos nuestra versión de aplicación rápida. Puede descargar nuestras aplicaciones en <a href=\"/app\" target=\"_blank\">obtener una aplicación</a> (o, por supuesto, a través de Play Store).",
+    "maps.2" => "Esta función de mapa también puede ser llamada desde la búsqueda de MetaGer (y viceversa). En cuanto hayas buscado un término en MetaGer, verás un nuevo foco de búsqueda \"Maps\" en la parte superior derecha.  Si hace clic en él, accederá a un mapa relacionado.",
+    "maps.3" => "Después de llamar al mapa, muestra los puntos encontrados por MetaGer (POIs = Puntos de Interés) también mostrados en la columna de la derecha. Al acercarse, esta lista se adapta a la sección del mapa. Si mantiene el ratón sobre un marcador en el mapa o en la lista, se resalta el correspondiente homólogo. Haga clic en \"Detalles\" para obtener información más detallada sobre este punto en la base de datos que aparece a continuación.",
    
 ];

caa752fb707abbaed0a3ec3d9a4a853dc8f2cf1f
Kim <kim@suma-ev.de>
2022-07-18 10:17:51 +0200

    add translation to help page

--- metager/lang/es/help/help.php
+++ metager/lang/es/help/help.php
@@ -3,6 +3,29 @@
 return [
     "achtung"	=>	"Atención:\r\nDado que desarrollamos y mejoramos nuestro motor de búsqueda  constantemente, puede suceder que siempre haya cambios en la estructura y función. Aunque intentamos adaptar la ayuda lo más rápido posible a los cambios, no podemos descartar la posibilidad de discrepancias temporales en partes de las instrucciones.",
     "title"	=>	"Ayuda de MetaGer",
-    "title.2"	=>	"Uso del motor de búsqueda",
+
+    'tableofcontents.1' => 'Las páginas principales',
+    'tableofcontents.1.1' => 'La página de inicio',
+    'tableofcontents.1.2' => 'La caja de búsqueda',
+    'tableofcontents.1.3' => 'La página de resultados',
+    'tableofcontents.1.4' => 'Configuración',
+
+    'tableofcontents.2' => 'Funciones y consejos útiles',
+    'tableofcontents.2.1' => 'Funciones de búsqueda',
+    'tableofcontents.2.2' => '!bangs',
+    'tableofcontents.2.3' => 'Buscar en la búsqueda',
+    'tableofcontents.2.4' => 'Añadir MetaGer',
+
+    'tableofcontents.3' => 'Funciones para el anonimato y la seguridad de los datos',
+    'tableofcontents.3.2' => 'Cookies & Co.',
+    'tableofcontents.3.3' => 'Tor Hidden Service',
+    'tableofcontents.3.4' => 'MetaGer-Proxy',
+    'tableofcontents.3.5' => 'Protección de los jóvenes',
+
+    'tableofcontents.4' => 'Más servicios en torno a la búsqueda',
+    'tableofcontents.4.1' => "Aplicación Android",
+    'tableofcontents.4.3' => 'Buscar palabra asociada',
+    'tableofcontents.4.4' => 'MetaGer Widget',
+    'tableofcontents.4.5' => 'MetaGer Maps',
 
 ];

21526783e68f85fa62781be83650f45882902af5
Kim <kim@suma-ev.de>
2022-07-29 12:36:58 +0200

    translating the about us page into spanish

---
+++ metager/lang/es/about.php
@@ -0,0 +1,65 @@
+<?php
+
+return [
+
+    'head.1' => 'sobre nosotros',
+    'head.2' => 'Crónica',
+    'head.3' => 'Lo que representamos',
+
+    'text.1'=>'<h3> <img id="about-lock" src="/img/lock.svg">Privacidad garantizada</h3>Nos preocupa la protección de datos y la privacidad. Por eso no rastreamos ni almacenamos los perfiles de los usuarios. Además, ofrecemos servicios para proteger su privacidad, por ejemplo, nuestro proxy de anonimización (función "abrir anónimo"). <h3><img id= "about-heart" src="/img/heart.svg"> Asociación sin ánimo de lucro</h3> MetaGer es un proyecto de la asociación sin ánimo de lucro <a href="https://suma-ev.de">SUMA-EV, Asociación para el Libre Acceso al Conocimiento</a>. SUMA-EV se compromete principalmente a promover la alfabetización mediática. <h3> <img id= "about-rainbow" src="/img/rainbow.svg"> Diverso y gratuito </h3> MetaGer aporta resultados diversos porque es un metabuscador. Hemos explicado exactamente lo que esto significa en nuestra <a href=":transparencia">declaración de transparencia</a>. Al publicar nuestro código fuente, demostramos que el libre acceso al conocimiento es importante para nosotros. Nuestro código fuente es gratuito y de código abierto.<h3> <img id="about-leaf" src="/img/leaf.svg"> Energía 100% verde </h3> La sostenibilidad y el consumo de recursos también es un tema importante para nosotros. Por eso prestamos atención al consumo energético de nuestros servicios y sólo utilizamos electricidad procedente de fuentes de energía renovables.',
+
+    'timeline.1' => 'Marzo 1996: Cebit Hannover',
+    'timeline.1.1' => 'La idea de MetaGer se le ocurre al ingeniero alemán Dr. Wolfgang Sander-Beuermann durante la feria de informática CeBIT en un almuerzo en una servilleta de papel. El trabajo en el primer prototipo comienza esa misma tarde',
+
+    'timeline.2' => 'Fin de 1996: MetaGer entra en funcionamiento',
+    'timeline.2.1' => 'MetaGer se desarrolla como un proyecto de investigación de la Universidad de Hannover y el centro regional de informática de Baja Sajonia',
+
+    'timeline.3' => '1997-2006: Página de inicio de MetaGer',
+    'timeline.3.1' => 'Este es el aspecto de MetaGer cuando se conecta por primera vez. MetaGer ofrece así un nuevo e innovador servicio',
+
+    'timeline.4' => '01.10.2012: Wechsel der Trägerschaft zum SUMA-EV',
+    'timeline.4.1' => 'MetaGer wird in den gemeinnützigen Verein <i>SUMA-EV – Verein für freien Wissenszugang</i> überführt. Die Kooperation mit der Universität Hannover wird fortgeführt.',
+
+    'timeline.5' => '2006-2015: Introducción de nuevos diseños',
+    'timeline.5.1' => 'La página de inicio original de MetaGer sufre muchos pequeños cambios en los años siguientes',
+
+    'timeline.6' => '29.08.2013: Lanzamiento de la versión en inglés',
+    'timeline.6.1' => 'Por primera vez, la interfaz de MetaGer se ofrece también en inglés',
+
+    'timeline.7' => 'Diciembre de 2013: servicio Tor',
+    'timeline.7.1' => 'MetaGer ofrece acceso dentro de la red de anonimización Tor como una característica relacionada con la seguridad',
+    
+    'timeline.8' => 'Marzo 2014: Implementación de la función "Abrir anónimamente"',
+    'timeline.8.1' => 'Con la nueva función "Abrir anónimamente", se ofrece un servicio de proxy. De este modo, los usuarios de MetaGer permanecen protegidos incluso después de la búsqueda. ',
+
+    'timeline.9' => '2015-2016: página de inicio de MetaGer',
+    'timeline.9.1' => 'Como parte de una modernización más amplia, se ofrece una nueva página de inicio simplificada',
+
+    'timeline.10' => 'Marzo 2016: Resultados de búsqueda en menos de 1,3 segundos',
+    'timeline.10.1' => 'Las amplias actualizaciones del núcleo de MetaGer están reduciendo el tiempo medio de respuesta del motor de búsqueda a menos de 1,3 segundos. En los próximos años, esta cifra se reducirá aún más, hasta casi la mitad',
+
+    'timeline.11' => '2016-2019: Página de inicio de MetaGer',
+    'timeline.11.1' => 'Otros pequeños ajustes consolidan gradualmente el naranja como color de MetaGer. ',
+
+    'timeline.12' => '16.08.2016: Liberación del código fuente',
+    'timeline.12.1' => 'MetaGer se publica como código abierto bajo la licencia libre GNU AGPL',
+
+    'timeline.13' => 'Diciembre de 2016: MetaGer Maps entra en funcionamiento',
+    'timeline.13.1' => 'Maps.MetaGer.de se lanza como un servicio de mapas respetuoso con la privacidad. Los mapas se basan en los datos del proyecto OpenStreetMap, pero tienen su propia visualización y planificador de rutas',
+
+    'timeline.14' => '2017: Relanzamiento de la versión inglesa de MetaGer',
+    'timeline.14.1' => 'A través de una extensa puesta a punto y de fuentes de resultados propias, la versión inglesa de MetaGer ha sido mejorada en gran medida',
+
+    'timeline.15' => 'Septiembre 2017: Revisión de los Quicktips',
+    'timeline.15.1' => 'Los Quicktips, que ofrecen consejos más allá de los resultados de búsqueda normales, se amplían en funcionalidad y se convierten en un proyecto separado. En lugar de estar situados encima de los resultados, ahora están en una barra lateral',
+
+    'timeline.16' => '2019-2020: página de inicio de MetaGer',
+    'timeline.16.1' => 'En la nueva página de inicio, se elaborarán y promocionarán por primera vez cuatro rasgos característicos de MetaGer',
+
+    'timeline.17' => '26.10.2020: Página de inicio actual y Modo Oscuro',
+
+    'timeline.17.1' => 'Como parte de un amplio rediseño de la página de inicio, los cuatro puntos de venta únicos de MetaGer serán revisados y presentados en un formato ampliado. Para no sobrecargar la función central de la búsqueda web, se puede llegar a las descripciones desplazándose. <br> Otra característica de la nueva revisión es el Modo Oscuro de toda la página. Esto permite utilizar MetaGer en todas las páginas en un diseño oscuro.',
+   
+    'timeline.18' => 'Hoy',
+    'timeline.18.1' => 'Hoy, MetaGer sigue evolucionando.'
+]; 

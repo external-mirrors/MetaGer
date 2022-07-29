5dca3d08a5e5b4842df461faf1eebf4119ef20cb
Kim <kim@suma-ev.de>
2022-07-29 15:31:43 +0200

    add spanish translation to keyboard navigation

---
+++ metager/lang/es/keyboard-navigation.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'keyboard-nav-title' => 'Navegación con el teclado',
+    'focus-results' => 'Saltar a los resultados',
+    'focus-foki' => 'Saltar a la selección de enfoque',
+    'focus-settings' => 'Saltar a la configuración de búsqueda',
+    'focus-navigation' => 'Saltar a la página de navegación',
+    'keyboard-nav-hint' => 'Consejo: Pulse la tecla escape para volver a este cuadro y para cerrar todos los menús desplegables.'
+];  

1a2379999409475181820029204c2f5083e621bc
Kim <kim@suma-ev.de>
2022-07-29 13:32:48 +0200

    translating icon alt into spanish

---
+++ metager/lang/es/icons.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'angle-double-right.alt' => 'Doble flecha a la derecha',
+    'icon-lupe.alt' => 'Buscar',
+    'chevron-down.alt' => '',
+    'menu.alt' => 'tres trazos horizontales',
+    'cogs.alt' => 'engranajes',
+    'ellipsis.alt' => 'Puntos de continuación vertical',
+    'ellipsis-horizontal.alt' => 'Puntos de continuación horizontales',
+    'home.alt' => 'casa',
+    'trashcan.alt' => 'basurero',
+    'x.alt' => 'X',
+    'floppy.alt' => 'floppy',
+    'Icon-settings' => 'engranajes'
+
+
+
+
+];

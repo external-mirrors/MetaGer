28d3eba1c462c9d5571d7b38e517f832d590b942
Kim <kim@suma-ev.de>
2022-07-29 12:59:26 +0200

    add translation to faktencheck page

--- metager/lang/es/faktencheck.php
+++ metager/lang/es/faktencheck.php
@@ -2,36 +2,61 @@
 
 return [
     "heading.1"	=>	"Verificación de hechos versus noticias falsas:",
-    "paragraph.1"	=>	"¿De dónde viene el mensaje?",
-    "list.1"	=>	"sitio web",
-    "list.1.1"	=>	"¿Este sitio web tiene una impresión? ¿Quién se menciona allí (nombre, empresa, ...) y qué se puede descubrir acerca de estas personas?",
-    "list.1.2"	=>	"¿Quién es el propietario del sitio web en la base de datos Whois? ¿Coincide esto con la impresión y lo que se puede averiguar sobre los propietarios del sitio web? (<a href=\"https://de.wikipedia.org/wiki/Whois\" target=\"_blank\" rel=\"noopener\"> https://de.wikipedia.org/wiki/Whois </a>)",
-    "list.1.3"	=>	"¿Hay un autor en el sitio web? ¿Qué se puede descubrir acerca de esta persona / s?",
-    "list.1.4"	=>	"Publica lo anterior ¿Gente frecuente / él sobre el tema? ¿Eres conocido en esta (materia) área? ¿Hay alguna entrada de Wikipedia sobre ti?",
-    "list.1.5"	=>	"¿Hay algún hecho en el texto que pueda verificarse en otro lugar? ¿Incluso fuera de las fuentes de internet?",
-    "list.1.6"	=>	"¿El estilo de la escritura indica si el texto debe ser satírico / irónico?",
-    "list.1.7"	=>	"¿Hay citas en el texto cuya corrección se puede verificar en otro lugar?",
-    "list.1.8"	=>	"¿El razonamiento en el texto es lógico y plausible?",
-    "list.2"	=>	"Facebook u otras redes sociales o foros",
-    "list.2.1"	=>	"¿Es un autor nombrado con un nombre que parece real? Si no: mensaje extremadamente cuestionable.",
-    "list.2.2"	=>	"¿Qué información adicional hay para este nombre?",
-    "list.2.3"	=>	"¿Se puede verificar si este nombre es real? ¿También se puede contactar a esta persona en otros canales de comunicación? ¿Puedes llamarla y hablar con ella sobre este texto?",
-    "list.2.4"	=>	"¿El perfil de este nombre está verificado en FB? (<a href=\"https://www.facebook.com/help/196050490547892\" target=\"_blank\" rel=\"noopener\"> https://www.facebook.com/help/196050490547892 </a>)",
-    "list.2.5"	=>	"¿Cuánto tiempo lleva este perfil en existencia?",
-    "list.2.6"	=>	"¿Cuántos amigos de FB tiene este perfil? ¿Qué se puede encontrar sobre los nombres de estos perfiles?",
-    "list.3"	=>	"Wikipedia",
-    "list.3.1"	=>	"¡No todo en Wikipedia es cierto!",
-    "list.3.2"	=>	"Verifique el historial de versiones: ¿Quién escribió qué y cuándo?",
-    "list.3.3"	=>	"¿Se puede saber algo sobre estos autores?",
-    "list.3.4"	=>	"¿Hay una página de discusión para esta entrada de Wikipedia de la que se puedan sacar más conclusiones?",
-    "list.4"	=>	"Correos electrónicos",
-    "list.4.1"	=>	"¡Falsificar correos electrónicos es MUY fácil! => Correos electrónicos falsos.",
-    "list.4.2"	=>	"¿El correo electrónico realmente proviene del remitente especificado? Verifique el encabezado del correo electrónico cuidadosamente y examine las direcciones IP y las rutas de entrega mencionadas allí (no es fácil).",
-    "list.4.3"	=>	"Utilice el correo electrónico firmado y mejor cifrado (no es fácil); Notas aquí: <a href=\"https://www.heise.de/ct/artikel/Ausgebootet-289538.html\" target=\"_blank\" rel=\"noopener\"> https://www.heise.de/ ct / artikel / Ausbootoot-289538.html </a>",
-    "list.5"	=>	"Fotos, videos",
-    "list.5.1"	=>	"Mira de cerca el fondo. Paisajismo, edificios, automóviles y matrículas, ropa, personas. ¿Es correcto? ¿Encaja con el texto asociado si es necesario?",
-    "list.5.2"	=>	"¿Puedes encontrar imágenes similares con la búsqueda de imágenes inversa de los motores de búsqueda de imágenes?",
-    "list.5.3"	=>	"¿Puedes leer metadatos de las imágenes con programas gráficos? ¿Estos metadatos coinciden con el contenido de la imagen?",
-    "list.6"	=>	"Hay una aplicación \"Fake News Check\" para Smartphone que también tiene en cuenta aspectos muy subjetivos y emocionales. Además, más enlaces para la verificación de hechos",
-    "list.7"	=>	"¡NO hay seguridad absoluta contra la falsificación!"
+
+    'paragraph.1' => 'Las Fake News se presentan a menudo en forma de artículo periodístico. Sin embargo, el enunciado del artículo o la presentación no se corresponde con la situación de hecho. A menudo, las declaraciones de las Fake News son deliberadamente inventadas o distorsionadas para lograr objetivos políticos o comerciales. (véase <a href="https://link.springer.com/book/10.1007/978-3-662-58695-2"> Appel 2020</a>) Entonces, ¿qué se puede hacer para saber si son Fake News?',
+
+    'paragraph.2' => 'Hay varias maneras de hacer esto. Para comprobar la calidad de la información, existe el llamado <a href="https://library.csuchico.edu/sites/default/files/craap-test.pdf">"test CRAAP"</a>. Se desarrolló en la Universidad Estatal de California, Chico. Esta prueba ofrece muchas preguntas útiles para ver si la información es fiable. Además de esta prueba, puede encontrar más información útil aquí:',
+
+    'list.1' => 'páginas web',
+
+    'list.1.1' => '<strong>Fecha</strong>: Las buenas fuentes suelen incluir la fecha de publicación de este artículo. Esto es, por tanto, una indicación de la calidad y la actualidad de la información',
+
+	'list.1.2' => '<strong>Autor</strong>: ¿Se nombra un autor? En la mayoría de las buenas fuentes, se nombra a un autor. Este es también un dato importante. Si se nombra a un autor, a menudo es útil ver si esta persona suele publicar algo sobre estos temas.  Además, puede buscar si esta persona es conocida en el área (temática) o si hay, por ejemplo, entradas de Wikipedia sobre esta persona',
+
+	'list.1.3' => '<strong>Fuentes citadas</strong>: ¿No se citan otras fuentes en el artículo? A menudo, esto puede ser un indicio de una declaración o una representación errónea. Además, hay que comprobar si la argumentación del texto es lógica y plausible',
+
+	'list.1.4' => '<strong>Impresión</strong>: Si hay una impresión: ¿Qué personas (nombres, empresa) figuran allí y qué se puede averiguar sobre ellas?',
+
+	'list.2'   => 'Soziale Medien',
+
+	'list.2.1' => '¿Hay un autor con un nombre que parece real? Si no es así, el mensaje es probablemente cuestionable. Además, está la cuestión de si hay información adicional sobre el nombre',
+
+	'list.2.2' => '¿Existen otros canales de comunicación en los que se pueda localizar a esta persona?',
+
+	'list.2.3' => 'En Facebook, hay una opción adicional para ver si esta persona está verificada.(<a href="https://www.facebook.com/help/196050490547892" target="_blank" rel="noopener">https://www.facebook.com/help/196050490547892</a>)',
+
+    'list.2.4' => '¿Desde cuándo está disponible este perfil?',
+
+    'list.2.5' => '¿Cuántos suscriptores o amigos tiene este perfil? ¿Qué se puede averiguar sobre los nombres de estas personas?',
+
+    'list.3'   => 'Wikipedia',
+
+	'list.3.1' => 'No todo lo que aparece en Wikipedia es correcto. Para comprobar la exactitud de la información, puedes hacer lo siguiente:',
+
+	'list.3.2' => 'Comprobar el historial de versiones: ¿Quién escribió qué y cuándo?',
+
+    'list.3.3' => '¿Se puede saber algo de estos autores?',
+
+    'list.3.4' => '¿Existe una página de discusión para esta entrada de Wikipedia de la que se puedan hacer más inferencias?',
+
+	'list.3.5' => 'Comprueba qué fuentes se dieron en la entrada de Wikipedia. A menudo también pueden servir como fuente de información adicional',
+
+	'list.4' => 'Correos electrónicos',
+
+    'list.4.1' => 'Falsificar correos electrónicos es muy fácil. Por lo tanto, hay muchos fakemails; ¿cómo se puede comprobar esto ahora?',
+
+    'list.4.2' => '¿El correo proviene realmente del remitente especificado? Para ello, compruebe el encabezamiento del correo electrónico. Además, puede examinar las direcciones IP y las rutas de entrega allí mencionadas. Sin embargo, esto no es fácil',
+
+    'list.4.3' => 'Utilizar correo electrónico firmado y, en el mejor de los casos, también cifrado. Esto tampoco es tan sencillo. Puede encontrar más información al respecto aquí: <a href="https://www.heise.de/ct/artikel/Ausgebootet-289538.html" target="_blank" rel="noopener">https://www.heise.de/ct/artikel/Ausgebootet-289538.html</a>',
+
+    'list.5' => 'Fotos, Videos',
+
+    'list.5.1' => 'Fíjate bien en el fondo. Paisaje, edificios, coches y matrículas, ropa, personas. ¿Es coherente, encaja con el texto correspondiente?',
+
+	'list.5.2' => '¿Se pueden encontrar imágenes similares con la búsqueda inversa de imágenes de los buscadores de imágenes?',
+
+    'list.5.3' => '¿Es posible leer los metadatos de las imágenes con programas gráficos? ¿Coinciden estos metadatos con el contenido de la imagen?',
+
+    'list.7' => 'Sin embargo, no hay seguridad absoluta contra las falsificaciones o fake news',
+
 ];

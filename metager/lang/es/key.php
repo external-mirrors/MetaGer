ef75d92d7ef176bf20db61e861a5142de8c81bfe
Kim <kim@suma-ev.de>
2022-07-29 13:59:57 +0200

    translating key page into spanish

---
+++ metager/lang/es/key.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'h1' => "Clave para tu búsqueda sin anuncios",
+    'p1' => 'MetaGer ofrece a los <a href=":url1">miembros de SUMA-EV</a> y a los generosos <a href=":url2">donantes</a> una clave para acceder a una cuota de búsquedas sin publicidad.',
+    'p2' => 'En esta página puede introducir su clave (si la conoce). Lo almacenamos en su PC con la ayuda de una cookie. De este modo, su navegador nos envía automáticamente la clave cada vez que realiza una búsqueda, para que podamos eliminar los anuncios por usted',
+    'p3' => 'Si miras la cookie, dice "key=xxxx". No se almacena ni se registra por nosotros de ninguna forma en ningún momento. En particular, no la utilizamos con fines de seguimiento',
+    'p4' => 'Importante: Para poder utilizar esta función, debe haber permitido las cookies en su navegador. La configuración se mantendrá guardada mientras su navegador almacene cookies',
+    'p5' => 'Para poder guardar la clave más allá de eso tienes las siguientes opciones:',
+    'li1' => 'Configura el siguiente enlace como página de inicio/marcador:',
+    'li2' => 'Si usas MetaGer como motor de búsqueda por defecto, puedes borrar la entrada después de introducir la clave y añadirla de nuevo. Si necesita introducir una "cadena de búsqueda" en su navegador, puede utilizar lo siguiente:',
+    'placeholder1' => 'Tecla Enter...',
+    'removeKey' => 'eliminar la clave actual',
+    'invalidKey' => 'La clave introducida no es válida',
+    'empty' => 'Su clave es válida pero ya no contiene búsquedas sin publicidad',
+    'backLink' => 'Volver a la última página',
+    'custom.h3' => 'wish key',
+    'custom.p1' => 'Los miembros del SUMA-EV tienen la posibilidad de elegir su propia clave',
+    'custom.p2' => 'El siguiente cambio de su clave es posible en :nextchange',
+    'custom.a1' => 'Configurar la llave de los deseos.'
+];

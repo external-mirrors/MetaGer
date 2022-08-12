3f59f0b5defc7b8c7a414b390283131818ff083d
Kim <kim@suma-ev.de>
2022-08-12 09:41:31 +0200

    add spanish translation to keychange

---
+++ metager/lang/es/keychange.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+
+    'h1' => 'clave del deseo',
+    'p1' => 'Con esta herramienta tienes la posibilidad de cambiar tu clave de socio actual ":clave". Las llaves de los miembros están vinculadas a su pertenencia a SUMA-EV. Así que esto se puede renovar automáticamente. Aunque no lo hagamos, por supuesto que teóricamente tendríamos la posibilidad de crear un vínculo entre la clave de socio y las búsquedas realizadas. Con el cambio a una llave de deseo, ese vínculo se nos hace teóricamente y prácticamente imposible',
+    'p2' => 'Además, puedes usar esta herramienta para crear una clave que sea más fácil de recordar. No obstante, conserve la llave original. Como la nueva clave no puede renovarse automáticamente, en algún momento dejará de ser válida. En ese caso, tendrás que repetir este proceso introduciendo tu clave de miembro original (:key) en MetaGer y llamando de nuevo a esta herramienta.',
+    'p3' => 'Por lo tanto, guarde ambas llaves en un lugar seguro y tenga en cuenta, en particular, que no estamos en condiciones de notificarle la nueva llave resultante de este cambio ni de restaurarla en caso de pérdida',
+    'p4' => 'Esta herramienta le guiará a través de los siguientes pasos para crear una nueva clave de deseo anónima:',
+   
+    'ol1.li1' => 'Elimina toda la información sobre la clave de miembro actual de tu navegador',
+    'ol1.li2' => 'Crea una URL con la que se puede crear una nueva clave. Esta URL ya no contiene ninguna información sobre la clave de socio',
+    'ol1.li3' => 'Crear una nueva llave de deseo',
+    'ol1.li4' => 'Guarda la nueva clave de deseo en el navegador',
+    'a1' => 'Leer - crear clave de deseo',
+    
+    'p5' => 'Su anterior clave de miembro ha sido eliminada del navegador. También hemos creado una URL que puede utilizar para crear la clave que desee. Esto es posible dentro de :validUntil en cualquier momento y desde cualquier dispositivo llamando a la siguiente URL en el navegador, o directamente utilizando el formulario de esta página.',
+    'p6' => 'Ahora puede introducir la clave que desee en el siguiente campo de texto. Para evitar solapamientos, añadimos algunos caracteres a la clave. Al hacer clic en generar, su nueva clave se generará y se mostrará en la siguiente página para que pueda guardarla.',
+    'input1label' => 'Introduzca la clave deseada (mínimo 4 y máximo 20 caracteres)',
+    'input1' => 'Introducir clave de petición',
+    'button1' => 'Generar la clave del deseo',
+
+    'p7' => ' Su nueva llave de deseo ha sido configurada con éxito. Por favor, recuérdalo bien. No se puede restaurar si se pierde. También dejará de ser válida en algún momento. Si esto ocurre, introduzca su clave de socio habitual en MetaGer y repita este proceso',
+    'p8' => 'Su nueva llave es:',
+];

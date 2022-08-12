f6e4fa941543fbaf6b73cb5b8d96975036a3f70f
Kim <kim@suma-ev.de>
2022-08-12 11:36:37 +0200

    add translation to contact page

--- metager/lang/es/kontakt.php
+++ metager/lang/es/kontakt.php
@@ -4,14 +4,16 @@ return [
     "headline.1" => "Contacto",
 
     "form.1"     => "Formulario de contacto seguro",
-    "form.2"     => "Con este formulario nos puede contactar encryptado.",
-    "form.5"     => "Su dirección de correo electrónico (opcional)",
+    'form.2' => 'Puede contactar con nosotros a través de este formulario',    "form.5"     => "Su dirección de correo electrónico (opcional)",
+    'form.name' => 'nombre (nombre apellido)',
+    'form.5' => 'Su dirección de correo electrónico',
     "form.6"     => "Su mensaje",
-    'form.7'     => '<strong>Su mensaje será encryptada antes de mandarla <a href="http://openpgpjs.org/.">OpenPGP.js</a> para esto necesitamos Javascript.</strong> Sino tiene activado Javascript su mensaje será enviada sin encryptación.',
-    "form.8"     => "Encriptar y enviar",
+    'form.7' => 'Asunto',
+    'form.8' => 'Enviar',
     "letter.1"   => "Por carta",
     "letter.2"   => "Preferimos que nos contacte por medios digitales. Si lo ve indispensable contactarnos vía correo fisico, nos puede escribir a la siguiente dirección:",
     "letter.3"   => "SUMA-EV Röselerstr. 3 30159 Hannover Germany",
     'error.1'    => "Lo sentimos, pero desafortunadamente no recibimos ningún dato con su solicitud de contacto. El mensaje no fue enviado.",
+    "error.2" => 'Se ha producido un error al enviar su mensaje. Puede ponerse en contacto con nosotros directamente en la siguiente dirección de correo electrónico: :email',
     'success.1'  => "Su mensaje nos fue enviado con éxito. ¡Muchas gracias por esto! Procesaremos esto lo antes posible y luego lo contactaremos nuevamente si es necesario."
 ];

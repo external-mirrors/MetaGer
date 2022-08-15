4ba6fd5b3ff499228b79878c8cb82964076f4582
Kim <kim@suma-ev.de>
2022-08-15 12:34:32 +0200

    add missing translation to plugin-page

--- metager/lang/es/plugin-page.php
+++ metager/lang/es/plugin-page.php
@@ -12,4 +12,27 @@ return [
     "head.8" => "Añadir MetaGer a Firefox Klar",
     "head.9" => "Añadir MetaGer a UCBrowser",
     "head.10" => "Añadir MetaGer a Samsung Internet",
+
+    'search-engine.1' => 'Qué hace un motor de búsqueda estándar',
+    'search-engine.2' => 'Con MetaGer como motor de búsqueda por defecto, puedes instruir a tu navegador para que MetaGer responda automáticamente a las búsquedas que se realizan a través de la barra de direcciones, por ejemplo',
+
+    'browser-download' => 'Descargar Firefox',
+    'firefox-plugin' => 'Establecer MetaGer como motor de búsqueda por defecto con el complemento',
+    'firefox-default-search' => 'Establecer MetaGer como motor de búsqueda por defecto sin complemento',
+    'default-search' => 'Establecer MetaGer como motor de búsqueda por defecto',
+    'default-page' => 'Establecer MetaGer como página de inicio',
+
+    //Unable: Opera Desktop, Safari Desktop, UC Browser Mobile, Safari Mobile
+    'desktop-unable' => 'Lamentablemente, el navegador que estás utilizando no ofrece la posibilidad de establecer MetaGer como motor de búsqueda (por defecto), pero <a href="https://www.mozilla.org/de/firefox/new/" target="_blank" rel="noopener">aquí</a> puedes instalar Firefox, un navegador de código abierto para el que tenemos instrucciones',
+    'mobile-unable' => 'Lamentablemente, el navegador que estás utilizando no ofrece la posibilidad de establecer MetaGer como motor de búsqueda (por defecto), pero <a href="https://www.mozilla.org/de/firefox/mobile/" target="_blank" rel="noopener">aquí</a> puedes instalar Firefox, un navegador de código abierto para el que tenemos instrucciones',
+
+    'desktop-notlisted' => 'Lamentablemente, aún no sabemos cómo configurar el motor de búsqueda (por defecto) en el navegador que estás utilizando, pero <a href="https://www.mozilla.org/de/firefox/mobile/" target="_blank" rel="noopener">aquí</a> puedes instalar Firefox, un navegador de código abierto donde sí lo sabemos.' ,
+    'mobile-notlisted' => 'Lamentablemente, aún no sabemos cómo configurar el motor de búsqueda (por defecto) en el navegador que utilizas, pero <a href="https://www.mozilla.org/de/firefox/mobile/" target="_blank" rel="noopener">aquí</a> puedes instalar Firefox, un navegador de código abierto donde sí lo sabemos.',
+
+
+
+
+
+
+
 ];

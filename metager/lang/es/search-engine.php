47b2b6a60ce53fb1e15f6e30181fe3397b4c7a41
Kim <kim@suma-ev.de>
2022-08-15 12:50:45 +0200

    add translation to search-engine-page

---
+++ metager/lang/es/search-engine.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'head.1' => 'Sobre nuestros motores de búsqueda',
+    'head.2' => 'MetaGer utiliza múltiples índices',
+
+
+    'text.1' => 'MetaGer es un <a href=":transparencia">motor de búsqueda</a>. Por ello, utilizamos los índices de otros motores de búsqueda. Para cada uno de estos buscadores hemos creado un breve perfil con los datos clave más importantes. Dado que no actualizamos constantemente este resumen, toda la información es sin garantía',
+
+    'text.2.1' => 'Nombre: ',
+    'text.2.2' => 'Índice utilizado: ',
+    'text.2.3' => 'Creado: ',
+    'text.2.4' => 'Sede central: ',
+    'text.2.5' => 'Operador: ',
+    'text.2.6' => 'Páginas estimadas en el índice: ',
+    'text.2.7' => 'Versión de MetaGer utilizada: ',
+];

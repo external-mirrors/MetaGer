dcf0c2d869422373eac1bed44c6f71b48324bee6
Kim <kim@suma-ev.de>
2022-08-15 12:47:16 +0200

    add missing translation to resultPage file

--- metager/lang/es/resultPage.php
+++ metager/lang/es/resultPage.php
@@ -4,4 +4,6 @@ return [
     "opensearch"	=>	"MetaGer: Buscar & encontrar seguro, proteger la privacidad",
     "startseite"	=>	"MetaGer página de inicio",
     "impressum"	=>	"Aviso legal"
+    'search-placeholder' => 'Introducir término(s) de búsqueda',
+    'metager3' => 'Estás en una prueba de MetaGer',
 ];

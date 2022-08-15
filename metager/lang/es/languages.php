2e8b2011e81404eda0a1284a1d3e4049b1657def
Kim <kim@suma-ev.de>
2022-08-15 10:41:10 +0200

    add translation to languages file

---
+++ metager/lang/es/languages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'header' => 'MetaGer - Traducciones',
+    'beschreibung.1' => 'Gracias por considerar apoyar a MetaGer en la traducción de sus textos. En la zona inferior se muestra un archivo y los textos correspondientes en forma de tabla. El campo "#ID" sólo sirve de orientación y no es importante para la traducción. En la siguiente columna encontrará o bien textos del idioma para el que nos faltan algunas traducciones, o bien un campo de texto. Si aquí se muestra un campo de texto para una fila, nos faltan los textos en el idioma especificado',
+    'beschreibung.2' => 'Puede apoyarnos mirando los textos de referencia en las siguientes columnas (a la derecha) y, si es posible, introduciendo una traducción en el idioma que busca en el campo de texto',
+    'progress' => ':uebersetzteTexte/:textCount Textos traducidos (:percentage%)',
+    'hinweis.1' => 'En cuanto esté satisfecho con sus textos, puede enviárnoslos automáticamente haciendo clic en el siguiente botón. Si hay más textos que faltan en el idioma especificado, su navegador le dirigirá a ellos',
+    'hinweis.2' => '<b>Nota</b>: Los textos enviados sólo son reconocidos por esta herramienta una vez que han sido vistos e insertados por nosotros. Si quiere guardar su trabajo para continuarlo en un momento posterior (aunque no hayamos podido hacernos cargo de su trabajo anterior), basta con copiar el enlace actual de la barra del navegador y volver a llamarlo en un momento posterior',
+    'hinweis.3' => '<b>Precaución</b>:  Al enviarlo, acepta que <a href="https://gitlab.metager.de/open-source/MetaGer" target="_blank" rel="noopener">publiquemos sus textos bajo licencia AGPL v3. Si desea que se le mencione por su nombre, comuníquenoslo por correo electrónico mediante el <a href="/kontakt">formulario de contacto</a>.',
+    'email' => '<b>Dirección de correo electrónico para consultas</b> (opcional):',
+    'synoptic' => ['hinweis' => 'el archivo fue editado por última vez.'],
+];

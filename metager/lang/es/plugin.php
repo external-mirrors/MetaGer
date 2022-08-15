7a9a8715c9b4e06c838257eb65e5bf783af57087
Kim <kim@suma-ev.de>
2022-08-15 12:46:51 +0200

    add missing translation to plugin file

--- metager/lang/es/plugin.php
+++ metager/lang/es/plugin.php
@@ -3,4 +3,7 @@
 
 return [
     "description"	=>	"MetaGer: Buscar y encontrar seguro, proteger la esfera privada"
+    'production' => 'MetaGer (es)',
+    'development' => 'Versión de desarrollo de MetaGer (es)'.
+
 ];

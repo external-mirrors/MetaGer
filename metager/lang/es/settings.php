e1019dd31428e3ef3fc754e53bbfde3eeb409f23
Kim <kim@suma-ev.de>
2022-08-19 16:39:08 +0200

    add spanish translation file for settings

--- metager/lang/es/settings.php
+++ metager/lang/es/settings.php
@@ -1,4 +1,54 @@
 <?php
 
 return [
+    'header.1' => 'Configuración de la búsqueda',
+    'text.1' => 'Aquí puede hacer ajustes de búsqueda permanentes para su búsqueda de MetaGer en el foco :focusName. Si no se guardan permanentemente, compruebe la configuración de su navegador para ver si borra las cookies guardadas al salir.',
+
+    'hint.header' => 'Configurar cookies',
+    'hint.text' => 'Para guardar su configuración de búsqueda, utilizamos cookies de identificación no personal. Estos se almacenan en texto plano en su navegador. Puedes ver un resumen de todas las configuraciones y cookies <a href=:link> que has hecho</a>.',
+    'hint.loadSettings' => 'Aquí encontrarás un enlace que puedes configurar como página de inicio o marcador para llevarte tu configuración actual. El enlace crea cookies con la configuración correspondiente cuando se llama.',
+
+    'header.2' => 'Motores de búsqueda utilizados',
+    'text.2' => 'A continuación puede ver todos los motores de búsqueda disponibles para este enfoque. Puedes activarlos y desactivarlos haciendo clic en el nombre. Los motores de búsqueda activados se muestran en verde. Apagado en rojo o gris.',
+    'disabledByFilter' => 'Desactivado por filtro de búsqueda:',
+
+    'header.3' => 'Filtro de búsqueda',
+    'text.3' => 'En este punto puedes establecer filtros de búsqueda de forma permanente. Cuando se selecciona un filtro de búsqueda, sólo están disponibles los motores de búsqueda que admiten este filtro. A la inversa, sólo se muestran los filtros de búsqueda compatibles con la selección actual del motor de búsqueda.',
+
+    'header.4' => 'Lista negra',
+    'text.4' => 'Aquí puede introducir los dominios que deben ser excluidos de su búsqueda. Si desea incluir todos los subdominios, comience con "*.". Un dominio por línea.',
+    'dirección' => 'entrada de dirección',
+
+    'save' => 'Guardar',
+    'reset' => 'Borrar todos los ajustes',
+    'back' => 'Volver a la última página',
+    'add' => 'Añadir',
+    'clear' => 'Lista negra vacía',
+    'copy' => 'Copia',
+    'hint' => '¡Estos ajustes afectan a todos los focos y subpáginas en general!',
+
+    'darkmode' => 'Cambiar el modo oscuro',
+    'system' => 'estándar del sistema',
+    'dark' => 'Oscuro',
+    'light' => 'Brillante',
+
+    'newTab' => 'Abrir los resultados en una nueva pestaña',
+    'off' => 'Off',
+    'on' => 'On',
+
+    'more' => 'Otros ajustes',
+
+    // Translations from the settings overview
+    'noSettings' => '¡Actualmente no hay ninguna configuración establecida!',
+    'allSettings.header' => 'Configuración establecida en :root',
+    'allSettings.text' => 'Aquí encontrará un resumen de todas las configuraciones y cookies que ha establecido. Puede eliminar entradas individuales o eliminarlas todas. Tenga en cuenta que los ajustes asociados ya no se utilizarán.',
+
+    'meaning' => 'significado',
+    'actions' => 'Acciones',
+
+    'engineDisabled' => 'El motor de búsqueda :engine no se consulta en el foco :focus',
+    'inFocus' => 'En foco',
+    'key' => 'Su clave para la búsqueda sin publicidad',
+    'blentry' => 'Entrada de la lista negra',
+    'removeCookie' => 'Eliminar esta cookie',
 ];

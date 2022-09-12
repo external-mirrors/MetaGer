d5f1a462062b6006bdc95741ebef22b84c239c9a
Kim <kim@suma-ev.de>
2022-09-12 13:47:29 +0200

    fix translation for headlines

--- metager/lang/de/help/easy-language/help.php
+++ metager/lang/de/help/easy-language/help.php
@@ -5,15 +5,15 @@ return [
     "title"                 => 'MetaGer - Hilfe in leichter Sprache',
     "easy.language.back"    => "Zurück",
     'tableofcontents.title' => '',
-    'tableofcontents.1' => 'Die Hauptseiten',
-    'tableofcontents.1.1' => 'Die Startseite',
-    'tableofcontents.1.2' => 'Das Suchfeld',
-    'tableofcontents.1.3' => 'Die Ergebnisseite',
+    'tableofcontents.1' => 'Die Haupt-Seiten',
+    'tableofcontents.1.1' => 'Die Start-Seite',
+    'tableofcontents.1.2' => 'Das Such-Feld',
+    'tableofcontents.1.3' => 'Die Ergebnis-Seite',
     'tableofcontents.1.4' => 'Einstellungen',
 
 
     'tableofcontents.2' => 'Nützliche Funktionen und Hinweise',
-    'tableofcontents.2.1' => 'Suchfunktionen',
+    'tableofcontents.2.1' => 'Such-Funktionen',
     'tableofcontents.2.2' => '!bangs',
     'tableofcontents.2.3' => 'Suche in der Suche',
     'tableofcontents.2.4' => 'MetaGer installieren',
@@ -22,14 +22,14 @@ return [
 
     'tableofcontents.4' => 'Weitere Dienste um die Suche herum',
     'tableofcontents.4.1' => 'Android-App',
-    'tableofcontents.4.3' => 'Suchwortassoziator',
+    'tableofcontents.4.3' => 'Such-Wort-Assoziator',
     'tableofcontents.4.4' => 'MetaGer Widget',
     'tableofcontents.4.5' => 'MetaGer Karte',
 
 
-    'tableofcontents.3' => 'Funktionen zur Anonymität und Datensicherheit',
+    'tableofcontents.3' => 'Funktionen zur Anonymität und Daten-Sicherheit',
     'tableofcontents.3.2' => 'Cookies & Co.',
     'tableofcontents.3.3' => 'Tor-Hidden-Service',
     'tableofcontents.3.4' => 'MetaGer-Proxy',
-    'tableofcontents.3.5' => 'Jugendschutz',
+    'tableofcontents.3.5' => 'Jugend-Schutz',
  ];

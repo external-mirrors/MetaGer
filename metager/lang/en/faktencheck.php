4d4ae5821ce8bc89176264a75f85a245918baa02
Dominik Hebeler <dominik@suma-ev.de>
2022-09-19 09:29:02 +0200

    fixed faktencheck website

--- metager/lang/en/faktencheck.php
+++ metager/lang/en/faktencheck.php
@@ -13,6 +13,7 @@ return [
             2   => '<strong>Autor</strong>: Is an author named? Good sources often name an author or anouther source of information. If an author is named it is often useful to find out more information about them and see if that person often publishes about this topic.',
             3   => '<strong>Other Sources</strong>: Are there other sources named? It might be worthwhile to also check those and see whether they detract from the plausibility of the primary source.',
             4   => '<strong>Imprint</strong>: If there is an imprint: What can be found out about the People/Companies mentioned there?',
+            0   => 'Websites',
         ],
         2   => [
             1   => 'Is an author named with a name that seems real? If this is not the case, the message is probably questionable. There is also the question of whether there is additional information about the name.',
@@ -20,6 +21,7 @@ return [
             3   => 'On Facebook, there is an additional option to see if this person is verified.(<a href="https://www.facebook.com/help/196050490547892" target="_blank" rel="noopener">https://www.facebook.com/help/196050490547892</a>)',
             4   => 'Since when does this profile exist?.',
             5   => 'How many subscribers or friends does this profile have? What can be found out about the names of these people?',
+            0   => 'Social Media',
         ],
         3   => [
             1   => 'Not everything that is written on Wikipedia is correct. To check how accurate the information is, you can do the following:',
@@ -27,16 +29,19 @@ return [
             3   => 'Can anything be found out about these authors?',
             4   => 'Is there a discussion page for this Wikipedia entry from which further information can be inferred?',
             5   => 'Check which sources were given in the Wikipedia entry. These can also often serve as a further source of information.',
+            0   => 'Wikipedia',
         ],
         4   => [
             1   => 'Faking emails is very easy. Thus, there are very many fakemails; now how to check this?',
             2   => 'Does the mail really come from the specified sender? To do this, check the header of the e-mail. In addition, you can examine the IP addresses and delivery routes mentioned there. However, this is not easy.',
             3   => 'Use signed and at best also encrypted e-mail. Again, this is not so simple. You can find more information about this here: <a href="https://www.heise.de/ct/artikel/Ausgebootet-289538.html" target="_blank" rel="noopener">https://www.heise.de/ct/artikel/Ausgebootet-289538.html</a>',
+            0   => 'Emails',
         ],
         5   => [
             1   => 'Look closely at the background. Landscape, buildings, cars and license plates, clothing, people. Is it coherent, does it fit with the corresponding text?',
             2   => 'Can similar images be found with the reverse image search of image search engines?',
             3   => 'Is it possible to read metadata of the images with graphics programs? Does this metadata match the image content?',
+            0   => 'Bilder, Videos',
         ],
         7   => 'However, there is no absolute security against fakes or fake news.',
     ],
--- metager/lang/es/faktencheck.php
+++ metager/lang/es/faktencheck.php
@@ -13,6 +13,7 @@ return [
             2   => '¿Quién es el propietario del sitio web en la base de datos Whois? ¿Coincide esto con la impresión y lo que se puede averiguar sobre los propietarios del sitio web? (<a href="https://de.wikipedia.org/wiki/Whois" target="_blank" rel="noopener"> https://de.wikipedia.org/wiki/Whois </a>)',
             3   => '¿Hay un autor en el sitio web? ¿Qué se puede descubrir acerca de esta persona / s?',
             4   => 'Publica lo anterior ¿Gente frecuente / él sobre el tema? ¿Eres conocido en esta (materia) área? ¿Hay alguna entrada de Wikipedia sobre ti?',
+            0   => 'sitio web',
         ],
         2   => [
             1   => '¿Es un autor nombrado con un nombre que parece real? Si no: mensaje extremadamente cuestionable.',
@@ -20,6 +21,7 @@ return [
             3   => '¿Se puede verificar si este nombre es real? ¿También se puede contactar a esta persona en otros canales de comunicación? ¿Puedes llamarla y hablar con ella sobre este texto?',
             4   => '¿El perfil de este nombre está verificado en FB? (<a href="https://www.facebook.com/help/196050490547892" target="_blank" rel="noopener"> https://www.facebook.com/help/196050490547892 </a>)',
             5   => '¿Cuánto tiempo lleva este perfil en existencia?',
+            0   => 'Facebook u otras redes sociales o foros',
         ],
         3   => [
             1   => '¡No todo en Wikipedia es cierto!',
@@ -27,16 +29,19 @@ return [
             3   => '¿Se puede saber algo sobre estos autores?',
             4   => '¿Hay una página de discusión para esta entrada de Wikipedia de la que se puedan sacar más conclusiones?',
             5   => 'Comprueba qué origen se indican en la entrada de Wikipedia. A menudo también pueden servir como origen de información adicional.',
+            0   => 'Wikipedia',
         ],
         4   => [
             1   => '¡Falsificar correos electrónicos es MUY fácil! => Correos electrónicos falsos.',
             2   => '¿El correo electrónico realmente proviene del remitente especificado? Verifique el encabezado del correo electrónico cuidadosamente y examine las direcciones IP y las rutas de entrega mencionadas allí (no es fácil).',
             3   => 'Utilice el correo electrónico firmado y mejor cifrado (no es fácil); Notas aquí: <a href="https://www.heise.de/ct/artikel/Ausgebootet-289538.html" target="_blank" rel="noopener"> https://www.heise.de/ ct / artikel / Ausbootoot-289538.html </a>',
+            0   => 'Correos electrónicos',
         ],
         5   => [
             1   => 'Mira de cerca el fondo. Paisajismo, edificios, automóviles y matrículas, ropa, personas. ¿Es correcto? ¿Encaja con el texto asociado si es necesario?',
             2   => '¿Puedes encontrar imágenes similares con la búsqueda de imágenes inversa de los motores de búsqueda de imágenes?',
             3   => '¿Puedes leer metadatos de las imágenes con programas gráficos? ¿Estos metadatos coinciden con el contenido de la imagen?',
+            0   => 'Fotos, videos',
         ],
         7   => '¡NO hay seguridad absoluta contra la falsificación!',
     ],

3c6a172e5f4fe440150e88b8d1366e7539636d81
Dominik Hebeler <dominik@suma-ev.de>
2022-09-30 12:15:17 +0200

    small text changes

--- metager/lang/de/about.php
+++ metager/lang/de/about.php
@@ -6,31 +6,31 @@ return [
     'head.3' => 'Wofür wir stehen',
     'head.4' => 'Was macht MetaGer besonders?',
 
-    'text.1'=>'<h3> <img id="about-lock" src="/img/lock.svg"> Garantierte Privatsphäre</h3>Uns sind Datenschutz und Privatsphäre wichtig. Daher tracken und speichern wir keine Nutzerprofile. Außerdem bieten wir Dienste zum Schutz Ihrer Privatsphäre an, zum Beispiel unseren anonymisierenden Proxy („Anonym öffnen“-Funktion). <h3><img id= "about-heart" src="/img/heart.svg"> Gemeinnütziger Verein</h3> MetaGer ist ein Projekt des gemeinnützigen Vereins <a href="https://suma-ev.de">SUMA-EV, Verein für freien Wissenszugang</a>. Der SUMA-EV setzt sich vor allem für die Förderung von Medienkompetenz ein. <h3> <img id= "about-rainbow" src="/img/rainbow.svg"> Vielfältig und Frei </h3> MetaGer bringt vielfältige Ergebnisse, da es eine Metasuchmaschine ist. Was das genau bedeutet, haben wir in unserer <a href=":transparenz">Transparenzerklärung</a> erklärt. Durch die Veröffentlichung unseres Quellcodes zeigen wir, dass uns freier Wissenszugang wichtig ist. Unser Quellcode ist frei und Open Source.<h3> <img id="about-leaf" src="/img/leaf.svg"> 100% Ökostrom </h3> Nachhaltigkeit und Ressourcenverbrauch ist auch für uns ein großes Thema. Daher achten wir auf den Energieverbrauch unserer Dienste und verwenden nur Strom aus erneuerbaren Energiequellen.',
-   
-    'text.2' =>'MetaGer ist anders als andere Suchmaschinen. Dies findet sich nicht nur in unserer Gemeinwohl-orientierten Ausrichtung und in unserem Fokus auf Datenschutz wieder, sondern auch durch einige einzigartige Funktionen:',
-    'text.3' =>'Möglichkeit des Erstellens einer persönlichen Blacklist',
-    'text.4' =>'Funktion der Suche in der Suche',
-    'text.5' =>'Werbefreie Suche möglich (für Mitglieder)',
-    'text.6' =>'Einbindung von Suchmaschinen-Projekten wie YaCy',
-    'text.7' =>'Einzige deutsche Suchmaschine, die Ergebnisse mehrerer großer Webindexe vereint',
-    'text.8' =>'Und vieles mehr..',
+    'text.1' => '<h3> <img id="about-lock" src="/img/lock.svg"> Garantierte Privatsphäre</h3>Uns sind Datenschutz und Privatsphäre wichtig. Daher tracken und speichern wir keine Nutzerprofile. Außerdem bieten wir Dienste zum Schutz Ihrer Privatsphäre an, zum Beispiel unseren anonymisierenden Proxy („Anonym öffnen“-Funktion). <h3><img id= "about-heart" src="/img/heart.svg"> Gemeinnütziger Verein</h3> MetaGer ist ein Projekt des gemeinnützigen Vereins <a href="https://suma-ev.de">SUMA-EV, Verein für freien Wissenszugang</a>. Der SUMA-EV setzt sich vor allem für die Förderung von Medienkompetenz ein. <h3> <img id= "about-rainbow" src="/img/rainbow.svg"> Vielfältig und Frei </h3> MetaGer bringt vielfältige Ergebnisse, da es eine Metasuchmaschine ist. Was das genau bedeutet, haben wir in unserer <a href=":transparenz">Transparenzerklärung</a> erklärt. Durch die Veröffentlichung unseres Quellcodes zeigen wir, dass uns freier Wissenszugang wichtig ist. Unser Quellcode ist frei und Open Source.<h3> <img id="about-leaf" src="/img/leaf.svg"> 100% Ökostrom </h3> Nachhaltigkeit und Ressourcenverbrauch ist auch für uns ein großes Thema. Daher achten wir auf den Energieverbrauch unserer Dienste und verwenden nur Strom aus erneuerbaren Energiequellen.',
+
+    'text.2' => 'MetaGer ist anders als andere Suchmaschinen. Dies spiegelt sich nicht nur in unserer Gemeinwohl-orientierten Ausrichtung und in unserem Fokus auf Datenschutz wieder, sondern auch durch einige einzigartige Funktionen:',
+    'text.3' => 'Möglichkeit des Erstellens einer persönlichen Blacklist',
+    'text.4' => 'Funktion der Suche in der Suche',
+    'text.5' => 'Werbefreie Suche möglich (für Mitglieder)',
+    'text.6' => 'Einbindung von Suchmaschinen-Projekten wie YaCy',
+    'text.7' => 'Einzige deutsche Suchmaschine, die Ergebnisse mehrerer großer Webindexe vereint',
+    'text.8' => 'Und vieles mehr…',
 
     'timeline.1' => 'März 1996: Cebit Hannover',
     'timeline.1.1' => 'Die Idee zu MetaGer kommt dem deutschen Ingenieur Dr. Wolfgang Sander-Beuermann während der Computermesse CeBIT beim Mittagessen auf einer Papierserviette. Die Arbeit am ersten Prototypen beginnt noch am gleichen Abend.',
-   
+
     'timeline.2' => 'Ende 1996: Inbetriebnahme von MetaGer',
     'timeline.2.1' => 'MetaGer entsteht als Forschungsprojekt der Universität Hannover und des regionalen Rechenzentrums Niedersachsen.',
-   
+
     'timeline.3' => '1997-2006: MetaGer Startseite',
     'timeline.3.1' => 'So sieht MetaGer aus, als es erstmals ans Netz geht. Mit MetaGer wird somit ein innovativer neuer Dienst zur Verfügung gestellt.',
-   
+
     'timeline.4' => '01.10.2012: Wechsel der Trägerschaft zum SUMA-EV',
     'timeline.4.1' => 'MetaGer wird in den gemeinnützigen Verein <i>SUMA-EV – Verein für freien Wissenszugang</i> überführt. Die Kooperation mit der Universität Hannover wird fortgeführt.',
-   
+
     'timeline.5' => '2006-2015: Einführung neuer Designs',
     'timeline.5.1' => 'Die ursprüngliche Startseite von MetaGer macht in den folgenden Jahren viele kleine Veränderungen durch.',
-    
+
     'timeline.6' => '29.08.2013: Start der englischen Version',
     'timeline.6.1' => 'Erstmals wird auch die Oberfläche von MetaGer in englischer Sprache angeboten.',
 
@@ -44,7 +44,7 @@ return [
     'timeline.9.1' => 'Als Teil umfangreicherer Modernisierungen wird eine neue vereinfachte Startseite angeboten.',
 
     'timeline.10' => 'März 2016: Suchergebnisse in unter 1,3 Sekunden',
-    'timeline.10.1' =>'Durch umfangreiche Modernisierungen im Kern von MetaGer sinkt die durchschnittliche Antwortzeit der Suchmaschine auf unter 1,3 Sekunden. Im Laufe der nächsten Jahre sinkt diese weiter auf fast die Hälfte.',
+    'timeline.10.1' => 'Durch umfangreiche Modernisierungen im Kern von MetaGer sinkt die durchschnittliche Antwortzeit der Suchmaschine auf unter 1,3 Sekunden. Im Laufe der nächsten Jahre sinkt diese weiter auf fast die Hälfte.',
 
     'timeline.11' => '2016-2019: MetaGer Startseite',
     'timeline.11.1' => 'Weitere kleine Anpassungen festigen nach und nach orange als MetaGer-Farbe. ',
@@ -72,4 +72,4 @@ return [
 
     'timeline.19' => 'Heute',
     'timeline.19.1' => 'Auch heute entwickelt sich MetaGer stetig weiter.'
-]; 
+];

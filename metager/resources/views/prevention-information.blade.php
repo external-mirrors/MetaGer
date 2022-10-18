2011f350d50adf5e2156bf0995dc9d3986ea209a
Kim <kim@suma-ev.de>
2022-10-18 12:43:53 +0200

    adding text to prevention page

--- metager/resources/views/prevention-information.blade.php
+++ metager/resources/views/prevention-information.blade.php
@@ -12,5 +12,26 @@
 	<p>{{ trans('prevention-information.text.1') }}</p>
 
 </div>
+<div class="card">
+	<h1>{{ trans('prevention-information.europe') }}</h1>
 
+	<h2>{{ trans('prevention-information.belgium') }}</h2>
+	<p>{!! trans('prevention-information.belgium.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.germany') }}</h2>
+	<p>{!! trans('prevention-information.germany.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.denmark') }}</h2>
+	<p>{!! trans('prevention-information.denmark.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.france') }}</h2>
+	<p>{!! trans('prevention-information.france.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.greece') }}</h2>
+	<p>{!! trans('prevention-information.greeece.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.italy') }}</h2>
+	<p>{!! trans('prevention-information.italy.1') !!}</p>
+
+</div>
 @endsection

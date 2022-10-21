056c29109a4b346c891337702008594df172435e
Dominik Hebeler <dominik@suma-ev.de>
2022-10-21 12:31:16 +0200

    showing maps link only for germany

--- metager/resources/views/parts/sidebar.blade.php
+++ metager/resources/views/parts/sidebar.blade.php
@@ -75,12 +75,14 @@
         <span>@lang('sidebar.nav25')</span>
       </a>
     </li>
+    @if (App\Localization::getLanguage() === "de")
     <li>
       <a  href="https://maps.metager.de" target="_blank" >
       <img src="/img/icon-map.svg" alt="" aria-hidden="true" id="sidebar-img-map"> 
         <span>{{ trans('sidebar.nav27') }}</span> 
       </a>
     </li>
+    @endif
     <hr>
     <li class="metager-dropdown">
       <input id="contactToggle" class="sidebarCheckbox" type="checkbox">

be0ee41da50c9d73748df4c44d6d9e4a3cd0808a
Kim <kim@suma-ev.de>
2022-10-21 14:22:24 +0200

    adding text to prevention page

--- metager/resources/views/prevention-information.blade.php
+++ metager/resources/views/prevention-information.blade.php
@@ -28,10 +28,61 @@
 	<p>{!! trans('prevention-information.france.1') !!}</p>
 
 	<h2>{{ trans('prevention-information.greece') }}</h2>
-	<p>{!! trans('prevention-information.greeece.1') !!}</p>
+	<p>{!! trans('prevention-information.greece.1') !!}</p>
 
 	<h2>{{ trans('prevention-information.italy') }}</h2>
 	<p>{!! trans('prevention-information.italy.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.latvia') }}</h2>
+	<p>{!! trans('prevention-information.latvia.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.lithuania') }}</h2>
+	<p>{!! trans('prevention-information.lithuania.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.luxembourg') }}</h2>
+	<p>{!! trans('prevention-information.luxembourg.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.netherlands') }}</h2>
+	<p>{!! trans('prevention-information.netherlands.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.norway') }}</h2>
+	<p>{!! trans('prevention-information.norway.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.austria') }}</h2>
+	<p>{!! trans('prevention-information.austria.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.poland') }}</h2>
+	<p>{!! trans('prevention-information.poland.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.portugal') }}</h2>
+	<p>{!! trans('prevention-information.portugal.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.czech') }}</h2>
+	<p>{!! trans('prevention-information.czech.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.russia') }}</h2>
+	<p>{!! trans('prevention-information.russia.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.serbia') }}</h2>
+	<p>{!! trans('prevention-information.serbia.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.spain') }}</h2>
+	<p>{!! trans('prevention-information.spain.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.sweden') }}</h2>
+	<p>{!! trans('prevention-information.sweden.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.switzerland') }}</h2>
+	<p>{!! trans('prevention-information.switzerland.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.ukraine') }}</h2>
+	<p>{!! trans('prevention-information.ukraine.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.hungary') }}</h2>
+	<p>{!! trans('prevention-information.hungary.1') !!}</p>
+
+	<h2>{{ trans('prevention-information.uk') }}</h2>
+	<p>{!! trans('prevention-information.uk.1') !!}</p>
 
 </div>
 @endsection

ee0c9f74e338153d69a5abe85ae74e3423ce105d
Dominik Hebeler <dominik@suma-ev.de>
2022-11-23 11:38:57 +0100

    fixed csp error for pagination

--- metager/resources/views/parts/pager.blade.php
+++ metager/resources/views/parts/pager.blade.php
@@ -1,7 +1,7 @@
 {{-- Show pagination --}}
 <nav class="mg-pager" aria-label="...">
-    <div @if($metager->getPage() === 1) class="disabled" @endif>
-        <a @if($metager->getPage() !== 1) href="javascript:history.back()" @endif>{{ trans('results.zurueck') }}</a>
+    <div id="last-search-link" @if($metager->getPage() === 1) class="disabled" @endif>
+        <a @if($metager->getPage() !== 1) href="#" @endif>{{ trans('results.zurueck') }}</a>
     </div>
     <div id="next-search-link" @if($metager->nextSearchLink() === "#") class="disabled" @endif>
         <a @if($metager->nextSearchLink() !== "#") href="{{ $metager->nextSearchLink() }}" @endif @if($metager->isFramed() && $metager->getOut() !== "results-with-style")target="_top"@else target="_self"@endif>{{ trans('results.weiter') }}</a>

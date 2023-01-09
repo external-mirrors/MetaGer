6d205ec4ad011fdafa05abe6bb5942aa7b3bdaad
Dominik Hebeler <dominik@suma-ev.de>
2023-01-09 10:47:58 +0100

    added some styling to middle page

--- metager/resources/views/layouts/resultpage/framedResultPage.blade.php
+++ metager/resources/views/layouts/resultpage/framedResultPage.blade.php
@@ -11,6 +11,14 @@
 	<link rel="alternate" hreflang="{{ $locale }}" href="{{ LaravelLocalization::getLocalizedUrl($locale, null, [], true) }}">
 	@endif
 	@endforeach
+    <link type="text/css" rel="stylesheet" href="{{ mix('css/themes/metager.css') }}" />
+	@if(Cookie::get('dark_mode') === "2")
+	<link type="text/css" rel="stylesheet" href="{{ mix('css/themes/metager-dark.css') }}" />
+	@elseif(Cookie::get('dark_mode') === "1")
+	<link type="text/css" rel="stylesheet" href="{{ mix('css/themes/metager.css') }}" />
+	@elseif(Request::input('out', '') !== "results-with-style" )
+	<link type="text/css" rel="stylesheet" media="(prefers-color-scheme:dark)" href="{{ mix('css/themes/metager-dark.css') }}" />
+	@endif
     <title>{{ Request::input('eingabe', '') }} - MetaGer</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
@@ -34,6 +42,27 @@
     </style>
 </head>
 <body>
+	<div id="resultpage-container">
+	<div id="whitespace"></div>
+	<div id="research-bar-container">
+		<div id="research-bar">
+			<div id="header-logo">
+				<a class="screen-large" href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/") }}" @if(!empty($metager) && $metager->isFramed())target="_top" @endif tabindex="4">
+					<h1><img src="/img/metager.svg" alt="MetaGer" /></h1>
+				</a>
+				<a class="screen-small" href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/") }}" @if(!empty($metager) && $metager->isFramed())target="_top" @endif>
+					<h1><img src="/img/metager-schloss-orange.svg" alt="MetaGer" /></h1>
+				</a>
+				<a class="lang" href="{{ LaravelLocalization::getLocalizedURL(null, route("lang-selector")) }}">
+					<span>{{ App\Localization::getRegion() }}</span>
+				</a>
+			</div>
+			<div id="header-searchbar">
+				@include('parts.searchbar', ['class' => 'resultpage-searchbar', 'request' => Request::method()])
+			</div>
+			<div class="sidebar-opener-placeholder"></div>
+		</div>
+	</div>
     <iframe id="mg-framed" src="{{ $frame_url }}" autofocus="true"></iframe>
     <script nonce="{{ $mgv }}">
         document.getElementById("mg-framed").src = "";

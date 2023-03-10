8ace6a1a4164ccafcc8c750169e1a2d56465ebf0
Dominik Hebeler <dominik@hebeler.club>
2023-03-10 10:02:24 +0100

    allow fetcher requests to bypass proxy

--- metager/app/Models/Key.php
+++ metager/app/Models/Key.php
@@ -16,7 +16,7 @@ class Key
     {
         $this->key = $key;
         $this->status = null;
-        $this->keyserver = config("metager.metager.keymanager.server") . "/keys/api/json";
+        $this->keyserver = config("app.url") . "/keys/api/json";
     }
 
     # always returns true or false
@@ -62,6 +62,8 @@ class Key
             ],
             "useragent" => "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0",
             "cacheDuration" => 0,
+            "proxy" => false,
+            // Don't use Http Proxy if defined in .env
             "name" => "Key Login",
         ];
         $mission = json_encode($mission);
@@ -103,6 +105,8 @@ class Key
             ],
             "cacheDuration" => 0,
             "name" => "Key Login",
+            "proxy" => false,
+            // Don't use Http Proxy if defined in .env
             "curlopts" => [
                 CURLOPT_POST => true,
                 CURLOPT_POSTFIELDS => json_encode(["amount" => $discharge])

c9bab2b1e0ca51a42d621a3e0e70e731741aa402
Dominik Hebeler <dominik@hebeler.club>
2023-04-03 09:56:33 +0200

    enabled additional js refresh

--- metager/resources/js/verify.js
+++ metager/resources/js/verify.js
@@ -4,7 +4,7 @@ require("fetch-ie8");
 try {
   // Should get blocked by csp
   eval("window.sp = 1;");
-} catch (err) {}
+} catch (err) { }
 
 let key = getKey();
 let url = "/img/logo.png?id=" + key;
@@ -16,9 +16,35 @@ if (navigator.webdriver) {
   url += "&wd";
 }
 
-fetch(url);
+let interval;
+fetch(url).then(result => {
+  interval = setInterval(verify, 100);
+});
 
 function getKey() {
   let nonce_element = document.querySelector("meta[name=nonce]");
   return nonce_element.content;
 }
+
+function verify() {
+  let styleSheet = getStyleSheet();
+  if (!styleSheet || !("cssRules" in styleSheet) || styleSheet.cssRules.length === 0) {
+    return false;
+  }
+  clearInterval(interval);
+  history.go();
+}
+
+function getStyleSheet() {
+  let styleSheets = document.styleSheets;
+  for (let i = 0; i < styleSheets.length; i++) {
+    let styleSheet = styleSheets[i];
+    let matches = styleSheet.href.match(/index\.css\?id=([a-f0-9]{32})$/);
+    if (!matches) {
+      continue;
+    }
+    return styleSheet;
+  }
+  return null;
+}
+

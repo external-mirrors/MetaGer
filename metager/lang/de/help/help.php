882a75077622778217a702412b266895636a5204
Kim <kim@suma-ev.de>
2023-04-17 11:26:27 +0200

    fix help lang file for translation tool

--- metager/lang/de/help/help.php
+++ metager/lang/de/help/help.php
@@ -4,32 +4,37 @@ return [
     "achtung"               => 'Achtung, da unsere Suchmaschine ständig weiterentwickelt und verbessert wird, kann es dazu kommen, dass sich immer wieder Änderungen an Aufbau und Funktion ergeben. Wir versuchen zwar die Hilfe schnellstmöglich den Änderungen entsprechend anzupassen, können jedoch nicht ausschließen, dass es zu temporären Unstimmigkeiten in Teilen der Erklärungen kommt.',
     "title"                 => 'MetaGer - Hilfe',
     "easy.language"         => "Zur Hilfe in leichter Sprache",
-    'tableofcontents.title' => '',
-    'tableofcontents.1.0' => 'Die Hauptseiten',
-    'tableofcontents.1.1' => 'Die Startseite',
-    'tableofcontents.1.2' => 'Das Suchfeld',
-    'tableofcontents.1.3' => 'Die Ergebnisseite',
-    'tableofcontents.1.4' => 'Einstellungen',
+    "tableofcontents" => [
+        "1" => [
+            '0' => 'Die Hauptseiten',
+            '1' => 'Die Startseite',
+            '2' => 'Das Suchfeld',
+            '3' => 'Die Ergebnisseite',
+            '4' => 'Einstellungen',
+        ],
+        "2" => [
+            '0' => 'Nützliche Funktionen und Hinweise',
+            '1' => 'Suchfunktionen',
+            '2' => '!bangs',
+            '3' => 'Suche in der Suche',
+            '4' => 'MetaGer hinzufügen',
+        ],
+        "3" => [
+            '0' => 'Funktionen zur Anonymität und Datensicherheit',
+            '2' => 'Cookies & Co.',
+            '3' => 'Tor-Hidden-Service',
+            '4' => 'MetaGer-Proxy',
+            '5' => 'Jugendschutz',
+        ],
 
+        "4" => [
+            '0' => 'Weitere Dienste um die Suche herum',
+            '1' => 'Android-App',
+            '3' => 'Suchwortassoziator',
+            '4' => 'MetaGer Widget',
+            '5' => 'MetaGer Maps',
+        ],
+        'title' => '',
+    ],
 
-    'tableofcontents.2.0' => 'Nützliche Funktionen und Hinweise',
-    'tableofcontents.2.1' => 'Suchfunktionen',
-    'tableofcontents.2.2' => '!bangs',
-    'tableofcontents.2.3' => 'Suche in der Suche',
-    'tableofcontents.2.4' => 'MetaGer hinzufügen',
-
-
-
-    'tableofcontents.4.0' => 'Weitere Dienste um die Suche herum',
-    'tableofcontents.4.1' => 'Android-App',
-    'tableofcontents.4.3' => 'Suchwortassoziator',
-    'tableofcontents.4.4' => 'MetaGer Widget',
-    'tableofcontents.4.5' => 'MetaGer Maps',
-
-
-    'tableofcontents.3.0' => 'Funktionen zur Anonymität und Datensicherheit',
-    'tableofcontents.3.2' => 'Cookies & Co.',
-    'tableofcontents.3.3' => 'Tor-Hidden-Service',
-    'tableofcontents.3.4' => 'MetaGer-Proxy',
-    'tableofcontents.3.5' => 'Jugendschutz',
 ];

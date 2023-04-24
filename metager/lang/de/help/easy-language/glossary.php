687987eab2b2fdbdba03060441d385e2eef8497c
Kim <kim@suma-ev.de>
2023-04-24 13:36:37 +0200

    delete useless text

--- metager/lang/de/help/easy-language/glossary.php
+++ metager/lang/de/help/easy-language/glossary.php
@@ -39,15 +39,7 @@ return [
         "entry7"=> [],
         "entry8"=> [],
         "entry9"=> [],
-        "1"=>"Placeholder",
-        "2"=>"Placeholder",
-        "3"=>"Placeholder",
-        "4"=>"Placeholder",
-        "5"=>"Placeholder",
-        "6"=>"Placeholder",
-        "7"=>"Placeholder",
-        "8"=>"Placeholder",
-        "9"=>"Placeholder",
+
 
 
     ],

083e62659b242b9f86eb928c6003df7dd46b65eb
Dominik Hebeler <dominik@hebeler.club>
2023-04-25 14:30:15 +0200

    do not disable scopia by default

--- metager/app/Models/parserSkripte/Scopia.php
+++ metager/app/Models/parserSkripte/Scopia.php
@@ -13,7 +13,6 @@ class Scopia extends Searchengine
     public function __construct($name, SearchengineConfiguration $configuration)
     {
         parent::__construct($name, $configuration);
-        $this->configuration->disabledByDefault = true;
     }
 
     public function loadResults($result)

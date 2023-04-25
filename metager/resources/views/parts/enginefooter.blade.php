88f41160e333eedc661714d27fcdbd145a5c5d8e
Dominik Hebeler <dominik@hebeler.club>
2023-04-25 11:36:17 +0200

    translated texts and added cost to display

--- metager/resources/views/parts/enginefooter.blade.php
+++ metager/resources/views/parts/enginefooter.blade.php
@@ -5,23 +5,23 @@ $settings = app(\App\SearchSettings::class);
 <div id="engine-footer">
     @if(sizeof($searchengines->getEnabledSearchengines()) > 0)
     <div class="enabled-engines">
-        <h3>Abgefragte Suchdienste</h3>
+        <h3>@lang('resultPage.engines.queried')</h3>
         <div class="engines disabled">
             @foreach(app(\App\Models\Configuration\Searchengines::class)->getEnabledSearchengines() as $sumaName => $suma)
-            <div class="engine">{{ $suma->configuration->infos->displayName }}</div>
+            <div class="engine">{{ $suma->configuration->infos->displayName }} {{ $suma->configuration->cost > 0 ? "(" . $suma->configuration->cost . " Token)" : "" }}</div>
             @endforeach
         </div>
     </div>
     @endif
     @if($searchengines->hasDisabledSearchenginesWithReason(\App\Models\DisabledReason::USER_CONFIGURATION))
     <div class="disabled-engines">
-        <h3>Suchdienste zur Abfrage hinzufügen</h3>
+        <h3>@lang('resultPage.engines.disabled')</h3>
         <div class="engines">
             @foreach(app(\App\Models\Configuration\Searchengines::class)->sumas as $sumaName => $suma)
             @if($suma->configuration->disabled && $suma->configuration->disabledReason === \App\Models\DisabledReason::USER_CONFIGURATION)
             <div class="engine disabled-by-configuration">
                 <a href="{{ LaravelLocalization::getLocalizedURL(null, route('resultpage', array_merge(Request::all(), [$settings->fokus . '_engine_' . $sumaName => 'on']))) }}">
-                    {{ $suma->configuration->infos->displayName }}
+                    {{ $suma->configuration->infos->displayName }} {{ $suma->configuration->cost > 0 ? "(" . $suma->configuration->cost . " Token)" : "" }}
                 </a>
             </div>
             @endif
@@ -31,11 +31,11 @@ $settings = app(\App\SearchSettings::class);
     @endif
     @if($searchengines->hasDisabledSearchenginesWithReason(\App\Models\DisabledReason::PAYMENT_REQUIRED))
     <div class="payment-required-engines">
-        <h3>Suchdienste verfügbar mit <a href="{{ app(\App\Models\Authorization\Authorization::class)->getAdfreeLink() }}">MetaGer Schlüssel</a></h3>
+        <h3>@lang('resultPage.engines.payment_required', ["link" => app(\App\Models\Authorization\Authorization::class)->getAdfreeLink()])</h3>
         <div class="engines disabled">
             @foreach($searchengines->sumas as $sumaName => $suma)
             @if($suma->configuration->disabled && $suma->configuration->disabledReason === \App\Models\DisabledReason::PAYMENT_REQUIRED)
-            <div class="engine disabled-by-configuration">{{ $suma->configuration->infos->displayName }}</div>
+            <div class="engine disabled-by-configuration">{{ $suma->configuration->infos->displayName }} {{ $suma->configuration->cost > 0 ? "(" . $suma->configuration->cost . " Token)" : "" }}</div>
             @endif
             @endforeach
         </div>

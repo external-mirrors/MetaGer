aeadf0f33b8f5a7ea62c40f447fd8e817b6f7d1a
Dominik Hebeler <dominik@hebeler.club>
2023-05-08 12:12:36 +0200

    fixed label and color of key icon

--- metager/lang/de/index.php
+++ metager/lang/de/index.php
@@ -11,8 +11,15 @@ return [
     'adfree' => 'MetaGer werbefrei nutzen',
     'plugin-title' => 'MetaGer zu Ihrem Browser hinzufügen',
 
-    'key.placeholder' => 'Schlüssel für werbefreie Suche eingeben',
-    'key.tooltip' => 'Schlüssel für werbefreie Suche eingeben',
+    'key' => [
+        'placeholder' => 'Schlüssel für werbefreie Suche eingeben',
+        'tooltip' => [
+            'nokey' => 'Werbefreie Suche einrichten',
+            'empty' => 'Token aufgebraucht. Jetzt auffüllen.',
+            'low' => 'Token bald aufgebraucht. Jetzt auffüllen.',
+            'full' => 'Werbefreie Suche aktiviert.'
+        ]
+    ],
     'placeholder' => 'MetaGer: Sicher suchen & finden',
     'searchbutton' => 'MetaGer-Suche',
 ];
--- metager/resources/views/parts/searchbar.blade.php
+++ metager/resources/views/parts/searchbar.blade.php
@@ -3,8 +3,12 @@
 		<div class="searchbar {{$class ?? ''}}">
 			<div class="search-input-submit">
 				<div id="search-key">
-					<a id="key-link" @if(app('App\Models\Authorization\Authorization')->canDoAuthenticatedSearch())class="authorized" @else class="unauthorized"@endif href="{{ LaravelLocalization::getLocalizedURL(null, "/keys/key/enter") }}" @if(!empty($metager) && $metager->isFramed())target="_top" @endif data-tooltip="{{ trans ('index.key.tooltip') }}" tabindex="0">
-						<img @if(app('App\Models\Authorization\Authorization')->canDoAuthenticatedSearch())src="/img/key-verified.svg" @else src="/img/key-icon.svg"@endif alt="" aria-hidden="true" id="searchbar-img-key">
+					<a id="key-link" @if(app('App\Models\Authorization\Authorization')->canDoAuthenticatedSearch())class="authorized" @else class="unauthorized"@endif href="{{ LaravelLocalization::getLocalizedURL(null, "/keys/key/enter") }}" @if(!empty($metager) && $metager->isFramed())target="_top" @endif 
+						data-tooltip="{{ app('App\Models\Authorization\Authorization')->getKeyTooltip() }}" tabindex="0">
+						<img 
+							src="{{ app('App\Models\Authorization\Authorization')->getKeyIcon() }}"
+							alt="" aria-hidden="true" id="searchbar-img-key"
+						>
 					</a>
 				</div>
 				<div class="search-input">

0de028bfa09cbd9b7217baa3c8d53bb776e87e3c
Dominik Hebeler <dominik@hebeler.club>
2023-06-14 14:43:03 +0200

    update to zammad backend

--- metager/app/Http/Controllers/MailController.php
+++ metager/app/Http/Controllers/MailController.php
@@ -2,19 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\ContactMail;
 use App\Localization;
 use App\Mail\Sprachdatei;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redis;
 use Illuminate\Http\Response;
-use Illuminate\Support\Str;
 use LaravelLocalization;
 use Mail;
-use Log;
 use Validator;
-use \PHP_IBAN\IBAN;
-use \PHP_IBAN\IBANCountry;
 
 class MailController extends Controller
 {
@@ -26,13 +21,12 @@ class MailController extends Controller
      */
     public function contactMail(Request $request)
     {
-        # Nachricht, die wir an den Nutzer weiterleiten:
+        // Nachricht, die wir an den Nutzer weiterleiten:
         $messageType = ""; # [success|error]
         $returnMessage = '';
-        $to_mail = Localization::getLanguage() === "de" ? config("metager.metager.ticketsystem.germanmail") : config("metager.metager.ticketsystem.englishmail");
 
 
-        # Wir benötigen 3 Felder von dem Benutzer wenn diese nicht übermittelt wurden, oder nicht korrekt sind geben wir einen Error zurück
+        // Wir benötigen 3 Felder von dem Benutzer wenn diese nicht übermittelt wurden, oder nicht korrekt sind geben wir einen Error zurück
         $input_data = $request->all();
 
         $maxFileSize = 5 * 1024;
@@ -44,124 +38,57 @@ class MailController extends Controller
                 'pcsrf' => new \App\Rules\PCSRF,
                 'attachments' => ['max:5'],
                 'attachments.*' => ['file', 'max:' . $maxFileSize],
+                'message' => 'required',
+                'subject' => 'required'
             ]
             ,
             ["size" => trans("validation.pcsrf")]
         );
 
         if ($validator->fails()) {
-            return response(view('kontakt.kontakt')->with('formerrors', $validator)->with('title', trans('titles.kontakt'))->with('navbarFocus', 'kontakt')->with("css", ["css/contact.css"])->with("js", ["js/contact.js"]));
+            return response(
+                view('kontakt.kontakt')
+                    ->with('formerrors', $validator)
+                    ->with('title', trans('titles.kontakt'))
+                    ->with('navbarFocus', 'kontakt')
+                    ->with("css", [mix("css/contact.css")])
+                    ->with("js", [mix("js/contact.js")])
+            );
         }
 
+        $to_mail = Localization::getLanguage() === "de" ? config("metager.metager.ticketsystem.germanmail") : config("metager.metager.ticketsystem.englishmail");
+        $group = Localization::getLanguage() === "de" ? "MetaGer (DE)" : "MetaGer (EN)";
         $name = $request->input('name', '');
+        $email = $request->input('email', 'noreply@metager.de');
+        $message = $request->input('message');
+        $subject = $request->input('subject');
 
-        $replyTo = $request->input('email', 'noreply@metager.de');
-        if ($replyTo === "") {
-            $replyTo = "noreply@metager.de";
-        } else {
-            $replyTo = $request->input('email');
-        }
-
-        if (!$request->filled('message') || !$request->filled('subject')) {
-            $messageType = "error";
-            $returnMessage = trans('kontakt.error.1');
-        } else {
-            $message = $request->input('message');
-            $subject = $request->input('subject');
-
-            # Wir versenden die Mail des Benutzers an uns:
-            $ip = $request->ip();
-            $date = (new Carbon())->toRfc822String();
-
-            $message_id = Str::uuid()->toString();
-            $from_host = substr($replyTo, strpos($replyTo, "@") + 1);
-            $boundary = md5($message);
-            $boundary_inline = md5($message_id);
-            $postdata = <<<POSTDATA
-            MIME-Version: 1.0
-            Received: by $ip with HTTP; $date
-            Date: $date
-            Delivered-To: $to_mail
-            Message-ID: <$message_id@$from_host>
-            Subject: $subject
-            From: $name <$replyTo>
-            To: $to_mail
-            Content-Type: multipart/mixed; boundary=$boundary
-
-            --$boundary
-            Content-Type: multipart/alternative; boundary=$boundary_inline
-
-            --$boundary_inline
-            Content-Type: text/plain; charset=UTF-8
-
-            $message
-
-            --$boundary_inline--
-            POSTDATA;
-
-            if ($request->has("attachments") && is_array($request->file("attachments"))) {
-                foreach ($request->file("attachments") as $attachment) {
-                    $file_content = base64_encode(file_get_contents($attachment->getRealPath()));
-                    $filename = $attachment->getClientOriginalName();
-                    $file_mimetype = $attachment->getMimeType();
-                    $postdata .= PHP_EOL . "--$boundary" . PHP_EOL;
-                    $postdata .= <<<ATTACHMENT
-                    Content-Type: $file_mimetype; charset=utf-8; name="$filename"
-                    Content-Disposition: attachment; filename="$filename"
-                    Content-Transfer-Encoding: base64
-
-                    $file_content
-                    ATTACHMENT;
-                }
-            }
-            $postdata .= PHP_EOL . "--$boundary--" . PHP_EOL;
-
-            $resulthash = md5($subject . $message);
-
-            $mission = [
-                "resulthash" => $resulthash,
-                "url" => config("metager.metager.ticketsystem.url"),
-                "useragent" => "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0",
-                "username" => null,
-                "password" => null,
-                "headers" => [
-                    "X-API-Key" => config("metager.metager.ticketsystem.apikey"),
-                    "Content-Length" => strlen($postdata)
-                ],
-                "cacheDuration" => 0,
-                "name" => "Ticket",
-                "curlopts" => [
-                    CURLOPT_POST => true,
-                    CURLOPT_POSTFIELDS => $postdata,
-                    CURLOPT_LOW_SPEED_TIME => 60,
-                    CURLOPT_CONNECTTIMEOUT => 10,
-                    CURLOPT_TIMEOUT => 60
-                ]
-            ];
-            $mission = json_encode($mission);
-            Redis::rpush(\App\MetaGer::FETCHQUEUE_KEY, $mission);
-
-            // Fetch the result
-            $answer = Redis::blpop($resulthash, 20);
-
-            // Fehlerfall
-            if (empty($answer) || (is_array($answer) && sizeof($answer) === 2 && $answer[1] === "no-result")) {
-                $messageType = "error";
-                $returnMessage = trans('kontakt.error.2', ["email" => $to_mail]);
-            } else {
-                $returnMessage = trans('kontakt.success.1', ["email" => $replyTo]);
-                $messageType = "success";
+        $attachments = [];
+        if ($request->has("attachments") && is_array($request->file("attachments"))) {
+            foreach ($request->file("attachments") as $attachment) {
+                $file_content = base64_encode(file_get_contents($attachment->getRealPath()));
+                $filename = $attachment->getClientOriginalName();
+                $file_mimetype = $attachment->getMimeType();
+                $attachments[] = [
+                    "filename" => $filename,
+                    "data" => $file_content,
+                    "mime-type" => $file_mimetype
+                ];
             }
         }
+        ContactMail::dispatch($to_mail, $group, $name, $email, $subject, $message, $attachments);
 
+        $returnMessage = trans('kontakt.success.1', ["email" => $email]);
+        $messageType = "success";
+        $test = LaravelLocalization::getNonLocalizedURL(mix("/css/contact.css"));
         return response(view('kontakt.kontakt')
             ->with('title', 'Kontakt')
             ->with($messageType, $returnMessage)
-            ->with("css", ["css/contact.css"])
-            ->with("js", ["js/contact.js"]));
+            ->with("css", [mix("/css/contact.css")])
+            ->with("js", [mix('/js/contact.js')]));
     }
 
-    #Ueberprueft ob ein bereits vorhandener Eintrag bearbeitet worden ist
+    // Ueberprueft ob ein bereits vorhandener Eintrag bearbeitet worden ist
     public static function isEdited($k, $v, $filename)
     {
         try {

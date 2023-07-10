70b991c6d7d2068fd8c61af10b50bee70494f952
Weblate <noreply@weblate.org>
2023-07-10 14:24:47 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Desktop-Safari
    Translate-URL: https://translate.metager.de/projects/metager/desktop-safari/

--- metager/lang/de/plugin-desktop/desktop-safari.php
+++ metager/lang/de/plugin-desktop/desktop-safari.php
@@ -1,8 +1,4 @@
 <?php
-    return[
-
-        //link to unable
-        'default-page-v10.1' => 'Klicken Sie <a href="/" target="_blank" rel="noopener">hier</a> um MetaGer in einem neuen Tab zu öffnen.',
-        'default-page-v10.2' => 'Klicken Sie oben links auf "Safari" und wählen Sie "Einstellungen".',
-        'default-page-v10.3' => 'Klicken Sie unter dem Punkt "Startseite" auf die Schaltfläche "Aktuelle Seite einstellen".'
-    ];
+return [
+    'default-page-v10.2' => 'Klicken Sie oben links auf "Safari" und wählen Sie "Einstellungen".',
+];

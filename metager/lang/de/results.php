91106c6f160ca3fc6423a41ea0827e628cfe0eb8
Weblate <noreply@weblate.org>
2023-07-10 14:25:21 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Results
    Translate-URL: https://translate.metager.de/projects/metager/results/

--- metager/lang/de/results.php
+++ metager/lang/de/results.php
@@ -1,14 +1,10 @@
 <?php
-
 return [
     'redirect' => '(mit redirect)',
     'weiter' => 'Weitersuchen',
     'zurueck' => 'Zurück',
     'backtotop' => 'Zum Anfang der Seite',
-    'filter' => 'Die Suchergebnisse wurden gefiltert: <strong>[:filter]</strong>. <a href=":link" target="_blank">Filter abschalten.</a>',
     'filter.default' => 'Sie erhalten die Benutzeroberfläche in <strong>:langName</strong>, aber Ergebnisse in <strong>ALLEN</strong> Sprachen. Für ausschließlich deutschsprachige Ergebnisse können Sie unter <a href="/settings" target="_blank">anpassen</a> die "Sprache auswählen".',
-
     'produktanzeigen' => 'Produktanzeigen:',
-    'query.alteration.new' => 'Enthält Ergebnisse für <a href=":link">:alteration <img class= "mg-icon" src="/img/icon-lupe.svg"></a>',
     'query.alteration.original' => 'Stattdessen nach <a href=":link">:original <img class= "mg-icon" src="/img/icon-lupe.svg"></a> suchen',
 ];

4d23df36e1bad13e0ff7cc260e8b1a48adeaa9f9
Weblate <noreply@weblate.org>
2023-07-10 14:25:39 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Sitesearch
    Translate-URL: https://translate.metager.de/projects/metager/sitesearch/

--- metager/lang/de/sitesearch.php
+++ metager/lang/de/sitesearch.php
@@ -1,16 +1,8 @@
 <?php
-
 return [
-    'head.1'      => 'MetaGer Sitesearch-Widget',
-    'head.2'      => 'Hier finden Sie ein Metager-Widget für Ihre Webseite.',
-    'head.3'      => 'Website auf der gesucht werden soll',
-    'head.4'      => 'Website eingeben...',
-    'head.5'      => 'Generieren',
-
+    'head.2' => 'Hier finden Sie ein Metager-Widget für Ihre Webseite.',
+    'head.4' => 'Website eingeben...',
     'generated.1' => 'Vorschau',
-    'generated.2' => 'Sicher suchen & finden mit MetaGer',
     'generated.3' => 'Suche mit MetaGer...',
-    'generated.4' => 'Suchen',
     'generated.5' => 'Code',
-    'generated.6' => 'all',
 ];

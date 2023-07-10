a9a11c7cf5dd87c0298ac9d775b0b78cbaa19fc4
Weblate <noreply@weblate.org>
2023-07-10 14:24:44 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Desktop-Ie
    Translate-URL: https://translate.metager.de/projects/metager/desktop-ie/

--- metager/lang/de/plugin-desktop/desktop-ie.php
+++ metager/lang/de/plugin-desktop/desktop-ie.php
@@ -1,10 +1,7 @@
 <?php
 return [
-    'default-search-v9.2' => 'Wählen Sie "Diese Suchmaschine als Standard einstellen" aus und klicken auf "Hinzufügen".',
     'default-search-v11.2' => 'Klicken Sie in Ihrem Browser oben rechts auf "<img class= "mg-icon" src="/img/icon-settings.svg">".',
-    'default-search-v11.4' => 'Klicken Sie im Bereich "Add-On-Typen" auf "Suchanbieter" und anschließend im rechten Bereich auf MetaGer.',
     'default-page-v9.1' => 'Klicken Sie <a href="/" target="_blank" rel="noopener">hier</a> um MetaGer in einem neuen Tab zu öffnen.',
-    'default-page-v9.3' => 'Im erscheinenden Popup wählen Sie "Diese Seite als einzige Startseite nutzen" und anschließend auf "Ja".',
     'default-search-v11' => [
         1 => 'Klicken Sie auf <a href="javascript:window.external.addSearchProvider($(\'link[rel=search]\').attr(\'href\'));">hier</a>, um MetaGer als Suchmaschine hinzuzufügen.',
         2 => 'Navigieren Sie zur rechten oberen Ecke und klicken Sie auf "<img class= "mg-icon" src="/img/icon-settings.svg">".',

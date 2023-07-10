f836b800bd3706efbfe4fb627e9d4a95b8cc8e46
Weblate <noreply@weblate.org>
2023-07-10 14:25:03 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Result
    Translate-URL: https://translate.metager.de/projects/metager/result/

--- metager/lang/de/result.php
+++ metager/lang/de/result.php
@@ -1,22 +1,15 @@
 <?php
-
 return [
-    'options.headline' => 'Optionen',
     'options.savetab' => 'Ergebnis speichern',
-    'options.1' => 'Auf dieser Domain neu suchen',
     'options.2' => ':host ausblenden',
-    'options.3' => '*.:domain ausblenden',
     'options.4' => 'Partner',
-    'options.5' => 'ANONYM ÖFFNEN',
     'options.6' => 'IN NEUEM TAB ÖFFNEN',
-    'options.7' => 'ÖFFNEN',
     'options.8' => 'WERBUNG ENTFERNEN',
-    'options.direct' => 'Direkt öffnen',
     'gefVon' => 'von',
     'advertisement' => 'Anzeige',
     'providers' => 'Anbietern',
     'proxytext' => 'Der Link wird anonymisiert geöffnet. Ihre Daten werden nicht zum Zielserver übertragen. Möglicherweise funktionieren manche Webseiten nicht wie gewohnt.',
     'metagerkeytext' => 'MetaGer werbefrei nutzen',
     'news' => 'Relevante Nachrichten',
-    'videos' => "Videos"
+    'videos' => "Videos",
 ];

1b3f1d123df5dcb7a333e03d1d02469c69519009
Weblate <noreply@weblate.org>
2023-07-10 14:25:26 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Metagersearch
    Translate-URL: https://translate.metager.de/projects/metager/metagersearch/

--- metager/lang/de/metaGerSearch.php
+++ metager/lang/de/metaGerSearch.php
@@ -1,10 +1,6 @@
 <?php
-
 return [
-    'quicktips.wikipedia.adress' => 'aus <a href="https://de.wikipedia.org" target="_blank" rel="noopener">Wikipedia, der freien Enzyklopädie</a>',
-    'quicktips.dictcc.adress'    => 'aus <a href="https://www.dict.cc/" target="_blank" rel="noopener">dict.cc</a>',
-    'quicktips.tips.title'       => 'Wussten Sie schon?',
-    'quicktips.bang.title'       => '!bang-Weiterleitung',
-    'quicktips.bang.buttonlabel' => 'Weitersuchen auf',
-    'quicktips.bang.from'        => 'von',
+    'quicktips.dictcc.adress' => 'aus <a href="https://www.dict.cc/" target="_blank" rel="noopener">dict.cc</a>',
+    'quicktips.bang.title' => '!bang-Weiterleitung',
+    'quicktips.bang.from' => 'von',
 ];

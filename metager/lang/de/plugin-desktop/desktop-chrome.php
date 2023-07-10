0df0af440d902a840e376733d042da040d531008
Weblate <noreply@weblate.org>
2023-07-10 14:24:46 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Desktop-Chrome
    Translate-URL: https://translate.metager.de/projects/metager/desktop-chrome/

--- metager/lang/de/plugin-desktop/desktop-chrome.php
+++ metager/lang/de/plugin-desktop/desktop-chrome.php
@@ -1,23 +1,10 @@
 <?php
-    return[
-        
-        //Instructions for Chrome version 49-52
-        'default-search-v49.1' => 'Klicken Sie in Ihrem Chrome oben rechts auf <img class= "mg-icon" src="/img/menu.svg"> und im folgenden Menü auf "Einstellungen", um die Einstellungen Ihres Chromes zu öffnen.',
-        'default-search-v49.2' => 'Im Bereich "Suche" klicken Sie auf "Suchmaschinen verwalten...".',
-        'default-search-v49.3' => 'In der unteren Hälfte des erscheinenden Menüs fahren Sie mit der Maus über den MetaGer-Eintrag. Auf der rechten Seite des Eintrags klicken sie auf die erscheinende blaue "Als Standard"-Schaltfläche',
-
-        //Instructions for Chrome version 53-58
-        'default-search-v53.1' => 'Klicken Sie in Ihrem Chrome oben rechts auf <img class= "mg-icon" src="/img/ellipsis.svg"> und im folgenden Menü auf "Einstellungen", um die Einstellungen Ihres Chromes zu öffnen.',
-        'default-search-v53.2' => 'Im Bereich "Suche" klicken Sie auf "Suchmaschinen verwalten...".',
-        'default-search-v53.3' => 'In der unteren Hälfte des erscheinenden Menüs fahren Sie mit der Maus über den MetaGer-Eintrag. Auf der rechten Seite des Eintrags klicken sie auf die erscheinende blaue "Als Standard"-Schaltfläche',
-
-        //Instructions for Chrome version >=59
-        'default-search-v59.1' => 'Klicken Sie in Ihrem Chrome oben rechts auf <img class= "mg-icon" src="/img/ellipsis.svg"> und im folgenden Menü auf "Einstellungen", um die Einstellungen Ihres Chromes zu öffnen.',
-        'default-search-v59.2' => 'Im Bereich "Suchmaschine" klicken Sie auf "Suchmaschinen verwalten..."',
-        'default-search-v59.3' => 'In der nun angezeigten Liste, finden Sie den Eintrag "MetaGer". Fahren Sie mit der Maus über den Eintrag und klicken rechts auf <img class= "mg-icon" src="/img/ellipsis.svg"> und anschließend auf "Als Standard festlegen"',
-
-        'default-page-v49.1' => 'Klicken Sie oben rechts im Browser auf <img class= "mg-icon" src="/img/ellipsis.svg"> und öffnen Sie die "Einstellungen".',
-        'default-page-v49.2' => 'Im Bereich "Beim Start" wählen Sie "Bestimmte Seite oder Seiten öffnen und klicken anschließend auf "Neue Seite hinzufügen" bzw. "Seite festlegen".',
-        'default-page-v49.3' => 'Tragen Sie ":link" als URL bei "Neue Seite hinzufügen" ein.',
-        'default-page-v49.4' => 'Hinweis: Alle hier sichtbaren Webseiten werden nun beim Start des Browsers geöffnet. Sie können Einträge entfernen, indem Sie mit der Maus drüber fahren und rechts auf das "x" klicken.',
-    ];
+return [
+    'default-search-v49.2' => 'Im Bereich "Suche" klicken Sie auf "Suchmaschinen verwalten...".',
+    //Instructions for Chrome version 53-58
+    'default-search-v53.1' => 'Klicken Sie in Ihrem Chrome oben rechts auf <img class= "mg-icon" src="/img/ellipsis.svg"> und im folgenden Menü auf "Einstellungen", um die Einstellungen Ihres Chromes zu öffnen.',
+    'default-search-v53.3' => 'In der unteren Hälfte des erscheinenden Menüs fahren Sie mit der Maus über den MetaGer-Eintrag. Auf der rechten Seite des Eintrags klicken sie auf die erscheinende blaue "Als Standard"-Schaltfläche',
+    'default-search-v59.2' => 'Im Bereich "Suchmaschine" klicken Sie auf "Suchmaschinen verwalten..."',
+    'default-page-v49.1' => 'Klicken Sie oben rechts im Browser auf <img class= "mg-icon" src="/img/ellipsis.svg"> und öffnen Sie die "Einstellungen".',
+    'default-page-v49.3' => 'Tragen Sie ":link" als URL bei "Neue Seite hinzufügen" ein.',
+];

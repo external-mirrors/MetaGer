c3a680ebef3b68700978d901516584af27e352bc
dominik <dominik@hebeler.club>
2023-07-10 13:34:07 +0000

    Translated using Weblate (Spanish)
    Currently translated at 92.3% (12 of 13 strings)
    
    Translation: MetaGer/Plugin-Page
    Translate-URL: https://translate.metager.de/projects/metager/plugin-page/es/

--- metager/lang/es/plugin-page.php
+++ metager/lang/es/plugin-page.php
@@ -1,32 +1,27 @@
 <?php
 return [
-    'head'                   => [
-        1    => 'Añadir MetaGer a Firefox',
-        2    => 'Añadir MetaGer a Chrome',
-        3    => 'Añadir MetaGer a Opera',
-        4    => 'Añadir MetaGer a Internet Explorer',
-        5    => 'Añadir MetaGer a Microsoft Edge',
-        6    => 'Añadir MetaGer a Safari',
-        7    => 'Añadir MetaGer a Vivaldi',
-        8    => 'Añadir MetaGer a Firefox Klar',
-        9    => 'Añadir MetaGer a UCBrowser',
-        10   => 'Añadir MetaGer a Samsung Internet',
-        0    => 'Instalar Firefox y añadir MetaGer',
+    'head' => [
+        2 => 'Añadir MetaGer a Chrome',
+        4 => 'Añadir MetaGer a Internet Explorer',
+        6 => 'Añadir MetaGer a Safari',
+        8 => 'Añadir MetaGer a Firefox Klar',
+        10 => 'Añadir MetaGer a Samsung Internet',
+        'Instala Firefox y añade MetaGer',
     ],
-    'open-modal'             => [
+    'open-modal' => [
         'title' => 'Añadir MetaGer a su navegador',
     ],
-    'search-engine'          => [
-        1   => '¿Qué hace un motor de búsqueda estándar?',
-        2   => 'Con MetaGer como motor de búsqueda por defecto, puedes indicar a tu navegador que permita a MetaGer responder automáticamente a las búsquedas que se realicen a través de la barra de direcciones, por ejemplo.',
+    'search-engine' => [
+        1 => '¿Qué hace un motor de búsqueda estándar?',
+        2 => 'Con MetaGer como motor de búsqueda por defecto, puedes indicar a tu navegador que permita a MetaGer responder automáticamente a las búsquedas que se realicen a través de la barra de direcciones, por ejemplo.',
     ],
-    'browser-download'       => 'Descargar Firefox',
-    'firefox-plugin'         => 'Establecer MetaGer como motor de búsqueda por defecto con el complemento',
+    'browser-download' => 'Descargar Firefox',
+    'firefox-plugin' => 'Establecer MetaGer como motor de búsqueda por defecto con el complemento',
     'firefox-default-search' => 'Establecer MetaGer como motor de búsqueda por defecto sin complemento',
-    'default-search'         => 'Establecer MetaGer como motor de búsqueda por defecto',
-    'default-page'           => 'Establecer MetaGer como página de inicio',
-    'desktop-unable'         => 'Lamentablemente, el navegador que estás utilizando no ofrece la opción de establecer MetaGer como motor de búsqueda (por defecto), pero <a href="https://www.mozilla.org/de/firefox/new/" target="_blank" rel="noopener">aquí</a> puedes instalar Firefox, un navegador de código abierto para el que tenemos instrucciones.',
-    'mobile-unable'          => 'Lamentablemente, el navegador que estás utilizando no ofrece la opción de establecer MetaGer como motor de búsqueda (por defecto), pero <a href="https://www.mozilla.org/de/firefox/mobile/" target="_blank" rel="noopener">aquí</a> puedes instalar Firefox, un navegador de código abierto para el que tenemos instrucciones.',
-    'desktop-notlisted'      => 'Desgraciadamente, aún no sabemos cómo configurar el motor de búsqueda (por defecto) en el navegador que utilizas, pero <a href="https://www.mozilla.org/de/firefox/mobile/" target="_blank" rel="noopener">aquí</a> puedes instalar Firefox, un navegador de código abierto donde sí lo sabemos.',
-    'mobile-notlisted'       => 'Desgraciadamente, aún no sabemos cómo configurar el motor de búsqueda (por defecto) en el navegador que utilizas, pero <a href="https://www.mozilla.org/de/firefox/mobile/" target="_blank" rel="noopener">aquí</a> puedes instalar Firefox, un navegador de código abierto donde sí lo sabemos.',
+    'default-search' => 'Establecer MetaGer como motor de búsqueda por defecto',
+    'default-page' => 'Establecer MetaGer como página de inicio',
+    'desktop-unable' => 'Lamentablemente, el navegador que estás utilizando no ofrece la opción de establecer MetaGer como motor de búsqueda (por defecto), pero <a href="https://www.mozilla.org/de/firefox/new/" target="_blank" rel="noopener">aquí</a> puedes instalar Firefox, un navegador de código abierto para el que tenemos instrucciones.',
+    'mobile-unable' => 'Lamentablemente, el navegador que estás utilizando no ofrece la opción de establecer MetaGer como motor de búsqueda (por defecto), pero <a href="https://www.mozilla.org/de/firefox/mobile/" target="_blank" rel="noopener">aquí</a> puedes instalar Firefox, un navegador de código abierto para el que tenemos instrucciones.',
+    'desktop-notlisted' => 'Desgraciadamente, aún no sabemos cómo configurar el motor de búsqueda (por defecto) en el navegador que utilizas, pero <a href="https://www.mozilla.org/de/firefox/mobile/" target="_blank" rel="noopener">aquí</a> puedes instalar Firefox, un navegador de código abierto donde sí lo sabemos.',
+    'mobile-notlisted' => 'Desgraciadamente, aún no sabemos cómo configurar el motor de búsqueda (por defecto) en el navegador que utilizas, pero <a href="https://www.mozilla.org/de/firefox/mobile/" target="_blank" rel="noopener">aquí</a> puedes instalar Firefox, un navegador de código abierto donde sí lo sabemos.',
 ];

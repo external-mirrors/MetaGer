b074e23d531afbec752e00ca9c2990e448443250
dominik <dominik@hebeler.club>
2023-07-10 13:39:58 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: MetaGer/Desktop-Vivaldi
    Translate-URL: https://translate.metager.de/projects/metager/desktop-vivaldi/de/

--- metager/lang/de/plugin-desktop/desktop-vivaldi.php
+++ metager/lang/de/plugin-desktop/desktop-vivaldi.php
@@ -1,11 +1,14 @@
 <?php
-    return[
-
-        //Instructions for Vivaldi version 3.3
-        'default-search-v3-3.1' => 'Klicken Sie mit der rechten Maustaste auf das Sucheingabefeld unter der Anleitung.',
-        'default-search-v3-3.2' => 'Wählen Sie im Kontextmenü "Suche erstellen..."',
-        'default-search-v3-3.3' => 'Klicken Sie im Popup auf "Als Standard-Suche festlegen" und anschließend auf "Hinzufügen".',
-
-        'default-page-v3-3.1' => 'Klicken Sie oben links auf das Vivaldi-Symbol und wählen Sie unter "Extras" den Menüpunkt "<img class= "mg-icon" src="/img/icon-settings.svg"> Einstellungen"',
-        'default-page-v3-3.2' => 'Im erscheinenden Fenster unter "Startseite" wählen Sie  "Benutzerdefiniert:" und tragen :link ein.',
-    ];
+return [
+    'default-search-v3-3.2' => 'Wählen Sie im Kontextmenü "Suche erstellen..."',
+    'default-page-v3-3.1' => 'Klicken Sie oben links auf das Vivaldi-Symbol und wählen Sie unter "Extras" den Menüpunkt "<img class= "mg-icon" src="/img/icon-settings.svg"> Einstellungen"',
+    'default-search-v3-3' => [
+        3 => 'Aktivieren Sie das Kontrollkästchen "Als Standardsuche festlegen" und klicken Sie im Popup-Fenster auf "Hinzufügen".',
+        1 => 'Klicken Sie mit der rechten Maustaste auf die Suchleiste am Ende dieser Anweisung',
+        2 => 'Wählen Sie im Kontextmenü "Als Suchmaschine hinzufügen...".',
+    ],
+    'default-page-v3-3' => [
+        2 => 'Wählen Sie unter "Homepage" die Option "Spezifische Seite" und geben Sie :link ein.',
+        1 => 'Klicken Sie auf das "Vivaldi"-Logo oben links und wählen Sie "<img class="mg-icon" src="/img/icon-settings.svg">" "Einstellungen" unter "Werkzeuge"',
+    ],
+];

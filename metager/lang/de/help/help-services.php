3b6a8b6c6a40ed712471b79c301dc8d6e1607125
Weblate <noreply@weblate.org>
2023-07-12 07:47:54 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Help-Services
    Translate-URL: https://translate.metager.de/projects/metager/help-services/

--- metager/lang/de/help/help-services.php
+++ metager/lang/de/help/help-services.php
@@ -9,11 +9,9 @@ return [
         'text' => "Weitere Dienste um die Suche herum",
     ],
     'maps' => [
-        '2' => "Diese Kartenfunktion kann auch von der MetaGer-Suche aufgerufen werden (und umgekehrt). Sobald Sie bei MetaGer nach einem Begriff gesucht haben, sehen Sie oben rechts einen neuen Suchfokus \"Maps\".  Beim Klick darauf gelangen Sie zu einer dazugehörigen Karte. .",
         'title' => "MetaGer Maps",
     ],
     'suchwortassoziator' => [
-        '2' => "Beispiel: Sie möchten mehr über Zeckenbisse und deren Gefahren wissen, aber ihnen fallen die medizinischen Fachbegriffe für Erkrankungen aus diesem Bereich nicht mehr ein. Die Eingabe des Wortes \"Zeckenbisse\" in den Web-Assoziator liefert dann u.a. die Begriffe \"Borreliose\" und \"fsme\".",
         'title' => "Suchwortassoziator",
     ],
     'title' => 'MetaGer - Hilfe',

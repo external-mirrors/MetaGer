d474dba97614064b79c2675325ceec980a3a14c8
Dominik Hebeler <dominik@hebeler.club>
2023-07-12 11:41:29 +0200

    fixed lang files for about page

--- metager/resources/views/about.blade.php
+++ metager/resources/views/about.blade.php
@@ -8,17 +8,17 @@
 </div>
 <div class="card">
 	<h1>{{ trans('about.head.3') }}</h1>
-	<h3><img id="about-lock" src="/img/lock.svg"> @lang('about.points.0.heading')</h3>
-	<p>@lang('about.points.0.text')</p>
+	<h3><img id="about-lock" src="/img/lock.svg"> @lang('about.points.privacy.heading')</h3>
+	<p>@lang('about.points.privacy.text')</p>
 
-	<h3><img id= "about-heart" src="/img/heart.svg"> @lang('about.points.1.heading')</h3>
-	<p>@lang('about.points.1.text')</p>
+	<h3><img id= "about-heart" src="/img/heart.svg"> @lang('about.points.association.heading')</h3>
+	<p>@lang('about.points.association.text')</p>
 
-	<h3><img id= "about-rainbow" src="/img/rainbow.svg"> @lang('about.points.2.heading', ["transparenz" => LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "transparency")])</h3>
-	<p>@lang('about.points.2.text')</p>
+	<h3><img id= "about-rainbow" src="/img/rainbow.svg"> @lang('about.points.diverse.heading', ["transparenz" => LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "transparency")])</h3>
+	<p>@lang('about.points.diverse.text')</p>
 
-	<h3><img id="about-leaf" src="/img/leaf.svg"> @lang('about.points.3.heading')</h3>
-	<p>@lang('about.points.3.text')</p>
+	<h3><img id="about-leaf" src="/img/leaf.svg"> @lang('about.points.renewable.heading')</h3>
+	<p>@lang('about.points.renewable.text')</p>
 </div>
 <div class="card">
 	<h1>{{ trans('about.head.4') }}</h1>

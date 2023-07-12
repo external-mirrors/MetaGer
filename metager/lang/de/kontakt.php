90eaaaf936fadc383fa90812a8be61a2c21661b3
Weblate <noreply@weblate.org>
2023-07-12 07:48:24 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Kontakt
    Translate-URL: https://translate.metager.de/projects/metager/kontakt/

--- metager/lang/de/kontakt.php
+++ metager/lang/de/kontakt.php
@@ -1,13 +1,9 @@
 <?php
 return [
-    'form.1' => 'Sicheres Kontaktformular',
     'form.name' => 'Name (Vorname Nachname)',
-    'form.6' => 'Ihre Nachricht',
     'form.8' => 'Senden',
-    'letter.1' => 'Per Brief-Post',
     'letter.3' => 'SUMA-EV
 Röselerstr. 3
 30159 Hannover
 Germany',
-    'error.2' => 'Beim Versand Ihrer Nachricht ist ein Fehler aufgetreten. Sie können uns direkt unter folgender E-Mail Adresse kontaktieren: :email',
 ];

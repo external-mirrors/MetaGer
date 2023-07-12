3d89939e420740bb0107de98f700b836803fa530
Dominik Hebeler <dominik@hebeler.club>
2023-07-12 12:28:09 +0200

    fixed german translations for startpage

--- metager/lang/de/index.php
+++ metager/lang/de/index.php
@@ -1,8 +1,6 @@
 <?php
 return [
-    'foki.science' => 'Wissenschaft',
     'plugin' => 'MetaGer installieren',
-    'adfree' => 'MetaGer werbefrei nutzen',
     'plugin-title' => 'MetaGer zu Ihrem Browser hinzufügen',
     'key' => [
         'placeholder' => 'Schlüssel für werbefreie Suche eingeben',
@@ -10,9 +8,17 @@ return [
             'nokey' => 'Werbefreie Suche einrichten',
             'empty' => 'Token aufgebraucht. Jetzt aufladen.',
             'low' => 'Token bald aufgebraucht. Jetzt aufladen.',
-            'full' => 'Werbefreie Suche aktiviert.',
-        ],
+            'full' => 'Werbefreie Suche aktiviert.'
+        ]
     ],
     'placeholder' => 'MetaGer: Sicher suchen & finden',
     'searchbutton' => 'MetaGer-Suche',
+    'foki' => [
+        'web' => 'Web',
+        'bilder' => 'Bilder',
+        'nachrichten' => 'Nachrichten',
+        'science' => 'Wissenschaft',
+        'produkte' => 'Produkte',
+    ],
+    'adfree' => 'MetaGer werbefrei nutzen',
 ];
--- metager/lang/de/mg-story.php
+++ metager/lang/de/mg-story.php
@@ -1,17 +1,49 @@
 <?php
 return [
     'four-reasons' => 'Warum MetaGer?',
-    'ngo.image.alt' => 'Herz',
-    'eco.image.alt' => 'grünes Blatt',
-    'btn-about-us' => 'Über uns',
+    'privacy' => [
+        'image' => [
+            'alt' => 'Sicherheitsschloss',
+        ],
+        'title' => 'Garantierte Privatsphäre',
+        'p' => 'Mit uns behalten Sie die volle Kontrolle über Ihre Daten. Mit der Anonym-Öffnen-Funktion bleiben Sie auch beim Weitersurfen geschützt. <br> Wir tracken nicht. Wir speichern nicht.',
+    ],
+    'ngo' => [
+        'title' => 'Gemeinnütziger Verein',
+        'image' => [
+            'alt' => 'Herz',
+        ],
+        'p' => 'MetaGer wird getragen vom gemeinnützigen SUMA-EV, Verein für freien Wissenszugang. Unterstützen Sie uns, indem Sie spenden oder Mitglied werden. Mitglieder suchen auf MetaGer werbefrei.',
+    ],
+    'diversity' => [
+        'title' => 'Vielfältig & Frei',
+        'image' => [
+            'alt' => 'Regenbogen',
+        ],
+        'p' => 'MetaGer schützt gegen Zensur, indem es Ergebnisse vieler Suchmaschinen kombiniert. Unsere Algorithmen sind transparent und für jeden einsehbar.<br>Der Quellcode ist frei und Open Source.',
+    ],
+    'eco' => [
+        'title' => '100% Ökostrom',
+        'image' => [
+            'alt' => 'grünes Blatt',
+        ],
+        'p' => 'Wir achten auf die Nachhaltigkeit und den Ressourcenverbrauch unserer Dienste. Wir verwenden nur Strom aus regenerativen Energiequellen.<br>Vom Server bis zur Kaffeemaschine.',
+    ],
     'btn-data-protection' => 'Datenschutzerklärung',
-    'btn-SUMA-EV' => 'Der SUMA-EV',
+    'btn-SUMA-EV' => 'SUMA-EV',
     'btn-donate' => 'Spendenformular',
     'btn-member' => 'Aufnahmeantrag',
     'btn-member-advantage' => 'Mitgliedschaft',
     'btn-mg-code' => 'MetaGer-Quellcode',
     'btn-mg-algorithm' => 'Unser Algorithmus',
     'btn-more' => 'Mehr dazu',
-    'plugin.btn-app' => 'MetaGer-App',
-    'btn-about-us' => 'Über uns',
+    'plugin' => [
+        'title' => 'Jetzt MetaGer installieren',
+        'btn-add' => 'Plugin hinzufügen',
+        'btn-app' => 'MetaGer App',
+        'image' => [
+            'alt' => 'Metager Apps',
+        ],
+        'p' => 'Mit unserem Plugin können Sie MetaGer als Standardsuchmaschine festlegen. Und mit der App nutzen Sie MetaGer ganz bequem auf Ihrem Smartphone.',
+    ],
 ];

fc81a386f9090b05aed8ae2b273eab9e5136db60
Weblate <noreply@weblate.org>
2023-07-12 07:48:12 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/App
    Translate-URL: https://translate.metager.de/projects/metager/app/

--- metager/lang/de/app.php
+++ metager/lang/de/app.php
@@ -1,13 +1,8 @@
 <?php
 return [
-    'disclaimer.1' => 'Aktuell können wir unsere Apps nur für Android Geräte zur Verfügung stellen.',
     'metager.1' => 'Diese App bringt die volle Power unserer Suchmaschine auf ihr Smartphone. Durchsuchen Sie das Internet unter Wahrung ihrer Privatsphäre mit nur einem Fingerwisch.',
-    'metager.playstore' => 'Google Playstore',
     'metager.manuell' => 'Manuelle Installation',
-    'maps.playstore' => 'Google Playstore',
     'maps.2' => 'Dadurch läuft der Karten- und Navigationsdienst auch auf Ihrem Smartphone optimal und schnell. Die App steigert die Performance im Vergleich zur Verwendung im mobilen Browser und bietet einige weitere Vorteile. Probieren Sie es aus!',
-    'maps.list.1' => 'Zugriff auf Positionsdaten  => Falls GPS an ihrem Mobiltelefon aktiviert ist, können wir dadurch Ihre Suchergebnisse verbessern. Außerdem schalten Sie damit die Funktion Schritt-für-Schritt Navigation frei. <b>Selbstverständlich werden diese Daten nirgendwo gespeichert und erst recht nicht an Dritte weiter gegeben!</b>',
     'head.4' => 'Installation',
-    'installation.2' => 'Wenn Sie unsere App manuell (ohne App-Store) installieren möchten, gehen Sie wie folgt vor:',
     'list.2' => 'Wählen Sie auf dieser Seite den Link "Manuelle Installation" aus, um die Installationsdatei auf ihr Mobilgerät herunter zu laden.',
 ];

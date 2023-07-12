b42d24300661c087c529fb68d37ff5f3276ccb0d
Weblate <noreply@weblate.org>
2023-07-12 07:47:48 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Mobile-Opera
    Translate-URL: https://translate.metager.de/projects/metager/mobile-opera/

--- metager/lang/de/plugin-mobile/mobile-opera.php
+++ metager/lang/de/plugin-mobile/mobile-opera.php
@@ -1,5 +1,4 @@
 <?php
 return [
-    'default-search-v60.2' => 'Wählen Sie im erscheinenden Menü "Suchmaschine hinzufügen...".',
     'default-search-v60.4' => 'Nun können Sie in einem neuen Tab durch das Tippen auf das Logo Ihrer Standardsuchmaschine MetaGer auswählen',
 ];

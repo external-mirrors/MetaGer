12dab3b151058b5e1ed31766141c644e6bda81ad
Dominik Hebeler <dominik@hebeler.club>
2023-07-12 14:57:53 +0200

    fix lang file

--- metager/lang/de/team.php
+++ metager/lang/de/team.php
@@ -1,6 +1,24 @@
 <?php
+
 return [
-    'role.2' => 'Jugendschutzbeauftragte',
-    'role.7' => 'Sekretariat',
-    'contact.2' => 'Sollten Sie jemals eine E-Mail mit seltsamem Inhalt bekommen, auf der unsere Namen und/oder unsere E-Mail Adressen als Absender genannt sind, lesen Sie bitte hier weiter: <a href="https://metager.de/wsb/fakemail/">https://metager.de/wsb/fakemail/</a>',
+    'role' => [
+        '0' => 'geschäftsführender Vorstand',
+        '1' => [
+            '0' => 'Vorstand',
+            '1' => 'Vorständin',
+        ],
+        '2' => 'Jugendschutzbeauftragte',
+        '3' => 'Pressereferentin',
+        '5' => 'Programmierer',
+        '6' => 'Programmiererin',
+        '7' => 'Sekretariat',
+        '8' => 'Founder',
+        '9' => 'Praktikant',
+    ],
+
+    'contact' => [
+        '1' => 'Fragen/Probleme zu MetaGer etc. bitte das <a href="/kontakt/">Kontaktformular mit Verschlüsselung</a> benutzten.',
+        '2' => 'Sollten Sie jemals eine E-Mail mit seltsamem Inhalt bekommen, auf der unsere Namen und/oder unsere E-Mail Adressen als Absender genannt sind, lesen Sie bitte hier weiter: <a href="https://metager.de/wsb/fakemail/">https://metager.de/wsb/fakemail/</a>',
+        '3' => 'Nur in begründeten Ausnahmefällen, wenn Sie bestimmte Personen direkt erreichen wollen, sollten Sie an diese mailen. Denn Team-Mitglieder können Urlaub haben, krank sein usw.',
+    ],
 ];

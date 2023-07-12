c199bf3d029b2ae06bd6d6ab31aee55ca4f0b9db
Weblate <noreply@weblate.org>
2023-07-12 07:47:38 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Desktop-Firefox
    Translate-URL: https://translate.metager.de/projects/metager/desktop-firefox/

--- metager/lang/de/plugin-desktop/desktop-firefox.php
+++ metager/lang/de/plugin-desktop/desktop-firefox.php
@@ -1,14 +1,9 @@
 <?php
 return [
-    'default-search-v52.2' => 'Wählen Sie im erscheinenden Menü das MetaGer-Logo mit dem Plussymbol.',
     'default-search-v52.4' => 'Im Dropdownmenü unter "Standardsuchmaschine" steht Ihnen nun MetaGer zur Verfügung.',
-    'default-search-v57.2' => 'Wählen Sie in den erscheinenden Einstellungen "Suchleiste zur Symbolleiste hinzufügen".',
     'default-search-v57.4' => 'Klicken Sie im erscheinenden Menü auf das MetaGer-Logo mit dem Plussymbol.',
     //Instructions for Firefox version >= 61
     'plugin' => 'Das Plugin für Firefox können Sie <a href="https://addons.mozilla.org/de/firefox/addon/metager-suche/?src=search" target="_blank">hier</a> herunterladen.',
-    'default-search-v61.2' => 'Geben Sie "about:preferences#search" in der Adressleiste ein, um in die Sucheinstellungen zu gelangen. Im Bereich "Standardsuchmaschine" wählen Sie MetaGer aus.',
     'default-page-v52.2' => 'Tragen Sie im Feld "Startseite" ":link" ein.',
-    'default-page-v61.2' => 'Wählen Sie auf der linken Seite die Kategorie "Startseite". ',
     'default-page-v61.4' => 'Tragen Sie in das erscheinende Textfeld ":link" ein.',
-    'default-search-v89.2' => 'Klicken Sie oben rechts im Browser auf <img class= "mg-icon" src="/img/menu.svg"> und öffnen Sie die "Einstellungen".',
 ];

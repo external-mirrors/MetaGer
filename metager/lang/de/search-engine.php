6773464fbffed84b0da59c83ab94e52555f53657
Weblate <noreply@weblate.org>
2023-07-12 07:48:12 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Search-Engine
    Translate-URL: https://translate.metager.de/projects/metager/search-engine/

--- metager/lang/de/search-engine.php
+++ metager/lang/de/search-engine.php
@@ -1,16 +1,11 @@
 <?php
 return [
-    'head' => [
-        '2' => 'MetaGer nutzt mehrere Indexe',
-    ],
     'text' => [
         '1' => [
             '1' => 'MetaGer ist eine <a href=":transparenz">Metasuchmaschine</a>. Deshalb nutzen wir die Indexe verschiedener anderer Suchmaschinen. Für jede dieser Suchmaschinen haben wir einen kurzen Steckbrief mit den wichtigsten Eckdaten erstellt. Da wir diese Übersicht nicht ständig aktualisieren, sind alle Angaben ohne Gewähr.',
-            '3' => 'Wir können leider nur allgemeine Websuchen oder Suchen im Bereich unserer Foki einbinden. Wenn Sie also Web-, Bilder-, Nachrichten- oder Produktsuchen anbieten, ist eine Einbindung möglich. Für weitere Informationen wenden Sie sich gerne an <a href=":contact">uns</a>.',
         ],
         '2' => [
             '2' => 'Verwendeter Index: ',
-            '4' => 'Hauptsitz: ',
             '6' => 'Geschätzte Seiten im Index: ',
         ],
     ],

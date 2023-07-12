0fa7bf21c3378d14354ada50dac9c9388178d5e4
Weblate <noreply@weblate.org>
2023-07-12 07:48:06 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Team
    Translate-URL: https://translate.metager.de/projects/metager/team/

--- metager/lang/de/team.php
+++ metager/lang/de/team.php
@@ -1,9 +1,6 @@
 <?php
 return [
-    'role.1.0' => 'Vorstand',
     'role.2' => 'Jugendschutzbeauftragte',
-    'role.5' => 'Programmierer',
     'role.7' => 'Sekretariat',
-    'role.9' => 'Praktikant',
     'contact.2' => 'Sollten Sie jemals eine E-Mail mit seltsamem Inhalt bekommen, auf der unsere Namen und/oder unsere E-Mail Adressen als Absender genannt sind, lesen Sie bitte hier weiter: <a href="https://metager.de/wsb/fakemail/">https://metager.de/wsb/fakemail/</a>',
 ];

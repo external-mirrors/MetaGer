db5b47b05e37bf4b866c7c9a9393df9219356dde
Weblate <noreply@weblate.org>
2023-07-12 07:48:09 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Languages
    Translate-URL: https://translate.metager.de/projects/metager/languages/

--- metager/lang/de/languages.php
+++ metager/lang/de/languages.php
@@ -1,9 +1,7 @@
 <?php
 return [
     'header' => 'MetaGer - Übersetzungen',
-    'beschreibung.2' => 'Du kannst uns unterstützen, indem du dir die Referenztexte in den folgenden Spalten (rechts daneben) ansiehst und nach Möglichkeit eine Übersetzung in der gesuchten Sprache in das Textfeld einträgst.',
     'progress' => ':uebersetzteTexte/:textCount Texten übersetzt (:percentage%)',
-    'hinweis.2' => '<b>Hinweis</b>: Die übermittelten Texte werden von diesem Tool erst erkannt, sobald diese von uns gesichtet und eingefügt wurden. Wenn du deine Arbeit sichern möchtest um diese zu einem späteren Zeitpunkt fortzusetzen (auch wenn wir deine bisherige Arbeit noch nicht übernehmen konnten), so reicht es vollkommen, den aktuellen Link aus deiner Browserleiste zu kopieren und zu einem späteren Zeitpunkt wieder aufzurufen.',
     'email' => '<b>E-Mail-Adresse für Rückfragen</b> (optional):',
     'synoptic' => [
         'hinweis' => 'Datei wurde zuletzt bearbeitet.',

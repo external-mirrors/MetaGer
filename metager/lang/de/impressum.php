911c8d6e5d7b7f55523fa1f04ff5c9a84ab16478
Weblate <noreply@weblate.org>
2023-07-12 07:48:23 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Impressum
    Translate-URL: https://translate.metager.de/projects/metager/impressum/

--- metager/lang/de/impressum.php
+++ metager/lang/de/impressum.php
@@ -1,10 +1,8 @@
 <?php
 return [
     'title' => 'Impressum',
-    'info.1' => 'Wikipedia-Eintrag zum <a href="http://de.wikipedia.org/wiki/Suma_e.V." target="_blank" rel="noopener">SUMA-EV</a>',
     'info.3' => 'Kontakt:
 Tel.: +4951134000070
 EMail: <a href="/kontakt">Kontaktformular</a>',
-    'info.6' => 'Jugendschutzbeauftragte: Manuela Branz <a href="mailto:jugendschutz@metager.de">jugendschutz@metager.de</a>',
     'info.9' => 'Haftungshinweis:',
 ];

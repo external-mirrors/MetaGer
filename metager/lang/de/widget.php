12b9cf6e2805950c85e3da5232c60bb98157358e
Weblate <noreply@weblate.org>
2023-07-12 07:48:32 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Widget
    Translate-URL: https://translate.metager.de/projects/metager/widget/

--- metager/lang/de/widget.php
+++ metager/lang/de/widget.php
@@ -1,6 +1,5 @@
 <?php
 return [
     'head' => 'MetaGer Widget',
-    'body.2' => 'Suche im Web',
     'body.4' => 'Hinweis: Sie dürfen das Widget nicht verwenden, wenn Sie auf Ihrer Seite damit den Eindruck zu erwecken versuchen, MetaGer sei Ihre Dienstleistung oder wenn der Eindruck erweckt werden sollte, Ihre Seiten seien die wahren MetaGer-Seiten (das ist alles schon vorgekommen). Insbesondere ist es aus diesem Grund nicht erlaubt, unser Logo zu entfernen.',
 ];

55cb5b0ade5e80d36efcd7e79ff3fbd31f098511
Dominik Hebeler <dominik@hebeler.club>
2023-07-12 14:00:07 +0200

    fixed lang file

--- metager/lang/en/lang-selector.php
+++ metager/lang/en/lang-selector.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'lang'        => [
+    'lang' => [
         'de' => 'German',
         'en' => 'English',
         'es' => 'Spanish',
@@ -13,12 +13,12 @@ return [
         'pl' => 'Polish',
         'fr' => 'French',
     ],
-    'h1'          => [
-        1   => 'Language selection',
+    'h1' => [
+        '1' => 'Language selection',
     ],
     'description' => 'Below is a list of all languages/regions currently supported by MetaGer search. Your search results will be optimized for the selected region. We use a cookie to save a changed setting.',
-    'detection'   => [
-        'title'       => 'What is the default language setting?',
+    'detection' => [
+        'title' => 'What is the default language setting?',
         'description' => 'We use language-specific URLs that can be used directly. If no language is defined in the URL, we use the language setting stored in your browser settings to preselect the most suitable region for you. In most cases, this will automatically give you search results for the correct region. Alternatively, you can change the above setting.',
     ],
 ];
--- metager/lang/es/lang-selector.php
+++ metager/lang/es/lang-selector.php
@@ -1,9 +1,9 @@
 <?php
 return [
-    'h1'          => [
-        1   => 'Selección de idiomas',
+    'h1' => [
+        '1' => 'Selección de idiomas',
     ],
-    'lang'        => [
+    'lang' => [
         'de' => 'Alemán',
         'en' => 'Inglés',
         'es' => 'Español',
@@ -17,8 +17,8 @@ return [
         'fr' => 'Francés',
     ],
     'description' => 'A continuación encontrará una lista de todos los idiomas/regiones actualmente soportados por MetaGer Search. Los resultados de su búsqueda se optimizarán para la región seleccionada. Utilizamos una cookie para guardar la configuración modificada.',
-    'detection'   => [
-        'title'       => '¿Qué configuración por defecto se utiliza para el idioma?',
+    'detection' => [
+        'title' => '¿Qué configuración por defecto se utiliza para el idioma?',
         'description' => 'Utilizamos URLs específicas para cada idioma que pueden utilizarse directamente. Si no se define ningún idioma en la URL, utilizamos la configuración de idioma almacenada en los ajustes de su navegador para preseleccionar la región más adecuada para usted. En la mayoría de los casos, recibirá automáticamente resultados de búsqueda para la región correcta. También puede cambiar esta configuración. En ese caso, no será necesaria una cookie.',
     ],
 ];

6a198ba39cf6275415deabba8b43f311437c55cf
Weblate <noreply@weblate.org>
2023-07-12 07:48:31 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Desktop-Opera
    Translate-URL: https://translate.metager.de/projects/metager/desktop-opera/

--- metager/lang/de/plugin-desktop/desktop-opera.php
+++ metager/lang/de/plugin-desktop/desktop-opera.php
@@ -1,7 +1,5 @@
 <?php
 return [
-    'default-search-v36.2' => 'Wählen Sie im Kontextmenü "Suchmaschine erstellen...".',
     //link to desktop-unable
     'default-page-v36.1' => 'Klicken Sie oben links im Browser auf das Opera-Symbol bzw. "Menü" und öffnen Sie die "Einstellungen".',
-    'default-page-v36.3' => 'Tragen Sie ":link" als URL bei "Neue Seite hinzufügen" ein.',
 ];

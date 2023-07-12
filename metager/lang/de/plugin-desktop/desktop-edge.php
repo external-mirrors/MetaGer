9648a799efd355089ffb4c54ca274807a2abc9d0
Weblate <noreply@weblate.org>
2023-07-12 07:47:49 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Desktop-Edge
    Translate-URL: https://translate.metager.de/projects/metager/desktop-edge/

--- metager/lang/de/plugin-desktop/desktop-edge.php
+++ metager/lang/de/plugin-desktop/desktop-edge.php
@@ -1,11 +1,7 @@
 <?php
 return [
-    'default-search-v15.2' => 'Scrollen Sie nach unten und klicken auf "Erweiterte Einstellungen".',
     'default-search-v15.4' => 'Wählen Sie "MetaGer: Sicher suchen & finden..." und klicken Sie auf "Als Standard"',
-    'default-search-v18.2' => 'Wählen Sie den Reiter "Erweitert".',
     'default-search-v18.4' => 'Wählen Sie "MetaGer: Sicher suchen & finden..." und klicken Sie auf "Als Standard".',
-    'default-search-v80.2' => 'Klicken Sie neben dem Eintrag von MetaGer auf "<img class= "mg-icon" src="/img/ellipsis-horizontal.svg">" und wählen Sie "Als Standard".',
     'default-page-v15.2' => 'Wählen Sie unter "Startseite festlegen" bzw. "Edge öffnen mit" "eine bestimmte Seite" und tragen dort ":link" ein.',
-    'default-page-v80.1' => 'Geben Sie "edge://settings/onStartup" in die Adressleiste ein, um in die Einstellungen "Beim Start" zu gelangen.',
     'default-page-v80.3' => 'Hinweis: Alle hier sichtbaren Webseiten werden nun beim Start des Browsers geöffnet. Sie können Einträge entfernen, indem Sie mit der Maus darüber fahren und rechts auf "<img class= "mg-icon" src="/img/ellipsis-horizontal.svg">" klicken.',
 ];

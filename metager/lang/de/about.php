ad7e6cee3d4c58df45e22a8d2c6894e291196cbb
Weblate <noreply@weblate.org>
2023-07-12 07:48:28 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/About
    Translate-URL: https://translate.metager.de/projects/metager/about/

--- metager/lang/de/about.php
+++ metager/lang/de/about.php
@@ -1,10 +1,7 @@
 <?php
 return [
-    'text.4' => 'Funktion der Suche in der Suche',
     'timeline.2.2' => 'MetaGer entsteht als Forschungsprojekt der Universität Hannover und des regionalen Rechenzentrums Niedersachsen.',
-    'timeline.6.2' => 'Erstmals wird auch die Oberfläche von MetaGer in englischer Sprache angeboten.',
     'timeline.10.2' => 'Durch umfangreiche Modernisierungen im Kern von MetaGer sinkt die durchschnittliche Antwortzeit der Suchmaschine auf unter 1,3 Sekunden. Im Laufe der nächsten Jahre sinkt diese weiter auf fast die Hälfte.',
-    'timeline.14.2' => 'Durch umfangreiche Feinabstimmungen und eigene Ergebnisquellen wird die englische MetaGer-Version stark verbessert.',
     'timeline.18.2' => 'Die Hilfe von MetaGer erhält eine neue Übersichtsseite. Diese strukturiert und visualisiert die Hilfeseiten durch einfach zu erkennende Symbole.',
     'head' => [
         1 => 'Über uns',

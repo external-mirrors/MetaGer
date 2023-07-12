e1aece63e3526f3b2ab8f254232afef442ce1ae8
Weblate <noreply@weblate.org>
2023-07-12 07:47:42 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Desktop-Chrome
    Translate-URL: https://translate.metager.de/projects/metager/desktop-chrome/

--- metager/lang/de/plugin-desktop/desktop-chrome.php
+++ metager/lang/de/plugin-desktop/desktop-chrome.php
@@ -1,10 +1,7 @@
 <?php
 return [
-    'default-search-v49.2' => 'Im Bereich "Suche" klicken Sie auf "Suchmaschinen verwalten...".',
     //Instructions for Chrome version 53-58
     'default-search-v53.1' => 'Klicken Sie in Ihrem Chrome oben rechts auf <img class= "mg-icon" src="/img/ellipsis.svg"> und im folgenden Menü auf "Einstellungen", um die Einstellungen Ihres Chromes zu öffnen.',
-    'default-search-v53.3' => 'In der unteren Hälfte des erscheinenden Menüs fahren Sie mit der Maus über den MetaGer-Eintrag. Auf der rechten Seite des Eintrags klicken sie auf die erscheinende blaue "Als Standard"-Schaltfläche',
     'default-search-v59.2' => 'Im Bereich "Suchmaschine" klicken Sie auf "Suchmaschinen verwalten..."',
-    'default-page-v49.1' => 'Klicken Sie oben rechts im Browser auf <img class= "mg-icon" src="/img/ellipsis.svg"> und öffnen Sie die "Einstellungen".',
     'default-page-v49.3' => 'Tragen Sie ":link" als URL bei "Neue Seite hinzufügen" ein.',
 ];

ecf4e61835357baba46ea3c9a09eaceb3f9ef4b8
Weblate <noreply@weblate.org>
2023-07-12 07:48:06 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Datenschutz
    Translate-URL: https://translate.metager.de/projects/metager/datenschutz/

--- metager/lang/de/datenschutz.php
+++ metager/lang/de/datenschutz.php
@@ -1,12 +1,9 @@
 <?php
 return [
     'head' => 'Datenschutz und Privatsphäre',
-    'general.3' => 'Nur Software, dessen Quellcode öffentlich zugänglich ist, kann von jedem überprüft werden.  In allen anderen Fällen müssen User <em>glauben</em>, was Betreiber oder "Gütesiegel"-Verkäufer behaupten.  MetaGer ist Freie Software unter der GNU-AGPLv3-Lizenz (siehe dazu die <a href="http://heise.de/-3295586" target="_blank" rel="noopener"> Nachricht im Heise-Ticker</a>). Der MetaGer-Quellcode steht Ihnen unter folgendem Link zur Verfügung: <a href="https://gitlab.metager.de/open-source/MetaGer" target="_blank" rel="noopener">https://gitlab.metager.de/open-source/MetaGer</a><br />Sie müssen uns nichts glauben: Sie können es selber kontrollieren!',
     'policy.2' => 'Wir speichern weder Ihre IP-Adresse, noch den
 <a href="http://www.heise.de/security/meldung/Fingerprinting-Viele-Browser-sind-ohne-Cookies-identifizierbar-1982976.html" target="_blank" rel="noopener">"Fingerabdruck" Ihres Browsers</a> (Ausnahme: Kurzfristige Speicherung gegen Hacking- und Bot-Attacken).',
-    'policy.6' => 'Die Datenübertragung von MetaGer erfolgt ausschlie&szlig;lich automatisch verschlüsselt über das https-Protokoll.',
     'policy.9' => 'Da der Zugang über das TOR-Netzwerk vielen Nutzern kompliziert erscheint, manchmal auch langsam ist, haben wir einen weiteren Weg implementiert, die Ergebnis-Webseiten ebenfalls anonym erreichen zu können: durch Anklicken des Links "anonym öffnen". Dadurch sind Ihre persönlichen Daten beim Klick auf MetaGer-Ergebnisse und sogar bei allen Folge-Klicks danach geschützt.',
-    'policy.13' => 'MetaGer wird von der deutschen gemeinnützigen Organisation <a href="http://suma-ev.de/" target="_blank" rel="noopener">SUMA-EV</a> in Zusammenarbeit mit der <a href="http://www.uni-hannover.de/" target="_blank" rel="noopener">Leibniz Universität Hannover</a> betrieben und weiterentwickelt.',
     'policy.18' => 'Wir schützen nicht nur Ihre Daten, sondern auch die Umwelt: Unsere Server werden ausschließlich mit Ökostrom betrieben.',
     'twitter' => 'Was andere über unser Privacy-Konzept auf Twitter sagen:',
 ];

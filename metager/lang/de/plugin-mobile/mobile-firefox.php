51ac5b913e6dce20dc1b18e441d6a7caadcd7e7c
Weblate <noreply@weblate.org>
2023-07-12 07:47:46 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Mobile-Firefox
    Translate-URL: https://translate.metager.de/projects/metager/mobile-firefox/

--- metager/lang/de/plugin-mobile/mobile-firefox.php
+++ metager/lang/de/plugin-mobile/mobile-firefox.php
@@ -1,10 +1,7 @@
 <?php
 return [
     'search-string' => 'Such-String:',
-    'default-search-vlt80.2' => 'Im geöffneten Kontextmenü können Sie "Suchmaschine hinzufügen" auswählen',
     //Instructions for Firefox Mobile version >= 80
     'default-search-v80.1' => 'Tippen Sie in Ihrem Browser unten rechts auf "<img class= "mg-icon" src="/img/ellipsis.svg">", um das Browsermenü zu öffnen.',
-    'default-search-v80.3' => 'In dem dann erscheinenden Menü wählen Sie in der Kategorie "Allgemein" den Menüpunkt "Suchen".',
     'default-search-v80.5' => 'Wählen Sie "Andere" aus und tragen darunter den Namen (MetaGer) und den Such-String ein.',
-    'default-search-v80.7' => 'Tippen Sie in der dann erscheinenden Liste auf MetaGer.',
 ];

db48f1c13ea5cb7d56eb759e4cbad31bb661f3f3
Weblate <noreply@weblate.org>
2023-07-12 07:47:59 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Staticpages
    Translate-URL: https://translate.metager.de/projects/metager/staticpages/

--- metager/lang/de/staticPages.php
+++ metager/lang/de/staticPages.php
@@ -1,5 +1,4 @@
 <?php
 return [
     'opensearch' => 'MetaGer: Sicher suchen & finden',
-    'meta.Keywords' => 'Internetsuche, privatsphäre, privacy, Suchmaschine, Datenschutz, Anonproxy, anonym suchen, Bildersuche, Suchmaschine, anonym, MetaGer, metager, metager.de',
 ];

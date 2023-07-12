a08d4ef305a819e36d350e8f72d4825ef1e7648d
Dominik Hebeler <dominik@hebeler.club>
2023-07-12 12:52:03 +0200

    removed unused translation component

--- metager/lang/de/datenschutz.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'head' => 'Datenschutz und Privatsphäre',
-    'policy.2' => 'Wir speichern weder Ihre IP-Adresse, noch den
-<a href="http://www.heise.de/security/meldung/Fingerprinting-Viele-Browser-sind-ohne-Cookies-identifizierbar-1982976.html" target="_blank" rel="noopener">"Fingerabdruck" Ihres Browsers</a> (Ausnahme: Kurzfristige Speicherung gegen Hacking- und Bot-Attacken).',
-    'policy.9' => 'Da der Zugang über das TOR-Netzwerk vielen Nutzern kompliziert erscheint, manchmal auch langsam ist, haben wir einen weiteren Weg implementiert, die Ergebnis-Webseiten ebenfalls anonym erreichen zu können: durch Anklicken des Links "anonym öffnen". Dadurch sind Ihre persönlichen Daten beim Klick auf MetaGer-Ergebnisse und sogar bei allen Folge-Klicks danach geschützt.',
-    'policy.18' => 'Wir schützen nicht nur Ihre Daten, sondern auch die Umwelt: Unsere Server werden ausschließlich mit Ökostrom betrieben.',
-    'twitter' => 'Was andere über unser Privacy-Konzept auf Twitter sagen:',
-];

baa64481e291e10e105e9985b0b021011c6d91ec
Weblate <noreply@weblate.org>
2023-07-12 07:48:34 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Icons
    Translate-URL: https://translate.metager.de/projects/metager/icons/

--- metager/lang/de/icons.php
+++ metager/lang/de/icons.php
@@ -1,9 +1,6 @@
 <?php
 return [
-    'icon-lupe.alt' => 'Suchen',
     'menu.alt' => 'Menü',
-    'ellipsis.alt' => 'Weitere Optionen',
     'home.alt' => 'Haus',
-    'x.alt' => 'Schließen',
     'Icon-settings' => 'Zahnrad',
 ];

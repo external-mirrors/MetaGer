117b25aab8762f77c4276fe3f5d549563ffd50fd
Weblate <noreply@weblate.org>
2023-07-12 07:48:32 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Mobile-Chrome
    Translate-URL: https://translate.metager.de/projects/metager/mobile-chrome/

--- metager/lang/de/plugin-mobile/mobile-chrome.php
+++ metager/lang/de/plugin-mobile/mobile-chrome.php
@@ -1,7 +1,5 @@
 <?php
 return [
-    'default-search-v83.2' => 'Wählen Sie den Menüpunkt "Einstellungen".',
     'default-search-v83.4' => 'Tippen Sie in der Kategorie "Kürzlich besucht" auf MetaGer.',
-    'default-page-v83.1' => 'Tippen Sie in Ihrem Browser oben rechts auf "<img class= "mg-icon" src="/img/ellipsis.svg">", um das Browsermenü zu öffnen.',
     'default-page-v83.3' => 'In dem dann erscheinenden Menü wählen Sie in der Kategorie "Erweitert" den Menüpunkt "Startseite".',
 ];

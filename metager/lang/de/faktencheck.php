db360e4d117c28968341be438241af34bcde6d88
Weblate <noreply@weblate.org>
2023-07-12 07:48:11 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Faktencheck
    Translate-URL: https://translate.metager.de/projects/metager/faktencheck/

--- metager/lang/de/faktencheck.php
+++ metager/lang/de/faktencheck.php
@@ -1,17 +1,10 @@
 <?php
 return [
-    'paragraph.1' => 'Fake News werden oft in Form eines journalistischen Beitrages präsentiert. Die Aussage des Artikels oder die Darstellung entspricht jedoch nicht der Faktenlage. Oft werden Aussagen bei Fake News bewusst erfunden oder verfälscht um politische oder kommerzielle Ziele zu erreichen. (siehe <a href="https://link.springer.com/book/10.1007/978-3-662-58695-2"> Appel 2020</a>) Was kann man also tun, um herauszufinden, ob es sich um Fake-News handelt?',
     'list.1.0' => 'Webseiten',
-    'list.1.2' => '<strong>Autor</strong>: Ist ein Autor genannt? Bei den meisten guten Quellen ist ein Autor benannt. Auch dies ist eine wichtige Angabe. Wenn ein Autor angegeben ist, ist es oft sinnvoll zu schauen, ob diese Person öfter etwas zu diesen Themen publiziert.  Zusätzlich kann man schauen, ob diese Person in dem (Fach)gebiet bekannt ist oder es z.B. Wikipedia-Einträge zu dieser gibt.',
     'list.1.4' => '<strong>Impressum</strong>: Wenn es ein Impressum gibt: Welche Personen (Namen, Firma) werden dort angegeben und was kann über diese herausgefunden werden?',
-    'list.2.1' => 'Ist ein Autor mit einem Namen genannt, der real erscheint? Ist dies nicht der Fall, ist die Nachricht wahrscheinlich fragwürdig. Hinzu kommt die Frage, ob es zusätzliche Informationen zu dem Namen gibt.',
     'list.2.3' => 'Bei Facebook gibt es zusätzlich die Möglichkeit zu schauen, ob diese Person verifiziert ist.(<a href="https://www.facebook.com/help/196050490547892" target="_blank" rel="noopener">https://www.facebook.com/help/196050490547892</a>)',
-    'list.2.5' => 'Wie viele Abonenten oder Freunde hat dieses Profil? Was kann über die Namen dieser Personen herausgefunden werden?',
     'list.3.1' => 'Nicht alles, was bei Wikipedia steht, stimmt auch. Um hier zu kontrollieren, wie akkurat die Informationen sind, kann man folgenes tun:',
-    'list.3.3' => 'Kann zu diesen Autoren etwas herausgefunden werden?',
     'list.3.5' => 'Überprüfen, welche Quellen im Wikipedia-Eintrag angegeben wurden. Diese können auch oft als weitere Informationsquelle dienen.',
-    'list.4.1' => 'E-Mails zu fälschen ist sehr einfach. Es gibt somit sehr viele Fakemails; wie kann man dies nun überprüfen?',
     'list.4.3' => 'Signierte und bestenfalls auch verschlüsselte E-Mail nutzen. Auch dies ist nicht so einfach. Weitere Informationen dazu können Sie hier finden: <a href="https://www.heise.de/ct/artikel/Ausgebootet-289538.html" target="_blank" rel="noopener">https://www.heise.de/ct/artikel/Ausgebootet-289538.html</a>',
-    'list.5.1' => 'Hintergrund genau ansehen. Landschaft, Gebäude, Autos und Kennzeichen, Kleidung, Personen. Ist das stimmig, passt es ggf. mit dem zugehörigen Text zusammen?',
     'list.5.3' => 'Kann man mit Grafikprogrammen Metadaten der Bilder auslesen? Passen diese Metadaten mit dem Bildinhalt zusammen?',
 ];

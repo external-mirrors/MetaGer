634e75d1db9cd2b3a4b16b9539a730697a7bb697
Dominik Hebeler <dominik@hebeler.club>
2023-07-12 14:13:39 +0200

    fixed lang file

--- metager/lang/de/metaGerSearch.php
+++ metager/lang/de/metaGerSearch.php
@@ -1,4 +1,20 @@
 <?php
+
 return [
-    'quicktips.bang.title' => '!bang-Weiterleitung',
+    'quicktips' => [
+        'wikipedia' => [
+            'adress' => 'aus <a href="https://de.wikipedia.org" target="_blank" rel="noopener">Wikipedia, der freien Enzyklopädie</a>',
+        ],
+        'dictcc' => [
+            'adress' => 'aus <a href="https://www.dict.cc/" target="_blank" rel="noopener">dict.cc</a>',
+        ],
+        'tips' => [
+            'title' => 'Wussten Sie schon?',
+        ],
+        'bang' => [
+            'title' => '!bang-Weiterleitung',
+            'buttonlabel' => 'Weitersuchen auf',
+            'from' => 'von',
+        ],
+    ],
 ];

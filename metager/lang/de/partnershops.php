9072f3d2caf4a05e6d74c2fb3934aa4a033bf541
Weblate <noreply@weblate.org>
2023-07-12 07:48:08 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Partnershops
    Translate-URL: https://translate.metager.de/projects/metager/partnershops/

--- metager/lang/de/partnershops.php
+++ metager/lang/de/partnershops.php
@@ -1,5 +1,4 @@
 <?php
 return [
     'heading' => 'MetaGer-Partnershop',
-    'paragraph.2' => 'Wenn der Link angeklickt wird, erhalten wir einen geringen Betrag zur Finanzierung von MetaGer. Beim Ergebnis wird das zugehörige Firmenlogo angezeigt. Auch daran erkennen Sie immer unsere Partnershops. Sie können diese Links zusammen mit jeglicher Werbung mit einem <a href=":link">MetaGer Schlüssel</a> entfernen.',
 ];

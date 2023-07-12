4a3bdf6022f1d87d982c26e27a770b31b1970e64
Weblate <noreply@weblate.org>
2023-07-12 07:47:45 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Mobile-Edge
    Translate-URL: https://translate.metager.de/projects/metager/mobile-edge/

--- metager/lang/de/plugin-mobile/mobile-edge.php
+++ metager/lang/de/plugin-mobile/mobile-edge.php
@@ -1,7 +1,5 @@
 <?php
 return [
-    'default-search-v45.2' => 'Wählen Sie den Menüpunkt "Einstellungen" auf der rechten Seite des erscheinenden Menüs.',
     'default-search-v45.4' => 'Tippen Sie auf "Standardsuchmaschine" und wählen Sie in der Kategorie "Kürzlich besucht" MetaGer aus.',
-    'default-page-v45.1' => 'Tippen Sie <a href="/" target="_blank" rel="noopener">hier</a>, um MetaGer in einem neuen Tab zu öffnen.',
     'default-page-v45.3' => 'Tippen Sie in der Kategorie "Grundlegend" auf "Startseite".',
 ];

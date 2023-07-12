c16539ab4e849cb58ac3d2e29727fd8285eaa20a
Dominik Hebeler <dominik@hebeler.club>
2023-07-12 14:46:29 +0200

    fix lang file

--- metager/lang/de/result.php
+++ metager/lang/de/result.php
@@ -1,12 +1,24 @@
 <?php
+
 return [
-    'options.2' => ':host ausblenden',
-    'options.6' => 'IN NEUEM TAB ÖFFNEN',
+    'options' => [
+        'headline' => 'Optionen',
+        'savetab' => 'Ergebnis speichern',
+        '1' => 'Auf dieser Domain neu suchen',
+        '2' => ':host ausblenden',
+        '3' => '*.:domain ausblenden',
+        '4' => 'Partner',
+        '5' => 'ANONYM ÖFFNEN',
+        '6' => 'IN NEUEM TAB ÖFFNEN',
+        '7' => 'ÖFFNEN',
+        '8' => 'WERBUNG ENTFERNEN',
+        'direct' => 'Direkt öffnen',
+    ],
     'gefVon' => 'von',
     'advertisement' => 'Anzeige',
     'providers' => 'Anbietern',
     'proxytext' => 'Der Link wird anonymisiert geöffnet. Ihre Daten werden nicht zum Zielserver übertragen. Möglicherweise funktionieren manche Webseiten nicht wie gewohnt.',
     'metagerkeytext' => 'MetaGer werbefrei nutzen',
     'news' => 'Relevante Nachrichten',
-    'videos' => "Videos",
+    'videos' => "Videos"
 ];

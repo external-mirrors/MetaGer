9669b35be622aac63f5629ea47c2877578cdac80
Weblate <noreply@weblate.org>
2023-07-12 07:47:50 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Help
    Translate-URL: https://translate.metager.de/projects/metager/help/

--- metager/lang/de/help/help.php
+++ metager/lang/de/help/help.php
@@ -2,14 +2,9 @@
 return [
     'achtung' => 'Achtung, da unsere Suchmaschine ständig weiterentwickelt und verbessert wird, kann es dazu kommen, dass sich immer wieder Änderungen an Aufbau und Funktion ergeben. Wir versuchen zwar die Hilfe schnellstmöglich den Änderungen entsprechend anzupassen, können jedoch nicht ausschließen, dass es zu temporären Unstimmigkeiten in Teilen der Erklärungen kommt.',
     'title' => 'MetaGer - Hilfe',
-    'tableofcontents.1.0' => 'Die Hauptseiten',
     'tableofcontents.1.2' => 'Das Suchfeld',
-    'tableofcontents.1.4' => 'Einstellungen',
     'tableofcontents.2.1' => 'Suchfunktionen',
-    'tableofcontents.2.3' => 'Suche in der Suche',
     'tableofcontents.4.0' => 'Weitere Dienste um die Suche herum',
-    'tableofcontents.4.3' => 'Suchwortassoziator',
     'tableofcontents.4.5' => 'MetaGer Maps',
-    'tableofcontents.3.2' => 'Cookies & Co.',
     'tableofcontents.3.4' => 'MetaGer-Proxy',
 ];

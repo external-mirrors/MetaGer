0e2c22b85bf881280001625be8a1079bd2e3ef2f
Dominik Hebeler <dominik@hebeler.club>
2023-07-12 13:54:50 +0200

    removed obsolete language translator

--- metager/lang/de/languages.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'header' => 'MetaGer - Übersetzungen',
-    'progress' => ':uebersetzteTexte/:textCount Texten übersetzt (:percentage%)',
-    'email' => '<b>E-Mail-Adresse für Rückfragen</b> (optional):',
-    'synoptic' => [
-        'hinweis' => 'Datei wurde zuletzt bearbeitet.',
-    ],
-];

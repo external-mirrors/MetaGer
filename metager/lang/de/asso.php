ad37848836a8265511531f060e5f22af74bd2c32
Weblate <noreply@weblate.org>
2023-07-12 07:48:19 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Asso
    Translate-URL: https://translate.metager.de/projects/metager/asso/

--- metager/lang/de/asso.php
+++ metager/lang/de/asso.php
@@ -1,6 +1,4 @@
 <?php
 return [
-    '1.1' => 'Wenn Sie mit Ihren Suchwörtern nicht weiter kommen: Vielleicht fehlen Ihnen die richtigen? Fragen Sie unseren Assoziator: Geben Sie hier einfach ein oder mehrere Wörter ein, zu denen Sie neue oder bessere Begriffe suchen. Versuchen Sie mit den dann gefundenen Wörtern eine neue Suche indem Sie das neue Wort in der Ausgabeseite anklicken. Es gilt unsere',
     'reasso.title' => 'Diesen Begriff assoziieren',
-    'search.placeholder' => 'Zu assoziierender Begriff',
 ];

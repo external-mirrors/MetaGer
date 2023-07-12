b8bbb71addb12cae0006941d0215eaac1761d446
Weblate <noreply@weblate.org>
2023-07-12 07:48:16 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Transparency
    Translate-URL: https://translate.metager.de/projects/metager/transparency/

--- metager/lang/de/transparency.php
+++ metager/lang/de/transparency.php
@@ -1,11 +1,7 @@
 <?php
 return [
-    'head.2' => 'MetaGer ist transparent',
     'head.4' => 'Was ist der Vorteil einer Metasuchmaschine?',
-    'head.compliance' => 'Wie reagiert MetaGer auf Anfragen von Behörden?',
     'text.2.1' => 'Um zu erklären, was Metasuchmaschinen sind, ergibt es Sinn, vorerst kurz grob zu erklären wie die Indexierung von normalen Suchmaschinen funktioniert. Diese Suchmaschinen beziehen ihre Suchergebnisse aus einer Datenbank von Webseiten, die man auch Index nennt. Die Suchmaschinen betreiben sogenannte „Crawler“, die Webseiten sammeln und diese zum Index (Datenbestand) hinzufügen. Der Crawler startet mit einer Menge von Webseiten und öffnet alle dort verlinkten Webseiten. Diese werden indexiert, also dem Index hinzugefügt. Anschließend öffnet der Crawler die auf diesen Webseiten verlinkten Webseiten und fährt so fort.',
-    'text.3' => 'Ein klarer Vorteil von Metasuchmaschinen ist es, dass der Nutzer nur eine einzige Suchanfrage braucht, um an die Ergebnisse mehrerer Suchmaschinen zu kommen. Die Metasuchmaschine gibt die relevanten Ergebnisse in einer nochmals sortierten Ergebnisliste aus. MetaGer ist keine reine Metasuchmaschine, da wir auch kleine eigene Indexe verwenden.',
     'text.5' => 'Sollte es weitere Fragen oder Unklarheiten geben, können Sie gerne unser <a href=":contact">Kontaktformular</a> nutzen und uns Ihre Fragen stellen!',
-    'table.compliance.th.authinfocomp' => 'Erfüllte Auskunftsanfragen',
     'alt.text.1' => 'Visuelle Darstellung von zwei Indexen die sich zu einem Meta-Index ergänzen',
 ];

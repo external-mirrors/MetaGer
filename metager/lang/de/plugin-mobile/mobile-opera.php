d77836e6d1c693f76bee97eb93064329fabe37a1
Dominik Hebeler <dominik@hebeler.club>
2023-07-12 14:18:19 +0200

    fix lang file

--- metager/lang/de/plugin-mobile/mobile-opera.php
+++ metager/lang/de/plugin-mobile/mobile-opera.php
@@ -1,4 +1,11 @@
 <?php
 return [
-    'default-search-v60.4' => 'Nun können Sie in einem neuen Tab durch das Tippen auf das Logo Ihrer Standardsuchmaschine MetaGer auswählen',
+
+    'default-search-v60' => [
+        '1' => 'Tippen und halten Sie mit dem Finger auf das Sucheingabefeld unter der Anleitung.',
+        '2' => 'Wählen Sie im erscheinenden Menü "Suchmaschine hinzufügen...".',
+        '3' => 'Tippen Sie im Popup auf "OK".',
+        '4' => 'Nun können Sie in einem neuen Tab durch das Tippen auf das Logo Ihrer Standardsuchmaschine MetaGer auswählen',
+    ],
+    //link to mobile-unable
 ];

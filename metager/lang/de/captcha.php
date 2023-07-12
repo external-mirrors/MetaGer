9df099775382eba88185e8f1134957bc39d3231d
Weblate <noreply@weblate.org>
2023-07-12 07:48:21 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Captcha
    Translate-URL: https://translate.metager.de/projects/metager/captcha/

--- metager/lang/de/captcha.php
+++ metager/lang/de/captcha.php
@@ -1,5 +1,4 @@
 <?php
 return [
-    '2' => 'Bitte geben Sie die 5 Zeichen aus dem Bild oder dem Audioplayer in die Eingabebox ein und bestätigen Sie mit "OK" um zur Ergebnisseite zu gelangen.',
     '4' => 'Auf diesem Gerät nicht erneut fragen (optional; setzt einen Cookie)',
 ];

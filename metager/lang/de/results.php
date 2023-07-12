b5289b43ff28f064983f3dccc7529f9a7a682e6d
Weblate <noreply@weblate.org>
2023-07-12 07:48:11 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Results
    Translate-URL: https://translate.metager.de/projects/metager/results/

--- metager/lang/de/results.php
+++ metager/lang/de/results.php
@@ -4,7 +4,5 @@ return [
     'weiter' => 'Weitersuchen',
     'zurueck' => 'Zurück',
     'backtotop' => 'Zum Anfang der Seite',
-    'filter.default' => 'Sie erhalten die Benutzeroberfläche in <strong>:langName</strong>, aber Ergebnisse in <strong>ALLEN</strong> Sprachen. Für ausschließlich deutschsprachige Ergebnisse können Sie unter <a href="/settings" target="_blank">anpassen</a> die "Sprache auswählen".',
     'produktanzeigen' => 'Produktanzeigen:',
-    'query.alteration.original' => 'Stattdessen nach <a href=":link">:original <img class= "mg-icon" src="/img/icon-lupe.svg"></a> suchen',
 ];

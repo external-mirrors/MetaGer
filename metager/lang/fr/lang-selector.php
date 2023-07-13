cefb0eb87e03058a4f061510ed7fd7f8a4e447fa
Anonymous <noreply@weblate.org>
2023-07-13 13:17:46 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: MetaGer/Lang-Selector
    Translate-URL: https://translate.metager.de/projects/metager/lang-selector/fr/

--- metager/lang/fr/lang-selector.php
+++ metager/lang/fr/lang-selector.php
@@ -1,1 +1,24 @@
 <?php
+return [
+    'lang' => [
+        'de' => 'Allemand',
+        'en' => 'Anglais',
+        'es' => 'Espagnol',
+        'nl' => 'Néerlandais',
+        'fi' => 'Finlandais',
+        'no' => 'Norvégien',
+        'it' => 'Italien',
+        'da' => 'Danois',
+        'sv' => 'Suédois',
+        'pl' => 'Polonais',
+        'fr' => 'Français',
+    ],
+    'description' => 'Vous trouverez ci-dessous une liste de toutes les langues/régions actuellement prises en charge par la recherche MetaGer. Les résultats de votre recherche seront optimisés pour la région sélectionnée. Nous utilisons un cookie pour sauvegarder les paramètres modifiés.',
+    'detection' => [
+        'title' => 'Quelle est la langue par défaut ?',
+        'description' => 'Nous utilisons des URL spécifiques à la langue qui peuvent être utilisés directement. Si aucune langue n\'est définie dans l\'URL, nous utilisons les paramètres linguistiques enregistrés dans les paramètres de votre navigateur pour présélectionner la région la plus appropriée pour vous. Dans la plupart des cas, vous obtiendrez automatiquement des résultats de recherche pour la bonne région. Vous pouvez également modifier ce paramètre.',
+    ],
+    'h1' => [
+        '1' => 'Sélection de la langue',
+    ],
+];

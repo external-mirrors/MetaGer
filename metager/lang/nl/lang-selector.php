980286ed9e74ac231d615b6e0becd84e4de88645
Anonymous <noreply@weblate.org>
2023-07-14 08:02:11 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: MetaGer/Lang-Selector
    Translate-URL: https://translate.metager.de/projects/metager/lang-selector/nl/

--- metager/lang/nl/lang-selector.php
+++ metager/lang/nl/lang-selector.php
@@ -1,2 +1,24 @@
 <?php
-return [];
+return [
+    'lang' => [
+        'de' => 'Duits',
+        'en' => 'Engels',
+        'es' => 'Spaans',
+        'nl' => 'Nederlands',
+        'fi' => 'Fins',
+        'no' => 'Noors',
+        'it' => 'Italiaans',
+        'da' => 'Deens',
+        'sv' => 'Zweeds',
+        'pl' => 'Pools',
+        'fr' => 'Frans',
+    ],
+    'h1' => [
+        '1' => 'Taalkeuze',
+    ],
+    'description' => 'Hieronder vind je een lijst van alle talen/regio\'s die momenteel worden ondersteund door MetaGer search. Je zoekresultaten worden geoptimaliseerd voor de geselecteerde regio. We gebruiken een cookie om een gewijzigde instelling op te slaan.',
+    'detection' => [
+        'title' => 'Wat is de standaardtaalinstelling?',
+        'description' => 'We gebruiken taalspecifieke URL\'s die direct kunnen worden gebruikt. Als er geen taal is gedefinieerd in de URL, gebruiken we de taalinstelling die is opgeslagen in uw browserinstellingen om vooraf de meest geschikte regio voor u te selecteren. In de meeste gevallen geeft dit automatisch zoekresultaten voor de juiste regio. Je kunt de bovenstaande instelling ook wijzigen.',
+    ],
+];

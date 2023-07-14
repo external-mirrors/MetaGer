a3c9118ea955ffcd33f4b4018215894f8b4baa53
Anonymous <noreply@weblate.org>
2023-07-14 08:33:18 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: MetaGer/Researchandtabs
    Translate-URL: https://translate.metager.de/projects/metager/researchandtabs/pl/

--- metager/lang/pl/researchandtabs.php
+++ metager/lang/pl/researchandtabs.php
@@ -1,2 +1,7 @@
 <?php
-return [];
+return [
+    'plugin' => [
+        '1' => 'Czy wiesz, że twoja przeglądarka :browser może używać MetaGer jako domyślnej wyszukiwarki?',
+        '2' => 'Pokaż mi jak',
+    ],
+];

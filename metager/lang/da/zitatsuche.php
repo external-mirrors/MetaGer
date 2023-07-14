77a1327244c2bd51ebb9d1cbc016eccb3245e70d
Anonymous <noreply@weblate.org>
2023-07-14 08:35:16 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MetaGer/Zitatsuche
    Translate-URL: https://translate.metager.de/projects/metager/zitatsuche/da/

--- metager/lang/da/zitatsuche.php
+++ metager/lang/da/zitatsuche.php
@@ -1,2 +1,7 @@
 <?php
-return [];
+return [
+    'title' => 'MetaGer - Citatsøgning',
+    'subtitle' => 'Indtast forespørgsler (forfattere eller citater) i nedenstående tekstfelt.',
+    'search-label' => 'Indtast forespørgsel',
+    'results-label' => 'Søgeresultater',
+];

1da436b1e0e9774831d206e57d674d1e374c869b
Anonymous <noreply@weblate.org>
2023-07-14 07:59:32 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Quicktip
    Translate-URL: https://translate.metager.de/projects/metager/quicktip/nl/

--- metager/lang/nl/quicktip.php
+++ metager/lang/nl/quicktip.php
@@ -1,2 +1,8 @@
 <?php
-return [];
+return [
+    'title' => 'MetaGer Sneltips',
+    'spende' => [
+        'title' => 'MetaGer zegt BEDANKT!',
+        'descr' => 'Uw steun helpt ons MetaGer te onderhouden.',
+    ],
+];

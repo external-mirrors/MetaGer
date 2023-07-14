db52ffdc656601df3bd60f9ff8477e28275d7d29
Anonymous <noreply@weblate.org>
2023-07-14 08:46:06 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: MetaGer/Auth
    Translate-URL: https://translate.metager.de/projects/metager/auth/fi/

--- metager/lang/fi/auth.php
+++ metager/lang/fi/auth.php
@@ -1,2 +1,5 @@
 <?php
-return [];
+return [
+    'failed' => 'Nämä valtakirjat eivät vastaa tietojamme.',
+    'throttle' => 'Liian monta kirjautumisyritystä. Yritä uudelleen :n sekunnin kuluttua.',
+];

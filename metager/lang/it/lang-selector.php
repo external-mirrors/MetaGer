94a14847d485d6fa6c682f3230e119abc6ba091f
Anonymous <noreply@weblate.org>
2023-07-14 09:35:38 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (17 of 17 strings)
    
    Translation: MetaGer/Lang-Selector
    Translate-URL: https://translate.metager.de/projects/metager/lang-selector/it/

--- metager/lang/it/lang-selector.php
+++ metager/lang/it/lang-selector.php
@@ -21,4 +21,8 @@ return [
         'title' => 'Qual è l\'impostazione predefinita della lingua?',
         'description' => 'Utilizziamo URL specifici per la lingua che possono essere utilizzati direttamente. Se nell\'URL non è definita alcuna lingua, utilizziamo la lingua memorizzata nelle impostazioni del browser per preselezionare la regione più adatta all\'utente. Nella maggior parte dei casi, i risultati della ricerca vengono forniti automaticamente per la regione corretta. In alternativa, è possibile modificare l\'impostazione di cui sopra.',
     ],
+    'translate' => [
+        'title' => 'Avete trovato un errore nelle nostre traduzioni?',
+        'description' => 'Se avete trovato un errore nelle nostre traduzioni, saremo lieti di accogliere i vostri suggerimenti direttamente attraverso la nostra <a href="https://translate.metager.de">istanza weblate</a> (non è necessario un account). In alternativa, potete inviarci le vostre segnalazioni tramite il nostro modulo di contatto <a href=":contactlink"></a> .',
+    ],
 ];

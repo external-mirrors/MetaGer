7aa320c24c09fdf92a8ed566d7ec4944930180f0
Anonymous <noreply@weblate.org>
2023-07-14 08:38:16 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MetaGer/Spendenaufruf
    Translate-URL: https://translate.metager.de/projects/metager/spendenaufruf/pl/

--- metager/lang/pl/spendenaufruf.php
+++ metager/lang/pl/spendenaufruf.php
@@ -1,2 +1,7 @@
 <?php
-return [];
+return [
+    'heading' => 'Razem z tobą',
+    'text' => 'rozwijamy i utrzymujemy MetaGer. Z Twoją pomocą możemy zachować prywatność i ochronę danych:',
+    'button' => 'Apel o darowizny',
+    'link' => '/pl/spende',
+];

6785fbdec5cf1c78c7c5a4bced993d1dbdcb7ee6
Anonymous <noreply@weblate.org>
2023-07-14 08:46:07 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: MetaGer/Auth
    Translate-URL: https://translate.metager.de/projects/metager/auth/sv/

--- metager/lang/sv/auth.php
+++ metager/lang/sv/auth.php
@@ -1,2 +1,5 @@
 <?php
-return [];
+return [
+    'failed' => 'Dessa uppgifter stämmer inte med våra register.',
+    'throttle' => 'För många inloggningsförsök. Försök igen om :sekunder sekunder.',
+];

54b13253fbf0ebb79ed31a9b3ca878ca864a4eee
Anonymous <noreply@weblate.org>
2023-07-14 08:32:32 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (18 of 18 strings)
    
    Translation: MetaGer/Result
    Translate-URL: https://translate.metager.de/projects/metager/result/fi/

--- metager/lang/fi/result.php
+++ metager/lang/fi/result.php
@@ -1,2 +1,23 @@
 <?php
-return [];
+return [
+    'providers' => 'palveluntarjoajat',
+    'options' => [
+        '8' => 'POISTAA MAINOKSIA',
+        'headline' => 'Vaihtoehdot',
+        'savetab' => 'Tallenna tulos TAB:iin',
+        '1' => 'Aloita uusi haku tällä verkkotunnuksella',
+        '2' => 'Piilota :host',
+        '3' => 'piilota *.:domain',
+        '4' => 'Kumppani',
+        '5' => 'AVATA NIMETTÖMÄNÄ',
+        '6' => 'AVAA UUDESSA VÄLILEHDESSÄ',
+        '7' => 'AVOIN',
+        'direct' => 'Suoraan auki',
+    ],
+    'gefVon' => 'by',
+    'advertisement' => 'Mainos',
+    'proxytext' => 'Tuloslinkki avataan nimettömänä. Tietojasi ei siirretä kohdepalvelimille. Jotkin verkkosivut eivät ehkä toimi tavalliseen tapaan.',
+    'metagerkeytext' => 'Käytä MetaGeria mainosvapaasti',
+    'news' => 'Asiaankuuluvat uutiset',
+    'videos' => 'Videot',
+];

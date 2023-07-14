ad5ae176284f886219a3f53bed70d0c87b6cb2d6
Anonymous <noreply@weblate.org>
2023-07-14 08:02:26 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: MetaGer/Auth
    Translate-URL: https://translate.metager.de/projects/metager/auth/nl/

--- metager/lang/nl/auth.php
+++ metager/lang/nl/auth.php
@@ -1,2 +1,5 @@
 <?php
-return [];
+return [
+    'failed' => 'Deze referenties komen niet overeen met onze gegevens.',
+    'throttle' => 'Te veel aanmeldingspogingen. Probeer het opnieuw over :seconden seconden.',
+];

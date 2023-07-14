97df5da21e54ef21975d6d938eb1c93407a70635
Anonymous <noreply@weblate.org>
2023-07-14 08:32:25 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (18 of 18 strings)
    
    Translation: MetaGer/Result
    Translate-URL: https://translate.metager.de/projects/metager/result/da/

--- metager/lang/da/result.php
+++ metager/lang/da/result.php
@@ -1,2 +1,23 @@
 <?php
-return [];
+return [
+    'options' => [
+        'savetab' => 'Gem resultatet i TAB',
+        'headline' => 'Valgmuligheder',
+        '1' => 'Start en ny søgning på dette domæne',
+        '2' => 'Skjul :host',
+        '3' => 'skjul *.:domæne',
+        '4' => 'Partner',
+        '5' => 'ÅBN ANONYMT',
+        '6' => 'ÅBN I NY FANE',
+        '7' => 'ÅBEN',
+        'direct' => 'Direkte åben',
+        '8' => 'FJERN REKLAMER',
+    ],
+    'gefVon' => 'af',
+    'advertisement' => 'Annonce',
+    'providers' => 'udbydere',
+    'proxytext' => 'Resultatlinket åbnes anonymt. Dine data vil ikke blive overført til destinationsservere. Nogle websider fungerer muligvis ikke som normalt.',
+    'metagerkeytext' => 'Brug MetaGer uden reklamer',
+    'news' => 'Relevante nyheder',
+    'videos' => 'Videoer',
+];

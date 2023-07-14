d6fe97fface35d82fe3510211fe346db60db5fb9
Anonymous <noreply@weblate.org>
2023-07-14 07:22:53 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: MetaGer/500
    Translate-URL: https://translate.metager.de/projects/metager/500/it/

--- metager/lang/it/500.php
+++ metager/lang/it/500.php
@@ -1,1 +1,5 @@
 <?php
+return [
+    'title' => 'Ops. Non sarebbe dovuto accadere.',
+    'text' => 'Qualcosa è andato storto con la sua richiesta. Ci occuperemo di questo problema il prima possibile.',
+];

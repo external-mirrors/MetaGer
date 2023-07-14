91349ce2e856eb571556db9f4b61f31bea66c053
Anonymous <noreply@weblate.org>
2023-07-14 07:33:27 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: MetaGer/Results
    Translate-URL: https://translate.metager.de/projects/metager/results/it/

--- metager/lang/it/results.php
+++ metager/lang/it/results.php
@@ -1,1 +1,12 @@
 <?php
+return [
+    'weiter' => 'di più',
+    'zurueck' => 'indietro',
+    'backtotop' => 'Torna all\'inizio',
+    'query' => [
+        'alteration' => [
+            'original' => 'Cercare invece <a href=":link">:original <img class="mg-icon" src="/img/icon-lupe.svg"></a>',
+            'new' => 'Contiene i risultati per <a href=":link">:alterazione <img class="mg-icon" src="/img/icon-lupe.svg"></a>',
+        ],
+    ],
+];

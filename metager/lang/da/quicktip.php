91a1d5cfd4d370d8a356c12fbf4c66868bfc629e
Anonymous <noreply@weblate.org>
2023-07-14 08:35:32 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Quicktip
    Translate-URL: https://translate.metager.de/projects/metager/quicktip/da/

--- metager/lang/da/quicktip.php
+++ metager/lang/da/quicktip.php
@@ -1,2 +1,8 @@
 <?php
-return [];
+return [
+    'spende' => [
+        'title' => 'MetaGer siger TAK!',
+        'descr' => 'Din støtte hjælper os med at vedligeholde MetaGer.',
+    ],
+    'title' => 'MetaGer Quicktips',
+];

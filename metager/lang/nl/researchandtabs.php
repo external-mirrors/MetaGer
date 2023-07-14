0cf57186e5d768898d7921ad5f46ab87d46a4b4f
Anonymous <noreply@weblate.org>
2023-07-14 07:59:02 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: MetaGer/Researchandtabs
    Translate-URL: https://translate.metager.de/projects/metager/researchandtabs/nl/

--- metager/lang/nl/researchandtabs.php
+++ metager/lang/nl/researchandtabs.php
@@ -1,2 +1,7 @@
 <?php
-return [];
+return [
+    'plugin' => [
+        '1' => 'Wist je dat je :browser MetaGer als standaard zoekmachine kan gebruiken?',
+        '2' => 'Laat me zien hoe',
+    ],
+];

8b2641c8df0dd7615efcf4e5d20694b0e2fc7379
Anonymous <noreply@weblate.org>
2023-07-14 08:44:37 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (15 of 15 strings)
    
    Translation: MetaGer/Lang-Selector
    Translate-URL: https://translate.metager.de/projects/metager/lang-selector/sv/

--- metager/lang/sv/lang-selector.php
+++ metager/lang/sv/lang-selector.php
@@ -1,2 +1,24 @@
 <?php
-return [];
+return [
+    'lang' => [
+        'da' => 'danska',
+        'sv' => 'Svenska',
+        'pl' => 'Polsk',
+        'fr' => 'Franska',
+        'de' => 'Tyska',
+        'en' => 'Engelska',
+        'es' => 'Spanska',
+        'nl' => 'Nederländska',
+        'fi' => 'finska',
+        'no' => 'Norska',
+        'it' => 'Italienska',
+    ],
+    'h1' => [
+        '1' => 'Val av språk',
+    ],
+    'description' => 'Nedan finns en lista över alla språk/regioner som för närvarande stöds av MetaGer-sökning. Dina sökresultat kommer att optimeras för den valda regionen. Vi använder en cookie för att spara en ändrad inställning.',
+    'detection' => [
+        'title' => 'Vilken är standardinställningen för språk?',
+        'description' => 'Vi använder språkspecifika URL-adresser som kan användas direkt. Om inget språk anges i webbadressen använder vi den språkinställning som finns lagrad i din webbläsares inställningar för att välja den lämpligaste regionen för dig. I de flesta fall kommer detta automatiskt att ge dig sökresultat för rätt region. Alternativt kan du ändra ovanstående inställning.',
+    ],
+];

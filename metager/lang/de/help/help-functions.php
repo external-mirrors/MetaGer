3a5d2560ca82b68cb2dd7f9e5dc612eebb849525
Kim <kim@suma-ev.de>
2023-07-17 12:37:41 +0200

    fix indentations and add useful text to bangs

--- metager/lang/de/help/help-functions.php
+++ metager/lang/de/help/help-functions.php
@@ -13,12 +13,7 @@ return [
         'explanation_b' => 'Manche Browser erwarten die Eingabe einer URL; diese lautet "https://metager.de/meta/meta.ger3?eingabe=%s" ohne Gänsefüßchen eintragen. Die URL können Sie selbst erzeugen, wenn Sie mit metager.de nach irgendetwas suchen und dann das, was oben im Adressfeld hinter "eingabe=" steht, mit %s ersetzen. Wenn Sie dann noch Probleme haben sollten, wenden Sie sich bitte an uns: <a href="/kontakt" target="_blank" rel="noopener">Kontaktformular</a>',
         'explanation_a' => 'Versuchen Sie bitte zuerst, das aktuelle Plugin zu installieren. Zum Installieren einfach auf den Link direkt unter dem Suchfeld klicken. Dort sollte Ihr Browser schon erkannt worden sein.',
     ],
-    "faq" => [
-        "18" => [
-            'h' => 'Warum werden !bangs nicht direkt geöffnet?',
-            'b' => 'Die !bang-„Weiterleitungen“ sind bei uns ein Teil unserer Quicktips und benötigen einen zusätzlichen „Klick“. Das war für uns eine schwierige Entscheidung, da die !bang dadurch weniger nützlich sind. Jedoch ist es leider nötig, da die Links, auf die weitergeleitet wird, nicht von uns stammen, sondern von einem Drittanbieter, DuckDuckGo.<p>Wir achten stehts darauf, dass unsere Nutzer jederzeit die Kontrolle behalten. Wir schützen daher auf zwei Arten: Zum Einen wird der eingegebene Suchbegriff niemals an DuckDuckGo übertragen, sondern nur das !bang. Zum Anderen bestätigt der Nutzer den Besuch des !bang-Ziels explizit. Leider können wir derzeit aus Personalgründen nicht alle diese !bangs prüfen oder selbst pflegen.',
-        ]
-    ],
+    
     "suchfunktion" => [
         "title" => "Suchfunktionen"
     ],
@@ -71,6 +66,8 @@ return [
     "bang"  => [
         "title" => "!bangs",
         "1" => "MetaGer unterstützt in geringem Umfang eine Schreibweise, die oft als „!bang“-Syntax bezeichnet wird.<br>Ein solches „!bang“ beginnt immer mit einem Ausrufezeichen und enthält keine Leerzeichen. Beispiele sind hier „!twitter“ oder „!facebook“.<br>Wird ein !bang, das wir unterstützen, in der Suchanfrage verwendet, erscheint in unseren Quicktips ein Eintrag, über den man die Suche auf Knopfdruck mit dem jeweiligen Dienst (hier Twitter oder Facebook) fortführen kann.",
+        "2" => 'Warum werden !bangs nicht direkt geöffnet?',
+        "3" => 'Die !bang-„Weiterleitungen“ sind bei uns ein Teil unserer Quicktips und benötigen einen zusätzlichen „Klick“. Das war für uns eine schwierige Entscheidung, da die !bang dadurch weniger nützlich sind. Jedoch ist es leider nötig, da die Links, auf die weitergeleitet wird, nicht von uns stammen, sondern von einem Drittanbieter, DuckDuckGo.<p>Wir achten stehts darauf, dass unsere Nutzer jederzeit die Kontrolle behalten. Wir schützen daher auf zwei Arten: Zum Einen wird der eingegebene Suchbegriff niemals an DuckDuckGo übertragen, sondern nur das !bang. Zum Anderen bestätigt der Nutzer den Besuch des !bang-Ziels explizit. Leider können wir derzeit aus Personalgründen nicht alle diese !bangs prüfen oder selbst pflegen.',
     ],
     "backarrow" => 'Zurück',
 ];
--- metager/resources/views/help/help-functions.blade.php
+++ metager/resources/views/help/help-functions.blade.php
@@ -5,66 +5,66 @@
 @section('content')
 <h1 class="page-title">{!! trans('help/help-functions.title') !!}</h1>
 <section>
-<div id="navigationsticky">
-	<a  class=back-button href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/hilfe") }}"><img class="back-arrow" src=/img/back-arrow.svg>{!! trans('help/help-functions.backarrow') !!}</a>
-</div>	
-<h2 id="searchfunctions">{!! trans('help/help-functions.suchfunktion.title') !!}</h2>
-	<h3 id="stopwordsearch">{!! trans('help/help-functions.stopworte.title') !!}</h3>
-	<div>
-		<p>{!! trans('help/help-functions.stopworte.1') !!}</p>
-		<ul class="dotlist">
-			<li>{!! trans('help/help-functions.stopworte.2') !!}</li>
-			<li class="nodot"><div class="search-example">{!! trans('help/help-functions.stopworte.3') !!}</div></li>
-		</ul>
-	</div>
-	<h3 id="urls">{!! trans('help/help-functions.urls.title') !!}</h3>
-	<div>
-		<p>{!! trans('help/help-functions.urls.explanation') !!}</p>
-		<ul class="dotlist">
-			<li>{!! trans('help/help-functions.urls.example_a') !!}</li>
-			<li class="nodot"><div class = "search-example">{!! trans('help/help-functions.urls.example_b') !!}</div></li>
-		</ul>
-	</div>
-	<h3 id="severalwords">{!! trans('help/help-functions.mehrwortsuche.title') !!}</h3>
-	<div>
-		<p>{!! trans('help/help-functions.mehrwortsuche.1') !!}</p>
-		<p>{!! trans('help/help-functions.mehrwortsuche.2') !!}</p>
-		<ul class="dotlist">
-			<li>{!! trans('help/help-functions.mehrwortsuche.3.text') !!}</li>
-			<li class="nodot"><div class = "search-example">{!! trans('help/help-functions.mehrwortsuche.3.example') !!}</div></li>
-			<li>{!! trans('help/help-functions.mehrwortsuche.4.text') !!}</li>
-			<li class="nodot"><div class = "search-example">{!! trans('help/help-functions.mehrwortsuche.4.example') !!}</div></li>
-		</ul>
-	</div>
-	<h3 id="exactsearch">{!! trans('help/help-functions.exactsearch.title') !!}</h3>
-	<div>
-		<p>{!! trans('help/help-functions.exactsearch.1') !!}</p>
-		<ul class="dotlist">
-			<li>{!! trans('help/help-functions.exactsearch.2') !!}</li>
-			<li class="nodot"><div class = "search-example">{!! trans('help/help-functions.exactsearch.example.1') !!}</div></li>
-			<li>{!! trans('help/help-functions.exactsearch.3') !!}</li>
-			<li class="nodot"><div class = "search-example">{!! trans('help/help-functions.exactsearch.example.2') !!}</div></li>
-		</ul>
-	</div>
-	</section>
-
-	<section id="bangs">
-		<h3>{!! trans('help/help-functions.bang.title') !!}</h3>
+	<div id="navigationsticky">
+		<a  class=back-button href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), "/hilfe") }}"><img class="back-arrow" src=/img/back-arrow.svg>{!! trans('help/help-functions.backarrow') !!}</a>
+	</div>	
+	<section id="searchfunctions" class="card">
+		<h2>{!! trans('help/help-functions.suchfunktion.title') !!}</h2>
+		<h3 id="stopwordsearch">{!! trans('help/help-functions.stopworte.title') !!}</h3>
 		<div>
-			<p>{!! trans('help/help-functions.bang.1') !!}</p>
+			<p>{!! trans('help/help-functions.stopworte.1') !!}</p>
+			<ul class="dotlist">
+				<li>{!! trans('help/help-functions.stopworte.2') !!}</li>
+				<li class="nodot"><div class="search-example">{!! trans('help/help-functions.stopworte.3') !!}</div></li>
+			</ul>
+		</div>
+		<h3 id="urls">{!! trans('help/help-functions.urls.title') !!}</h3>
+		<div>
+			<p>{!! trans('help/help-functions.urls.explanation') !!}</p>
+			<ul class="dotlist">
+				<li>{!! trans('help/help-functions.urls.example_a') !!}</li>
+				<li class="nodot"><div class = "search-example">{!! trans('help/help-functions.urls.example_b') !!}</div></li>
+			</ul>
+		</div>
+		<h3 id="severalwords">{!! trans('help/help-functions.mehrwortsuche.title') !!}</h3>
+		<div>
+			<p>{!! trans('help/help-functions.mehrwortsuche.1') !!}</p>
+			<p>{!! trans('help/help-functions.mehrwortsuche.2') !!}</p>
+			<ul class="dotlist">
+				<li>{!! trans('help/help-functions.mehrwortsuche.3.text') !!}</li>
+				<li class="nodot"><div class = "search-example">{!! trans('help/help-functions.mehrwortsuche.3.example') !!}</div></li>
+				<li>{!! trans('help/help-functions.mehrwortsuche.4.text') !!}</li>
+				<li class="nodot"><div class = "search-example">{!! trans('help/help-functions.mehrwortsuche.4.example') !!}</div></li>
+			</ul>
+		</div>
+		<h3 id="exactsearch">{!! trans('help/help-functions.exactsearch.title') !!}</h3>
+		<div>
+			<p>{!! trans('help/help-functions.exactsearch.1') !!}</p>
+			<ul class="dotlist">
+				<li>{!! trans('help/help-functions.exactsearch.2') !!}</li>
+				<li class="nodot"><div class = "search-example">{!! trans('help/help-functions.exactsearch.example.1') !!}</div></li>
+				<li>{!! trans('help/help-functions.exactsearch.3') !!}</li>
+				<li class="nodot"><div class = "search-example">{!! trans('help/help-functions.exactsearch.example.2') !!}</div></li>
+			</ul>
 		</div>
 	</section>
-	<section id="keyexplain">
+	<section id="bangs" class="card">
+		<h3>{!! trans('help/help-functions.bang.title') !!}</h3>
+		<p>{!! trans('help/help-functions.bang.1') !!}</p>
+		<h4>{!! trans('help/help-functions.bang.2') !!}</h4>	
+		<p>{!! trans('help/help-functions.bang.3') !!}</p>	
+	
+	</section>
+	<section id="keyexplain" class="card">
 		<h3>{!! trans('help/help-functions.key.title') !!}</h3>
 		<div>
+			<p>{!! trans('help/help-functions.key.1') !!}</p>
 			<ul>
-				<p>{!! trans('help/help-functions.key.1') !!}</p>
 				<li>{!! trans('help/help-functions.key.2') !!}</li>
 				<li>{!! trans('help/help-functions.key.3') !!}</li>
 				<li>{!! trans('help/help-functions.key.4') !!}</li>
 				<li>{!! trans('help/help-functions.key.5') !!}</li>
 				<li>{!! trans('help/help-functions.key.6') !!}</li>
-
 			</ul>
 			<h4>{!! trans('help/help-functions.key.colors.title') !!}</h4>
 			<p>{!! trans('help/help-functions.key.colors.1') !!}</p>
@@ -76,13 +76,15 @@
 			</ul>
 		</div>
 	</section>
-	<section id="selist">
+	<section id="selist" class="card">
 		<h3>{!! trans('help/help-functions.selist.title') !!}</h3>
 		<div>
 			<p>{!! trans('help/help-functions.selist.explanation_a') !!}</p>
 			<p>{!! trans('help/help-functions.selist.explanation_b') !!}</p>
-
 		</div>
 	</section>
+</section>
+
+
 
 @endsection

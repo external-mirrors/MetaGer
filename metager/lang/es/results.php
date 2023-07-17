be3f60b2a6e9605b6d3e70656a9541e54fad87c0
Anonymous <noreply@weblate.org>
2023-07-17 14:21:49 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: MetaGer/Results
    Translate-URL: https://translate.metager.de/projects/metager/results/es/

--- metager/lang/es/results.php
+++ metager/lang/es/results.php
@@ -9,4 +9,15 @@ return [
             'new' => 'Contiene resultados para <a href=":link">:alteration <img class= "mg-icon" src="/img/ellipsis-horizontal.svg"></a>',
         ],
     ],
+    'images' => [
+        'external' => [
+            'heading' => "Faltan algunos resultados",
+            'description' => "Sólo has buscado en la base de datos de Pixabay. Mejor buscar con una clave MetaGer.",
+            'buy' => "Comprar una llave MetaGer",
+            'or' => "o",
+            'google' => "Buscar en Google",
+            'bing' => "Buscar en Bing",
+            'save' => "Recordar decisión (con cookie)",
+        ],
+    ],
 ];

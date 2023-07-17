dea474d0bc2e75048c8ef409aa265e91c637bc53
Dominik Hebeler <dominik@hebeler.club>
2023-07-17 16:16:40 +0200

    added translations

--- metager/resources/views/resultpages/results_images.blade.php
+++ metager/resources/views/resultpages/results_images.blade.php
@@ -5,28 +5,26 @@
 </div>
 @include('parts.pager')
 <div id="external-search">
-	<h3>Hier fehlen einige Suchergebnisse</h3>
+	<h3>@lang("results.images.external.heading")</h3>
 	<div class="texts">
-		<div>Sie haben lediglich die Datenbank von Pixabay durchsucht. Besser suchen Sie mit einem MetaGer Schlüssel.</div>
+		<div>@lang("results.images.external.description")</div>
 	</div>
 	<div class="external-links">
-		<a href="{{ app(\App\Models\Authorization\Authorization::class)->getAdfreeLink() }}" class="btn btn-primary">MetaGer Schlüssel kaufen</a>
-		<div class="divider">oder</div>
+		<a href="{{ app(\App\Models\Authorization\Authorization::class)->getAdfreeLink() }}" class="btn btn-primary">@lang("results.images.external.buy")</a>
+		<div class="divider">@lang("results.images.external.or")</div>
 		<form id="external-engines-form" class="external-engines" method="POST">
 			@php
 			$expiration = now()->addHour(1);
 			@endphp
 			<input type="hidden" name="expiration" value="{{ $expiration }}">
 			<input type="hidden" name="signature" value="{{ hash_hmac('sha256', $expiration, config('app.key')) }}">
-			<button type="submit" name="bilder_setting_external" value="google" class="btn btn-default">Suchen auf Google</button>
-			<button type="submit" name="bilder_setting_external" value="bing" class="btn btn-default">Suchen auf Bing</button>
+			<button type="submit" name="bilder_setting_external" value="google" class="btn btn-default">@lang("results.images.external.google")</button>
+			<button type="submit" name="bilder_setting_external" value="bing" class="btn btn-default">@lang("results.images.external.bing")</button>
 		</form>
 		<div class="spacer"></div>
 		<div class="input-group">
 			<input type="checkbox" name="save-external-engine" id="save-external-engine" form="external-engines-form" value="1">
-			<label for="save-external-engine">Diese Entscheidung merken </label>
-			<button type="button" class="hover" id="save-external-hint">?</button>
-			<div class="hint">Wir merken uns Ihre Entscheidung mit Hilfe eines Cookies und leiten Sie bei der nächsten Bildersuche direkt zum ausgewählten Anbieter.</div>
+			<label for="save-external-engine">@lang("results.images.external.save")</label>
 		</div>
 	</div>
 </div>

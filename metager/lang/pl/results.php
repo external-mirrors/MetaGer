a7050d5694d0568fecd088759102728b6bd2e696
Anonymous <noreply@weblate.org>
2023-07-17 14:22:12 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: MetaGer/Results
    Translate-URL: https://translate.metager.de/projects/metager/results/pl/

--- metager/lang/pl/results.php
+++ metager/lang/pl/results.php
@@ -9,4 +9,15 @@ return [
             'new' => 'Zawiera wyniki dla <a href=":link">:alteration <img class="mg-icon" src="/img/icon-lupe.svg"></a>.',
         ],
     ],
+    'images' => [
+        'external' => [
+            'save' => "Zapamiętaj decyzję (z plikiem cookie)",
+            'heading' => "Brakuje tutaj niektórych wyników",
+            'description' => "Przeszukałeś tylko bazę danych Pixabay. Lepiej przeszukać za pomocą klucza MetaGer.",
+            'buy' => "Kup klucz MetaGer",
+            'or' => "lub",
+            'google' => "Wyszukiwanie w Google",
+            'bing' => "Wyszukiwanie w Bing",
+        ],
+    ],
 ];

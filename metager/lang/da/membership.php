feab5557e0200f684f93e2566d08c3f074f74361
Anonymous <noreply@weblate.org>
2023-07-19 09:31:27 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Membership
    Translate-URL: https://translate.metager.de/projects/metager/membership/da/

--- metager/lang/da/membership.php
+++ metager/lang/da/membership.php
@@ -1,18 +1,13 @@
 <?php
 return [
     'title' => 'Dit medlemskab af SUMA-EV',
-    'description' => 'Tak fordi du overvejer <a href=\'https://suma-ev.de/mitglieder/\' target=\'_blank\'>medlemskab</a> i vores non-profit støtteforening. For at kunne behandle din ansøgning har vi kun brug for nogle få oplysninger, som du kan udfylde her.',
     'startpage' => 'Tilbage til startsiden',
     'contact' => [
-        'name' => [
-            'label' => 'Dit navn',
-        ],
         'email' => [
             'label' => 'Din e-mailadresse',
         ],
     ],
     'fee' => [
-        'title' => '2. Dit medlemskontingent',
         'amount' => [
             'custom' => [
                 'label' => 'Brugerdefineret beløb',
@@ -21,17 +16,14 @@ return [
     ],
     'payment' => [
         'interval' => [
-            'title' => '3. Dit betalingsinterval',
             'six-monthly' => 'halvårlig',
-            'monthly' => 'månedligt',
         ],
         'method' => [
             'directdebit' => [
                 'label' => 'SEPA direkte debitering',
-                'accountholder' => [
-                    'placeholder' => 'John Smith',
-                ],
             ],
         ],
     ],
+    'non-de' => 'Desværre kan vi i øjeblikket kun acceptere ansøgninger om optagelse fra tysktalende lande. Du er meget velkommen til at støtte os med en donation på <a href=":donationlink"></a> .',
+    'back' => 'Tilbage til startsiden',
 ];

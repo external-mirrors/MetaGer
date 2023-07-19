c2c357c8e4342959f2d349682be24981b998b24b
Weblate <noreply@weblate.org>
2023-07-19 09:31:11 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Membership
    Translate-URL: https://translate.metager.de/projects/metager/membership/

--- metager/lang/es/membership.php
+++ metager/lang/es/membership.php
@@ -1,49 +1,37 @@
 <?php
 return [
-    'title'       => 'Su afiliación a SUMA-EV',
-    'description' => 'Gracias por considerar la posibilidad de <a href=\'https://suma-ev.de/mitglieder/\' target=\'_blank\'>afiliarse</a> a nuestra asociación de apoyo sin ánimo de lucro. Para tramitar su solicitud, sólo necesitamos unos pocos datos, que puede rellenar aquí.',
-    'submit'      => 'Donar',
-    'success'     => 'Muchas gracias por enviarnos su solicitud. La tramitaremos lo antes posible. Le enviaremos un correo electrónico con más información a la dirección indicada.',
-    'startpage'   => 'Volver a la página de inicio',
-    'contact'     => [
-        'title' => '1. Sus datos de contacto',
-        'name'  => [
-            'label'       => 'Su nombre',
-            'placeholder' => 'Max Mustermann / Muster GmbH',
+    'title' => 'Su afiliación a SUMA-EV',
+    'submit' => 'Donar',
+    'startpage' => 'Volver a la página de inicio',
+    'contact' => [
+        'name' => [
+            'label' => 'Su nombre',
         ],
         'email' => [
-            'label'       => 'Su dirección de correo electrónico',
-            'placeholder' => 'max@mustermann.de',
+            'label' => 'Su dirección de correo electrónico',
         ],
     ],
-    'fee'         => [
-        'title'       => '2. Su cuota de afiliación',
-        'description' => 'Seleccione a continuación la cuota de afiliación (mensual) que desee.',
-        'amount'      => [
+    'fee' => [
+        'title' => '2. Su cuota de afiliación',
+        'amount' => [
             'custom' => [
-                'label'       => 'Cantidad deseada',
-                'placeholder' => '5,00€',
+                'label' => 'Cantidad deseada',
             ],
         ],
     ],
-    'payment'     => [
+    'payment' => [
         'interval' => [
-            'title'       => '3. Su garantía de pago',
-            'annual'      => 'anual',
+            'title' => '3. Su garantía de pago',
             'six-monthly' => 'semestral',
-            'quarterly'   => 'trimestral',
-            'monthly'     => 'mensualmente',
+            'monthly' => 'mensualmente',
         ],
-        'method'   => [
-            'title'        => '4. Su forma de pago',
-            'directdebit'  => [
-                'label'         => 'Adeudo directo SEPA',
+        'method' => [
+            'directdebit' => [
+                'label' => 'Adeudo directo SEPA',
                 'accountholder' => [
-                    'label'       => 'Titular de la cuenta (si es diferente)',
                     'placeholder' => 'Max Mustermann',
                 ],
             ],
-            'banktransfer' => 'Transferencia bancaria',
         ],
     ],
 ];

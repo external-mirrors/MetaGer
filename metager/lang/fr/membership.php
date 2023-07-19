867190b3e2b8e66a2e317bde41cc8bc704e4cdf0
Anonymous <noreply@weblate.org>
2023-07-19 09:31:21 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Membership
    Translate-URL: https://translate.metager.de/projects/metager/membership/fr/

--- metager/lang/fr/membership.php
+++ metager/lang/fr/membership.php
@@ -3,35 +3,21 @@ return [
     'title' => 'Votre adhésion à SUMA-EV',
     'payment' => [
         'method' => [
-            'directdebit' => [
-                'accountholder' => [
-                    'label' => 'Titulaire du compte (si différent)',
-                ],
-            ],
             'banktransfer' => 'Virement bancaire',
         ],
         'interval' => [
-            'title' => '3. Votre intervalle de paiement',
             'six-monthly' => 'semestrielle',
-            'monthly' => 'mensuel',
         ],
     ],
     'submit' => 'Faire un don',
-    'startpage' => 'Retour à la page d\'accueil',
     'contact' => [
         'name' => [
             'label' => 'Votre nom',
         ],
-        'email' => [
-            'label' => 'Votre adresse électronique',
-        ],
     ],
     'fee' => [
         'title' => '2. Votre cotisation',
-        'amount' => [
-            'custom' => [
-                'label' => 'Montant personnalisé',
-            ],
-        ],
     ],
+    'non-de' => 'Malheureusement, nous ne pouvons actuellement accepter que les demandes d\'admission pour les pays germanophones. Vous pouvez nous soutenir en faisant un don à <a href=":donationlink"></a> .',
+    'back' => 'Retour à la page d\'accueil',
 ];

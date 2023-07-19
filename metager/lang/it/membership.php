7b7f3d7b7ff93176df5390e5a7589809eff6e742
Anonymous <noreply@weblate.org>
2023-07-19 09:31:22 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Membership
    Translate-URL: https://translate.metager.de/projects/metager/membership/it/

--- metager/lang/it/membership.php
+++ metager/lang/it/membership.php
@@ -3,35 +3,27 @@ return [
     'payment' => [
         'method' => [
             'directdebit' => [
-                'accountholder' => [
-                    'label' => 'Titolare del conto (se diverso)',
-                ],
                 'label' => 'Addebito diretto SEPA',
             ],
-            'banktransfer' => 'Bonifico bancario',
         ],
         'interval' => [
             'annual' => 'annuale',
-            'quarterly' => 'trimestrale',
         ],
     ],
     'title' => 'La vostra adesione a SUMA-EV',
-    'success' => 'Grazie mille per averci inviato la sua candidatura. La elaboreremo il prima possibile. In seguito riceverete una mail con ulteriori informazioni da parte nostra all\'indirizzo indicato.',
     'contact' => [
         'title' => '1. I vostri dati di contatto',
-        'name' => [
-            'placeholder' => 'Max Mustermann / Muster GmbH',
-        ],
         'email' => [
             'placeholder' => 'max@mustermann.de',
         ],
     ],
     'fee' => [
-        'description' => 'Selezionare di seguito la quota di adesione desiderata (mensile).',
         'amount' => [
             'custom' => [
                 'placeholder' => '5,00€',
             ],
         ],
     ],
+    'non-de' => 'Purtroppo, al momento possiamo accettare solo domande di ammissione per i Paesi di lingua tedesca. Siete invitati a sostenerci con una donazione <a href=":donationlink"></a> .',
+    'back' => 'Torna alla pagina iniziale',
 ];

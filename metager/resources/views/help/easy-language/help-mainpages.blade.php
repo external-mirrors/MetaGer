91c99e4e62e308e65d5c6c6e71e27ce2e1b6f37e
Kim <kim@suma-ev.de>
2023-07-31 12:14:04 +0200

    add engl pics to easy lang help

--- metager/resources/views/help/easy-language/help-mainpages.blade.php
+++ metager/resources/views/help/easy-language/help-mainpages.blade.php
@@ -61,13 +61,22 @@
         <h3 id="ergebnis">{!! trans('help/easy-language/help-mainpages.resultpage.title') !!}</h3>
 		<div>
 			<p>{!! trans('help/easy-language/help-mainpages.resultpage.foci.1') !!}</p>
+			@if (App\Localization::getLanguage() == "de")
 			<img class="help-easy-language-image lm-only" src="/img/help-search-focus-lm.png"/>
 			<img class="help-easy-language-image dm-only" src="/img/help-search-focus-dm.png"/>
+			@else
+			<img class="help-easy-language-image lm-only" src="/img/help-search-focus-lm-en.png"/>
+			<img class="help-easy-language-image dm-only" src="/img/help-search-focus-dm-en.png"/>
+			@endif
 			<p>{!! trans('help/easy-language/help-mainpages.resultpage.foci.2') !!}</p>
-
 			<p>{!! trans('help/easy-language/help-mainpages.resultpage.choice') !!}</p>
+			@if (App\Localization::getLanguage() == "de")
 			<img class="help-easy-language-image lm-only" src="/img/help-settings-filter-lm.png"/>
 			<img class="help-easy-language-image dm-only" src="/img/help-settings-filter-dm.png"/>
+			@else
+			<img class="help-easy-language-image lm-only" src="/img/help-settings-filter-lm-en.png"/>
+			<img class="help-easy-language-image dm-only" src="/img/help-settings-filter-dm-en.png"/>
+			@endif
 			<h4>{!! trans('help/easy-language/help-mainpages.resultpage.filter.title') !!}</h4>
 			<p>{!! trans('help/easy-language/help-mainpages.resultpage.filter.1') !!}</p>
 
@@ -122,52 +131,94 @@
 	<section id="help-settings" class="help-section card">
 		<h3 id="einstellungen">{!! trans('help/easy-language/help-mainpages.settings.title') !!}</h3>
 		<h4>{!! trans('help/easy-language/help-mainpages.settings.metagerkey.title') !!}</h4>
+		@if (App\Localization::getLanguage() == "de")
 		<img class="help-easy-language-image lm-only" src="/img/help-settings-key-lm-01.png"/>
 		<img class="help-easy-language-image dm-only" src="/img/help-settings-key-dm-01.png"/>
+		@else
+		<img class="help-easy-language-image lm-only" src="/img/help-settings-key-lm-01-en.png"/>
+		<img class="help-easy-language-image dm-only" src="/img/help-settings-key-dm-01-en.png"/>
+		@endif
 		<p>{!! trans('help/easy-language/help-mainpages.settings.metagerkey.1') !!}</p>
+		@if (App\Localization::getLanguage() == "de")
 		<img class="help-easy-language-image lm-only" src="/img/help-settings-key-lm.png"/>
 		<img class="help-easy-language-image dm-only" src="/img/help-settings-key-dm.png"/>
+		@else
+		<img class="help-easy-language-image lm-only" src="/img/help-settings-key-lm-en.png"/>
+		<img class="help-easy-language-image dm-only" src="/img/help-settings-key-dm-en.png"/>
+		@endif
 		<p>{!! trans('help/easy-language/help-mainpages.settings.metagerkey.2') !!}</p>
 
 		<h4>{!! trans('help/easy-language/help-mainpages.settings.searchengine.title') !!}</h4>
 		<p>{!! trans('help/easy-language/help-mainpages.settings.searchengine.1') !!}</p>
+		@if (App\Localization::getLanguage() == "de")
 		<img id="help-easy-language-used-search-engines-image" class="help-easy-language-image lm-only" src="/img/help-settings-search-engines-lm.png"/>
 		<img id="help-easy-language-used-search-engines-image" class="help-easy-language-image dm-only" src="/img/help-settings-search-engines-dm.png"/>
+		@else
+		<img id="help-easy-language-used-search-engines-image" class="help-easy-language-image lm-only" src="/img/help-settings-search-engines-lm-en.png"/>
+		<img id="help-easy-language-used-search-engines-image" class="help-easy-language-image dm-only" src="/img/help-settings-search-engines-dm-en.png"/>
+		@endif
 		<p>{!! trans('help/easy-language/help-mainpages.settings.searchengine.2') !!}</p>
 
 		<h4>{!! trans('help/easy-language/help-mainpages.settings.filter.title') !!}</h4>
 		<p>{!! trans('help/easy-language/help-mainpages.settings.filter.1.0') !!}</p>
+		@if (App\Localization::getLanguage() == "de")
 		<img id="help-easy-language-searchfilter-image" class="help-easy-language-image lm-only" src="/img/help-settings-search-filter-lm.png"/>
 		<img id="help-easy-language-searchfilter-image" class="help-easy-language-image dm-only" src="/img/help-settings-search-filter-dm.png"/>
+		@else
+		<img id="help-easy-language-searchfilter-image" class="help-easy-language-image lm-only" src="/img/help-settings-search-filter-lm-en.png"/>
+		<img id="help-easy-language-searchfilter-image" class="help-easy-language-image dm-only" src="/img/help-settings-search-filter-dm-en.png"/>
+		@endif
 		<p>{!! trans('help/easy-language/help-mainpages.settings.filter.1.1') !!}</p>
 		<p>{!! trans('help/easy-language/help-mainpages.settings.filter.1.2') !!}</p>
 		<p>{!! trans('help/easy-language/help-mainpages.settings.filter.1.3') !!}</p>
 		<p>{!! trans('help/easy-language/help-mainpages.settings.filter.1.4') !!}</p>
 		<p>{!! trans('help/easy-language/help-mainpages.settings.filter.2') !!}</p>
+		@if (App\Localization::getLanguage() == "de")
+
 		<img id="help-easy-language-safesearch-image" class="help-easy-language-image lm-only" src="/img/help-settings-safesearch-lm.png"/>
 		<img id="help-easy-language-safesearch-image" class="help-easy-language-image dm-only" src="/img/help-settings-safesearch-dm.png"/>
+		@else
+		<img id="help-easy-language-safesearch-image" class="help-easy-language-image lm-only" src="/img/help-settings-safesearch-lm-en.png"/>
+		<img id="help-easy-language-safesearch-image" class="help-easy-language-image dm-only" src="/img/help-settings-safesearch-dm-en.png"/>
+		@endif
 		<p>{!! trans('help/easy-language/help-mainpages.settings.filter.3') !!}</p>
-
 		<h4>{!! trans('help/easy-language/help-mainpages.settings.blacklist.title') !!}</h4>
+		@if (App\Localization::getLanguage() == "de")
 		<img class="help-easy-language-image lm-only" id="easy-help-mainpage-blacklist-image" src="/img/help-settings-blacklist-lm.png"/>
 		<img class="help-easy-language-image dm-only" id="easy-help-mainpage-blacklist-image" src="/img/help-settings-blacklist-dm.png"/>
+		@else
+		<img class="help-easy-language-image lm-only" id="easy-help-mainpage-blacklist-image" src="/img/help-settings-blacklist-lm-en.png"/>
+		<img class="help-easy-language-image dm-only" id="easy-help-mainpage-blacklist-image" src="/img/help-settings-blacklist-dm-en.png"/>
+		@endif
 		<p>{!! trans('help/easy-language/help-mainpages.settings.blacklist.1') !!}</p>
 		<p>{!! trans('help/easy-language/help-mainpages.settings.blacklist.2') !!}</p>
 		<h4>{!! trans('help/easy-language/help-mainpages.settings.moresettings') !!}</h4>
+		@if (App\Localization::getLanguage() == "de")
 		<img id="help-easy-language-more-settings" class="help-easy-language-image lm-only" src="/img/help-more-settings-lm.png"/>
 		<img id="help-easy-language-more-settings" class="help-easy-language-image dm-only" src="/img/help-more-settings-dm.png"/>
+		@else
+		<img id="help-easy-language-more-settings" class="help-easy-language-image lm-only" src="/img/help-more-settings-lm-en.png"/>
+		<img id="help-easy-language-more-settings" class="help-easy-language-image dm-only" src="/img/help-more-settings-dm-en.png"/>
+		@endif
 		<h4>{!! trans('help/easy-language/help-mainpages.settings.darkmode.title') !!}</h4>
 		<p>{!! trans('help/easy-language/help-mainpages.settings.darkmode.1') !!}</p>
 		<h4>{!! trans('help/easy-language/help-mainpages.settings.newtab.title') !!}</h4>
 		<p>{!! trans('help/easy-language/help-mainpages.settings.newtab.1') !!}</p>
+		@if (App\Localization::getLanguage() == "de")
 		<h4>{!! trans('help/easy-language/help-mainpages.settings.cite.title') !!}</h4>
 		<p>{!! trans('help/easy-language/help-mainpages.settings.cite.1') !!}</p>
 		<img id="easy-help-mainpages-settings-cite" class="help-easy-language-image lm-only" src="/img/help-settings-cite-lm.png"/>
 		<img id="easy-help-mainpages-settings-cite" class="help-easy-language-image dm-only" src="/img/help-settings-cite-dm.png"/>
+		@else
+		@endif
 		<h4>{!! trans('help/easy-language/help-mainpages.settings.cookies.title') !!}</h4>
-		
+		@if (App\Localization::getLanguage() == "de")
 		<img class="help-easy-language-image lm-only" src="/img/help-settings-recover-lm.png"/>
 		<img class="help-easy-language-image dm-only" src="/img/help-settings-recover-dm.png"/>
+		@else
+		<img class="help-easy-language-image lm-only" src="/img/help-settings-recover-lm-en.png"/>
+		<img class="help-easy-language-image dm-only" src="/img/help-settings-recover-dm-en.png"/>
+		@endif
 		<p>{!! trans('help/easy-language/help-mainpages.settings.cookies.1') !!}</p>	
     </section>
 </section>

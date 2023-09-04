104b55b958748235f90028e7e78f95ce219ec498
Kim <kim@suma-ev.de>
2023-09-04 15:24:44 +0200

    change more button into current version

--- metager/lang/en/help/easy-language/help-mainpages.php
+++ metager/lang/en/help/easy-language/help-mainpages.php
@@ -69,15 +69,15 @@ return [
                 "1" => 'By clicking here, you open the result in a protected manner. <br> It is like a shield of protection. <br> The shield hides your location from the websites you visit.<br> You can find more information about Open Anonymously <a href = "/hilfe/easy-language/privacy-protection#proxy/">here<a title="A person is anonymous. So, nobody knows: Who is this person? Click here for more information." href="/hilfe/easy-language/glossary#anonymous-open" ><img class="glossary-icon lm-only" src="/img/glossary-icon-lm.svg"/><img class="glossary-icon dm-only" src="/img/glossary-icon-dm.svg"/></a></a>.',
             ],
             "more" => [
-                "title" => 'More:',
-                "1" => 'When you click on <strong>MORE</strong>, there are new options. <br> The result will look different. <br> It will look like this:',
+                "title" => '<img id="help-ellipsis-image" src="/img/ellipsis.svg"/>:',
+                "1" => 'When you click on <img id="help-ellipsis-image" src="/img/ellipsis.svg"/>, there are new options. <br> The result will look different. <br> It will look like this:',
             ],
             "2" => 'There are new options:',
             "domainnewsearch" => [
                 "title" => 'Start a new search on this domain<a title="Word explanation" href="/hilfe/easy-language/glossary#domain" ><img class="glossary-icon lm-only" src="/img/glossary-icon-lm.svg"/><img class="glossary-icon dm-only" src="/img/glossary-icon-dm.svg"/></a>:',
                 "1" => 'By pressing this button, you search only on this page.',
                 "2" => 'Example:',
-                "3" => 'You find a result from the Duden website. <br> You click on <strong>MORE</strong> for that result. <br> Then you click on the button <strong>start a new search on this domain<a title="Word explanation" href="/hilfe/easy-language/glossary#domain" ><img class="glossary-icon lm-only" src="/img/glossary-icon-lm.svg"/><img class="glossary-icon dm-only" src="/img/glossary-icon-dm.svg"/></a></strong>. <br> Then you search again, but only on the Duden website.',
+                "3" => 'You find a result from the Duden website. <br> You click on <img id="help-ellipsis-image" src="/img/ellipsis.svg"/> for that result. <br> Then you click on the button <strong>start a new search on this domain<a title="Word explanation" href="/hilfe/easy-language/glossary#domain" ><img class="glossary-icon lm-only" src="/img/glossary-icon-lm.svg"/><img class="glossary-icon dm-only" src="/img/glossary-icon-dm.svg"/></a></strong>. <br> Then you search again, but only on the Duden website.',
             ],
             "hideresult" => [
                 "title" => 'Hide:',

d53124c67fa20b3e54411ec97267987861d6e4b9
Dominik Hebeler <dominik@hebeler.club>
2023-09-05 10:10:35 +0200

    fix link to contact page

--- metager/lang/en/team.php
+++ metager/lang/en/team.php
@@ -15,7 +15,7 @@ return [
         '9' => 'trainee',
     ],
     'contact' => [
-        '1' => 'Please report your questions/problems about MetaGer etc. using the <a href="/en/kontakt/">contact form with encryption</a>.',
+        '1' => 'Please report your questions/problems about MetaGer etc. using the <a href=":link_contact">contact form</a>.',
         '2' => 'If you get any emails with strange contents from us please read more about this: <a href="https://metager.de/wsb/fakemail/">https://metager.de/wsb/fakemail/</a>',
         '3' => 'Only in reasoned exceptions, if you want to reach someone directly, you should mail them. Because team members might be on vacation, sick etc.',
     ],

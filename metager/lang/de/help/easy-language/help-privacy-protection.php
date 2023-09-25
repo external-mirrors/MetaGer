7b3d30cb49164ef6c3a397965aca423d1371d73c
Kim <kim@suma-ev.de>
2023-09-25 10:48:25 +0200

    change text for easy lang help

--- metager/lang/de/help/easy-language/help-privacy-protection.php
+++ metager/lang/de/help/easy-language/help-privacy-protection.php
@@ -7,9 +7,9 @@ return [
     
     "datenschutz"  => [
         "title" => "Anonymität und Daten-Sicherheit",
-        "1" => 'Tracking-Cookies, Session-IDs und IP-Adressen',
+        "1" => 'Cookies',
         "2" => 'Bei MetaGer ist man anonym. <br> Das heißt man ist unerkannt. <br> Wir speichern keine Daten mit denen man jemanden erkennen kann. <br> Wir bieten 2 Funktionen dafür an: <br> Tor-Hidden-Service und <br> Anonym öffnen',
-        "3" => "Bei <a href = \"/hilfe/easy-language/privacy-protection#torhidden\">Tor-Hidden-Service</a> finden sie mehr über das Thema. <br> Bei <a href = \"/hilfe/easy-language/privacy-protection#proxy\">MetaGer-Proxy</a> finden Sie mehr über das Thema <strong>Anonym öffnen</strong>. <br> Die Funktionen sind unter <strong>Dienste</strong> in der Navigations-leiste erreichbar.",
+        "3" => "Bei <a href = \"/hilfe/easy-language/privacy-protection#torhidden\">Tor-Hidden-Service</a> finden sie mehr über das Thema. <br> Bei <a href = \"/hilfe/easy-language/privacy-protection#proxy\">Anonym öffnen</a> finden Sie mehr über das Thema. <br> Die Funktionen sind unter <strong>Dienste</strong> in der Navigations-leiste erreichbar.",
     ],
     "tor"  => [
         "title" => "Tor-Hidden-Service",
@@ -17,7 +17,7 @@ return [
         "2" => "MetaGer erreichen Sie im Tor-Browser dann unter: http://metagerv65pwclop2rsfzg4jwowpavpwd6grhhlvdgsswvo6ii4akgyd.onion",    
     ],
     "proxy"  => [
-        "title" => "MetaGer-Proxy (Anonym öffnen)",
+        "title" => "Anonym öffnen",
         "1" => "Mit unserem MetaGer-Proxy kann man Web-Seiten anonym öffnen. <br> Das heißt, dass man unerkannt bleibt. <br> Der MetaGer-Proxy ist also wie ein Schutz-Schild. <br> Wenn Sie unerkannt suchen wollen geht das so: <br> Bei den Ergebnissen gibt es den Knopf <strong>Anonym öffnen</strong>. <br> So sieht der Knopf aus:",
         "2" => "Wenn man auf den Knopf drückt sucht man unerkannt. <br> Wenn Sie oben in der Such-Leiste eine neue Web-Seite eingeben, <br> suchen Sie nicht mehr unerkannt.",
     ],

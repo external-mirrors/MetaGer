538f097eee96c497d2af7f7f286c4632eb9ec2ee
Dominik Hebeler <dominik@suma-ev.de>
2023-10-11 16:04:12 +0200

    add masonry polyfill

--- metager/resources/js/imagesearch.js
+++ metager/resources/js/imagesearch.js
@@ -1,13 +1,15 @@
 (() => {
     // Polyfill for Browsers without Has selector
     let tries = 0;
-    let timeout = setTimeout(() => {
+    let interval = setInterval(polyfillHasSelector, 100);
+
+    function polyfillHasSelector() {
         let supported = getComputedStyle(document.querySelector("body")).getPropertyValue("--supports-selector-has");
         if (supported != "0" && supported != "1") {
             tries++;
             return;
         }
-        clearTimeout(timeout);
+        clearInterval(interval);
         if (supported == "0") {
             console.log("CSS Has Selector not supported by your browser. Enabling JS Polyfill.");
             let maxWidth = getComputedStyle(document.querySelector("body")).getPropertyValue("--full-screen-details-breakpoint");
@@ -26,5 +28,29 @@
                 document.querySelector("body").style.overflow = "auto";
             })
         }
-    })
+    }
+})();
+
+(() => {
+
+    let interval = setInterval(polyfillGrid, 100);
+
+    function polyfillGrid() {
+        // Polyfill for Browsers that do not support masonry grid style
+        let gridContainer = document.querySelector("#results-container > .image-container");
+        if (!gridContainer) {
+            return;
+        } else {
+            clearInterval(interval);
+        }
+        if (getComputedStyle(gridContainer).gridTemplateRows == 'masonry') {
+            return;
+
+        }
+        console.log("CSS only Grid masonry is not supported by your browser. Enabling JS Polyfill.");
+        gridContainer.classList.add("js-masonry");
+        const Masonry = require("masonry-layout");
+        let masonry = new Masonry(gridContainer, { gutter: 10 });
+        console.log(masonry);
+    }
 })();

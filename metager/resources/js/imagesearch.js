9032b2bf5accf578dc61635b65e280e912df1c0e
Dominik Hebeler <dominik@suma-ev.de>
2023-10-12 12:14:01 +0200

    using a more suitable masonry library

--- metager/resources/js/imagesearch.js
+++ metager/resources/js/imagesearch.js
@@ -37,7 +37,7 @@
 
     function polyfillGrid() {
         // Polyfill for Browsers that do not support masonry grid style
-        let gridContainer = document.querySelector("#results-container > .image-container");
+        let gridContainer = document.querySelector("#results-container > .image-container > .images");
         if (!gridContainer) {
             return;
         } else {
@@ -45,12 +45,35 @@
         }
         if (getComputedStyle(gridContainer).gridTemplateRows == 'masonry') {
             return;
-
         }
         console.log("CSS only Grid masonry is not supported by your browser. Enabling JS Polyfill.");
         gridContainer.classList.add("js-masonry");
+        const Macy = require("macy");
+        let macy_layout = Macy({
+            container: gridContainer,
+            columns: 6,
+            margin: 10,
+            waitForImages: false,
+            breakAt: {
+                1450: 5,
+                1200: 4,
+                965: 3,
+                550: 2
+            }
+        });
+        macy_layout.runOnImageLoad(() => {
+            macy_layout.recalculate(true);
+        }, true);
+
+        // Recalculate layout on resize
+        window.addEventListener("resize", e => {
+            macy_layout.recalculate(true);
+        });
+
+        /*
+        gridContainer.classList.add("js-masonry");
         const Masonry = require("masonry-layout");
         let masonry = new Masonry(gridContainer, { gutter: 10 });
-        console.log(masonry);
+        console.log(masonry);*/
     }
 })();
--- metager/resources/views/layouts/image_result.blade.php
+++ metager/resources/views/layouts/image_result.blade.php
@@ -1,7 +1,7 @@
 <label class="image" for="result-checkbox-{{ $index }}" data-width="{{ $result->image->thumbnail_width }}"
     data-height="{{ $result->image->thumbnail_height }}">
     <div title="{{ $result->titel }}" class="image">
-        <img src="{{ $result->image->thumbnail_proxy }}" alt="{{ $result->titel }}" loading="lazy"
+        <img src="{{ $result->image->thumbnail_proxy }}" alt="{{ $result->titel }}" loading="lazy" fetchpriority="low"
             width="{{ $result->image->thumbnail_width }}" height="{{ $result->image->thumbnail_height }}" />
         <!--<div>{{ $result->gefVon[0] }}</div>-->
     </div>

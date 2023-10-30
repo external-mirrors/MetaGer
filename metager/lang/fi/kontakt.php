6cb427caf06b13a4f25069b5c1ef36964527f890
Anonymous <noreply@weblate.org>
2023-10-30 11:12:10 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (21 of 21 strings)
    
    Translation: MetaGer/Kontakt
    Translate-URL: https://translate.metager.de/projects/metager/kontakt/fi/

--- metager/lang/fi/kontakt.php
+++ metager/lang/fi/kontakt.php
@@ -9,12 +9,14 @@ return [
     ],
     'headline' => [
         '1' => 'Ota yhteyttä',
+        '2' => 'Sähköposti',
+        'pgp' => 'Salaus',
     ],
     'form' => [
-        '1' => 'Turvallinen yhteydenottolomake',
-        '2' => 'Tämän lomakkeen kautta voit ottaa meihin yhteyttä sähköpostitse.',
-        'name' => 'Etunimi, sukunimi',
-        '5' => 'Sähköpostiosoitteesi',
+        '1' => 'Anonyymi yhteydenottolomake',
+        '2' => 'Voit lähettää meille nimettömän viestin tällä lomakkeella. Jos päätät olla ilmoittamatta sähköpostiosoitettasi, et tietenkään saa vastausta.',
+        'name' => 'Nimi',
+        '5' => 'Sähköpostiosoitteesi (vapaaehtoinen)',
         '6' => 'Viestisi',
         '7' => 'Aihe',
         '8' => 'Lähetä',
@@ -27,5 +29,13 @@ return [
 Röselerstr. 3\r
 30159 Hannover\r
 Saksa",
+    ],
+    'email' => [
+        'text' => 'Voit ottaa meihin yhteyttä lähettämällä sähköpostia osoitteeseen: <a href="mailto::mail">:mail</a>',
+        'pgp' => [
+            'description' => 'Sähköpostimme on kryptografisesti allekirjoitettu. Jos haluat tarkistaa allekirjoituksen tai lähettää sähköpostisi salattuna, käytä seuraavaa julkista avainta. Jos haluat saada salatun vastauksen, liitä julkinen avaimesi salattuun ja allekirjoitettuun sähköpostiisi.',
+            'pubkey' => 'PGP Publiclickey: <a href="/download/pubkey.asc" download="0x2185CC8F3CA782EC.asc">0x2185CC8F3CA782EC</a> tai osoitteessa <a href=":keyserver" target="_blank" rel="noopener">keys.openpgp.org.</a>',
+            'fingerprint' => 'PGP Fingerprint: 5FA5 2398 C382 B498 B14A B7F6 2185 CC8F 3CA7 82EC',
+        ],
     ],
 ];

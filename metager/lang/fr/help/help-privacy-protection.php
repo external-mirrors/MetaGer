7c4ac9c67233711e0729b64d2ef36d02d28a8a32
Anonymous <noreply@weblate.org>
2023-12-14 17:31:08 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (19 of 19 strings)
    
    Translation: MetaGer/Help-Privacy-Protection
    Translate-URL: https://translate.metager.de/projects/metager/help-privacy-protection/fr/

--- metager/lang/fr/help/help-privacy-protection.php
+++ metager/lang/fr/help/help-privacy-protection.php
@@ -3,29 +3,35 @@ return [
     'title' => 'MetaGer - Aide',
     'maps' => [
         'title' => 'Cartes MetaGer',
-        '3' => 'Les cartes sont rendues avant (sauf pour les trois dernières) et disponibles rapidement. Le niveau de zoom peut être modifié à l\'aide de la molette de la souris ou des boutons "+ / -" situés dans le coin supérieur gauche de la carte.',
-        '1' => 'La protection de la vie privée à l\'ère de la mondialisation des données nous a également incités à développer <a href="https://maps.metager.de" target="_blank">https://maps.metager.de</a>: Le seul (à notre connaissance) planificateur d\'itinéraires qui offre une fonctionnalité complète via le navigateur et l\'application - sans stocker les emplacements des utilisateurs.  Tout cela est vérifiable, car notre logiciel est open source. Pour l\'utilisation de maps.metager.de, nous recommandons notre version rapide de l\'application. Vous pouvez télécharger nos applications sur <a href="/app" target="_blank">Get an App</a> (ou bien sûr via le Play Store).',
-        '2' => 'Après chargement, la carte affiche les POI selon les résultats de MetaGer. Ils apparaissent également dans la colonne de droite. Le passage de la souris sur un POI met en évidence son homologue. Cliquez sur "Détails" pour obtenir des informations complémentaires (base de données Nominatim) sur ce POI.',
+        '3' => 'Au chargement, la carte affiche les points (POI = Points d\'Intérêt) trouvés par MetaGer, qui sont également listés dans la colonne de droite. Lors d\'un zoom, cette liste s\'adapte à la section de la carte. En passant la souris sur un marqueur de la carte ou de la liste, l\'élément correspondant est mis en évidence. Cliquez sur "Détails" pour obtenir plus d\'informations sur ce point à partir de la base de données ci-dessous.',
+        '1' => 'La préservation de la vie privée à l\'ère des géants mondiaux de l\'information nous a également conduits à développer <a href="https://maps.metager.de" target="_blank">https://maps.metager.de</a>: le (à notre connaissance) seul planificateur d\'itinéraires qui offre une fonctionnalité complète via un navigateur et une application sans stocker les emplacements de l\'utilisateur. Tout cela est vérifiable car notre logiciel est open source. Pour l\'utilisation de maps.metager.de, nous recommandons notre version rapide de l\'application. Vous pouvez télécharger nos applications à partir de <a href="/app" target="_blank">ici</a> (ou bien sûr aussi à partir du Play Store).',
+        '2' => 'Cette fonction cartographique est également accessible à partir de la recherche MetaGer (et vice versa). Une fois que vous avez recherché un terme dans MetaGer, vous verrez apparaître un nouvel axe de recherche "Cartes" dans le coin supérieur droit. En cliquant dessus, vous accéderez à la carte correspondante.',
     ],
     'backarrow' => 'Retour',
     'datenschutz' => [
-        '1' => 'Cookies de suivi, identifiants de session, adresses IP',
         '3' => 'Cliquez / touchez le menu des hamburgers pour utiliser les outils.',
     ],
     'tor' => [
-        'title' => 'Service caché Tor',
-        '1' => 'MetaGer fournit le niveau de sécurité le plus élevé possible pour votre vie privée (adresses IP anonymes, serveurs soumis à la loi allemande sur la protection de la vie privée). Pour des besoins de sécurité supplémentaires ou par crainte de serveurs compromis, vous pouvez utiliser la branche MetaGer-Tor. Veuillez télécharger le navigateur spécialisé à partir de <a href="https://www.torproject.org/" target="_blank" rel="noopener">https://www.torproject.org/</a>. Vous y trouverez également de l\'aide.',
-        '2' => 'MetaGer Adresse Tor : http://metagerv65pwclop2rsfzg4jwowpavpwd6grhhlvdgsswvo6ii4akgyd.onion',
+        'title' => 'Service caché TOR <a title="For easy help, click here" href="/hilfe/easy-language/privacy-protection#eh-torhidden" ><img class="easy-help-icon lm-only" src="/img/help-questionmark-icon-lm.svg"/><img class="easy-help-icon dm-only" src="/img/help-questionmark-icon-dm.svg"/></a>',
+        '1' => 'Depuis de nombreuses années, MetaGer cache et ne stocke pas les adresses IP. Cependant, ces adresses sont temporairement visibles sur le serveur MetaGer pendant qu\'une recherche est en cours : si MetaGer était compromis, un attaquant pourrait lire et stocker vos adresses. Pour répondre aux exigences de sécurité les plus élevées, nous exploitons une instance de MetaGer sur le réseau Tor : le service caché MetaGer TOR - accessible via : <a href="/tor/" target="_blank" rel="noopener">https://metager.de/tor/</a>. Pour l\'utiliser, vous avez besoin d\'un navigateur spécial, que vous pouvez télécharger à partir de <a href="https://www.torproject.org/" target="_blank" rel="noopener">https://www.torproject.org/</a>.',
+        '2' => 'Vous pouvez accéder à MetaGer dans le navigateur Tor à l\'adresse suivante : http://metagerv65pwclop2rsfzg4jwowpavpwd6grhhlvdgsswvo6ii4akgyd.onion .',
     ],
     'proxy' => [
-        'title' => 'Serveur proxy MetaGer',
-        '1' => 'Cliquez ou touchez "ouvrir anonymement" pour utiliser le serveur proxy de MetaGer. La protection fournie est limitée au site web que vous avez atteint à partir de notre page de résultats. La protection persiste tant que vous voyez https://proxy.suma-ev.de/?url=...in dans le champ d\'adresse de votre navigateur.',
+        'title' => 'Anonymisation du serveur proxy MetaGer <a title="For easy help, click here" href="/hilfe/easy-language/privacy-protection#eh-proxy" ><img class="easy-help-icon lm-only" src="/img/help-questionmark-icon-lm.svg"/><img class="easy-help-icon dm-only" src="/img/help-questionmark-icon-dm.svg"/></a>',
+        '1' => 'Pour l\'utiliser, il vous suffit de cliquer sur "OPEN ANONYMOUSLY" en bas du résultat sur la page de résultats de MetaGer. Votre demande sera alors acheminée vers le site web cible via notre serveur proxy anonyme, et vos données personnelles resteront entièrement protégées. Important : si vous suivez des liens sur les pages à partir de ce point, vous resterez protégé par le proxy. Toutefois, vous ne pouvez pas saisir une nouvelle adresse dans le champ d\'adresse situé en haut de la page. Dans ce cas, vous perdrez votre protection. Vous pouvez vérifier si vous êtes toujours protégé dans le champ d\'adresse, qui affichera : https://proxy.suma-ev.de/?url=here est l\'adresse actuelle.',
     ],
     'content' => [
-        'title' => 'Contenu inapproprié / protection de la jeunesse',
+        'title' => 'Contenu douteux / Protection de la jeunesse <a title="For easy help, click here" href="/help/easy-language/privacy-protection#eh-content" ><img class="easy-help-icon lm-only" src="/img/help-questionmark-icon-lm.svg"/><img class="easy-help-icon dm-only" src="/img/help-questionmark-icon-dm.svg"/></a>',
         'explanation' => [
-            '1' => 'J\'ai trouvé des résultats avec un contenu illégal.',
-            '2' => 'Si un contenu (présenté par MetaGer) vous semble illégal, veuillez nous envoyer un courriel à <a href="mailto:jugendschutz@metager.de" target="_blank" rel="noopener">jugendschutz@metager.de</a>.',
+            '1' => 'J\'ai reçu des "hits" que je trouve non seulement ennuyeux, mais qui contiennent aussi, à mon avis, des contenus illégaux !',
+            '2' => 'Si vous trouvez sur l\'internet un contenu que vous considérez comme illégal ou préjudiciable aux mineurs, vous pouvez contacter <a href="mailto:hotline@jugendschutz.net" target="_blank" rel="noopener">hotline@jugendschutz.net</a> par courrier électronique ou visiter <a href="http://www.jugendschutz.net/" target="_blank" rel="noopener">www.jugendschutz.net</a> et remplir le formulaire de plainte qui y est disponible. Il est utile de fournir une brève note sur ce que vous considérez comme inadmissible et sur la manière dont vous êtes tombé sur ce contenu. Vous pouvez également nous signaler directement tout contenu douteux. Pour ce faire, envoyez un courriel à notre responsable de la protection de la jeunesse (<a href="mailto:jugendschutz@metager.de" target="_blank" rel="noopener">jugendschutz@metager.de</a>).',
         ],
     ],
+    'privacy' => [
+        'title' => "Anonymat et sécurité des données",
+        '2' => 'Aucune de ces données n\'est utilisée, stockée, conservée ou traitée d\'une autre manière chez MetaGer (exception : stockage à court terme pour la protection contre le piratage et les attaques de robots). Parce que nous considérons ce sujet extrêmement important, nous avons également créé des moyens pour vous aider à atteindre le plus haut niveau de sécurité : le service caché MetaGer TOR et notre serveur proxy anonyme.',
+        '3' => "De plus amples informations sont disponibles ci-dessous. Les fonctions sont accessibles sous \"Services\" dans la barre de navigation.",
+        '1' => 'Cookies de suivi, identifiants de session et adresses IP <a title="For easy help, click here" href="/hilfe/easy-language/privacy-protection#eh-tracking" ><img class="easy-help-icon lm-only" src="/img/help-questionmark-icon-lm.svg"/><img class="easy-help-icon dm-only" src="/img/help-questionmark-icon-dm.svg"/></a>',
+    ],
+    'easy-help' => 'En cliquant sur le symbole <a title="For easy help, click here" href="/hilfe/easy-language/privacy-protection" ><img class="easy-help-icon lm-only" src="/img/help-questionmark-icon-lm.svg"/><img class="easy-help-icon dm-only" src="/img/help-questionmark-icon-dm.svg"/></a> , vous accéderez à une version simplifiée de l\'aide.',
 ];

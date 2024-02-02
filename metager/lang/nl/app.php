892faf67749dd6db70af0f7401b506437a3e6bd1
Anonymous <noreply@weblate.org>
2024-02-02 13:27:20 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: MetaGer/App
    Translate-URL: https://translate.metager.de/projects/metager/app/nl/

--- metager/lang/nl/app.php
+++ metager/lang/nl/app.php
@@ -10,14 +10,12 @@ return [
     'maps' => [
         '1' => 'Deze app biedt een native integratie van <a href="https://maps.metager.de" target="_blank">MetaGer Maps</a> (mogelijk gemaakt door <a href="https://www.openstreetmap.de/" target="_blank">Openstreetmap</a>) op je mobiele Android-toestel.',
         '2' => 'Daarom werkt de routeplanner en de navigatieservice erg snel op je smartphone. De app is sneller vergeleken met het gebruik in een mobiele webbrowser. En er zijn nog meer voordelen - bekijk het zelf!',
-        '3' => 'Na de eerste start wordt om de volgende rechten gevraagd:',
+        '3' => 'De APK voor handmatige installatie is ongeveer 4x zo groot als de installatie in de Playstore (~250MB) omdat het bibliotheken bevat voor alle gangbare CPU-architecturen. De geïntegreerde updater kent de CPU-architectuur van je apparaat en installeert de juiste (kleine) versie van de app bij de eerste update. Als je de architectuur van je apparaat zelf kent, kun je ook <a href="https://gitlab.metager.de/metagermaps/android/-/releases/permalink/latest" target="_blank">direct het kleine pakket</a> installeren.',
         'list' => [
             '1' => 'Toegang tot plaatsbepalingsgegevens => Met geactiveerde GPS kunnen we betere zoekresultaten bieden. Hiermee krijg je toegang tot de stap-voor-stap navigatie. <b> Natuurlijk slaan we je gegevens niet op en geven we je gegevens niet door aan derden.</b>',
-            '2' => 'Toegang tot afbeeldingen, media en bestanden op het apparaat => Deze toestemming is nodig voor de automatische update van de App. Als er een nieuwere versie is, kan deze worden opgeslagen in je downloadarchief en wordt deze automatisch geïnstalleerd. Zonder deze toestemming moet je elke update handmatig installeren.',
+            '2' => 'De APK voor handmatige installatie heeft een geïntegreerde updater. Om het updateprogramma te laten werken, vraagt de app toestemming om meldingen te plaatsen om je op de hoogte te stellen van een beschikbare update en gebruikt hij de Android-toestemming REQUEST_INSTALL_PACKAGES zodat hij je kan vragen om de update van de app te installeren.',
         ],
-    ],
-    'installation' => [
-        '2' => 'Volg deze stappen voor een handmatige installatie:',
+        '4' => 'Na de eerste start wordt om de volgende rechten gevraagd:',
     ],
     'list' => [
         '2' => 'Gebruik hier "Handmatige installatie" om het bestand te downloaden.',

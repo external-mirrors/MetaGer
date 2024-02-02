59f71c87120e60fe1b44ee7bad556b812c43b20e
Anonymous <noreply@weblate.org>
2024-02-02 13:27:10 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: MetaGer/App
    Translate-URL: https://translate.metager.de/projects/metager/app/es/

--- metager/lang/es/app.php
+++ metager/lang/es/app.php
@@ -19,14 +19,12 @@ return [
     'maps' => [
         '1' => 'Esta aplicación proporciona una integración nativa de <a href="https://maps.metager.de" target="_blank">MetaGer Maps</a> (impulsado por <a href="https://www.openstreetmap.de/" target="_blank">Openstreetmap</a>) en su dispositivo móvil Android.',
         '2' => 'De este modo el servicio de mapas y navegación también funciona de manera óptima y rápida en su smartphone. La aplicación aumenta el rendimiento en comparación con el uso en el navegador móvil y ofrece algunas otras ventajas. ¡Intentalo!',
-        '3' => 'Después del primer inicio, la aplicación solicita los siguientes permisos:',
+        '3' => 'El APK para la instalación manual es aproximadamente 4 veces el tamaño de la instalación playstore (~ 250 MB), ya que contiene las bibliotecas para todas las arquitecturas de CPU comunes. El actualizador integrado conocerá la arquitectura de la CPU de tu dispositivo e instalará la versión correcta (pequeña) de la aplicación en la primera actualización. Si usted mismo conoce la arquitectura de su dispositivo también puede <a href="https://gitlab.metager.de/metagermaps/android/-/releases/permalink/latest" target="_blank">instalar directamente el paquete pequeño</a>.',
         'list' => [
             '1' => 'Acceso a datos de posición => Si el GPS está activado en su teléfono móvil, podemos mejorar sus resultados de búsqueda. Además habilita la función de navegación paso a paso.<b>Por supuesto, estos datos no se almacenan en ningún lugar y ciertamente no se transmiten a terceros.</b>',
-            '2' => 'Acceso a fotos, medios y archivos en el dispositivo => Esta autorización es necesaria para que esta aplicación pueda actualizarse sola. Si hay una nueva versión de la aplicación disponible, puede guardarla en su directorio de descarga y luego instalarla automáticamente desde allí. Si no desea otorgar este permiso, deberá reinstalar manualmente la aplicación cada vez que haya una actualización disponible.',
+            '2' => 'El APK para la instalación manual tiene un actualizador integrado. Para que el actualizador funcione, la aplicación le pedirá permiso para publicar notificaciones con el fin de notificarle de una actualización disponible y utiliza el permiso de Android REQUEST_INSTALL_PACKAGES para que pueda pedirle que instale la actualización de la aplicación.',
         ],
-    ],
-    'installation' => [
-        '2' => 'Si desea instalar nuestra aplicación manualmente (sin App Store), proceda de la siguiente manera:',
+        '4' => 'Tras el primer inicio se le pedirán los siguientes permisos:',
     ],
     'list' => [
         '2' => 'Seleccione el enlace "Instalación manual" en esta página para descargar el archivo de instalación en su dispositivo móvil.',

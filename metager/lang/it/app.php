10ec0f0abca87b1589d5fe5a2bf0a1581c958745
Anonymous <noreply@weblate.org>
2024-02-02 13:27:14 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (16 of 16 strings)
    
    Translation: MetaGer/App
    Translate-URL: https://translate.metager.de/projects/metager/app/it/

--- metager/lang/it/app.php
+++ metager/lang/it/app.php
@@ -7,16 +7,14 @@ return [
         '4' => 'Installazione',
     ],
     'maps' => [
-        '3' => 'Dopo il primo avvio verranno richieste le seguenti autorizzazioni:',
+        '3' => 'L\'APK per l\'installazione manuale è circa 4 volte più grande di quello del playstore (~250MB) perché contiene librerie per tutte le architetture CPU più comuni. Il programma di aggiornamento integrato conosce l\'architettura della CPU del dispositivo e installa la versione corretta (piccola) dell\'app al primo aggiornamento. Se si conosce l\'architettura del proprio dispositivo, si può anche <a href="https://gitlab.metager.de/metagermaps/android/-/releases/permalink/latest" target="_blank">installare direttamente il pacchetto piccolo</a>.',
         '1' => 'Questa App fornisce un\'integrazione nativa di <a href="https://maps.metager.de" target="_blank">MetaGer Maps</a> (powered by <a href="https://www.openstreetmap.de/" target="_blank">Openstreetmap</a>) sul vostro dispositivo mobile Android.',
         '2' => 'Pertanto, il pianificatore di itinerari e il servizio di navigazione funzionano molto velocemente sul vostro smartphone. L\'applicazione è più veloce rispetto all\'utilizzo in un browser web mobile. E ci sono altri vantaggi... scopriteli!',
         'list' => [
             '1' => 'Accesso ai dati di posizionamento => Con il GPS attivato possiamo fornire risultati di ricerca migliori. In questo modo si ottiene l\'accesso alla navigazione passo-passo. <b> Naturalmente, non memorizziamo i vostri dati e non li cediamo a terzi.</b>',
-            '2' => 'Accesso a immagini, media e file sul dispositivo => Questa autorizzazione è necessaria per l\'aggiornamento automatico dell\'App. Se esiste una versione più recente, questa può essere memorizzata nell\'archivio dei download e verrà installata automaticamente. Senza questa autorizzazione è necessario eseguire un\'installazione manuale ogni volta che c\'è un aggiornamento.',
+            '2' => 'L\'APK per l\'installazione manuale ha un programma di aggiornamento integrato. Affinché il programma di aggiornamento funzioni, l\'applicazione chiederà il permesso di inviare notifiche per avvisare l\'utente di un aggiornamento disponibile e utilizzerà il permesso Android REQUEST_INSTALL_PACKAGES per chiedere all\'utente di installare l\'aggiornamento dell\'applicazione.',
         ],
-    ],
-    'list' => [
-        '3' => 'Ora aprite il file sul vostro dispositivo. In questo modo verrà installata l\'applicazione.',
+        '4' => 'Dopo il primo avvio verranno richieste le seguenti autorizzazioni:',
     ],
     'disclaimer' => [
         '1' => 'Al momento disponiamo solo di una versione Android della nostra applicazione.',
@@ -27,8 +25,5 @@ return [
         'playstore' => 'Google Playstore',
         'fdroid' => 'Negozio F-Droid',
         'manuell' => 'Installazione manuale',
-    ],
-    'installation' => [
-        '2' => 'Per l\'installazione manuale, attenersi alla seguente procedura:',
     ],
 ];

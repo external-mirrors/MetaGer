a495e57a3c0208164ff2481668f83d453f5bdb91
Anonymous <noreply@weblate.org>
2024-04-15 11:34:02 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MetaGer/Impressum
    Translate-URL: https://translate.metager.de/projects/metager/impressum/es/

--- metager/lang/es/impressum.php
+++ metager/lang/es/impressum.php
@@ -6,9 +6,14 @@ return [
     ],
     'info' => [
         '1' => 'Articulo de Wikipedia de <a href="http://de.wikipedia.org/wiki/Suma_e.V." target="_blank" rel="noopener">SUMA-EV</a>',
-        '2' => "SUMA-EV\r\nRöselerstr. 3\r\nD-30159 Hannover\r\nDeutschland/Germany",
-        '3' => "Contacto:\r\nTel.: +4951134000070\r\nEMail: <a href=\"/es/kontakt\">Formulario encriptado</a>",
-        '4' => 'Junta directiva: Dominik Hebeler, Carsten Riel, Manuela Branz',
+        '2' => "SUMA-EV\r
+Röselerstr. 3\r
+D-30159 Hannover\r
+Deutschland/Germany",
+        '3' => "Contacto:\r
+Tel.: +4951134000070\r
+EMail: <a href=\"/es/kontakt\">Formulario encriptado</a>",
+        '4' => 'Junta Directiva: Dominik Hebeler, Phil Höfer, Carsten Riel, Manuela Branz',
         '6' => 'Encargado de protección de menores: Manuela Branz <a href="mailto:jugendschutz@metager.de">jugendschutz@metager.de</a>',
         '8' => '"SUMA-EV - Verein für freien Wissenszugang" es una asociación sin fines de lucro, registrado en el registro de asociaciones del Amtsgericht Hannover bajo numero VR200033. Número de identificación a efectos del IVA:  DE 300 464 091 La "Gottfried Wilhelm Leibniz Universität Hannover" es una entidad del derecho publico.',
         '9' => 'Exención de responsabilidad',

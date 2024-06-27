a74449687f44f07b3e25d734e40636f3ec3008ed
Dominik Hebeler <dominik@suma-ev.de>
2024-06-27 12:40:26 +0200

    disable yahoo-ads

--- metager/app/Models/Configuration/Searchengines.php
+++ metager/app/Models/Configuration/Searchengines.php
@@ -88,7 +88,9 @@ class Searchengines
         }
 
         // Enable Yahoo Ads if query is unauthorized and yahoo is disabled
-        if (!app(Authorization::class)->canDoAuthenticatedSearch() && $settings->fokus !== "bilder" && array_key_exists("yahoo", $this->sumas) && array_key_exists("yahoo-ads", $this->sumas)) {
+        // Do not enable yahoo-ads anymore. For now temporarily
+        // ToDo: Remove or enable again
+        if (1 == 0 && !app(Authorization::class)->canDoAuthenticatedSearch() && $settings->fokus !== "bilder" && array_key_exists("yahoo", $this->sumas) && array_key_exists("yahoo-ads", $this->sumas)) {
             if ($this->sumas["yahoo"]->configuration->disabled === true) {
                 $this->sumas["yahoo-ads"]->configuration->disabled = false;
             }

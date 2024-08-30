f8839e226d0071da12d0822a863bdc478daa214e
Weblate <noreply-mt-weblate@weblate.org>
2024-08-30 19:36:26 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (70 of 70 strings)
    
    Translation: MetaGer/Logs
    Translate-URL: https://translate.metager.de/projects/metager/logs/pl/

--- metager/lang/pl/logs.php
+++ metager/lang/pl/logs.php
@@ -1,1 +1,28 @@
 <?php
+return [
+    'login' => [
+        'email' => 'Adres e-mail',
+        'submit' => 'Prześlij',
+    ],
+    'overview' => [
+        'invoice-data' => [
+            'save' => "Zapisz",
+            'heading' => "Data faktury",
+            'email' => "Adres e-mail",
+            'full_name' => "Nazwa",
+        ],
+    ],
+    'api_keys' => [
+        'delete' => "Usuń",
+        'thead' => [
+            'actions' => "Działania",
+        ],
+        'copy' => "Kopia",
+    ],
+    'orders' => [
+        'thead' => [
+            'invoice' => "Faktura",
+        ],
+        'heading' => "Zamówienia",
+    ],
+];

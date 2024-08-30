1d0d6f971a468ff9ee05eed3bfe376a2d6912a1d
Weblate <noreply-mt-weblate@weblate.org>
2024-08-30 19:36:17 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (70 of 70 strings)
    
    Translation: MetaGer/Logs
    Translate-URL: https://translate.metager.de/projects/metager/logs/nl/

--- metager/lang/nl/logs.php
+++ metager/lang/nl/logs.php
@@ -1,1 +1,28 @@
 <?php
+return [
+    'orders' => [
+        'thead' => [
+            'invoice' => "Factuur",
+        ],
+        'heading' => "Bestellingen",
+    ],
+    'login' => [
+        'email' => 'E-mailadres',
+        'submit' => 'Stuur  in',
+    ],
+    'overview' => [
+        'invoice-data' => [
+            'heading' => "Factuurdatum",
+            'email' => "E-mailadres",
+            'full_name' => "Naam",
+            'save' => "Sla",
+        ],
+    ],
+    'api_keys' => [
+        'thead' => [
+            'actions' => "Acties",
+        ],
+        'copy' => "Kopie",
+        'delete' => "Verwijder",
+    ],
+];

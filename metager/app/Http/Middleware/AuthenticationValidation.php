556a8c36dfa5f440b0ec10726597bc9e77b19218
Dominik Hebeler <dominik@suma-ev.de>
2024-09-10 15:51:22 +0200

    disallow unauthenticated searches

---
+++ metager/app/Http/Middleware/AuthenticationValidation.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Authorization\Authorization;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthenticationValidation
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $whitelisted_ips = explode(",", config("metager.metager.unauth_whitelist"));
+        if (!app(Authorization::class)->canDoAuthenticatedSearch() && !in_array($request->ip(), $whitelisted_ips)) {
+            return redirect(route("startpage"));
+        }
+        return $next($request);
+    }
+}

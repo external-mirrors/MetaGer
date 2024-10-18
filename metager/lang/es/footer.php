1465f5a672046396b59086a535f56a5df31a697c
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2024-10-18 03:49:10 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Footer
    Translate-URL: https://translate.metager.de/projects/metager/footer/es/

--- metager/lang/es/footer.php
+++ metager/lang/es/footer.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'sumaev' => [
-        '1' => 'MetaGer mantiene y desarrolla el',
+        '1' => 'MetaGer mantiene y desarrolla el ',
         '2' => 'SUMA-EV',
         'link' => 'https://suma-ev.de',
     ],

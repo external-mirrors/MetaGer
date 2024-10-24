d10ae36d382578e6ebcf061910282c888af4144e
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2024-10-24 03:45:09 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: MetaGer/Mobile-Chrome
    Translate-URL: https://translate.metager.de/projects/metager/mobile-chrome/es/

--- metager/lang/es/plugin-mobile/mobile-chrome.php
+++ metager/lang/es/plugin-mobile/mobile-chrome.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'default-search-v83' => [
-        '1' => 'En su navegador, pulse "<img class= "mg-icon" src="/img/ellipsis.svg">"</img> en la parte superior derecha para<img class= "mg-icon" src="/img/ellipsis.svg">abrir el menú del navegador</img>.',
+        '1' => 'Pulse "<img class= "mg-icon" src="/img/ellipsis.svg">"</img> en la parte superior derecha de su navegador<img class= "mg-icon" src="/img/ellipsis.svg">para abrir el menú del mismo</img>.',
         '2' => 'Seleccione la opción de menú "Ajustes".',
         '3' => 'En el menú que aparece entonces, seleccione la opción "Motor de búsqueda" en la categoría "Ajustes básicos".',
         '4' => 'Toque MetaGer en la categoría de "Visitados recientemente".',

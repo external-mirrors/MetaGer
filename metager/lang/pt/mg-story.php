0bb05c942ca1950202e44647e662aab2e22fe5e9
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2024-10-28 14:53:51 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: MetaGer/Mg-Story
    Translate-URL: https://translate.metager.de/projects/metager/mg-story/pt/

--- metager/lang/pt/mg-story.php
+++ metager/lang/pt/mg-story.php
@@ -1,4 +1,12 @@
 <?php
 return [
-
+    'plugin' => [
+        'btn-app' => 'MetaGer App',
+    ],
+    'eco' => [
+        'title' => '100% de energia renovável',
+    ],
+    'privacy' => [
+        'title' => 'Privacidade garantida',
+    ],
 ];

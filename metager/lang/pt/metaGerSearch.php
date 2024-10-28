449b7e4d08cb0d27491bb46881b7244953cd88a4
DeepL <noreply-mt-deepl@weblate.org>
2024-10-28 14:53:11 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MetaGer/Metagersearch
    Translate-URL: https://translate.metager.de/projects/metager/metagersearch/pt/

--- metager/lang/pt/metaGerSearch.php
+++ metager/lang/pt/metaGerSearch.php
@@ -1,4 +1,19 @@
 <?php
 return [
-
+    'quicktips' => [
+        'wikipedia' => [
+            'adress' => 'de <a href="https://en.wikipedia.org" target="_blank" rel="noopener"> Wikipedia, A Enciclopédia Livre</a>',
+        ],
+        'dictcc' => [
+            'adress' => 'de <a href="https://www.dict.cc/" target="_blank" rel="noopener">dict.cc</a>',
+        ],
+        'tips' => [
+            'title' => 'Sabia que?',
+        ],
+        'bang' => [
+            'title' => '!bang redireccionamento',
+            'buttonlabel' => 'Continuar para',
+            'from' => 'de',
+        ],
+    ],
 ];

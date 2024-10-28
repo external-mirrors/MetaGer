9a2d7e458a79b0c466bafc82e2ad14f1d49bf917
DeepL <noreply-mt-deepl@weblate.org>
2024-10-28 14:42:08 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: MetaGer/Adblocker
    Translate-URL: https://translate.metager.de/projects/metager/adblocker/pt/

--- metager/lang/pt/adblocker.php
+++ metager/lang/pt/adblocker.php
@@ -1,1 +1,10 @@
 <?php
+return [
+    'heading' => "Bloqueador de anúncios no MetaGer",
+    'adblocker' => "Em alguns casos, estas ligações de afiliados são falsamente ocultadas pelos bloqueadores de anúncios. Se for afetado e pretender que os resultados da pesquisa sejam apresentados na íntegra, tem as seguintes opções:",
+    'options' => [
+        'disable' => "Desativar o bloqueador de anúncios para o MetaGer. Os resultados de pesquisa com hiperligações de afiliados deixam de estar ocultos.",
+        'key' => "Utilize a nossa pesquisa <a href=\":keylink\">sem anúncios</a>. Os resultados da sua pesquisa deixarão de conter quaisquer ligações de afiliados.",
+    ],
+    'free' => "A utilização do MetaGer é gratuita, mas a sua manutenção não é. Sempre financiámos o nosso serviço através de publicidade não personalizada. Uma nova adição é a pesquisa opcional sem anúncios usando uma chave <a href=\":keylink\">MetaGer</a>. A nossa publicidade consiste tanto em publicidade tradicional de pesquisa como em ligações de afiliados nos resultados da pesquisa. Com este rendimento podemos pagar à nossa equipa, os custos do servidor e, claro, os serviços de pesquisa que utilizamos como fontes.",
+];

6379a34443b2889d6aca3ae21a10bf52620cc677
DeepL <noreply-mt-deepl@weblate.org>
2024-10-28 14:54:07 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (23 of 23 strings)
    
    Translation: MetaGer/Help
    Translate-URL: https://translate.metager.de/projects/metager/help/pt/

--- metager/lang/pt/help/help.php
+++ metager/lang/pt/help/help.php
@@ -1,4 +1,31 @@
 <?php
 return [
-
+    'tableofcontents' => [
+        '3' => [
+            '3' => 'Serviço Tor Hidden',
+            '4' => 'Servidor proxy MetaGer',
+            '0' => 'Privacidade',
+            '2' => 'Rastreio e cookies',
+        ],
+        '4' => [
+            '3' => 'MetaGer-Web-Associator',
+            '5' => 'Mapas MetaGer',
+            '0' => 'Serviços',
+            '1' => 'Android-App',
+        ],
+        '1' => [
+            '1' => 'A página inicial',
+            '0' => 'Utilização do motor de busca',
+            '2' => 'O campo de pesquisa',
+            '3' => 'A página de resultados',
+        ],
+        '2' => [
+            '3' => 'Pesquisar na pesquisa',
+            '2' => '!bangs',
+            '4' => 'Adicionar MetaGer',
+            '0' => 'Funções e dicas úteis',
+        ],
+    ],
+    'achtung' => 'Aviso! A estrutura e a funcionalidade do nosso sítio Web estão sujeitas a constantes desenvolvimentos e alterações. Tentamos atualizar as nossas páginas de ajuda o mais rapidamente possível, mas não podemos evitar erros temporários.',
+    'easy.language' => "para uma ajuda fácil",
 ];

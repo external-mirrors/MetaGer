919a4c3ccf3f535e0fc66ee05c659ede4a3dbba2
DeepL <noreply-mt-deepl@weblate.org>
2024-10-28 14:54:29 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (2 of 2 strings)
    
    Translation: MetaGer/Researchandtabs
    Translate-URL: https://translate.metager.de/projects/metager/researchandtabs/pt/

--- metager/lang/pt/researchandtabs.php
+++ metager/lang/pt/researchandtabs.php
@@ -1,3 +1,7 @@
 <?php
 return [
+    'plugin' => [
+        '1' => 'Sabia que o seu navegador :pode utilizar o MetaGer como motor de pesquisa predefinido?',
+        '2' => 'Mostre-me como',
+    ],
 ];

9f7161b8b473fe96bb792ca57169241b3e941025
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2024-10-28 14:58:20 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: MetaGer/Websearch
    Translate-URL: https://translate.metager.de/projects/metager/websearch/pt/

--- metager/lang/pt/websearch.php
+++ metager/lang/pt/websearch.php
@@ -1,3 +1,11 @@
 <?php
 return [
+    'head' => [
+        '7' => 'Código',
+        '5' => 'Pesquisar com MetaGer...',
+        '8' => 'Pesquisar',
+        '2' => 'Aqui encontra um Metager-Widget para o seu sítio Web.',
+        '3' => 'Pré-visualização',
+        'copy' => 'Cópia',
+    ],
 ];

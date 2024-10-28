a79ea7038bdff4ba43fe78d89890189348b3b415
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2024-10-28 15:01:25 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (19 of 19 strings)
    
    Translation: MetaGer/Help-Privacy-Protection
    Translate-URL: https://translate.metager.de/projects/metager/help-privacy-protection/pt/

--- metager/lang/pt/help/help-privacy-protection.php
+++ metager/lang/pt/help/help-privacy-protection.php
@@ -25,5 +25,10 @@ return [
     'easy-help' => 'Ao clicar no símbolo <a title="For easy help, click here" href="/hilfe/easy-language/privacy-protection" ><img class="easy-help-icon lm-only" src="/img/help-questionmark-icon-lm.svg"/><img class="easy-help-icon dm-only" src="/img/help-questionmark-icon-dm.svg"/></a> , acederá a uma versão simplificada da ajuda.',
     'maps' => [
         '1' => 'Preservar a privacidade na era dos gigantes globais de dados também nos levou a desenvolver <a href="https://maps.metager.de" target="_blank">https://maps.metager.de</a>: o (até onde sabemos) único planeador de rotas que oferece todas as funcionalidades através de um navegador e de uma aplicação sem armazenar as localizações dos utilizadores. Tudo isto é verificável porque o nosso software é de código aberto. Para utilizar o maps.metager.de, recomendamos a nossa versão rápida da aplicação. Pode descarregar as nossas aplicações a partir de <a href="/app" target="_blank">aqui</a> (ou, claro, também a partir da Play Store).',
+        '3' => "Aquando do carregamento, o mapa apresenta os pontos (POIs = Pontos de Interesse) encontrados pelo MetaGer, que também são listados na coluna da direita. Ao fazer zoom, esta lista adapta-se à secção do mapa. Ao passar o rato sobre um marcador no mapa ou na lista, o item correspondente é realçado. Clique em 'Detalhes' para obter mais informações sobre esse ponto a partir da base de dados abaixo.",
+        '2' => "Esta função de mapa também pode ser acedida a partir da pesquisa no MetaGer (e vice-versa). Depois de ter pesquisado um termo no MetaGer, verá um novo foco de pesquisa \"Mapas\" no canto superior direito. Clicando nele, acederá a um mapa correspondente.",
+        'title' => "Mapas MetaGer",
     ],
+    'backarrow' => 'Voltar',
+    'title' => 'MetaGer - Ajuda',
 ];

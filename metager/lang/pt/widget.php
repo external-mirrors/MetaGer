2d9d66ae478cf52ac614722257e6d0a016ed2355
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2024-10-28 14:55:25 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: MetaGer/Widget
    Translate-URL: https://translate.metager.de/projects/metager/widget/pt/

--- metager/lang/pt/widget.php
+++ metager/lang/pt/widget.php
@@ -6,4 +6,5 @@ return [
         '4' => 'Atenção: Não deve utilizar este widget se a sua implementação sugerir que o MetaGer é o seu serviço ou que o seu sítio é a verdadeira página inicial do MetaGer (tudo isto já aconteceu). Por conseguinte, é especialmente proibido remover o nosso logótipo.',
         '2' => 'Pesquisar na Web',
     ],
+    'head' => 'MetaGer Widget',
 ];

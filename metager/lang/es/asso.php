8dad66e189e0a95383c591fb10eaa30c9caf066d
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2024-10-28 14:53:55 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (6 of 6 strings)
    
    Translation: MetaGer/Asso
    Translate-URL: https://translate.metager.de/projects/metager/asso/es/

--- metager/lang/es/asso.php
+++ metager/lang/es/asso.php
@@ -4,7 +4,7 @@ return [
         '1' => 'Asociador - MetaGer',
     ],
     '1' => [
-        '1' => 'Si no obtiene el resultado deseado con las palabras clave utilizadas, quizás es porque le faltan las palabras  correctas o mas adecuadas. Consulte nuestro asociador: Ingrese una o más palabras para las que está buscando términos nuevos o mejores. Ahora intente una nueva búsqueda haciendo clic en uno de los términos nuevos que aparecen en la página de resultados. Se aplica nuestra política de protección de datos',
+        '1' => 'Si no obtiene el resultado deseado con las palabras clave utilizadas, quizás es porque le faltan las palabras  correctas o mas adecuadas. Consulte nuestro asociador: Ingrese una o más palabras para las que está buscando términos nuevos o mejores. Ahora intente una nueva búsqueda haciendo clic en uno de los términos nuevos que aparecen en la página de resultados. Se aplica nuestra política de protección de datos ',
         '2' => 'Protección de Datos',
     ],
     'reasso' => [

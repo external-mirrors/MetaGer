5447c40155b777662911a08a551730072606b286
DeepL <noreply-mt-deepl@weblate.org>
2024-10-28 15:01:25 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (19 of 19 strings)
    
    Translation: MetaGer/Help-Privacy-Protection
    Translate-URL: https://translate.metager.de/projects/metager/help-privacy-protection/pt/

--- metager/lang/pt/help/help-privacy-protection.php
+++ metager/lang/pt/help/help-privacy-protection.php
@@ -1,5 +1,26 @@
 <?php
-
 return [
-
+    'tor' => [
+        'title' => 'Serviço Oculto TOR <a title="For easy help, click here" href="/hilfe/easy-language/privacy-protection#eh-torhidden" ><img class="easy-help-icon lm-only" src="/img/help-questionmark-icon-lm.svg"/><img class="easy-help-icon dm-only" src="/img/help-questionmark-icon-dm.svg"/></a>',
+        '2' => "Pode aceder ao MetaGer no browser Tor em: http://metagerv65pwclop2rsfzg4jwowpavpwd6grhhlvdgsswvo6ii4akgyd.onion .",
+        '1' => "Durante muitos anos, o MetaGer tem estado a esconder e a não armazenar endereços IP. No entanto, estes endereços estão temporariamente visíveis no servidor do MetaGer enquanto uma pesquisa está a decorrer: se o MetaGer for comprometido, um atacante poderá ler e armazenar os seus endereços. Para cumprir os mais elevados requisitos de segurança, utilizamos uma instância do MetaGer na rede Tor: o MetaGer TOR Hidden Service - acessível através de: <a href=\"/tor/\" target=\"_blank\" rel=\"noopener\">https://metager.de/tor/</a>. Para o utilizar, é necessário um browser especial, que pode ser descarregado a partir de <a href=\"https://www.torproject.org/\" target=\"_blank\" rel=\"noopener\">https://www.torproject.org/</a>.",
+    ],
+    'content' => [
+        'title' => 'Conteúdo questionável / Proteção dos jovens <a title="For easy help, click here" href="/help/easy-language/privacy-protection#eh-content" ><img class="easy-help-icon lm-only" src="/img/help-questionmark-icon-lm.svg"/><img class="easy-help-icon dm-only" src="/img/help-questionmark-icon-dm.svg"/></a>',
+        'explanation' => [
+            '2' => 'Se encontrar algo na Internet que considere ilegal ou prejudicial para menores, pode contactar <a href="mailto:hotline@jugendschutz.net" target="_blank" rel="noopener">hotline@jugendschutz.net</a> por correio eletrónico ou visitar <a href="http://www.jugendschutz.net/" target="_blank" rel="noopener">www.jugendschutz.net</a> e preencher o formulário de queixa aí disponível. É útil fornecer uma breve nota sobre o que considera inadmissível e como se deparou com esse conteúdo. Também pode denunciar conteúdos questionáveis diretamente a nós. Para o fazer, envie uma mensagem de correio eletrónico para o nosso responsável pela proteção dos jovens (<a href="mailto:jugendschutz@metager.de" target="_blank" rel="noopener">jugendschutz@metager.de</a>).',
+            '1' => 'Recebi "hits" que não só considero incómodos como também contêm, na minha opinião, conteúdos ilegais!',
+        ],
+    ],
+    'privacy' => [
+        '1' => 'Cookies de rastreio, IDs de sessão e endereços IP <a title="For easy help, click here" href="/hilfe/easy-language/privacy-protection#eh-tracking" ><img class="easy-help-icon lm-only" src="/img/help-questionmark-icon-lm.svg"/><img class="easy-help-icon dm-only" src="/img/help-questionmark-icon-dm.svg"/></a>',
+        '3' => "Mais informações podem ser encontradas abaixo. As funções estão acessíveis em \"Serviços\" na barra de navegação.",
+        '2' => 'Nenhum destes dados é utilizado, armazenado, retido ou processado aqui na MetaGer (exceção: armazenamento a curto prazo para proteção contra ataques de hackers e bots). Porque consideramos este tópico extremamente importante, também criámos formas de o ajudar a atingir o mais alto nível de segurança: o serviço MetaGer TOR Hidden e o nosso servidor proxy anónimo.',
+        'title' => "Anonimato e segurança dos dados",
+    ],
+    'proxy' => [
+        'title' => 'Anonimização do servidor proxy MetaGer <a title="For easy help, click here" href="/hilfe/easy-language/privacy-protection#eh-proxy" ><img class="easy-help-icon lm-only" src="/img/help-questionmark-icon-lm.svg"/><img class="easy-help-icon dm-only" src="/img/help-questionmark-icon-dm.svg"/></a>',
+        '1' => "Para o utilizar, basta clicar em \"OPEN ANONYMOUSLY\" na parte inferior do resultado na página de resultados do MetaGer. O seu pedido será então encaminhado para o sítio Web de destino através do nosso servidor proxy anónimo, e os seus dados pessoais permanecerão totalmente protegidos. Importante: se seguir ligações nas páginas a partir deste ponto, continuará protegido pelo proxy. No entanto, não pode introduzir um novo endereço no campo de endereço no topo. Nesse caso, perderá a proteção. Pode ver se ainda está protegido no campo de endereço, que mostrará: https://proxy.suma-ev.de/?url=here é o endereço atual.",
+    ],
+    'easy-help' => 'Ao clicar no símbolo <a title="For easy help, click here" href="/hilfe/easy-language/privacy-protection" ><img class="easy-help-icon lm-only" src="/img/help-questionmark-icon-lm.svg"/><img class="easy-help-icon dm-only" src="/img/help-questionmark-icon-dm.svg"/></a> , acederá a uma versão simplificada da ajuda.',
 ];

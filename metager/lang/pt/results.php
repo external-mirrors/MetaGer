e41a08018821f59e880b3a5937f73421eae2b9fe
DeepL <noreply-mt-deepl@weblate.org>
2024-10-28 14:57:22 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: MetaGer/Results
    Translate-URL: https://translate.metager.de/projects/metager/results/pt/

--- metager/lang/pt/results.php
+++ metager/lang/pt/results.php
@@ -1,3 +1,25 @@
 <?php
 return [
+    'query' => [
+        'alteration' => [
+            'new' => 'Contém resultados para <a href=":link">:alteração <img class= "mg-icon" src="/img/icon-lupe.svg"></a>',
+            'original' => 'Em vez disso, procure por <a href=":link">:original <img class= "mg-icon" src="/img/icon-lupe.svg"></a>',
+        ],
+    ],
+    'images' => [
+        'external' => [
+            'bing' => "Pesquisar no Bing",
+            'buy' => "Comprar uma chave MetaGer",
+            'google' => "Pesquisar no Google",
+            'description' => "Só pesquisou na base de dados de Pixabay. É melhor procurar com uma chave MetaGer.",
+            'heading' => "Faltam aqui alguns resultados",
+            'or' => "ou",
+            'save' => "Recordar decisão (com cookie)",
+        ],
+    ],
+    'results' => [
+        'summary' => 'Mostrar :resultcount de cerca de :totalresults resultados da pesquisa',
+    ],
+    'backtotop' => 'voltar ao topo',
+    'zurueck' => 'voltar',
 ];

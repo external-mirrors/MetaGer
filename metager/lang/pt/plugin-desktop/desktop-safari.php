11283122b8db49cfaabe4773f7a80666bfcf4e0b
DeepL <noreply-mt-deepl@weblate.org>
2024-10-28 14:50:40 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Desktop-Safari
    Translate-URL: https://translate.metager.de/projects/metager/desktop-safari/pt/

--- metager/lang/pt/plugin-desktop/desktop-safari.php
+++ metager/lang/pt/plugin-desktop/desktop-safari.php
@@ -1,4 +1,8 @@
 <?php
 return [
-
+    'default-page-v10' => [
+        '1' => 'Clique em <a href="/" target="_blank" rel="noopener">aqui</a> para abrir o MetaGer num novo separador.',
+        '2' => 'No canto superior esquerdo, clique em "Safari" e selecione "Preferências".',
+        '3' => 'Em "Geral", clique no botão "Definir para a página atual".',
+    ],
 ];

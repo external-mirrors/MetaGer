4b2a58add5b7aa2d00baaef9db2e5093471bebaa
DeepL <noreply-mt-deepl@weblate.org>
2024-10-28 14:51:27 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Quicktip
    Translate-URL: https://translate.metager.de/projects/metager/quicktip/pt/

--- metager/lang/pt/quicktip.php
+++ metager/lang/pt/quicktip.php
@@ -1,3 +1,8 @@
 <?php
 return [
+    'title' => 'Dicas rápidas do MetaGer',
+    'spende' => [
+        'title' => 'MetaGer diz OBRIGADO!',
+        'descr' => 'O seu apoio ajuda-nos a manter o MetaGer.',
+    ],
 ];

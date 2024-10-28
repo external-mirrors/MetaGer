92829a99d137a88512c70a0f9b36f0face2b9f63
DeepL <noreply-mt-deepl@weblate.org>
2024-10-28 14:52:41 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: MetaGer/Mobile-Chrome
    Translate-URL: https://translate.metager.de/projects/metager/mobile-chrome/pt/

--- metager/lang/pt/plugin-mobile/mobile-chrome.php
+++ metager/lang/pt/plugin-mobile/mobile-chrome.php
@@ -1,4 +1,16 @@
 <?php
 return [
-
+    'default-search-v83' => [
+        '1' => 'Toque em "<img class= "mg-icon" src="/img/ellipsis.svg">" no canto superior direito.',
+        '2' => 'Selecionar "Definições".',
+        '3' => 'Em "Básicos", selecione "Motor de busca".',
+        '4' => 'Em "Visitados recentemente", selecione "MetaGer".',
+        '5' => 'Se o MetaGer não aparecer na lista, utilize a barra de pesquisa no final destas instruções e tente novamente.',
+    ],
+    'default-page-v83' => [
+        '1' => 'Toque em "<img class= "mg-icon" src="/img/ellipsis.svg">" no canto superior direito.',
+        '2' => 'Selecionar "Definições".',
+        '3' => 'Em "Avançadas", selecione "Página inicial".',
+        '4' => 'Escolha "Introduzir endereço Web personalizado" e introduza ":link" .',
+    ],
 ];

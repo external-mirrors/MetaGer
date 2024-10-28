75f92bf2f8855b01a6aa6c283f31f9ec2146b51e
DeepL <noreply-mt-deepl@weblate.org>
2024-10-28 14:56:14 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MetaGer/Staticpages
    Translate-URL: https://translate.metager.de/projects/metager/staticpages/pt/

--- metager/lang/pt/staticPages.php
+++ metager/lang/pt/staticPages.php
@@ -1,3 +1,9 @@
 <?php
 return [
+    'meta' => [
+        'language' => 'en',
+        'Description' => 'Pesquise com segurança e respeite a sua privacidade. O conhecimento digital do mundo deve ser de livre acesso, sem paternalismo e sem a tutela de Estados ou corporações.',
+        'Keywords' => 'Pesquisa na Internet, privacidade, motor de busca, proteção de dados, Anonproxy, proxy anónimo, pesquisa anónima, pesquisa de imagens, anónimo, MetaGer, metager, metager.de, metager.net',
+    ],
+    'opensearch' => 'MetaGer: pesquisa e localização protegidas pela privacidade',
 ];

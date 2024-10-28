a746645a6464952e6fab8192d503721aecf4a0cb
DeepL <noreply-mt-deepl@weblate.org>
2024-10-28 14:57:37 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (14 of 14 strings)
    
    Translation: MetaGer/Resultpage
    Translate-URL: https://translate.metager.de/projects/metager/resultpage/pt/

--- metager/lang/pt/resultPage.php
+++ metager/lang/pt/resultPage.php
@@ -1,3 +1,21 @@
 <?php
 return [
+    'skiplinks' => [
+        'settings' => 'Saltar para as definições de pesquisa',
+        'navigation' => 'Saltar para a navegação',
+        'query' => 'Saltar para o campo de introdução da consulta de pesquisa',
+        'return' => 'Pode voltar a este menu em qualquer altura, premindo a tecla de escape',
+        'heading' => 'Saltar rapidamente para o conteúdo',
+        'results' => 'Saltar para os resultados da pesquisa',
+    ],
+    'engines' => [
+        'queried' => 'Serviços de pesquisa consultados',
+        'disabled' => 'Adicionar serviços de pesquisa à consulta',
+        'payment_required' => 'Serviços de pesquisa disponíveis com <a href=\':link\'>MetaGer key</a>',
+    ],
+    'metager3' => 'Está atualmente a utilizar uma versão de teste do MetaGer.',
+    'impressum' => 'aviso de sítio',
+    'opensearch' => 'MetaGer: pesquisa e localização seguras, proteção da privacidade',
+    'startseite' => 'Página inicial do MetaGer',
+    'search-placeholder' => 'A sua consulta de pesquisa...',
 ];

3c4bfc74c60d96e1965f1fee5fa8daaee142f001
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2025-03-26 08:26:00 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Plugin
    Translate-URL: https://translate.metager.de/projects/metager/plugin/pl/

--- metager/lang/pl/plugin.php
+++ metager/lang/pl/plugin.php
@@ -2,4 +2,5 @@
 return [
     'description' => 'MetaGer: bezpieczne wyszukiwanie i znajdowanie, ochrona prywatności',
     'development' => 'MetaGer Development (pl)',
+    'short_name' => 'MetaGer',
 ];

384142efd335d917e336de44aa65310f5a595520
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2025-03-26 08:26:01 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Plugin
    Translate-URL: https://translate.metager.de/projects/metager/plugin/da/

--- metager/lang/da/plugin.php
+++ metager/lang/da/plugin.php
@@ -2,4 +2,5 @@
 return [
     'description' => 'MetaGer: sikker søgning og find, beskyttelse af privatlivets fred',
     'development' => 'MetaGer Development (da)',
+    'short_name' => 'MetaGer',
 ];

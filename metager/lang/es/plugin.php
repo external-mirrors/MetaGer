8e4b45c4bc0c562ed2ffa1c0bf9efbdfd081cba2
DeepL <noreply-mt-deepl@weblate.org>
2025-03-26 08:25:57 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Plugin
    Translate-URL: https://translate.metager.de/projects/metager/plugin/es/

--- metager/lang/es/plugin.php
+++ metager/lang/es/plugin.php
@@ -2,4 +2,5 @@
 return [
     'description' => 'MetaGer: Buscar y encontrar seguro, proteger la esfera privada',
     'development' => 'Versión de desarrollo de MetaGer (es)',
+    'short_name' => 'MetaGer',
 ];

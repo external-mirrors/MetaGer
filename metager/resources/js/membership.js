f32041504bd8710081ab7dde71b1713044f95a87
Dominik Hebeler <dominik@suma-ev.de>
2025-04-01 16:56:11 +0200

    modify input fields for membership form

--- metager/resources/js/membership.js
+++ metager/resources/js/membership.js
@@ -17,10 +17,92 @@ document.querySelectorAll("input[name=payment-method]").forEach((input) => {
   });
 });
 
+(() => {
+  document.querySelectorAll("input[type=radio][name=amount], input[name=interval]").forEach(element => {
+    element.addEventListener("change", updateIntervalAmount);
+  })
+  document.querySelector("#amount-custom-value").addEventListener("keyup", (e) => {
+    let element = e.target;
+    let value = parseFloat(element.value);
+    if (isNaN(value)) return;
+
+    if (value > 100) {
+      element.value = 100;
+    }
+
+    updateIntervalAmount();
+  });
+  updateIntervalAmount();
+})();
+
+(() => {
+  validateAmount();
+  document.querySelector("#amount-custom-value").addEventListener("change", validateAmount);
+})();
+
+function validateAmount() {
+  let element = document.querySelector("#amount-custom-value");
+  let value = parseFloat(element.value);
+  if (isNaN(value)) {
+    document.querySelector("#reduction-container").classList.add("hidden");
+    element.value = "";
+    return
+  };
+
+  if (element.value != value) {
+    element.value = value;
+  }
+
+  if (value < 2.5) {
+    element.value = 2.5;
+  }
+
+  if (value < 5) {
+    document.querySelector("#reduction-container").classList.remove("hidden");
+  } else {
+    document.querySelector("#reduction-container").classList.add("hidden");
+  }
+}
+
+function updateIntervalAmount() {
+  document.querySelectorAll("input[name=interval] + label").forEach(element => {
+    element.querySelector("span").textContent = "";
+  });
+  let interval = document.querySelector("input[name=interval]:checked").value;
+  let amount = document.querySelector("input[type=radio][name=amount]:checked").value;
+  if (amount == "custom") {
+    amount = document.querySelector("#amount-custom-value").value;
+  }
+  amount = parseFloat(amount);
+  if (isNaN(amount)) return;
+
+  let amount_string = "";
+  if (amount / Math.floor(amount) != 1) {
+    amount_string = "~"
+  }
+  switch (interval) {
+    case "quarterly":
+      amount *= 3;
+      break;
+    case "six-monthly":
+      amount *= 6;
+      break;
+    case "annual":
+      amount *= 12;
+      break;
+    default:
+      return;
+  }
+  let amountContainer = document.querySelector("input[name=interval]:checked + label > span");
+  if (amountContainer) {
+    amount_string += amount.toFixed(2);
+    amountContainer.textContent = " (" + amount_string + "€)";
+  }
+}
+
 function updateIBANRequired() {
   let required =
     document.querySelector("#payment-method-directdebit").checked == true;
-  console.log(required);
   let iban_input = document.querySelector("#iban");
   if (required) {
     iban_input.setAttribute("required", required);

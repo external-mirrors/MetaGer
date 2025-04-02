c864d0b049f2377b6fba8f871fcc3c85278d69e7
Dominik Hebeler <dominik@suma-ev.de>
2025-04-02 17:00:47 +0200

    add migrations for applications

---
+++ metager/database/migrations/2025_04_02_165255_membership_directdebit.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('membership_directdebit', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('iban');
+            $table->string('bic')->nullable();
+            $table->string('name')->nullable();
+            $table->timestamp('created_at')->nullable(false)->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('membership_directdebit');
+    }
+};

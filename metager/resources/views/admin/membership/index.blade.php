2c04a7c848b309b1d8dc685801299e121f96f644
Dominik Hebeler <dominik@suma-ev.de>
2025-05-12 09:25:10 +0200

    welcome mail

--- metager/resources/views/admin/membership/index.blade.php
+++ metager/resources/views/admin/membership/index.blade.php
@@ -3,37 +3,105 @@
 @section('title', $title)
 
 @section('content')
+    @if(request()->filled("success"))
+        <div class="alert alert-success">{{ request("success") }}</div>
+    @endif
+    @if(request()->filled("error"))
+        <div class="alert alert-danger">{{ request("error") }}</div>
+    @endif
     <div id="membership" , class="card">
-        <h1>Aufnahmeanträge</h1>
-        <table>
-            <thead>
-                <th>Name</th>
-                <th>Beitrag</th>
-                <th>Zahlungsmethode</th>
-                <th></th>
-            </thead>
-            <tbody>
-                @foreach(DB::table("membership")->get() as $membership)
-                    <tr>
-                        <td>{{ $membership->company . implode(" ", [$membership->title, $membership->firstname, $membership->lastname]) }}
-                        </td>
-                        <td>{{ $membership->amount . "€ " . $membership->interval }}</td>
-                        <td>{{ $membership->payment_method }}</td>
-                        <td>
-                            <form method="POST" action="{{ route("membership_admin_accept") }}">
-                                <input type="hidden" name="id" value="{{ $membership->id }}">
-                                <input type="submit" name="action" value="Annehmen" class="btn btn-default">
-                            </form>
-                        </td>
-                        <td>
-                            <form method="POST" action="{{ route("membership_admin_deny") }}">
-                                <input type="hidden" name="id" value="{{ $membership->id }}">
-                                <input type="submit" id="membership-deny" name="action" value="Ablehnen" class="btn btn-danger">
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <h1>Mitgliedsanträge Admin</h1>
+        @if(sizeof($membership_applications) === 0 && sizeof($reductions) === 0)
+            <div class="alert alert-success">Aktuell nichts zu tun</div>
+        @endif
+        @if(sizeof($membership_applications) > 0)
+            <h1>Aufnahmeanträge</h1>
+            <table>
+                <thead>
+                    <th>Name</th>
+                    <th>Beitrag</th>
+                    <th>Zahlungsmethode</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    @foreach($membership_applications as $membership_application)
+                        <tr>
+                            <td>
+                                <a href="mailto:{{ $membership_application->email }}" target="_blank">
+                                    {{ $membership_application->company . implode(" ", [$membership_application->title, $membership_application->firstname, $membership_application->lastname]) }}
+                                </a>
+                            </td>
+                            <td>{{ number_format($membership_application->amount, 2, ",", ".") . "€ " . $membership_application->interval }}
+                            </td>
+                            <td>
+                                <div>{{ $membership_application->payment_method }}</div>
+                                @if($membership_application->payment_method === "directdebit")
+                                    @if($membership_application->accountholder !== null)
+                                        <div>Kontoinhaber: {{ $membership_application->accountholder }}</div>
+                                    @endif
+                                    <div>IBAN: {{ iban_to_human_format($membership_application->iban) }}</div>
+                                @elseif($membership_application->payment_method === "paypal")
+                                    <div>PayPal Vault: {{ $membership_application->vault_id }}</div>
+                                @endif
+                            </td>
+                            <td>
+                                <form method="POST" action="{{ route("membership_admin_accept") }}">
+                                    <input type="hidden" name="id" value="{{ $membership_application->id }}">
+                                    <input type="submit" name="action" value="Annehmen" class="btn btn-default">
+                                </form>
+                            </td>
+                            <td>
+                                <form method="POST" action="{{ route("membership_admin_deny") }}">
+                                    <input type="hidden" name="id" value="{{ $membership_application->id }}">
+                                    <input type="submit" id="membership-deny" name="action" value="Ablehnen" class="btn btn-danger">
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+        @if(sizeof($reductions) > 0)
+            <h1>Nachweise für Beitragsminderung</h1>
+            <table>
+                <thead>
+                    <th>Name</th>
+                    <th>Beitrag</th>
+                    <th>Nachweis</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    @foreach($reductions as $reduction)
+                        <tr>
+                            <td>{{ $reduction->company . implode(" ", [$reduction->title, $reduction->firstname, $reduction->lastname]) }}
+                            </td>
+                            <td>{{ $reduction->amount . "€ " . $reduction->interval }}</td>
+                            <td>
+                                <a href="{{ route("membership_admin_reduction", ["reduction_id" => $reduction->id]) }}"
+                                    target="_blank">Nachweis
+                                    für Beitragsminderung</a>
+                            </td>
+                            <td>
+                                <form method="POST" action="{{ route("membership_admin_reduction_accept") }}">
+                                    <input type="hidden" name="id" value="{{ $reduction->id }}">
+                                    <input type="date" name="reduction_until" id="reduction-until"
+                                        min="{{ now()->format("Y-m-d") }}" max="{{ now()->addYears(20)->format("Y-m-d") }}"
+                                        value="{{ now()->addYear()->format("Y-m-d") }}">
+                                    <input type="submit" id="membership-reduction-deny" name="action" value="Annehmen"
+                                        class="btn btn-success">
+                                </form>
+                            </td>
+                            <td>
+                                <form method="POST" action="{{ route("membership_admin_reduction_deny") }}">
+                                    <input type="hidden" name="id" value="{{ $reduction->id }}">
+                                    <input type="submit" id="membership-reduction-deny" name="action" value="Ablehnen"
+                                        class="btn btn-danger">
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
     </div>
 @endsection
--- metager/resources/views/mail/membership/welcome.blade.php
+++ metager/resources/views/mail/membership/welcome.blade.php
@@ -1,12 +1,72 @@
 <x-mail::message>
-# @lang("membership/welcome_mail.subject")
+# {{ $contact["addressee_display"] }},
 
-The body of your message.
+@lang('membership/welcome_mail.general', ["member_count" => $membership_count])
 
-<x-mail::button :url="''">
-Button Text
-</x-mail::button>
+# @lang("membership/welcome_mail.membership.title")
 
-Thanks,<br>
-{{ config('app.name') }}
+@lang("membership/welcome_mail.membership.description", ["amount" => number_format($payments[0]["amount"], 2, ","), "due" => $payments[0]["due_date"]->format("d.m.Y")])  
+
+@switch($membership["Beitrag.Zahlungsweise:label"])
+@case("Banküberweisung")
+@lang("membership/welcome_mail.membership.banktransfer", ['interval' => __("membership/welcome_mail.membership.interval." . $payments[0]["payment_interval_string"]), 'mandate' => $membership["Beitrag.Zahlungsreferenz"]])
+
+> `SUMA-EV`\
+> `DE64 4306 0967 4075 0332 01`\
+> `GENODEM1GLS`\
+> `GLS Gemeinschaftsbank, Bochum`
+@break
+@case("Lastschrift")
+@lang("membership/welcome_mail.membership.directdebit", ['interval' => __("membership/welcome_mail.membership.interval." . $payments[0]["payment_interval_string"]), 'mandate' => $membership["Beitrag.Zahlungsreferenz"], 'iban' => iban_to_obfuscated_format($membership["Beitrag.IBAN"])])
+@break
+@endswitch
+
+## @lang("membership/welcome_mail.membership.next_payments"):
+<x-mail::table>
+
+| @lang("membership/welcome_mail.membership.due")    | @lang("membership/welcome_mail.membership.amount")       |
+| :-----------: | :-----------: |
+@foreach($payments as $payment)
+| {{ $payment["due_date_in_the_past"] ? __("membership/welcome_mail.membership.now") : $payment["due_date"]->format("d.m.Y") }} | {{ number_format($payment["amount"], 2, ",") }}€ | 
+@endforeach
+
+</x-mail::table>
+
+@if(\App\Localization::getLanguage() === "de")
+# @lang("membership/welcome_mail.websites.title")
+
+@lang("membership/welcome_mail.websites.description")
+@endif
+
+# @lang("membership/welcome_mail.key.title")
+
+@lang("membership/welcome_mail.key.description_first", ["infos" => url("keys")])
+
+> {{ $membership["MetaGer_Key.Key"] }}
+
+@lang("membership/welcome_mail.key.description_second", ["startpage_link" => url("/")])
+
+> [{{ route("loadSettings", ["key" => $membership["MetaGer_Key.Key"]]) }}]({{ route("loadSettings", ["key" => $membership["MetaGer_Key.Key"]]) }})
+
+@lang("membership/welcome_mail.key.description_third")
+
+## @lang("membership/welcome_mail.key.extension")
+
+> [Firefox]({{ $plugin_firefox_url }}) | [Chrome]({{ $plugin_chrome_url }}) | [Edge]({{ $plugin_edge_url }})
+
+@lang("membership/welcome_mail.key.description_fourth", ["anonymous_token_link" => url("/keys/help/anonymous-token")])\
+@lang("membership/welcome_mail.key.description_fifth")\
+@lang("membership/welcome_mail.key.description_sixth")
+
+# @lang("membership/welcome_mail.mastodon.title")
+
+@lang("membership/welcome_mail.mastodon.description_first")\
+@lang("membership/welcome_mail.mastodon.description_second")\
+@lang("membership/welcome_mail.mastodon.description_third", ["email" => $contact["email_primary.email"]])\
+\
+@lang("membership/welcome_mail.greeting"),\
+[SUMA-EV](https://suma-ev.de) & [Metager]({{ url("/") }})\
+Postfach 51 01 43\
+D-30631 Hannover\
+Tel: [+4951134000070](tel:+4934000070) Email: [verein@metager.de](mailto:verein@metager.de)
 </x-mail::message>

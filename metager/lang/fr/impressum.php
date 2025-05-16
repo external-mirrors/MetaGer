b76a94a194573e5497ff85fbf671e1c191145710
DeepL <noreply-mt-deepl@weblate.org>
2025-05-16 09:20:49 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (10 of 10 strings)
    
    Translation: MetaGer/Impressum
    Translate-URL: https://translate.metager.de/projects/metager/impressum/fr/

--- metager/lang/fr/impressum.php
+++ metager/lang/fr/impressum.php
@@ -3,7 +3,7 @@ return [
     'info' => [
         '3' => "Contact :\r
 Tél : +4951134000070\r
-Courriel : <a href=\"/en-GB/kontakt\">Formulaire de contact crypté</a>",
+Courriel : <a href=\":contact_link\">Formulaire de contact</a>",
         '4' => 'Conseil d\'administration : Dominik Hebeler, Phil Höfer, Carsten Riel, Manuela Branz',
         '6' => 'Commissaire à la protection de la jeunesse : Manuela Branz <a href="mailto:jugendschutz@metager.de">jugendschutz@metager.de</a>',
         '8' => '"SUMA-EV - Verein für freien Wissenszugang" est une association caritative, inscrite au registre des associations de l\'Amtsgericht Hannover sous le numéro VR200033. Numéro d\'identification de la taxe sur les ventes : DE 300 464 091. La "Gottfried Wilhelm Leibniz Universität Hannover" est un organisme statutaire.',

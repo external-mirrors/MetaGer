68fbbdfc1a5fa26b55943371e8a4a7edb0449735
Dominik Hebeler <dominik@suma-ev.de>
2025-06-16 10:24:39 +0200

    move warning about blacklists

--- metager/resources/views/resultpages/results_images.blade.php
+++ metager/resources/views/resultpages/results_images.blade.php
@@ -61,7 +61,24 @@
         </div>
     @endfor
 </div>
-
+@if(sizeof($metager->getUserDomainBlacklist()) > 0)
+    <div class="alert alert-warning">
+        @if(sizeof($metager->getUserDomainBlacklist()) <= 3)
+            @lang('metaGer.formdata.domainBlacklist', ['domain' => implode(", ", $metager->getUserDomainBlacklist())])
+        @else
+            @lang('metaGer.formdata.domainBlacklistCount', ['count' => sizeof($metager->getUserDomainBlacklist())])
+        @endif
+    </div>
+@endif
+@if(sizeof($metager->getUserHostBlacklist()) > 0)
+    <div class="alert alert-warning">
+        @if(sizeof($metager->getUserHostBlacklist()) <= 3)
+            @lang('metaGer.formdata.hostBlacklist', ['host' => implode(", ", $metager->getUserHostBlacklist())])
+        @else
+            @lang('metaGer.formdata.hostBlacklistCount', ['count' => sizeof($metager->getUserHostBlacklist())])
+        @endif
+    </div>
+@endif
 @include('parts.pager')
 @if (!app(\App\Models\Authorization\Authorization::class)->canDoAuthenticatedSearch())
     <div id="external-search">

10c33dc8c7d3efbfaf41f2eff451b59995681edb
Dominik Hebeler <dominik@suma-ev.de>
2025-06-16 10:09:22 +0200

    remove more ads remainders

--- metager/resources/views/resultpages/results.blade.php
+++ metager/resources/views/resultpages/results.blade.php
@@ -1,23 +1,8 @@
-<div id="results" role="list" aria-label="@lang('results.results.summary', ['resultcount' => sizeof($metager->getResults()), 'totalresults' => $metager->getTotalResultCount()])">
+<div id="results" role="list"
+    aria-label="@lang('results.results.summary', ['resultcount' => sizeof($metager->getResults()), 'totalresults' => $metager->getTotalResultCount()])">
     @include('parts.alteration')
-    @if ($mobile)
-        @include('layouts.ad', ['ad' => $metager->popAd()])
-    @else
-        @for ($i = 0; $i < 2; $i++)
-            @include('layouts.ad', ['ad' => $metager->popAd()])
-        @endfor
-    @endif
     {{-- Create results and ongoing ads --}}
     @foreach ($metager->getResults() as $index => $result)
-        @if ($mobile)
-            @if ($index % 4 === 0)
-                @include('layouts.ad', ['ad' => $metager->popAd()])
-            @endif
-        @else
-            @if ($index % 7 === 0)
-                @include('layouts.ad', ['ad' => $metager->popAd()])
-            @endif
-        @endif
         @if ($index === 2)
             @include('layouts.resultpage.news', ['news' => $metager->news])
         @endif

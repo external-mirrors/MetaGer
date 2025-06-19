a0a23d5893e49c6af91af3d5b2f12bc771bb7a3f
Weblate <noreply@weblate.org>
2025-06-19 12:18:34 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Membership
    Translate-URL: https://translate.metager.de/projects/metager/membership/

--- metager/lang/de/membership.php
+++ metager/lang/de/membership.php
@@ -5,29 +5,28 @@ return [
     'success' => 'Herzlichen Dank für die Übermittlung Ihres Aufnahmeantrags. Wir werden diesen möglichst schnell bearbeiten. Anschließend erhalten Sie eine Mail mit weiteren Informationen von uns an die angegebene Addresse.',
     'application' => [
         'description' => 'Vielen Dank, dass Sie eine <a href="https://suma-ev.de/mitglieder/" target="_blank">Mitgliedschaft</a> in unserem gemeinnützigen Trägerverein erwägen. Um Ihren Antrag bearbeiten zu können benötigen wir lediglich ein paar Informationen, die Sie hier ausfüllen können.',
-        'update' => 'Nachfolgend sehen Sie die bei uns für Ihre Mitgliedschaft hinterlegten Informationen. Mit einem Klick auf "Bearbeiten" können Sie diese ändern. Eine Änderung der Kontaktdaten ist hier nicht möglich. Sollten diese sich geändert haben, senden Sie uns bitte eine <a href=":contact_link" target="_blank">Mail</a> mit Ihren neuen Informationen.',
     ],
     'data' => [
         'description' => 'Folgende Daten haben wir für Ihren Antrag erfasst:',
         'name' => 'Name',
         'email' => 'Email-Adresse',
-        "company" => "Firmenname",
-        "amount" => "Mitgliedsbeitrag",
-        "payment_method" => "Zahlungsmethode",
-        "payment_methods" => [
-            "banktransfer" => "Banküberweisung",
-            "directdebit" => "Lastschrift",
-            "paypal" => "PayPal",
-            "card" => "Kreditkarte"
+        'company' => "Firmenname",
+        'amount' => "Mitgliedsbeitrag",
+        'payment_method' => "Zahlungsmethode",
+        'payment_methods' => [
+            'banktransfer' => "Banküberweisung",
+            'directdebit' => "Lastschrift",
+            'paypal' => "PayPal",
+            'card' => "Kreditkarte",
         ],
-        "payment" => [
-            "interval" => [
-                "monthly" => "monatlich",
-                "quarterly" => "vierteljährlich",
-                "six-monthly" => "halbjährlich",
-                "annual" => "jährlich"
-            ]
-        ]
+        'payment' => [
+            'interval' => [
+                'monthly' => "monatlich",
+                'quarterly' => "vierteljährlich",
+                'six-monthly' => "halbjährlich",
+                'annual' => "jährlich",
+            ],
+        ],
     ],
     'key' => [
         'description' => 'Für die Nutzung von MetaGer wird der folgende Schlüssel verwendet und von uns aufgeladen. Falls Sie bereits angemeldet waren wurde Ihr bestehender Schlüssel benutzt.',
@@ -35,5 +34,4 @@ return [
         'now' => 'Er ist bereits aufgeladen und kann sofort verwendet werden.',
     ],
     'back' => 'Zurück zur Startseite',
-
 ];

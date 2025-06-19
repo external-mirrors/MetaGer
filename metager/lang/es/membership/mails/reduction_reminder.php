e36e1b347be1b52b91d50d44205fe541d35e7aae
Weblate <noreply-mt-weblate@weblate.org>
2025-06-19 17:26:44 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MetaGer/Membership - Mails - Reduction Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-reduction-reminder/es/

--- metager/lang/es/membership/mails/reduction_reminder.php
+++ metager/lang/es/membership/mails/reduction_reminder.php
@@ -3,5 +3,5 @@ return [
     'subject' => '[SUMA-EV & MetaGer] El justificante de la cuota de socio reducida debe presentarse',
     'description' => "Su justificante de cuota de afiliación reducida debe actualizarse hasta **:fecha**. Su cuota de afiliación se fijará en nuestra cuota mínima sin una nueva prueba.",
     'continue' => "Por favor, cargue una prueba adecuada para justificar una cuota de afiliación reducida o elija una cuota superior o igual a nuestra cuota mínima de 5€ al mes haciendo clic en el botón de abajo:",
-    'continue_button' => "",
+    'continue_button' => "Actualizar la afiliación",
 ];

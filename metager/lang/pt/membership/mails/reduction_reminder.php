cf99714433ca7ba3c104793202c9ddedf15c5002
Weblate <noreply-mt-weblate@weblate.org>
2025-06-19 17:27:00 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MetaGer/Membership - Mails - Reduction Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-reduction-reminder/pt/

--- metager/lang/pt/membership/mails/reduction_reminder.php
+++ metager/lang/pt/membership/mails/reduction_reminder.php
@@ -3,5 +3,5 @@ return [
     'subject' => '[SUMA-EV & MetaGer] O vosso comprovativo de redução da quota deve ser entregue',
     'description' => "O seu comprovativo para uma taxa de inscrição reduzida tem de ser atualizado até **:data**. Sem um novo comprovativo, a sua taxa de inscrição será fixada na nossa taxa mínima.",
     'continue' => "Carregue um comprovativo adequado para justificar uma redução da taxa de inscrição ou escolha uma taxa superior ou igual à nossa taxa mínima de 5 euros por mês, clicando no botão abaixo:",
-    'continue_button' => "",
+    'continue_button' => "Atualizar o estatuto de membro",
 ];

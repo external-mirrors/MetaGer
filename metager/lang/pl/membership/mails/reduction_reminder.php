bc0941b5fbb3f3c5aff77c334f51235d529e35f0
DeepL <noreply-mt-deepl@weblate.org>
2025-06-19 17:26:57 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MetaGer/Membership - Mails - Reduction Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-reduction-reminder/pl/

--- metager/lang/pl/membership/mails/reduction_reminder.php
+++ metager/lang/pl/membership/mails/reduction_reminder.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'subject' => '',
-    'description' => "",
-    'continue' => "",
+    'subject' => '[SUMA-EV & MetaGer] Dowód na obniżoną składkę członkowską jest wymagany',
+    'description' => "Dowód uprawniający do obniżonej opłaty członkowskiej musi zostać zaktualizowany do **:date**. Opłata członkowska zostanie ustawiona na naszą minimalną opłatę bez nowego dowodu.",
+    'continue' => "Prześlij odpowiedni dowód uzasadniający obniżoną opłatę członkowską lub wybierz opłatę wyższą lub równą naszej minimalnej opłacie w wysokości 5 € miesięcznie, klikając przycisk poniżej:",
     'continue_button' => "",
 ];

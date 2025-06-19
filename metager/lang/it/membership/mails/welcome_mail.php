c4f18d179f57e293ab9abc8585c1f3df3aa8485f
Weblate <noreply-mt-weblate@weblate.org>
2025-06-19 16:19:37 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (33 of 33 strings)
    
    Translation: MetaGer/Membership - Mails - Welcome
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-welcome/it/

--- metager/lang/it/membership/mails/welcome_mail.php
+++ metager/lang/it/membership/mails/welcome_mail.php
@@ -17,10 +17,13 @@ return [
         'now' => 'Ora',
         'interval' => [
             'halbjährlich' => 'semestrale',
+            'jährlich' => 'annualmente',
         ],
+        'title' => 'Soci',
     ],
     'websites' => [
         'description' => 'Su [suma-ev.de/beitraege/](https://suma-ev.de/beitraege/) sono disponibili molte delle precedenti circolari e newsletter. I precedenti comunicati stampa del SUMA-EV si trovano su [suma-ev.de/presse/](https://suma-ev.de/presse/). Siete inoltre invitati a farvi inserire nella nostra [lista pubblica dei membri](https://suma-ev.de/mitglieder/liste-unserer-mitglieder/). In questo caso, è sufficiente comunicarcelo brevemente.',
+        'title' => 'Soci',
     ],
     'key' => [
         'title' => 'Ricerca con MetaGer',

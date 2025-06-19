3c518564fba8c3fef26c211693828623936aedbb
DeepL <noreply-mt-deepl@weblate.org>
2025-06-19 12:19:31 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: MetaGer/Membership
    Translate-URL: https://translate.metager.de/projects/metager/membership/es/

--- metager/lang/es/membership.php
+++ metager/lang/es/membership.php
@@ -3,4 +3,35 @@ return [
     'title' => 'Su afiliación a SUMA-EV',
     'back' => 'Volver a la página de inicio',
     'non-de' => 'Lamentablemente, por el momento sólo podemos aceptar solicitudes de admisión de países de habla alemana. Le invitamos a que nos apoye con una donación a <a href=":donationlink"></a> .',
+    'key' => [
+        'description' => 'Para utilizar MetaGer, se utiliza la siguiente clave, que nosotros recargamos. Si ya estaba conectado, se utilizó su clave existente.',
+        'later' => 'La primera recarga tiene lugar después de que se haya tramitado su solicitud',
+        'now' => 'Ya está cargada y puede utilizarse inmediatamente.',
+    ],
+    'application' => [
+        'description' => 'Gracias por considerar la posibilidad de afiliarse a <a href="https://suma-ev.de/en/mitglieder/" target="_blank"></a> en nuestra asociación sin ánimo de lucro. Para tramitar su solicitud, sólo necesitamos unos pocos datos, que puede rellenar aquí.',
+        'cancel' => [
+            'application' => 'Borrar solicitud de afiliación',
+            'update' => 'Descartar cambios',
+        ],
+        'update_hint' => 'Los cambios solicitados para su afiliación serán revisados/aceptados en breve. Si está satisfecho con el estado mostrado puede abandonar esta página. De lo contrario, puede realizar más cambios o eliminar su solicitud de cambio con el botón de abajo.',
+        'payment_block' => 'Intentaremos autorizar el pago de su próxima cuota de afiliación para validar su método de pago, pero el pago sólo se ejecutará si vence en las próximas dos semanas y se anulará en caso contrario.',
+    ],
+    'data' => [
+        'description' => 'Hemos registrado los siguientes datos para su solicitud:',
+        'payment' => [
+            'interval' => [
+                'six-monthly' => "Semestral",
+                'annual' => "anualmente",
+            ],
+        ],
+        'payment_methods' => [
+            'directdebit' => "Domiciliación bancaria",
+            'card' => "Tarjeta de crédito",
+        ],
+        'company' => "Nombre de la empresa",
+        'payment_method' => "Forma de pago",
+        'amount' => "Cuota de afiliación",
+    ],
+    'success' => 'Muchas gracias por enviar su solicitud de afiliación. La tramitaremos lo antes posible. Le enviaremos un correo electrónico con más información a la dirección indicada.',
 ];

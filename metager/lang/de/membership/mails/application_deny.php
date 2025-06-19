c263dee47109ddf8db503c7170e11dfa609f4fa7
DeepL <noreply-mt-deepl@weblate.org>
2025-06-19 12:44:31 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: MetaGer/Membership - Mails - Application Deny
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-application-deny/de/

--- metager/lang/de/membership/mails/application_deny.php
+++ metager/lang/de/membership/mails/application_deny.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'subject' => '',
-    'description' => "",
-    'description_reason' => "",
-    'continue' => "",
-    'continue_button' => "",
+    'subject' => '[SUMA-EV & MetaGer] Ihr Antrag auf Mitgliedschaft wurde abgelehnt',
+    'description' => "Ihr Antrag auf Mitgliedschaft wurde abgelehnt",
+    'description_reason' => "Ihr Antrag auf Mitgliedschaft wurde mit der folgenden Begründung abgelehnt:",
+    'continue' => "Sie können jederzeit einen neuen Mitgliedsantrag stellen:",
+    'continue_button' => "Mitglied werden",
 ];

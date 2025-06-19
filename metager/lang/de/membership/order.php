85b22fcefdc6a4670aaa2fee2fc6b785f4027ed8
Dominik Hebeler <dominik@suma-ev.de>
2025-06-19 15:10:16 +0200

    add translations for welcome mails

--- metager/lang/de/membership/order.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    "default_description" => "SUMA-EV Mitgliedsbeitrag",
-    "default_softdescription" => "Mitgliedsbeitrag",
-    "vault" => [
-        "description" => "SUMA-EV Mitgliedsbeitrag - Fällig im gewählten Zahlungsintervall.",
-    ]
-];

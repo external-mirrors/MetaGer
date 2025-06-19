a080aab033fe6b3f0b9293c55917fe723f8916c8
Weblate <noreply-mt-weblate@weblate.org>
2025-06-19 17:26:38 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MetaGer/Membership - Mails - Reduction Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-reduction-reminder/da/

--- metager/lang/da/membership/mails/reduction_reminder.php
+++ metager/lang/da/membership/mails/reduction_reminder.php
@@ -3,5 +3,5 @@ return [
     'subject' => '[SUMA-EV & MetaGer] Dit bevis for et reduceret medlemskontingent skal indsendes',
     'description' => "Dit bevis for nedsat kontingent skal opdateres indtil **:dato**. Dit medlemskontingent vil blive sat til vores minimumsgebyr uden et nyt bevis.",
     'continue' => "Upload venligst et passende bevis for at retfærdiggøre et reduceret medlemsgebyr eller vælg et gebyr over eller lig med vores minimumsgebyr på 5 € pr. måned ved at klikke på knappen nedenfor:",
-    'continue_button' => "",
+    'continue_button' => "Opdatering af medlemskab",
 ];

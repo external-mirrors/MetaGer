2d8b4d7e60fcae14e1b4a1d5cb6780a3bc4cbb03
DeepL <noreply-mt-deepl@weblate.org>
2025-06-19 15:48:31 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Membership - Order
    Translate-URL: https://translate.metager.de/projects/metager/membership-order/pl/

--- metager/lang/pl/membership/order.php
+++ metager/lang/pl/membership/order.php
@@ -1,4 +1,7 @@
 <?php
-
 return [
+    'default_description' => "Opłata członkowska SUMA-EV",
+    'vault' => [
+        'description' => "Składka członkowska SUMA-EV - płatna w wybranym okresie płatności.",
+    ],
 ];

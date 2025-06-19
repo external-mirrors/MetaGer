77bd7864c49cb526728d2fd5d2b91746cb5bcc54
DeepL <noreply-mt-deepl@weblate.org>
2025-06-19 15:48:26 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Membership - Order
    Translate-URL: https://translate.metager.de/projects/metager/membership-order/da/

--- metager/lang/da/membership/order.php
+++ metager/lang/da/membership/order.php
@@ -1,4 +1,7 @@
 <?php
-
 return [
+    'default_description' => "SUMA-EV medlemsgebyr",
+    'vault' => [
+        'description' => "SUMA-EV medlemskontingent - forfalder ved det valgte betalingsinterval.",
+    ],
 ];

2008c47b78cae4a4192a42b8b0b3a4cf9adf8107
DeepL <noreply-mt-deepl@weblate.org>
2025-06-19 12:19:06 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: MetaGer/Membership
    Translate-URL: https://translate.metager.de/projects/metager/membership/pt/

--- metager/lang/pt/membership.php
+++ metager/lang/pt/membership.php
@@ -3,4 +3,35 @@ return [
     'title' => 'A sua adesão à SUMA-EV',
     'non-de' => 'Infelizmente, atualmente só podemos aceitar pedidos de admissão para países de língua alemã. É muito bem-vindo se nos apoiar com um donativo <a href=":donationlink"></a> .',
     'back' => 'Voltar à página inicial',
+    'key' => [
+        'description' => 'Para utilizar o MetaGer, a seguinte chave é utilizada e carregada por nós. Se já estava registado, foi utilizada a sua chave existente.',
+        'later' => 'A primeira recarga é efectuada após o processamento da candidatura',
+        'now' => 'Já está carregado e pode ser utilizado imediatamente.',
+    ],
+    'application' => [
+        'description' => 'Obrigado por considerar a adesão ao <a href="https://suma-ev.de/en/mitglieder/" target="_blank"></a> na nossa associação sem fins lucrativos. Para podermos processar a sua candidatura, precisamos apenas de algumas informações, que pode preencher aqui.',
+        'cancel' => [
+            'application' => 'Eliminar o pedido de adesão',
+            'update' => 'Alterações de descarte',
+        ],
+        'update_hint' => 'As alterações solicitadas para a sua inscrição serão analisadas/aceites em breve. Se estiver satisfeito com o estado apresentado, pode sair desta página. Caso contrário, pode efetuar mais alterações ou eliminar o seu pedido de alteração através do botão abaixo.',
+        'payment_block' => 'Tentaremos autorizar um pagamento para a sua próxima quota de membro para validar o seu método de pagamento, mas o pagamento só será executado se for devido nas duas semanas seguintes e anulado caso contrário.',
+    ],
+    'data' => [
+        'description' => 'Registámos os seguintes dados para a sua aplicação:',
+        'payment' => [
+            'interval' => [
+                'six-monthly' => "Semestral",
+                'annual' => "anualmente",
+            ],
+        ],
+        'payment_methods' => [
+            'directdebit' => "Débito direto",
+            'card' => "Cartão de crédito",
+        ],
+        'company' => "Nome da empresa",
+        'payment_method' => "Método de pagamento",
+        'amount' => "Taxa de adesão",
+    ],
+    'success' => 'Muito obrigado por ter enviado o seu pedido de adesão. Iremos processá-lo o mais rapidamente possível. Receberá então uma mensagem eletrónica com mais informações da nossa parte para o endereço fornecido.',
 ];

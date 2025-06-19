5af934042aea89ddfeeb4cf22d8da74e68923b3e
DeepL <noreply-mt-deepl@weblate.org>
2025-06-19 17:26:50 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MetaGer/Membership - Mails - Reduction Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-reduction-reminder/fr/

--- metager/lang/fr/membership/mails/reduction_reminder.php
+++ metager/lang/fr/membership/mails/reduction_reminder.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'subject' => '',
-    'description' => "",
-    'continue' => "",
+    'subject' => '[SUMA-EV & MetaGer] Votre preuve de réduction de la cotisation est attendue.',
+    'description' => "Votre preuve de cotisation réduite doit être mise à jour jusqu'à **:date**. Sans nouvelle preuve, votre cotisation sera fixée à la cotisation minimale.",
+    'continue' => "Veuillez télécharger une preuve appropriée pour justifier une cotisation réduite ou choisir une cotisation supérieure ou égale à notre cotisation minimale de 5€ par mois en cliquant sur le bouton ci-dessous :",
     'continue_button' => "",
 ];

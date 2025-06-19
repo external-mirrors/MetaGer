48869ae225d678b0955b8cc8ef6ccfa835220b2e
DeepL <noreply-mt-deepl@weblate.org>
2025-06-19 16:18:30 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MetaGer/Membership - Mails - Reduction Deny
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-reduction-deny/it/

--- metager/lang/it/membership/mails/reduction_deny.php
+++ metager/lang/it/membership/mails/reduction_deny.php
@@ -1,4 +1,7 @@
 <?php
-
 return [
+    'subject' => '[SUMA-EV & MetaGer] La tua prova per la riduzione della quota associativa è stata rifiutata',
+    'description' => "La prova caricata per giustificare una quota associativa ridotta è stata rifiutata con la seguente motivazione:",
+    'continue' => "Si prega di caricare una prova adeguata per giustificare una quota associativa ridotta o di scegliere una quota superiore o uguale alla nostra quota minima di 5€ al mese:",
+    'continue_button' => "Aggiornamento dei soci",
 ];

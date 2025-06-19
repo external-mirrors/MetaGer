d2b39f1716198650235989c32d518895f351ecb4
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2025-06-19 12:19:36 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (26 of 26 strings)
    
    Translation: MetaGer/Membership
    Translate-URL: https://translate.metager.de/projects/metager/membership/fr/

--- metager/lang/fr/membership.php
+++ metager/lang/fr/membership.php
@@ -3,4 +3,14 @@ return [
     'title' => 'Votre adhésion à SUMA-EV',
     'non-de' => 'Malheureusement, nous ne pouvons actuellement accepter que les demandes d\'admission pour les pays germanophones. Vous pouvez nous soutenir en faisant un don à <a href=":donationlink"></a> .',
     'back' => 'Retour à la page d\'accueil',
+    'data' => [
+        'payment' => [
+            'interval' => [
+                'quarterly' => "trimestrielle",
+                'monthly' => "mensuel",
+            ],
+        ],
+        'name' => 'Nom',
+        'email' => 'Adresse électronique',
+    ],
 ];

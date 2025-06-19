e64a3abd3ef13d16737ba742032eed75c3650904
Weblate <noreply-mt-weblate@weblate.org>
2025-06-19 17:26:54 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MetaGer/Membership - Mails - Reduction Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-reduction-reminder/nl/

--- metager/lang/nl/membership/mails/reduction_reminder.php
+++ metager/lang/nl/membership/mails/reduction_reminder.php
@@ -3,5 +3,5 @@ return [
     'subject' => '[SUMA-EV & MetaGer] Je bewijs voor een gereduceerd lidmaatschapsgeld is verschuldigd',
     'description' => "Je bewijs voor een gereduceerd lidmaatschapsgeld moet worden bijgewerkt tot **:datum**. Zonder een nieuw bewijs zal je lidmaatschapsbijdrage worden vastgesteld op ons minimumbedrag.",
     'continue' => "Upload een geschikt bewijs om een lagere lidmaatschapsbijdrage te rechtvaardigen of kies een bijdrage boven of gelijk aan onze minimumbijdrage van 5€ per maand door op de knop hieronder te klikken:",
-    'continue_button' => "",
+    'continue_button' => "Lidmaatschap bijwerken",
 ];

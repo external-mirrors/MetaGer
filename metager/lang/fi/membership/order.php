5145807d1a486d50623916355a886d0fac1f3b33
DeepL <noreply-mt-deepl@weblate.org>
2025-06-19 15:48:28 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Membership - Order
    Translate-URL: https://translate.metager.de/projects/metager/membership-order/fi/

--- metager/lang/fi/membership/order.php
+++ metager/lang/fi/membership/order.php
@@ -1,4 +1,7 @@
 <?php
-
 return [
+    'default_description' => "SUMA-EV:n jäsenmaksu",
+    'vault' => [
+        'description' => "SUMA-EV:n jäsenmaksu - Erääntyy maksettavaksi valitulla maksuvälillä.",
+    ],
 ];

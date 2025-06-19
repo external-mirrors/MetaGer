2beda2165dbbfa6782aa501c683e96d30bbe79d4
DeepL <noreply-mt-deepl@weblate.org>
2025-06-19 15:48:30 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Membership - Order
    Translate-URL: https://translate.metager.de/projects/metager/membership-order/nl/

--- metager/lang/nl/membership/order.php
+++ metager/lang/nl/membership/order.php
@@ -1,4 +1,7 @@
 <?php
-
 return [
+    'default_description' => "SUMA-EV lidmaatschapsgeld",
+    'vault' => [
+        'description' => "SUMA-EV lidmaatschapsgeld - Verschuldigd op het geselecteerde betalingsinterval.",
+    ],
 ];

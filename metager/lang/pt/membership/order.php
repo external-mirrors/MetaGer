799255e4e6d750e8e75b97301701bca540fbba62
Weblate <noreply-mt-weblate@weblate.org>
2025-06-19 15:48:32 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (3 of 3 strings)
    
    Translation: MetaGer/Membership - Order
    Translate-URL: https://translate.metager.de/projects/metager/membership-order/pt/

--- metager/lang/pt/membership/order.php
+++ metager/lang/pt/membership/order.php
@@ -4,4 +4,5 @@ return [
         'description' => "Quota de adesão SUMA-EV - Vencimento no intervalo de pagamento selecionado.",
     ],
     'default_description' => "Quota de filiação SUMA-EV",
+    'default_softdescription' => "Taxa de adesão",
 ];

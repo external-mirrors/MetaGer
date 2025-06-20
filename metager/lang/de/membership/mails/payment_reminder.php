7b3386939c65ac97c51935ca0ebc4d78cab50719
Dominik Hebeler <dominik@hebeler.club>
2025-06-20 18:59:54 +0200

    small text change

--- metager/lang/de/membership/mails/payment_reminder.php
+++ metager/lang/de/membership/mails/payment_reminder.php
@@ -9,7 +9,7 @@ return [
     'description' => 'Ihr Mitgliedsbeitrag war am **:date** fällig. Bitte überweisen Sie den ausstehenden Betrag von **:amount** bis **:due** unter Angabe des Zahlungsgrundes (**:payment_reference**) auf unser Konto:',
     'description_expired' => 'Die Erfahrung hat uns gezeigt, dass der Austritt aus dem <a href="https://suma-ev.de">SUMA-EV</a> oft nicht mit einer Distanzierung von unseren Werten und Ideen zu tun hat, sondern meist finanzielle Gründe hat. Wir hoffen daher, dass Sie sich irgendwann vorstellen können, unserem Verband wieder beizutreten.',
     'description_rejoin' => 'In diesem Fall überweisen Sie einfach Ihren Mitgliedsbeitrag in Höhe von **:amount** unter Angabe des Verwendungszwecks (**:payment_reference**) auf unser Konto:',
-    'edit' => 'Alternativ können Sie Ihre Mitgliedschaft aktualisieren und uns eine geeignete Zahlungsmethode zur Abbuchung erteilen (Lastschrift, PayPal, Kreditkarte)',
+    'edit' => 'Alternativ können Sie Ihre Mitgliedschaft aktualisieren und uns zur Abbuchung von einer geeigneten Zahlungsmethode autorisieren (Lastschrift, PayPal, Kreditkarte)',
     'mastodon' => 'Ihr Mastodon-Konto auf unserer [Instanz](https://suma-ev.social) wird eingefroren und kann nicht mehr verwendet werden. Bitte stellen Sie sicher, dass Sie rechtzeitig auf eine öffentliche Instanz umziehen. Wir werden alle mit Ihrem Konto verknüpften Daten 6 Monate nach dem Ende Ihrer Mitgliedschaft löschen.',
     'terminate' => 'Ohne Zahlungseingang müssen wir Ihre Mitgliedschaft bei [SUMA-EV](https://suma-ev.de) leider mit Wirkung von <b> kündigen :expiration</b>',
     'expired' => 'Mit Bedauern bestätigen wir heute, dass Ihre Mitgliedschaft bei [SUMA-EV](https://suma-ev.de) beendet ist.',

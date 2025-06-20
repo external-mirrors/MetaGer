53a37fe48814223ad1add907c5072c2f3e506a3c
Dominik Hebeler <dominik@suma-ev.de>
2025-06-20 15:18:07 +0200

    striped table

--- metager/resources/js/admin/membership.js
+++ metager/resources/js/admin/membership.js
@@ -14,5 +14,5 @@
 })();
 
 (async () => {
-    window.history.replaceState(null, "Title", "/" + window.location.href.substring(window.location.href.lastIndexOf('/') + 1).split("?")[0]);
+    window.history.replaceState(null, "Title", window.location.href.split("?")[0]);
 })();

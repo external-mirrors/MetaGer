089b583a29b64332332d4b75fd39f53668f16453
Dominik Hebeler <dominik@hebeler.club>
2025-06-20 18:49:54 +0200

    fix some translation replacement tokens that got translated

--- metager/lang/da/membership/mails/payment_reminder.php
+++ metager/lang/da/membership/mails/payment_reminder.php
@@ -2,16 +2,16 @@
 return [
     'subject' => [
         'first' => '[SUMA-EV & MetaGer] Påmindelse om betaling af dit medlemskontingent',
-        'second' => '[SUMA-EV & MetaGer] Dit medlemskab slutter på :dato uden betaling',
+        'second' => '[SUMA-EV & MetaGer] Dit medlemskab slutter på :date uden betaling',
         'expired' => '[SUMA-EV & MetaGer] Dit medlemskab er udløbet',
     ],
-    'description' => 'Dit medlemskontingent skulle være betalt den **:dato**. Overfør venligst det udestående beløb på **:beløb** til vores bankkonto inden **:forfaldsdato** med angivelse af årsagen til betalingen (**:betalingsreference**):',
+    'description' => 'Dit medlemskontingent skulle være betalt den **:date**. Overfør venligst det udestående beløb på **:amount** til vores bankkonto inden **:due** med angivelse af årsagen til betalingen (**:payment_reference**):',
     'description_expired' => 'Erfaringen har vist os, at det at forlade <a href="https://suma-ev.de">SUMA-EV</a> ofte ikke handler om at tage afstand fra vores værdier og ideer, men som regel har økonomiske årsager. Vi håber derfor, at der kan komme et tidspunkt, hvor du kan forestille dig at blive medlem af vores forening igen.',
-    'description_rejoin' => 'Hvis det er tilfældet, kan du bare overføre dit medlemskontingent på **:beløb** til vores bankkonto med angivelse af årsagen til betalingen (**:betalingsreference**):',
+    'description_rejoin' => 'Hvis det er tilfældet, kan du bare overføre dit medlemskontingent på **:amount** til vores bankkonto med angivelse af årsagen til betalingen (**:payment_reference**):',
     'edit' => 'Alternativt kan du opdatere dit medlemskab og godkende en passende betalingsmetode, som vi kan debitere fra (Directdebit, PayPal, Creditcard).',
     'edit_button' => "Opdatering af medlemskab",
     'key_charge' => 'Den automatiske genopladning af din nøgle til brug af MetaGer (**:key**) er blevet deaktiveret indtil videre.',
     'mastodon' => 'Din Mastodon-konto på vores [instans] (https://suma-ev.social) vil blive frosset og kan ikke længere bruges. Sørg for at flytte til en offentlig instans i god tid. Vi sletter alle data, der er knyttet til din konto, 6 måneder efter afslutningen af dit medlemskab.',
-    'terminate' => 'Uden modtagelse af betaling er vi desværre nødt til at opsige dit medlemskab af [SUMA-EV] (https://suma-ev.de) med virkning fra <b>:udløb</b>',
+    'terminate' => 'Uden modtagelse af betaling er vi desværre nødt til at opsige dit medlemskab af [SUMA-EV] (https://suma-ev.de) med virkning fra <b>:expiration</b>',
     'expired' => 'Det er med beklagelse, at vi i dag bekræfter, at dit medlemskab af [SUMA-EV] (https://suma-ev.de) er ophørt.',
 ];

775afbcf88062c878f8e2537921ddaf88686b7c8
DeepL <noreply-mt-deepl@weblate.org>
2025-06-20 16:40:47 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: MetaGer/Membersip - Mails - Payment Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membersip-mails-payment-reminder/nl/

--- metager/lang/nl/membership/mails/payment_reminder.php
+++ metager/lang/nl/membership/mails/payment_reminder.php
@@ -3,10 +3,15 @@ return [
     'subject' => [
         'first' => '[SUMA-EV & MetaGer] Betalingsherinnering voor je lidmaatschapsgeld',
         'expired' => '[SUMA-EV & MetaGer] Je lidmaatschap is verlopen',
+        'second' => '[SUMA-EV & MetaGer] Je lidmaatschap eindigt op :datum zonder betaling',
     ],
     'description_expired' => 'De ervaring heeft ons geleerd dat het verlaten van de <a href="https://suma-ev.de">SUMA-EV</a> vaak geen verband houdt met het afstand nemen van onze waarden en ideeën, maar meestal financiële redenen heeft. We hopen daarom dat er een moment komt waarop je je kunt voorstellen weer lid te worden van onze vereniging.',
     'edit' => 'Je kunt ook je lidmaatschap bijwerken en een geschikte betaalmethode autoriseren (automatische incasso, PayPal, creditcard).',
     'edit_button' => "Lidmaatschap bijwerken",
     'mastodon' => 'Je Mastodon account op onze [instantie](https://suma-ev.social) zal worden bevroren en kan niet langer worden gebruikt. Zorg ervoor dat je op tijd verhuist naar een publieke instantie. We zullen alle gegevens die gekoppeld zijn aan uw account 6 maanden na het einde van uw lidmaatschap verwijderen.',
     'expired' => 'Tot onze spijt bevestigen wij vandaag dat jouw lidmaatschap van [SUMA-EV](https://suma-ev.de) is beëindigd.',
+    'description' => 'Uw lidmaatschapsgeld was verschuldigd op **:datum**. Gelieve het openstaande bedrag van **:bedrag** over te maken op onze bankrekening voor **:vervaldatum**, met vermelding van uw reden voor betaling (**:betalings_referentie**):',
+    'key_charge' => 'Het automatisch opladen van je sleutel voor het gebruik van MetaGer (**:sleutel**) is voorlopig uitgeschakeld.',
+    'terminate' => 'Zonder ontvangst van de betaling moeten we helaas je lidmaatschap van [SUMA-EV](https://suma-ev.de) opzeggen met ingang van <b>:expiration</b>',
+    'description_rejoin' => 'In dat geval kun je het lidmaatschapsgeld van **:bedrag** gewoon overmaken op onze bankrekening, met vermelding van de reden van betaling (**:payment_reference**):',
 ];

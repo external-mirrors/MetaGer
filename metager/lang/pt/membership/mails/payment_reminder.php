fede9a310ae7ed173baffe17beb781dd955a992f
DeepL <noreply-mt-deepl@weblate.org>
2025-06-20 16:40:47 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: MetaGer/Membersip - Mails - Payment Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membersip-mails-payment-reminder/pt/

--- metager/lang/pt/membership/mails/payment_reminder.php
+++ metager/lang/pt/membership/mails/payment_reminder.php
@@ -3,6 +3,7 @@ return [
     'subject' => [
         'expired' => '[SUMA-EV & MetaGer] A sua inscrição expirou',
         'first' => '[SUMA-EV & MetaGer] Lembrete de pagamento da quota de membro',
+        'second' => '[SUMA-EV & MetaGer] A tua inscrição termina em :data sem pagamento',
     ],
     'edit_button' => "Atualizar o estatuto de membro",
     'description_expired' => 'A experiência tem-nos mostrado que o abandono da <a href="https://suma-ev.de">SUMA-EV</a> não está muitas vezes relacionado com o distanciamento dos nossos valores e ideias, mas tem geralmente razões financeiras. Esperamos, por isso, que chegue um momento em que se possa imaginar a juntar-se novamente à nossa associação.',
@@ -10,4 +11,7 @@ return [
     'key_charge' => 'O recarregamento automático da sua chave para a utilização do MetaGer (**:key**) foi desativado por enquanto.',
     'mastodon' => 'A sua conta Mastodon na nossa [instância](https://suma-ev.social) será congelada e não poderá mais ser usada. Por favor, certifique-se de que muda para uma instância pública em tempo útil. Eliminaremos todos os dados associados à sua conta 6 meses após o fim da sua inscrição.',
     'expired' => 'É com pesar que confirmamos hoje que a sua adesão à [SUMA-EV](https://suma-ev.de) terminou.',
+    'description' => 'A sua quota era devida em **:data**. Queira transferir o montante pendente de **:montante** para a nossa conta bancária até **:data**, indicando o motivo do pagamento (**:referência_do_pagamento**):',
+    'terminate' => 'Se não recebermos o pagamento, teremos infelizmente de anular a sua adesão à [SUMA-EV](https://suma-ev.de) com efeitos a partir de <b>:expiração</b>',
+    'description_rejoin' => 'Se for esse o caso, basta transferir a sua quotização de **:montante** para a nossa conta bancária, indicando o motivo do pagamento (**:referência_do_pagamento**):',
 ];

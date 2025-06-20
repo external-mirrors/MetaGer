5f48c18038b473925a4b317422f89c856c177da0
DeepL <noreply-mt-deepl@weblate.org>
2025-06-20 16:40:47 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: MetaGer/Membersip - Mails - Payment Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membersip-mails-payment-reminder/pl/

--- metager/lang/pl/membership/mails/payment_reminder.php
+++ metager/lang/pl/membership/mails/payment_reminder.php
@@ -3,6 +3,7 @@ return [
     'subject' => [
         'first' => '[SUMA-EV & MetaGer] Przypomnienie o płatności składki członkowskiej',
         'expired' => '[SUMA-EV & MetaGer] Twoje członkostwo wygasło',
+        'second' => '[SUMA-EV & MetaGer] Twoje członkostwo kończy się w dniu :data bez płatności',
     ],
     'description_expired' => 'Doświadczenie pokazało nam, że opuszczenie <a href="https://suma-ev.de">SUMA-EV</a> często nie jest związane z dystansowaniem się od naszych wartości i idei, ale zwykle ma przyczyny finansowe. Dlatego mamy nadzieję, że nadejdzie czas, kiedy będziesz mógł sobie wyobrazić ponowne dołączenie do naszego stowarzyszenia.',
     'edit' => 'Alternatywnie możesz zaktualizować swoje członkostwo i autoryzować odpowiednią metodę płatności, z której będziemy pobierać środki (polecenie zapłaty, PayPal, karta kredytowa).',
@@ -10,4 +11,7 @@ return [
     'key_charge' => 'Automatyczne ładowanie klucza do korzystania z MetaGer (**:key**) zostało na razie wyłączone.',
     'mastodon' => 'Twoje konto Mastodon na naszej [instancji](https://suma-ev.social) zostanie zamrożone i nie będzie mogło być dłużej używane. Upewnij się, że przeniesiesz się do publicznej instancji w odpowiednim czasie. Usuniemy wszystkie dane powiązane z kontem 6 miesięcy po zakończeniu członkostwa.',
     'expired' => 'Z przykrością potwierdzamy, że Twoje członkostwo w [SUMA-EV](https://suma-ev.de) dobiegło końca.',
+    'description' => 'Twoja składka członkowska była wymagalna w dniu **:date**. Prosimy o przelanie zaległej kwoty w wysokości **:amount** na nasze konto bankowe do **:due**, podając powód płatności (**:payment_reference**):',
+    'terminate' => 'Bez otrzymania płatności będziemy niestety musieli anulować członkostwo w [SUMA-EV](https://suma-ev.de) ze skutkiem od <b>:wygaśnięcie</b>',
+    'description_rejoin' => 'W takim przypadku możesz po prostu przelać swoją opłatę członkowską w wysokości **:kwota** na nasze konto bankowe, podając powód płatności (**:płatność_referencja**):',
 ];

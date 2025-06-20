d6f5792517b22d4c0788040b280a736576fde02c
DeepL <noreply-mt-deepl@weblate.org>
2025-06-20 16:40:45 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: MetaGer/Membersip - Mails - Payment Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membersip-mails-payment-reminder/da/

--- metager/lang/da/membership/mails/payment_reminder.php
+++ metager/lang/da/membership/mails/payment_reminder.php
@@ -3,6 +3,7 @@ return [
     'subject' => [
         'first' => '[SUMA-EV & MetaGer] Påmindelse om betaling af dit medlemskontingent',
         'expired' => '[SUMA-EV & MetaGer] Dit medlemskab er udløbet',
+        'second' => '[SUMA-EV & MetaGer] Dit medlemskab slutter på :dato uden betaling',
     ],
     'description_expired' => 'Erfaringen har vist os, at det at forlade <a href="https://suma-ev.de">SUMA-EV</a> ofte ikke handler om at tage afstand fra vores værdier og ideer, men som regel har økonomiske årsager. Vi håber derfor, at der kan komme et tidspunkt, hvor du kan forestille dig at blive medlem af vores forening igen.',
     'edit' => 'Alternativt kan du opdatere dit medlemskab og godkende en passende betalingsmetode, som vi kan debitere fra (Directdebit, PayPal, Creditcard).',
@@ -10,4 +11,7 @@ return [
     'key_charge' => 'Den automatiske genopladning af din nøgle til brug af MetaGer (**:key**) er blevet deaktiveret indtil videre.',
     'mastodon' => 'Din Mastodon-konto på vores [instans] (https://suma-ev.social) vil blive frosset og kan ikke længere bruges. Sørg for at flytte til en offentlig instans i god tid. Vi sletter alle data, der er knyttet til din konto, 6 måneder efter afslutningen af dit medlemskab.',
     'expired' => 'Det er med beklagelse, at vi i dag bekræfter, at dit medlemskab af [SUMA-EV] (https://suma-ev.de) er ophørt.',
+    'description' => 'Dit medlemskontingent skulle være betalt den **:dato**. Overfør venligst det udestående beløb på **:beløb** til vores bankkonto inden **:forfaldsdato** med angivelse af årsagen til betalingen (**:betalingsreference**):',
+    'terminate' => 'Uden modtagelse af betaling er vi desværre nødt til at opsige dit medlemskab af [SUMA-EV] (https://suma-ev.de) med virkning fra <b>:udløb</b>',
+    'description_rejoin' => 'Hvis det er tilfældet, kan du bare overføre dit medlemskontingent på **:beløb** til vores bankkonto med angivelse af årsagen til betalingen (**:betalingsreference**):',
 ];

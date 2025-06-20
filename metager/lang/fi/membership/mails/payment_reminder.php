51a0d255c963c53105e0e020c30870559b9e9fa7
DeepL <noreply-mt-deepl@weblate.org>
2025-06-20 16:40:46 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: MetaGer/Membersip - Mails - Payment Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membersip-mails-payment-reminder/fi/

--- metager/lang/fi/membership/mails/payment_reminder.php
+++ metager/lang/fi/membership/mails/payment_reminder.php
@@ -3,6 +3,7 @@ return [
     'subject' => [
         'first' => '[SUMA-EV & MetaGer] Muistutus jäsenmaksun maksamisesta',
         'expired' => '[SUMA-EV & MetaGer] Jäsenyytesi on päättynyt.',
+        'second' => '[SUMA-EV & MetaGer] Jäsenyytesi päättyy :päivänä ilman maksua.',
     ],
     'description_expired' => 'Kokemus on osoittanut meille, että <a href="https://suma-ev.de">SUMA-EV</a> -verkkosivuston jättäminen ei useinkaan liity arvoista ja ajatuksista etääntymiseen, vaan siihen on yleensä taloudellisia syitä. Siksi toivomme, että saattaa tulla aika, jolloin voitte kuvitella liittyvänne jälleen yhdistykseemme.',
     'edit' => 'Vaihtoehtoisesti voit päivittää jäsenyytesi ja valtuuttaa sopivan maksutavan, josta voimme veloittaa (suoraveloitus, PayPal, luottokortti).',
@@ -10,4 +11,7 @@ return [
     'key_charge' => 'MetaGerin käyttöavaimen (**:key**) automaattinen lataus on toistaiseksi poistettu käytöstä.',
     'mastodon' => 'Mastodon-tilisi [instanssi](https://suma-ev.social) jäädytetään, eikä sitä voi enää käyttää. Varmista, että siirryt hyvissä ajoin julkiseen instanssiin. Poistamme kaikki tiliisi liittyvät tiedot 6 kuukauden kuluttua jäsenyytesi päättymisestä.',
     'expired' => 'Vahvistamme valitettavasti tänään, että jäsenyytesi [SUMA-EV](https://suma-ev.de) on päättynyt.',
+    'description' => 'Jäsenmaksunne erääntyi **:päivämäärä**. Pyydämme teitä siirtämään maksamatta olevan määrän **:määrä** pankkitilillemme **:eräpäivään** mennessä ja ilmoittamaan maksun perusteen (**:maksun_viite**):',
+    'terminate' => 'Ilman maksun vastaanottamista joudumme valitettavasti peruuttamaan jäsenyytesi [SUMA-EV](https://suma-ev.de) <b>:stä alkaen.</b>',
+    'description_rejoin' => 'Jos näin on, voit vain siirtää jäsenmaksusi **:summan** pankkitilillemme ja ilmoittaa maksun perusteen (**:maksun_viite**):',
 ];

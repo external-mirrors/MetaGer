4c64d32177aa72aac72b02eb17ded58fff707ae5
DeepL <noreply-mt-deepl@weblate.org>
2025-06-23 03:45:01 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: MetaGer/Desktop-Vivaldi
    Translate-URL: https://translate.metager.de/projects/metager/desktop-vivaldi/da/

--- metager/lang/da/plugin-desktop/desktop-vivaldi.php
+++ metager/lang/da/plugin-desktop/desktop-vivaldi.php
@@ -1,12 +1,12 @@
 <?php
 return [
     'default-search-v3-3' => [
-        1 => 'Højreklik på søgefeltet i slutningen af denne vejledning.',
-        2 => 'Vælg "Tilføj som søgemaskine..." i kontekstmenuen.',
-        3 => 'Markér feltet "Indstil som standardsøgning", og klik på "Tilføj" i popup-vinduet.',
+        1 => 'Højreklik på søgefeltet i slutningen af denne instruktion',
+        2 => 'Vælg "Tilføj som søgemaskine ..." i kontekstmenuen.',
+        3 => 'Markér feltet "Indstil som standardsøgning", og klik på "Tilføj" i pop op-vinduet.',
     ],
     'default-page-v3-3' => [
-        1 => 'Gå til øverste venstre hjørne, klik på "Vivaldi"-logoet, og vælg "<img class="mg-icon" src="/img/icon-settings.svg">" "Indstillinger" under "Værktøjer".',
-        2 => 'Under "Hjemmeside" vælger du "Specifik side" og indtaster :link .',
+        1 => 'Naviger til øverste venstre hjørne, klik på "Vivaldi"-logoet, og vælg "<img class="mg-icon" src="/img/icon-settings.svg">" "Indstillinger" under "Værktøjer".',
+        2 => 'Vælg "Specifik side" under "Hjemmeside", og indtast :link .',
     ],
 ];

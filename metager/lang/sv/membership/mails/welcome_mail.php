5938c5ed8da94e01d4779951178912c21000ece5
dominik <dominik@suma-ev.de>
2025-06-23 06:55:17 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (33 of 33 strings)
    
    Translation: MetaGer/Membership - Mails - Welcome
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-welcome/sv/

--- metager/lang/sv/membership/mails/welcome_mail.php
+++ metager/lang/sv/membership/mails/welcome_mail.php
@@ -5,7 +5,7 @@ return [
         'next_payments' => 'Nästa bidrag',
         'due' => 'Förfallodag',
         'description' => 'Detta e-postmeddelande är också en bekräftelse på ditt medlemskap. Vänligen bekräfta kortfattat att du har mottagit detta e-postmeddelande. Din medlemsavgift på **:belopp€** förfaller till betalning första gången den **:förfallodagen**.',
-        'banktransfer' => 'Vänligen överför detta **:intervall** till följande konto, med angivande av användningsändamål (**:mandat**):',
+        'banktransfer' => 'Vänligen överför detta **:interval** till följande konto, med angivande av användningsändamål (**:mandate**):',
         'directdebit' => 'Vi kommer att debitera detta **:intervall** med mandatreferens **:mandat** från ditt konto **:iban**.',
         'paypal' => 'Vi kommer att debitera detta **:intervall** från det angivna PayPal-kontot.',
         'card' => 'Vi kommer att debitera detta **:intervall** från det angivna kreditkortet.',

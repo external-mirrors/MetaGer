368e19cfb70c050caad9587f64e08da7e718b089
dominik <dominik@suma-ev.de>
2025-06-23 09:12:48 +0000

    Translated using Weblate (Finnish)
    Currently translated at 100.0% (34 of 34 strings)
    
    Translation: MetaGer/Membership - Mails - Welcome
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-welcome/fi/

--- metager/lang/fi/membership/mails/welcome_mail.php
+++ metager/lang/fi/membership/mails/welcome_mail.php
@@ -22,7 +22,7 @@ return [
     'general' => "Tervetuloa SUMA-EV:hen! Saanko ensin kysyä, miten sait tietää meistä? Oletan hakukoneemme MetaGerin kautta? Yhdistyksellä on nyt kanssasi :member_count jäseniä monilta eri aloilta. Seuraavassa on muutamia lisäselvityksiä ja vinkkejä, jotka ovat enemmän kuin hyödyllisiä SUMA-EV:lle.",
     'membership' => [
         'description' => 'Tämä sähköposti on myös vahvistus jäsenyydestäsi. Vahvista lyhyesti, että olet saanut tämän sähköpostin. Jäsenmaksusi, jonka suuruus on **:amount**, erääntyy ensimmäisen kerran **:due**.',
-        'banktransfer' => 'Siirrä tämä **:intervalli** seuraavalle tilille ja ilmoita käyttötarkoitus (**:valtuutus**):',
+        'banktransfer' => 'Siirrä tämä **:interval** seuraavalle tilille ja ilmoita käyttötarkoitus (**:mandate**):',
         'directdebit' => 'Me veloitamme tämän **:interval** valtuutuksen viitteellä **:mandate** tililtäsi **:iban**.',
         'paypal' => 'Me veloitamme tämän **:interval** määritetyltä PayPal-tililtä.',
         'card' => 'Veloitamme tämän **:interval** määritetyltä luottokortilta.',

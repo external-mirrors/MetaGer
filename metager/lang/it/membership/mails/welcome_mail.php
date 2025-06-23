10a3d56ea921d169c349c4ad4ad71d31454d6a42
dominik <dominik@suma-ev.de>
2025-06-23 08:59:53 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (34 of 34 strings)
    
    Translation: MetaGer/Membership - Mails - Welcome
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-welcome/it/

--- metager/lang/it/membership/mails/welcome_mail.php
+++ metager/lang/it/membership/mails/welcome_mail.php
@@ -4,7 +4,7 @@ return [
     'subject' => "[SUMA-EV & MetaGer] Benvenuti a SUMA-EV",
     'general' => "Benvenuti a SUMA-EV! Posso chiederti innanzitutto come ci hai scoperto? Immagino attraverso il nostro motore di ricerca MetaGer? Insieme a voi, l'associazione conta ora :member_count membri provenienti da diversi settori. Qui di seguito troverete alcune spiegazioni e consigli più che utili per il SUMA-EV.",
     'membership' => [
-        'description' => 'Questa e-mail è anche la conferma della vostra adesione. La preghiamo di confermare brevemente di aver ricevuto questa e-mail. La quota associativa di **:importo** è dovuta per la prima volta a **:scadenza**.',
+        'description' => 'Questa e-mail è anche la conferma della vostra adesione. La preghiamo di confermare brevemente di aver ricevuto questa e-mail. La quota associativa di **:amount** è dovuta per la prima volta a **:due**.',
         'banktransfer' => 'Si prega di trasferire questo **:intervallo** al seguente conto, indicando lo scopo dell\'utilizzo (**:mandato**):',
         'directdebit' => 'Addebiteremo questo **:intervallo** con il riferimento del mandato **:mandato** dal vostro conto **:iban**.',
         'paypal' => 'Addebiteremo questo **:intervallo** sul conto PayPal specificato.',

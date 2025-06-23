cf5071fb917275f7b00991cf985fe78babc9d3e5
DeepL <noreply-mt-deepl@weblate.org>
2025-06-23 03:45:01 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MetaGer/Partnershops
    Translate-URL: https://translate.metager.de/projects/metager/partnershops/da/

--- metager/lang/da/partnershops.php
+++ metager/lang/da/partnershops.php
@@ -2,8 +2,8 @@
 return [
     'heading' => 'MetaGer-Partnershop',
     'paragraph' => [
-        '1' => 'Når du ser resultater med en label, der siger \'Partnershop\', er det bare et almindeligt resultat. Det er ikke rangeret anderledes eller foretrukket.',
-        '2' => 'Når der klikkes på linket, modtager vi et lille beløb til at finansiere MetaGer, og det tilknyttede firmalogo vises sammen med resultatet. Du kan også altid genkende vores partnerbutikker fra dette. Du kan fjerne disse links sammen med alle reklamer med en <a href=":link">MetaGer-nøgle</a>.',
+        '1' => 'Når du ser resultater med en etiket, hvor der står "Partnershop", er det bare et almindeligt resultat. Det er ikke rangeret anderledes eller foretrukket.',
+        '2' => 'Når der klikkes på linket, modtager vi et lille beløb til finansiering af MetaGer, og det tilknyttede firmalogo vises sammen med resultatet. Du kan også altid genkende vores partnerbutikker ud fra dette. Du kan fjerne disse links sammen med enhver reklame med en <a href=":link">MetaGer-nøgle</a>.',
         '3' => 'Dit SUMA-EV-medlemskab inkluderer en MetaGer-nøgle til en værdi af dit medlemskontingent. <a href="/membership">Bliv medlem!</a>',
     ],
 ];

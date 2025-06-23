fe4558331574355c3927f281c39120e00c7a35d1
dominik <dominik@suma-ev.de>
2025-06-23 06:46:26 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: MetaGer/Membersip - Mails - Payment Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membersip-mails-payment-reminder/nl/

--- metager/lang/nl/membership/mails/payment_reminder.php
+++ metager/lang/nl/membership/mails/payment_reminder.php
@@ -10,7 +10,7 @@ return [
     'edit_button' => "Lidmaatschap bijwerken",
     'mastodon' => 'Je Mastodon account op onze [instantie](https://suma-ev.social) zal worden bevroren en kan niet langer worden gebruikt. Zorg ervoor dat je op tijd verhuist naar een publieke instantie. We zullen alle gegevens die gekoppeld zijn aan uw account 6 maanden na het einde van uw lidmaatschap verwijderen.',
     'expired' => 'Tot onze spijt bevestigen wij vandaag dat jouw lidmaatschap van [SUMA-EV](https://suma-ev.de) is beëindigd.',
-    'description' => 'Uw lidmaatschapsgeld was verschuldigd op **:datum**. Gelieve het openstaande bedrag van **:bedrag** over te maken op onze bankrekening voor **:vervaldatum**, met vermelding van uw reden voor betaling (**:betalings_referentie**):',
+    'description' => 'Uw lidmaatschapsgeld was verschuldigd op **:date**. Gelieve het openstaande bedrag van **:amount** over te maken op onze bankrekening voor **:due**, met vermelding van uw reden voor betaling (**:payment_reference**):',
     'key_charge' => 'Het automatisch opladen van je sleutel voor het gebruik van MetaGer (**:sleutel**) is voorlopig uitgeschakeld.',
     'terminate' => 'Zonder ontvangst van de betaling moeten we helaas je lidmaatschap van [SUMA-EV](https://suma-ev.de) opzeggen met ingang van <b>:expiration</b>',
     'description_rejoin' => 'In dat geval kun je het lidmaatschapsgeld van **:bedrag** gewoon overmaken op onze bankrekening, met vermelding van de reden van betaling (**:payment_reference**):',

5c4915574df584dfd20c8dc211b25d3fe8e8d8b3
dominik <dominik@suma-ev.de>
2025-06-23 09:13:35 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (34 of 34 strings)
    
    Translation: MetaGer/Membership - Mails - Welcome
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-welcome/es/

--- metager/lang/es/membership/mails/welcome_mail.php
+++ metager/lang/es/membership/mails/welcome_mail.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'membership' => [
-        'banktransfer' => 'Por favor, transfiera este **:intervalo** a la siguiente cuenta, indicando la finalidad de uso (**:mandato**):',
+        'banktransfer' => 'Por favor, transfiera este **:interval** a la siguiente cuenta, indicando la finalidad de uso (**:mandate**):',
         'directdebit' => 'Cargaremos este **:interval** con la referencia de mandato **:mandate** en su cuenta **:iban**.',
         'now' => 'Ahora',
         'description' => 'Este correo electrónico es también la confirmación de su afiliación. Por favor, confirme brevemente que ha recibido este correo electrónico. Su cuota de afiliación por importe de **:amount** vence por primera vez a las **:due**.',

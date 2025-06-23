1b81493bcffaa6297c98b948c2183e99686f339e
dominik <dominik@suma-ev.de>
2025-06-23 06:47:17 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: MetaGer/Membersip - Mails - Payment Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membersip-mails-payment-reminder/it/

--- metager/lang/it/membership/mails/payment_reminder.php
+++ metager/lang/it/membership/mails/payment_reminder.php
@@ -11,7 +11,7 @@ return [
     'key_charge' => 'La ricarica automatica della chiave per l\'utilizzo di MetaGer (**:key**) è stata disattivata per il momento.',
     'mastodon' => 'Il tuo account Mastodon sulla nostra [istanza](https://suma-ev.social) sarà congelato e non potrà più essere utilizzato. Assicuratevi di passare per tempo a un\'istanza pubblica. Cancelleremo tutti i dati collegati al tuo account 6 mesi dopo la fine della tua iscrizione.',
     'expired' => 'Con rammarico le confermiamo oggi che la sua adesione al [SUMA-EV](https://suma-ev.de) è terminata.',
-    'description' => 'La quota associativa doveva essere versata il **:data**. La preghiamo di versare l\'importo residuo di **:importo** sul nostro conto bancario entro **:scadenza**, indicando la causale del pagamento (**:riferimento_di_pagamento**):',
+    'description' => 'La quota associativa doveva essere versata il **:date**. La preghiamo di versare l\'importo residuo di **:amount** sul nostro conto bancario entro **:due**, indicando la causale del pagamento (**:payment_reference**):',
     'description_rejoin' => 'In tal caso è sufficiente versare la quota associativa di **:importo** sul nostro conto corrente bancario, indicando la causale del pagamento (**:riferimento_di_pagamento**):',
     'terminate' => 'Senza il ricevimento del pagamento, saremo purtroppo costretti a cancellare la tua iscrizione a [SUMA-EV](https://suma-ev.de) con effetto da <b>:scadenza</b>',
 ];

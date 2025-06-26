4b14e47fb78207c1c4ba330b224dd78e78432418
dominik <dominik@suma-ev.de>
2025-06-26 07:38:16 +0000

    Translated using Weblate (English)
    Currently translated at 100.0% (76 of 76 strings)
    
    Translation: MetaGer/Settings
    Translate-URL: https://translate.metager.de/projects/metager/settings/en/

--- metager/lang/en/settings.php
+++ metager/lang/en/settings.php
@@ -98,7 +98,7 @@ return [
     ],
     'disabledBecausePaymentRequired' => 'You can use the following search engines with a <a href=":link" target="_blank">MetaGer key</a>.',
     'no-engines' => 'With the current search settings, no search engine is queried.',
-    'cost' => 'We calculate <strong>:cost tokens</strong> per search query with the current settings.',
+    'cost' => 'We charge <strong>:cost Token</strong> per search query with the current settings.',
     'cost-free' => 'Your searches are <strong>free</strong> with the current settings.',
     'free' => 'free',
     'limited' => 'limited',

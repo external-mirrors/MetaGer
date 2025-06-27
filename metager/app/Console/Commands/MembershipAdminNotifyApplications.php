cf80847b85c288d9e84a4d892316eae122b344f6
Dominik Hebeler <dominik@suma-ev.de>
2025-06-27 10:07:38 +0200

    include an additional atomic lock for the job

--- metager/app/Console/Commands/MembershipAdminNotifyApplications.php
+++ metager/app/Console/Commands/MembershipAdminNotifyApplications.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Mail\Membership\MembershipAdminApplicationNotification;
 use App\Models\Membership\MembershipApplication;
+use Cache;
 use Illuminate\Console\Command;
 use Mail;
 
@@ -28,22 +29,30 @@ class MembershipAdminNotifyApplications extends Command
      */
     public function handle()
     {
-        // Delete unfinished applications but send a notification to the user
-        $finished = MembershipApplication::finishedAdmin()->orderBy("updated_at", "desc")->get();
-        $updates = MembershipApplication::updateRequestsAdmin()->orderBy("updated_at", "desc")->get();
-        $reductions = MembershipApplication::reductionRequests()->orderBy("updated_at", "desc")->get();
+        /**
+         * Since Laravels Schedule::onOneServer failed multiple times now
+         * We'll implement a manual atomic lock here.
+         * It doesn't need to be released since the job doesn't run more often than every 5 minutes
+         */
+        $lock = Cache::lock("console:commands:membership:notify-admin:lock", 60 * 5);
+        if ($lock->get()) {
+            // Delete unfinished applications but send a notification to the user
+            $finished = MembershipApplication::finishedAdmin()->orderBy("updated_at", "desc")->get();
+            $updates = MembershipApplication::updateRequestsAdmin()->orderBy("updated_at", "desc")->get();
+            $reductions = MembershipApplication::reductionRequests()->orderBy("updated_at", "desc")->get();
 
-        if (sizeof($finished) === 0 && sizeof($updates) === 0 && sizeof($reductions) === 0)
-            return;
+            if (sizeof($finished) === 0 && sizeof($updates) === 0 && sizeof($reductions) === 0)
+                return;
 
-        $subject = $this->argument("subject");
-        if (empty($subject)) {
-            $subject = "[SUMA-EV] Unbearbeitete Mitgliedsanträge";
-        }
+            $subject = $this->argument("subject");
+            if (empty($subject)) {
+                $subject = "[SUMA-EV] Unbearbeitete Mitgliedsanträge";
+            }
 
-        if (!empty(config("metager.metager.membership.notification_address"))) {
-            $mail = new MembershipAdminApplicationNotification($finished, $updates, $reductions, $subject);
-            Mail::mailer("membership")->send($mail);
+            if (!empty(config("metager.metager.membership.notification_address"))) {
+                $mail = new MembershipAdminApplicationNotification($finished, $updates, $reductions, $subject);
+                Mail::mailer("membership")->send($mail);
+            }
         }
     }
 }
--- metager/app/Console/Commands/MembershipPaymentReminder.php
+++ metager/app/Console/Commands/MembershipPaymentReminder.php
@@ -6,6 +6,7 @@ use App\Mail\Membership\PaymentMethodFailed;
 use App\Mail\Membership\PaymentReminder;
 use App\Mail\Membership\ReductionReminder;
 use App\Models\Membership\CiviCrm;
+use Cache;
 use Illuminate\Console\Command;
 use Mail;
 
@@ -30,48 +31,55 @@ class MembershipPaymentReminder extends Command
      */
     public function handle()
     {
-        $chargebacks = CiviCrm::FIND_CHARGEBACKS();
-        foreach ($chargebacks as $chargeback) {
-            $mail = new PaymentMethodFailed($chargeback);
-            if (Mail::mailer("membership")->send($mail)) {
-                CiviCrm::UPDATE_MEMBERSHIP_RAW($chargeback, ['Beitrag.Zahlungsweise:label' => 'Banküberweisung', 'Beitrag.IBAN' => '', 'Beitrag.BIC' => '', 'Beitrag.Kontoinhaber' => '', 'Beitrag.PayPal_Vault' => '', 'Beitrag.PayPal_ID' => '']);
-                CiviCrm::MEMBERSHIP_RENEW(true);
+        /**
+         * Since Laravels Schedule::onOneServer failed multiple times now
+         * We'll implement a manual atomic lock here.
+         * It doesn't need to be released since the job doesn't run more often than every 5 minutes
+         */
+        $lock = Cache::lock("console:commands:membership:payment-reminder:lock", 60 * 5);
+        if ($lock->get()) {
+            $chargebacks = CiviCrm::FIND_CHARGEBACKS();
+            foreach ($chargebacks as $chargeback) {
+                $mail = new PaymentMethodFailed($chargeback);
+                if (Mail::mailer("membership")->send($mail)) {
+                    CiviCrm::UPDATE_MEMBERSHIP_RAW($chargeback, ['Beitrag.Zahlungsweise:label' => 'Banküberweisung', 'Beitrag.IBAN' => '', 'Beitrag.BIC' => '', 'Beitrag.Kontoinhaber' => '', 'Beitrag.PayPal_Vault' => '', 'Beitrag.PayPal_ID' => '']);
+                    CiviCrm::MEMBERSHIP_RENEW(true);
+                }
+            }
+
+            $reductions = CiviCrm::FIND_REDUCTION_REMINDER();
+            foreach ($reductions as $reduction) {
+                $mail = new ReductionReminder($reduction);
+                if (Mail::mailer("membership")->send($mail)) {
+                    $reduction->reduction = null;
+                    $reduction->amount = 5;
+                    CiviCrm::UPDATE_MEMBERSHIP($reduction, ['Beitrag.Erm_igt_bis']);
+                }
+            }
+
+            $first_reminders = CiviCrm::FIND_FIRST_REMINDER();
+            foreach ($first_reminders as $first_reminder) {
+                $mail = new PaymentReminder($first_reminder, PaymentReminder::REMINDER_STAGE_FIRST);
+                if (Mail::mailer("membership")->send($mail)) {
+                    CiviCrm::UPDATE_MEMBERSHIP_RAW($first_reminder, ['Beitrag.Zahlungsstatus:label' => 'Erste Zahlungserinnerung']);
+                }
+            }
+
+            $second_reminders = CiviCrm::FIND_SECOND_REMINDER();
+            foreach ($second_reminders as $second_reminder) {
+                $mail = new PaymentReminder($second_reminder, PaymentReminder::REMINDER_STAGE_SECOND);
+                if (Mail::mailer("membership")->send($mail)) {
+                    CiviCrm::UPDATE_MEMBERSHIP_RAW($second_reminder, ['Beitrag.Zahlungsstatus:label' => 'Zweite Zahlungserinnerung']);
+                }
+            }
+
+            $final_reminders = CiviCrm::FIND_ABORTED_REMINDER();
+            foreach ($final_reminders as $final_reminder) {
+                $mail = new PaymentReminder($final_reminder, PaymentReminder::REMINDER_STAGE_ABORTED);
+                if (Mail::mailer("membership")->send($mail)) {
+                    CiviCrm::UPDATE_MEMBERSHIP_RAW($final_reminder, ['Beitrag.Zahlungsstatus:label' => 'Unterbrochen']);
+                }
             }
         }
-
-        $reductions = CiviCrm::FIND_REDUCTION_REMINDER();
-        foreach ($reductions as $reduction) {
-            $mail = new ReductionReminder($reduction);
-            if (Mail::mailer("membership")->send($mail)) {
-                $reduction->reduction = null;
-                $reduction->amount = 5;
-                CiviCrm::UPDATE_MEMBERSHIP($reduction, ['Beitrag.Erm_igt_bis']);
-            }
-        }
-
-        $first_reminders = CiviCrm::FIND_FIRST_REMINDER();
-        foreach ($first_reminders as $first_reminder) {
-            $mail = new PaymentReminder($first_reminder, PaymentReminder::REMINDER_STAGE_FIRST);
-            if (Mail::mailer("membership")->send($mail)) {
-                CiviCrm::UPDATE_MEMBERSHIP_RAW($first_reminder, ['Beitrag.Zahlungsstatus:label' => 'Erste Zahlungserinnerung']);
-            }
-        }
-
-        $second_reminders = CiviCrm::FIND_SECOND_REMINDER();
-        foreach ($second_reminders as $second_reminder) {
-            $mail = new PaymentReminder($second_reminder, PaymentReminder::REMINDER_STAGE_SECOND);
-            if (Mail::mailer("membership")->send($mail)) {
-                CiviCrm::UPDATE_MEMBERSHIP_RAW($second_reminder, ['Beitrag.Zahlungsstatus:label' => 'Zweite Zahlungserinnerung']);
-            }
-        }
-
-        $final_reminders = CiviCrm::FIND_ABORTED_REMINDER();
-        foreach ($final_reminders as $final_reminder) {
-            $mail = new PaymentReminder($final_reminder, PaymentReminder::REMINDER_STAGE_ABORTED);
-            if (Mail::mailer("membership")->send($mail)) {
-                CiviCrm::UPDATE_MEMBERSHIP_RAW($final_reminder, ['Beitrag.Zahlungsstatus:label' => 'Unterbrochen']);
-            }
-        }
-
     }
 }

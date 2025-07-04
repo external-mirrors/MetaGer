9cdf172c8e794eb2a0014f839af47a655c2d6335
DeepL <noreply-mt-deepl@weblate.org>
2025-07-04 06:54:32 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (34 of 34 strings)
    
    Translation: MetaGer/Membership - Mails - Welcome
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-welcome/es/

--- metager/lang/es/membership/mails/welcome_mail.php
+++ metager/lang/es/membership/mails/welcome_mail.php
@@ -4,7 +4,7 @@ return [
         'banktransfer' => 'Por favor, transfiera este **:interval** a la siguiente cuenta, indicando la finalidad de uso (**:mandate**):',
         'directdebit' => 'Cargaremos este **:interval** con la referencia de mandato **:mandate** en su cuenta **:iban**.',
         'now' => 'Ahora',
-        'description' => 'Este correo electrónico es también la confirmación de su afiliación. Por favor, confirme brevemente que ha recibido este correo electrónico. Su cuota de afiliación por importe de **:amount** vence por primera vez a las **:due**.',
+        'description' => 'Este correo electrónico es también la confirmación de su afiliación. Por favor, confirme brevemente que ha recibido este correo electrónico. La próxima cuota de afiliación, por importe de **:importe**, vencerá el **:vencimiento**.',
         'paypal' => 'Cargaremos este **:interval** en la cuenta PayPal especificada.',
         'card' => 'Cargaremos este **:interval** en la tarjeta de crédito especificada.',
         'mandate' => 'Finalidad de uso',

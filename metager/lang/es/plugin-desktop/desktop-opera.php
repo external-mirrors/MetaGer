5c292d98ae0d4162ff7ac2b22cb69e716c84c587
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2025-07-22 03:45:05 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (7 of 7 strings)
    
    Translation: MetaGer/Desktop-Opera
    Translate-URL: https://translate.metager.de/projects/metager/desktop-opera/es/

--- metager/lang/es/plugin-desktop/desktop-opera.php
+++ metager/lang/es/plugin-desktop/desktop-opera.php
@@ -1,14 +1,14 @@
 <?php
 return [
     'default-search-v36' => [
-        '1' => 'Haga clic con el botón derecho del ratón en la barra de búsqueda al final de esta instrucción.',
-        '2' => 'Seleccione "Crear motor de búsqueda..." en el menú contextual.',
-        '3' => 'Haga clic en "Crear" en la ventana emergente.',
+        '1' => 'Haga un clic derecho del ratón en el campo de entrada de búsqueda al final de estas instrucciones.',
+        '2' => 'Seleccione la opción "Crear motor de búsqueda ..." en el menú contextual.',
+        '3' => 'En la ventana emergente, haga clic en "Crear".',
     ],
     'default-page-v36' => [
-        '1' => 'Navegue hasta la esquina superior izquierda y haga clic en el logotipo de "Opera" o en "Menú" y haga clic en "Configuración".',
-        '2' => 'En "Al iniciar", elija "Abrir una página específica o un conjunto de páginas" y haga clic en "Añadir una nueva página".',
-        '3' => 'Introduzca ":link" como URL y haga clic en "Añadir".',
-        '4' => 'Sugerencia: todas las páginas de esta ventana se abrirán al inicio. Puede eliminar las entradas situando el ratón sobre ellas y haciendo clic en "<img class="mg-icon" src="/img/ellipsis-horizontal.svg">".',
+        '1' => 'En la parte superior izquierda del navegador haga clic en el símbolo de Opera o "Menú" y pulse en la opción "Configuración".',
+        '2' => 'En la sección "En inicio", seleccione la opción "Abrir una página especifica o conjunto de páginas" y luego haga clic en "Agregar nueva página" o "Establecer página".',
+        '3' => 'Escriba  ":link" como URL de la página web en "Agregar nueva página".',
+        '4' => 'NOTA: Todos los sitios web que aparecen ahora en la lista se abrirán cuando se inicie el navegador. Puede borrar las entradas de la lista haciendo clic en "<img class= "mg-icon" src="/img/ellipsis-horizontal.svg">" junto a la entrada que desea eliminar.',
     ],
 ];

f4ef9b2855a540a3d1ba1ef263a7e5b5d4f09de8
Weblate Translation Memory <noreply-mt-weblate-translation-memory@weblate.org>
2025-07-23 03:45:03 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MetaGer/Partnershops
    Translate-URL: https://translate.metager.de/projects/metager/partnershops/de/

--- metager/lang/de/partnershops.php
+++ metager/lang/de/partnershops.php
@@ -2,8 +2,8 @@
 return [
     'heading' => 'MetaGer-Partnershop',
     'paragraph' => [
-        '1' => 'Wenn Sie Ergebnisse mit der Bezeichnung "Partnershop" sehen, handelt es sich um ein normales Ergebnis. Es wird nicht anders gewertet oder bevorzugt.',
-        '2' => 'Wenn der Link angeklickt wird, erhalten wir einen kleinen Betrag zur Finanzierung von MetaGer, und das zugehörige Firmenlogo wird mit dem Ergebnis angezeigt. Daran können Sie immer auch unsere Partnershops erkennen. Sie können diese Links zusammen mit jeglicher Werbung mit einem <a href=":link">MetaGer-Schlüssel</a> entfernen.',
-        '3' => 'Ihre SUMA-EV-Mitgliedschaft beinhaltet einen MetaGer-Schlüssel im Wert Ihres Mitgliedsbeitrags. <a href="/membership">Werden Sie Mitglied!</a>',
+        '1' => 'Wenn in der MetaGer-Ergebnisliste Links erscheinen, bei denen das Wort "Partnershop" steht, dann sind das zunächst normale Ergebnisse einer MetaGer-Suche. Sie werden im Ranking nicht bevorzugt oder höher eingestuft, sondern wie alle anderen Suchergebnisse auch behandelt.',
+        '2' => 'Wenn der Link angeklickt wird, erhalten wir einen geringen Betrag zur Finanzierung von MetaGer. Beim Ergebnis wird das zugehörige Firmenlogo angezeigt. Auch daran erkennen Sie immer unsere Partnershops. Sie können diese Links zusammen mit jeglicher Werbung mit einem <a href=":link">MetaGer Schlüssel</a> entfernen.',
+        '3' => 'In Ihrer SUMA-EV Mitgliedschaft ist ein MetaGer Schlüssel im Wert Ihres Mitgliedsbeitrags enthalten. <a href="/membership">Werden Sie Mitglied!</a>',
     ],
 ];

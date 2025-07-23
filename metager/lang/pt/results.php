d11cab75b3515d7f35e6425a1450b962b3dc31b2
Weblate <noreply@weblate.org>
2025-07-23 11:51:42 +0000

    Update translation files
    Updated by "Cleanup translation files" hook in Weblate.
    
    Translation: MetaGer/Results
    Translate-URL: https://translate.metager.de/projects/metager/results/

--- metager/lang/pt/results.php
+++ metager/lang/pt/results.php
@@ -6,17 +6,6 @@ return [
             'original' => 'Em vez disso, procure por <a href=":link">:original <img class= "mg-icon" src="/img/icon-lupe.svg"></a>',
         ],
     ],
-    'images' => [
-        'external' => [
-            'bing' => "Pesquisar no Bing",
-            'buy' => "Comprar uma chave MetaGer",
-            'google' => "Pesquisar no Google",
-            'description' => "Só pesquisou na base de dados de Pixabay. É melhor procurar com uma chave MetaGer.",
-            'heading' => "Faltam aqui alguns resultados",
-            'or' => "ou",
-            'save' => "Recordar decisão (com cookie)",
-        ],
-    ],
     'results' => [
         'summary' => 'Mostrar :resultcount de cerca de :totalresults resultados da pesquisa',
     ],

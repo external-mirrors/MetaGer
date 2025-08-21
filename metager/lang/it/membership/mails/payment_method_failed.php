7f9a23d3cc934d63f3fc9eed883ca74551b2037d
DeepL <noreply-mt-deepl@weblate.org>
2025-08-21 12:17:39 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: MetaGer/Membership - Mails - Paymentmethod Failed
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-paymentmethod-failed/it/

--- metager/lang/it/membership/mails/payment_method_failed.php
+++ metager/lang/it/membership/mails/payment_method_failed.php
@@ -2,6 +2,6 @@
 return [
     'subject' => '[SUMA-EV & MetaGer] Il tuo metodo di pagamento non è riuscito.',
     'description' => 'Non siamo stati in grado di addebitare la quota di iscrizione al suo metodo di pagamento. Per questo motivo è stato rimosso. Affinché la tua iscrizione possa continuare, dovrai pagare la tua quota associativa tramite bonifico bancario utilizzando il riferimento di pagamento **:riferimento_di_pagamento** al seguente conto:',
-    'edit' => 'In alternativa, potete aggiornare la vostra iscrizione e autorizzare un metodo di pagamento adeguato per l\'addebito (addebito diretto, PayPal, carta di credito).',
+    'edit' => 'In alternativa, potete aggiornare la vostra iscrizione e autorizzare un metodo di pagamento adeguato per l\'addebito (addebito diretto, PayPal).',
     'edit_button' => "Aggiornamento dei soci",
 ];

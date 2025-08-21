f9df35c95706b570554c560812ca2ed463d85eec
DeepL <noreply-mt-deepl@weblate.org>
2025-08-21 12:17:36 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: MetaGer/Membersip - Mails - Payment Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membersip-mails-payment-reminder/nl/

--- metager/lang/nl/membership/mails/payment_reminder.php
+++ metager/lang/nl/membership/mails/payment_reminder.php
@@ -6,7 +6,7 @@ return [
         'second' => '[SUMA-EV & MetaGer] Je lidmaatschap eindigt op :date zonder betaling',
     ],
     'description_expired' => 'De ervaring heeft ons geleerd dat het verlaten van de <a href="https://suma-ev.de">SUMA-EV</a> vaak geen verband houdt met het afstand nemen van onze waarden en ideeën, maar meestal financiële redenen heeft. We hopen daarom dat er een moment komt waarop je je kunt voorstellen weer lid te worden van onze vereniging.',
-    'edit' => 'Je kunt ook je lidmaatschap bijwerken en een geschikte betaalmethode autoriseren (automatische incasso, PayPal, creditcard).',
+    'edit' => 'Je kunt ook je lidmaatschap bijwerken en een geschikte betaalmethode autoriseren (automatische incasso, PayPal).',
     'edit_button' => "Lidmaatschap bijwerken",
     'mastodon' => 'Je Mastodon account op onze [instantie](https://suma-ev.social) zal worden bevroren en kan niet langer worden gebruikt. Zorg ervoor dat je op tijd verhuist naar een publieke instantie. We zullen alle gegevens die gekoppeld zijn aan uw account 6 maanden na het einde van uw lidmaatschap verwijderen.',
     'expired' => 'Tot onze spijt bevestigen wij vandaag dat jouw lidmaatschap van [SUMA-EV](https://suma-ev.de) is beëindigd.',

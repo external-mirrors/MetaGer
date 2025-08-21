6b89acdd890cfb00f614d2b0f91ca501a5c07305
Weblate <noreply-mt-weblate@weblate.org>
2025-08-21 12:29:21 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: MetaGer/Membership - Mails - Paymentmethod Card
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-paymentmethod-card/da/

--- metager/lang/da/membership/mails/payment_method_card.php
+++ metager/lang/da/membership/mails/payment_method_card.php
@@ -4,4 +4,5 @@ return [
     'description' => 'Du har for nylig meldt dig ind i SUMA-EV og valgt et kreditkort som betalingsmetode. Men i modsætning til, hvad vores betalingsprocessor lovede, tillader de os ikke at bruge det gemte kreditkort til at betale dine medlemsgebyrer uden din bekræftelse for hver betaling. Det er selvfølgelig ikke sådan, kreditkortintegrationen var planlagt, og vores tests afslørede ikke dette problem. Det betyder, at vi har været nødt til at fjerne kreditkort som en mulighed for betaling af medlemsgebyr. Vi har opdateret dit medlemskab til at bruge bankoverførsel som en midlertidig betalingsmetode og givet dig en gratis måneds medlemskab for ulejligheden. Dine gemte kreditkortdata er også blevet slettet.',
     'description2' => 'Du kan betale dit medlemskab ved at foretage en bankoverførsel med betalingsreferencen **:payment_reference** til følgende konto:',
     'edit' => 'Alternativt kan du opdatere dit medlemskab og godkende en passende betalingsmetode, som vi automatisk kan debitere fra (Directdebit, PayPal).',
+    'edit_button' => "Opdatering af medlemskab",
 ];

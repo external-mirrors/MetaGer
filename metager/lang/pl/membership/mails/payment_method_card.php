860efa41855e5270c393c7dc4bcd51ff5af93c39
Weblate <noreply-mt-weblate@weblate.org>
2025-08-21 12:29:50 +0000

    Translated using Weblate (Polish)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: MetaGer/Membership - Mails - Paymentmethod Card
    Translate-URL: https://translate.metager.de/projects/metager/membership-mails-paymentmethod-card/pl/

--- metager/lang/pl/membership/mails/payment_method_card.php
+++ metager/lang/pl/membership/mails/payment_method_card.php
@@ -4,4 +4,5 @@ return [
     'description' => 'Niedawno dołączyłeś do SUMA-EV, wybierając kartę kredytową jako metodę płatności. Jednak w przeciwieństwie do tego, co obiecał nasz procesor płatności, nie pozwala nam on na użycie zapisanej karty kredytowej do opłacenia składek członkowskich bez potwierdzenia każdej płatności. Oczywiście nie tak zaplanowano integrację karty kredytowej, a nasze testy nie ujawniły tego problemu. Oznacza to, że musieliśmy usunąć kartę kredytową jako opcję płatności składki członkowskiej. Zaktualizowaliśmy twoje członkostwo, aby używać przelewu bankowego jako tymczasowej metody płatności i przyznaliśmy ci darmowy miesiąc członkostwa za niedogodności. Zapisane dane karty kredytowej również zostały usunięte.',
     'description2' => 'Płatności za członkostwo można dokonać, wykonując przelew bankowy z numerem płatności **:payment_reference** na następujące konto:',
     'edit' => 'Alternatywnie możesz zaktualizować swoje członkostwo i autoryzować odpowiednią metodę płatności, z której będziemy automatycznie pobierać środki (Directdebit, PayPal).',
+    'edit_button' => "Aktualizacja członkostwa",
 ];

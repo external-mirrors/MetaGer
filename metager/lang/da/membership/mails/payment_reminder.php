c5275a5bea7220e7541f32cf5aa68f098f801669
DeepL <noreply-mt-deepl@weblate.org>
2025-08-21 12:17:33 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: MetaGer/Membersip - Mails - Payment Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membersip-mails-payment-reminder/da/

--- metager/lang/da/membership/mails/payment_reminder.php
+++ metager/lang/da/membership/mails/payment_reminder.php
@@ -8,7 +8,7 @@ return [
     'description' => 'Dit medlemskontingent skulle være betalt den **:date**. Overfør venligst det udestående beløb på **:amount** til vores bankkonto inden **:due** med angivelse af årsagen til betalingen (**:payment_reference**):',
     'description_expired' => 'Erfaringen har vist os, at det at forlade <a href="https://suma-ev.de">SUMA-EV</a> ofte ikke handler om at tage afstand fra vores værdier og ideer, men som regel har økonomiske årsager. Vi håber derfor, at der kan komme et tidspunkt, hvor du kan forestille dig at blive medlem af vores forening igen.',
     'description_rejoin' => 'Hvis det er tilfældet, kan du bare overføre dit medlemskontingent på **:amount** til vores bankkonto med angivelse af årsagen til betalingen (**:payment_reference**):',
-    'edit' => 'Alternativt kan du opdatere dit medlemskab og godkende en passende betalingsmetode, som vi kan debitere fra (Directdebit, PayPal, Creditcard).',
+    'edit' => 'Alternativt kan du opdatere dit medlemskab og godkende en passende betalingsmetode, som vi kan debitere fra (Directdebit, PayPal).',
     'edit_button' => "Opdatering af medlemskab",
     'key_charge' => 'Den automatiske genopladning af din nøgle til brug af MetaGer (**:key**) er blevet deaktiveret indtil videre.',
     'mastodon' => 'Din Mastodon-konto på vores [instans] (https://suma-ev.social) vil blive frosset og kan ikke længere bruges. Sørg for at flytte til en offentlig instans i god tid. Vi sletter alle data, der er knyttet til din konto, 6 måneder efter afslutningen af dit medlemskab.',

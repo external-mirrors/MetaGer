433f4993e4ab8fe64ff09c41459b40458f5966d6
DeepL <noreply-mt-deepl@weblate.org>
2025-08-21 12:17:37 +0000

    Translated using Weblate (Portuguese)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: MetaGer/Membersip - Mails - Payment Reminder
    Translate-URL: https://translate.metager.de/projects/metager/membersip-mails-payment-reminder/pt/

--- metager/lang/pt/membership/mails/payment_reminder.php
+++ metager/lang/pt/membership/mails/payment_reminder.php
@@ -7,7 +7,7 @@ return [
     ],
     'edit_button' => "Atualizar o estatuto de membro",
     'description_expired' => 'A experiência tem-nos mostrado que o abandono da <a href="https://suma-ev.de">SUMA-EV</a> não está muitas vezes relacionado com o distanciamento dos nossos valores e ideias, mas tem geralmente razões financeiras. Esperamos, por isso, que chegue um momento em que se possa imaginar a juntar-se novamente à nossa associação.',
-    'edit' => 'Em alternativa, pode atualizar a sua inscrição e autorizar um método de pagamento adequado para débito (débito direto, PayPal, cartão de crédito)',
+    'edit' => 'Em alternativa, pode atualizar a sua inscrição e autorizar um método de pagamento adequado para débito (débito direto, PayPal)',
     'key_charge' => 'O recarregamento automático da sua chave para a utilização do MetaGer (**:key**) foi desativado por enquanto.',
     'mastodon' => 'A sua conta Mastodon na nossa [instância](https://suma-ev.social) será congelada e não poderá mais ser usada. Por favor, certifique-se de que muda para uma instância pública em tempo útil. Eliminaremos todos os dados associados à sua conta 6 meses após o fim da sua inscrição.',
     'expired' => 'É com pesar que confirmamos hoje que a sua adesão à [SUMA-EV](https://suma-ev.de) terminou.',

322af0ee5833843ef5f048b6bf4dc4261abee2da
DeepL <noreply-mt-deepl@weblate.org>
2025-11-04 13:18:22 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (31 of 31 strings)
    
    Translation: MetaGer/Index
    Translate-URL: https://translate.metager.de/projects/metager/index/fr/

--- metager/lang/fr/index.php
+++ metager/lang/fr/index.php
@@ -30,14 +30,15 @@ return [
     'lang' => 'quelle langue',
     'searchreset' => 'supprimer la recherche',
     'searchbar-replacement' => [
-        'message' => 'Veuillez vous connecter avec votre clé pour utiliser le moteur de recherche MetaGer. Il n\'est disponible que sans publicité !',
+        'message' => 'Veuillez vous connecter avec votre clé pour utiliser le moteur de recherche MetaGer. Il n\'est disponible que sans publicité et <a href=":anonym_link">anonyme</a> !',
         'login' => 'Connexion avec la clé',
-        'start' => 'Créer une nouvelle clé',
+        'start' => 'Recharger la nouvelle clé',
         'why' => 'pourquoi ?',
         'key_error' => "La clé introduite n'était pas valide. Veuillez vérifier la saisie.",
         'login_code_error' => "Le code de connexion saisi n'était pas valide. Conseil : les codes de connexion ne sont valables que lorsqu'ils sont visibles sur un autre appareil !",
         'payment_id_error' => "Vous avez introduit un identifiant de paiement qui n'est pas une clé correcte. Votre clé comporte 36 caractères.",
         'new_key' => 'Pas encore de clé ?',
         'extension' => 'Restez connecté et anonyme grâce à notre extension web',
+        'not_logged_in' => 'Vous n\'êtes pas connecté.',
     ],
 ];

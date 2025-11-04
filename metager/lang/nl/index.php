559fe3029c3db28ed4f651e18fdcd3849164bfc8
DeepL <noreply-mt-deepl@weblate.org>
2025-11-04 13:18:25 +0000

    Translated using Weblate (Dutch)
    Currently translated at 100.0% (31 of 31 strings)
    
    Translation: MetaGer/Index
    Translate-URL: https://translate.metager.de/projects/metager/index/nl/

--- metager/lang/nl/index.php
+++ metager/lang/nl/index.php
@@ -31,13 +31,14 @@ return [
     'searchreset' => 'invoer zoekopdracht verwijderen',
     'searchbar-replacement' => [
         'login' => 'Inloggen met sleutel',
-        'start' => 'Een nieuwe sleutel maken',
-        'message' => 'Log in met je sleutel om de MetaGer zoekmachine te gebruiken. Het is alleen advertentie-vrij beschikbaar!',
+        'start' => 'Nieuwe sleutel bijvullen',
+        'message' => 'Log in met je sleutel om de MetaGer zoekmachine te gebruiken. Het is alleen beschikbaar advertentie-vrij & <a href=":anonym_link">anoniem</a>!',
         'why' => 'waarom?',
         'key_error' => "De ingevoerde sleutel is ongeldig. Controleer de invoer.",
         'login_code_error' => "De ingevoerde inlogcode was niet geldig. Tip: Inlogcodes zijn alleen geldig als ze zichtbaar zijn op een ander apparaat!",
         'payment_id_error' => "Je hebt een betalings-id ingevoerd die geen correcte sleutel is. Je sleutel is 36 tekens lang.",
         'new_key' => 'Nog geen sleutel?',
         'extension' => 'Blijf ingelogd en anoniem met onze webextensie',
+        'not_logged_in' => 'Je bent momenteel niet ingelogd.',
     ],
 ];
